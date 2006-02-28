3ded4ec5969d9f9f0f3f7908b0973c821e7bab40
Aaron Mackey <amackey@_.com>
2006-02-28 19:26:42 +0000

    nitpicky cleanups
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@10885 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/Utility/ECAnnotater.pm
+++ Load/lib/perl/Utility/ECAnnotater.pm
@@ -1,9 +1,8 @@
 package ApiCommonData::Load::Utility::ECAnnotater;
 
 use strict;
-use DBI;
-use CBIL::Util::Disp;
-use GUS::Model::DoTS::Transcript;
+use warnings;
+
 use GUS::Model::DoTS::TranslatedAAFeature;
 use GUS::Model::DoTS::AASequenceEnzymeClass;
 use GUS::Model::SRes::EnzymeClass;
@@ -18,54 +17,63 @@ sub new {
 
 
 sub addEnzymeClassAssoc {
-   my ($self, $seqId, $ecNumber, $relId, $evid) = @_;
+  my ($self, $seqId, $ecNumber, $relId, $evid) = @_;
 
-       my $gusEcId = $self->_getEcId($ecNumber,$relId);
-       my $gusEcAsc = GUS::Model::DoTS::AASequenceEnzymeClass->new( {
-                                'aa_sequence_id'=>$seqId, 
-                                'enzyme_class_id'=>$gusEcId,
-                                'evidence_code'=> $evid,
-                                } );
-         unless ($gusEcAsc->retrieveFromDB()) {
-            $gusEcAsc->submit();
-         }
+  my $gusEcId = $self->_getEcId($ecNumber, $relId);
+  my $gusEcAsc =
+    GUS::Model::DoTS::AASequenceEnzymeClass->new( {
+						   'aa_sequence_id'  => $seqId, 
+						   'enzyme_class_id' => $gusEcId,
+						   'evidence_code'   => $evid,
+						  } );
+  unless ($gusEcAsc->retrieveFromDB()) {
+    $gusEcAsc->submit();
+  }
 }
 
 
-
 sub getAASeqIdFromFeatId {
-   my ($self, $featId) = @_;
+  my ($self, $featId) = @_;
 
-   my $gusTAAF = GUS::Model::DoTS::TranslatedAAFeature->new( { 'na_feature_id' => $featId, } );
-   $gusTAAF->retrieveFromDB() || die "no translated aa sequence: $featId";
-   my $gusAASeq = $gusTAAF->getAaSequenceId();
+  my $gusTAAF = GUS::Model::DoTS::TranslatedAAFeature->new( { 'na_feature_id' => $featId, } );
 
-return $gusAASeq
+  $gusTAAF->retrieveFromDB()
+    or die "no translated aa sequence: $featId";
+
+  my $gusAASeq = $gusTAAF->getAaSequenceId();
+
+  return $gusAASeq;
 }
 
 
 sub getAASeqIdFromGeneId {
-   my ($self, $featId) = @_;
+  my ($self, $featId) = @_;
 
-   my $gusTAAF = GUS::Model::DoTS::TranslatedAASequence->new( { 'source_id' => $featId, } );
-   $gusTAAF->retrieveFromDB() || die "no translated aa sequence: $featId";
-   my $gusAASeq = $gusTAAF->getId();
+  my $gusTAAF = GUS::Model::DoTS::TranslatedAASequence->new( { 'source_id' => $featId, } );
 
-return $gusAASeq
+  $gusTAAF->retrieveFromDB()
+    or die "no translated aa sequence: $featId";
+
+  my $gusAASeq = $gusTAAF->getId();
+
+  return $gusAASeq;
 }
 
 
 sub _getEcId {
    my ($self, $ecNum, $relId) = @_;
 
-       my $gusEcObj = GUS::Model::SRes::EnzymeClass->new( {
-                                 'ec_number' => $ecNum,
-                                 'external_database_release_id' => $relId,
-                                 } );
-       $gusEcObj->retrieveFromDB() || die "no such EC entry: $ecNum";
-       my $gusECId = $gusEcObj->getId();
+   my $gusEcObj = GUS::Model::SRes::EnzymeClass->new( {
+						       'ec_number' => $ecNum,
+						       'external_database_release_id' => $relId,
+						      } );
 
-return $gusECId;
+   $gusEcObj->retrieveFromDB()
+     or die "no such EC entry: $ecNum";
+
+   my $gusECId = $gusEcObj->getId();
+
+   return $gusECId;
 }
 
 return 1;
