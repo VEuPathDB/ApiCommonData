55e721115167c364c0e1d36eda45146e81432d2a
Jennifer Dommer <jldommer@_.com>
2006-02-28 16:33:18 +0000

    fixed typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@10879 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertOrthologousGroups.pm
+++ Load/plugin/perl/InsertOrthologousGroups.pm
@@ -152,7 +152,7 @@ sub run {
     $seqGroupLoaded++;
 
     foreach my $element (@$foundIds) {
-      if(my $geneFeatureId = ApiCommonDataData::Load::Util::getGeneFeatureId($self, $element)) {
+      if(my $geneFeatureId = ApiCommonData::Load::Util::getGeneFeatureId($self, $element)) {
 
         my $seqSeqGroup = GUS::Model::DoTS::SequenceSequenceGroup->
           new({sequence_id => $geneFeatureId,
