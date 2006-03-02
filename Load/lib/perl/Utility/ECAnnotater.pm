e53c499cd060c2cd7b7bc1f1116de9cc7eb1982c
Ed Robinson <erobinso@_.com>
2006-03-02 17:26:58 +0000

    Modified to take a hash as input.  Added documentation and made variable names more informative.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@10909 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/Utility/ECAnnotater.pm
+++ Load/lib/perl/Utility/ECAnnotater.pm
@@ -3,7 +3,6 @@ package ApiCommonData::Load::Utility::ECAnnotater;
 use strict;
 use warnings;
 
-use GUS::Model::DoTS::TranslatedAAFeature;
 use GUS::Model::DoTS::AASequenceEnzymeClass;
 use GUS::Model::SRes::EnzymeClass;
 
@@ -16,47 +15,29 @@ sub new {
 }
 
 
-sub addEnzymeClassAssoc {
-  my ($self, $seqId, $ecNumber, $relId, $evid) = @_;
+sub addEnzymeClassAssociation {
+  my ($self, $ecAssociation) = @_;
 
-  my $gusEcId = $self->_getEcId($ecNumber, $relId);
-  my $gusEcAsc =
+#          my $ecAssociation = {
+#                    'ecNumber' => 'The raw EC number (e.g. 1.23.4.6),
+#                    'evidenceDescription' => 'String describing where the term comes from, e.g. Annotation Center',
+#                    'releaseId' => 'External Database Release Id of the Enzyme Database,
+#                    'sequenceId' => 'GUS Id of the AA sequence',
+#                              };
+
+  $self->{plugin}->userError("addEnzymeClassAssoc requires a hash as input with the following named attributes as input: ecNumber, evidenceDescripition, releaseId, sequenceId") unless (exists($ecAssociation->{'ecNumber'}) && exists($ecAssociation->{'evidenceDescription'}) && exists($ecAssociation->{'releaseId'}) && exists($ecAssociation->{'sequenceId'}));
+
+  my $gusEcId = $self->_getEcId($ecAssociation->{'ecNumber'}, $ecAssociation->{'releaseId'});
+  my $gusEcAssociation =
     GUS::Model::DoTS::AASequenceEnzymeClass->new( {
-						   'aa_sequence_id'  => $seqId, 
-						   'enzyme_class_id' => $gusEcId,
-						   'evidence_code'   => $evid,
-						  } );
-  unless ($gusEcAsc->retrieveFromDB()) {
-    $gusEcAsc->submit();
+                                      'aa_sequence_id'  => $ecAssociation->{'sequenceId'},
+                                      'enzyme_class_id' => $gusEcId,
+                                      'evidence_code'   => $ecAssociation->{'evidenceDescription'},
+                                                   } );
+  unless ($gusEcAssociation->retrieveFromDB()) {
+    $gusEcAssociation->submit();
+
   }
-}
-
-
-sub getAASeqIdFromFeatId {
-  my ($self, $featId) = @_;
-
-  my $gusTAAF = GUS::Model::DoTS::TranslatedAAFeature->new( { 'na_feature_id' => $featId, } );
-
-  $gusTAAF->retrieveFromDB()
-    or die "no translated aa sequence: $featId";
-
-  my $gusAASeq = $gusTAAF->getAaSequenceId();
-
-  return $gusAASeq;
-}
-
-
-sub getAASeqIdFromGeneId {
-  my ($self, $featId) = @_;
-
-  my $gusTAAF = GUS::Model::DoTS::TranslatedAASequence->new( { 'source_id' => $featId, } );
-
-  $gusTAAF->retrieveFromDB()
-    or die "no translated aa sequence: $featId";
-
-  my $gusAASeq = $gusTAAF->getId();
-
-  return $gusAASeq;
 }
 
 
