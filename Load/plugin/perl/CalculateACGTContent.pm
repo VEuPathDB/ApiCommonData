c22da6d50e5e2a4c3db5ecd5fa7372940fb08297
Jennifer Dommer <jldommer@_.com>
2006-04-05 14:27:50 +0000

    moved to new location
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@11584 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/plugin/perl/CalculateACGTContent.pm
@@ -0,0 +1,150 @@
+package ApiCommonData::Load::Plugin::CalculateACGTContent;
+
+use strict;
+use warnings;
+
+use GUS::PluginMgr::Plugin;
+use base qw(GUS::PluginMgr::Plugin);
+
+use GUS::Model::DoTS::NASequence;
+use Bio::Tools::SeqStats;
+
+my $argsDeclaration =[];
+
+my $purposeBrief = <<PURPOSEBRIEF;
+Calculates the A,C,G,T, other counts for NA sequences.
+PURPOSEBRIEF
+
+my $purpose = <<PLUGIN_PURPOSE;
+Calculates the counts for the different nucleic acids for all of the NA sequences for a given external database release.
+PLUGIN_PURPOSE
+
+my $tablesAffected =
+  [
+   ['DoTS.NASequence' =>
+    'the A/C/G/T_count fields are updated if the entry exists'
+   ],
+  ];
+
+my $tablesDependedOn = [];
+
+my $howToRestart = <<PLUGIN_RESTART;
+PLUGIN_RESTART
+
+my $failureCases = <<PLUGIN_FAILURE_CASES;
+PLUGIN_FAILURE_CASES
+
+my $notes = <<PLUGIN_NOTES;
+PLUGIN_NOTES
+
+my $documentation = { purposeBrief => $purposeBrief,
+		      purpose => $purpose,
+		      tablesAffected => $tablesAffected,
+		      tablesDependedOn => $tablesDependedOn,
+		      howToRestart => $howToRestart,
+		      failureCases => $failureCases,
+		      notes => $notes,
+		    };
+
+sub new {
+
+  my $class = shift;
+  $class = ref $class || $class;
+  my $self = {};
+
+  bless $self, $class;
+
+  $self->initialize({ requiredDbVersion => 3.5,
+		      cvsRevision =>  '$Revision$',
+		      name => ref($self),
+		      argsDeclaration   => $argsDeclaration,
+		      documentation     => $documentation
+		    });
+  return $self;
+}
+
+sub run {
+  my ($self) = @_;
+  my @seqArray;
+  my $count=0;
+
+  $self->getSeqId(\@seqArray);
+
+  foreach my $seqId (@seqArray){
+    $self->getMonomerCount($seqId);
+
+    $count++;
+    if($count % 1000 == 0) {
+      $self->log("Updated $count sequences.");
+      $self->undefPointerCache();
+    }
+
+  }
+
+  return $self->log("Successfully updated $count sequences.");
+}
+
+sub getSeqId{
+  my ($self, $seqArray) = @_;
+
+my $sql = <<EOSQL;
+SELECT distinct na_sequence_id
+      FROM DoTS.NASequence
+EOSQL
+
+  my $queryHandle = $self->getQueryHandle();
+  my $sth = $queryHandle->prepareAndExecute($sql);
+
+  while(my $seqId = $sth->fetchrow_array()){
+    push(@$seqArray, $seqId);
+  }
+}
+
+sub getMonomerCount{
+  my ($self, $seqId)=@_;
+  my $monomersHash;
+  my $countA = 0;
+  my $countT = 0;
+  my $countC = 0;
+  my $countG = 0;
+  my $countOther = 0;
+
+  my $naSeqObj = GUS::Model::DoTS::NASequence->new({na_sequence_id => $seqId});
+  $naSeqObj->retrieveFromDB();
+  my $naSeq = $naSeqObj->getSequence();
+
+  my $seqobj = Bio::PrimarySeq->new(-seq=>$naSeq,
+				    -alphabet=>'dna');
+
+  my $seqStats  =  Bio::Tools::SeqStats->new(-seq=>$seqobj);
+
+  $monomersHash = $seqStats->count_monomers();
+  foreach my $base (keys %$monomersHash) {
+    if ($base eq 'A'){
+      $countA = $$monomersHash{$base};
+    }
+    elsif ($base eq 'T'){
+      $countT = $$monomersHash{$base};
+    }
+    elsif ($base eq 'C'){
+      $countC = $$monomersHash{$base};
+    }
+    elsif ($base eq 'G'){
+      $countG = $$monomersHash{$base};
+    }
+    else{
+      $countOther = $$monomersHash{$base};
+    }
+  }
+
+  $naSeqObj->setACount($countA);
+  $naSeqObj->setTCount($countT);
+  $naSeqObj->setCCount($countC);
+  $naSeqObj->setGCount($countG);
+  $naSeqObj->setOtherCount($countOther);
+
+  $naSeqObj->submit();
+  $self->undefPointerCache();
+}
+
+1;
