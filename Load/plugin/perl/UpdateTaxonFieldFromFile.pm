1950a2f244f9dd136df82bdd4ddb2b41508c8e94
Deborah Pinney <pinney@upenn.edu>
2006-05-22 18:36:39 +0000

    new plugin, not tested and not finished,compiles
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@12065 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/plugin/perl/UpdateTaxonFieldFromFile.pm
@@ -0,0 +1,252 @@
+package ApiCommonData::Load::Plugin::InsertUserProjectGroupRows;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+use strict;
+
+use GUS::PluginMgr::Plugin;
+use GUS::Model::SRes::TaxonName;
+use GUS::Model::SRes::Taxon;
+
+$| = 1;
+
+
+# ---------------------------------------------------------------------------
+# Load Arguments
+# ---------------------------------------------------------------------------
+
+sub getArgumentsDeclaration{
+  my $argsDeclaration =
+    [
+     fileArg({name => 'file',
+	      descr => 'input file containing the source_id and either the correspoding ncbi_tax_id or taxon_name',
+	      constraintFunc => undef,
+	      reqd => 1,
+	      isList => 0,
+	      mustExist => 1
+	     }),
+     stringArg({name => 'sourceIdRegex',
+		descr => 'regex used to identify the source_id in each line',
+		constraintFunc => undef,
+		reqd => 1,
+		isList => 0
+	       }),
+     stringArg({name => 'taxonNameRegex',
+		descr => 'regex used to identify the taxon_name,must have this or ncbiTaxIdRegex',
+		constraintFunc => undef,
+		reqd => 0,
+		isList => 0
+	       }),
+     stringArg({name => 'ncbiTaxIdRegex',
+		descr => 'regex used to identify the ncbi tax_id,must have this or taxonNameRegex',
+		constraintFunc => undef,
+		reqd => 0,
+		isList => 0
+	       }),
+     stringArg({name => 'table',
+		descr => 'fully specified table to be updated, e.g. DoTS::ExternalAASequence',
+		constraintFunc=> undef,
+		reqd  => 1,
+		isList => 0
+	       })
+    ];
+  return $argsDeclaration;
+}
+
+# ----------------------------------------------------------------------
+# Documentation
+# ----------------------------------------------------------------------
+
+sub getDocumentation {
+  my $purposeBrief = "Updates specified table's taxon_id field";
+
+  my $purpose = "Update the specified table's taxon_id from information supplied in the input file";
+
+  my $tablesAffected = "";
+
+  my $tablesDependedOn = [['SRes::TaxonName', 'the taxon_id will be identified using the taxon name if the regex is supplied'],['SRes::Taxon','the taxon_id will be identified using the ncbi tax_id if the the regex is supplied']];
+
+  my $howToRestart = "No extra steps required for restart";
+
+  my $failureCases = "";
+
+  my $notes = "the table to be updated must contain source_id and taxon_id attributes and the file supplied must contain a source_id and either a taxon_name or ncbi_tax_id for which a taxon_id can be identified";
+
+  my $documentation = {purpose=>$purpose, purposeBrief=>$purposeBrief, tablesAffected=>$tablesAffected, tablesDependedOn=>$tablesDependedOn, howToRestart=>$howToRestart, failureCases=>$failureCases,notes=>$notes};
+
+  return $documentation;
+}
+
+# ----------------------------------------------------------------------
+# create and initalize new plugin instance.
+# ----------------------------------------------------------------------
+
+sub new {
+  my ($class) = @_;
+  my $self = {};
+  bless($self,$class);
+
+  my $documentation = &getDocumentation();
+  my $argumentDeclaration = &getArgumentsDeclaration();
+
+
+  $self->initialize({requiredDbVersion => 3.5,
+		     cvsRevision => '$Revision$',
+                     name => ref($self),
+                     revisionNotes => '',
+                     argsDeclaration => $argumentDeclaration,
+                     documentation => $documentation});
+
+  return $self;
+}
+
+# ----------------------------------------------------------------------
+# run method to do the work
+# ----------------------------------------------------------------------
+
+sub run {
+  my ($self) = @_;
+
+  $self->logAlgInvocationId();
+  $self->logCommit();
+  $self->logArgs();
+
+  my $table = $self->getArg('table');
+
+  $self->userError("table must be in format GUS::Model::TableSpace::Table or TableSpace::Table") if ($table !~ /[\bGUS::Model::\w+::\w+\b|\b\w+::\w+\b]/);
+
+  $table = $table =~ /\bGUS::Model::\w+::\w+\b/ ? $table : "GUS::Model::$table";
+
+  my $sourceIdHsh = $self->processFile();
+
+  my ($processed,$updatedRows) = $self->updateRows($sourceIdHsh,$table);
+
+  my $resultDescrip = "Taxon_id field updated in $updatedRows rows of $table based on input file, $processed input lines processedx";
+
+  $self->setResultDescr($resultDescrip);
+  $self->logData($resultDescrip);
+}
+
+
+sub processFile {
+  my ($self) = @_;
+
+  my $file = $self->getArg('file');
+
+  my %taxonIdHsh;
+
+  my %sourceIdHsh;
+
+  my $sourceRegex = $self->getArg('sourceIdRegex');
+
+  open(FILE,$file) || $self->userError ("can't open $file for reading");
+
+  while(<FILE>){
+    chomp;
+
+    my $line = $_;
+
+    my $source_id;
+
+    if (/$sourceRegex/ && $1) { 
+      $source_id = $1; 
+    }
+    else {
+      my $forgotParens = ($sourceRegex !~ /\(/)? "(Forgot parens?)" : "";
+      $self->userError("Unable to parse source_id from $line using regex '$sourceRegex' $forgotParens");
+    }
+
+    my $taxon_id = $self->getTaxonId($line,\%taxonIdHsh);
+
+    $sourceIdHsh{$source_id} = $taxon_id;
+  }
+
+  return \%sourceIdHsh;
+}
+
+sub getTaxonId {
+  my ($self,$line,$taxonIdHsh) = @_;
+
+  my ($regex,$id);
+
+  if ($self->getArg('taxonNameRegex')) {
+    $regex = $self->getArg('taxonNameRegex');
+  }
+  elsif ($self->getArg('ncbiTaxIdRegex')) {
+    $regex = $self->getArg('ncbiTaxIdRegex');
+  }
+  else {
+    $self->userError("must supply either ncbiTaxIdRegex or taxonNameRegex");
+  }
+
+  if ($line !~ /$regex/ || ! $1) {
+    $self->userError("Unable to parse $line using regex '$regex'");
+  }
+
+  if ($taxonIdHsh->{$1}> 0) {
+    $id = $taxonIdHsh->{$1};
+  }
+  else {
+    $id = $self->getArg('taxonNameRegex') ? $self->getIdFromTaxonName($1) : $self->getIdFromTaxon($1) ;
+    $taxonIdHsh->{$1} = $id;
+  }
+
+  return $id;
+}
+
+sub getIdFromTaxonName {
+  my ($self,$name) = @_;
+
+  if ($name eq "") {
+    $name = 'unknown';
+  }
+
+  my $taxonName = GUS::Model::SRes::TaxonName->new({'name' => $name});
+
+  $taxonName->retrievefromDB();
+
+  my $id = $taxonName->getId();
+
+  return $id;
+}
+
+sub getIdfromTaxon {
+  my ($self,$ncbiTaxId) = @_;
+
+  if (! $ncbiTaxId) {
+    $ncbiTaxId = 32644;
+  }
+
+  my $taxon = GUS::Model::SRes::Taxon->new({'ncbi_tax_id' => $ncbiTaxId});
+
+  $taxon->retrievefromDB();
+
+  my $id = $taxon->getId();
+
+  return $id;
+}
+
+sub updateRows {
+  my ($self,$sourceIdHsh,$table) = @_;
+
+  my ($processed,$submitted);
+
+  eval ("require $table");
+
+  foreach my $sourceId (keys %{$sourceIdHsh}) {
+    my $row = $table->new({'source_id' => $sourceId});
+
+    $row->retrievefromDB();
+
+    $row->setTaxonId($sourceIdHsh->{$sourceId}) unless ($sourceIdHsh->{$sourceId} == $row->getTaxonId());
+
+    $submitted += $row->submit();
+
+    $processed++;
+  }
+
+  return ($processed,$submitted);
+}
+
+
+
+1;
