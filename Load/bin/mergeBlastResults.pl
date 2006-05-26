5b79995eaa80437454e340e92fba27565aa67951
John Brestelli <jbrestell@gmail.com>
2006-05-26 20:12:52 +0000

    Broken copy... good progress on parsing individual file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@12115 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/mergeBlastResults.pl
+++ Load/bin/mergeBlastResults.pl
@@ -5,7 +5,9 @@ use warnings;
 
 use lib "$ENV{GUS_HOME}/lib/perl";
 use Getopt::Long qw(GetOptions);
+use File::Basename;
 
+use Data::Dumper;
 
 =pod
 
@@ -28,8 +30,12 @@ GetOptions("verbose!"           => \$verbose,
 &usage("Must Specify input and output files") unless ($inputFile && $outputFile);
 &usage("Input file $inputFile does not exist") unless (-e $inputFile);
 
-my $adjustedFile = &_adjustCoordinates($inputFile);
+#&_adjustCoordinates($inputFile);
+#my $dirname = File::Basename::dirname($inputFile);
 
+
+my $similarities = &_parseFile($inputFile);
+&_removeDuplicates($similarities);
 
 #---------------------------------------------------------------
 
@@ -75,36 +81,74 @@ sub _adjustCoordinates {
   close (RESULTS);
 }
 
+sub _parseFile {
+  (my $file) = @_;
 
-sub _removeDuplicates{
-  my ($file) = @_;
-  my $discard;
-  my %similarities;
-  my $id;
-  my $sum;
+  my %seen;
+  my $similarities = {};
+  my ($id, $subjectId);
 
   open (TEMP, "< $file") or die "Couldn't open $file for reading: $!\n";
 
   while(my $line = <TEMP>){
     chomp $line;
 
-    if($similarities{$line}){
+    while($seen{$line}) {
       while ($line = <TEMP>){
-	last if ($line =~ /Sum/);
+        chomp($line);
+        last if ($line =~ /Sum/);
       }
-	chomp $line;
+      last if(!$line);
     }
+    last if(!$line);
 
     if ($line =~ /^\>(\S+)/){
-      my $id = $1;
+      $id = $1;
     }
+    elsif ($line =~ /Sum/) {
+      $seen{$line} = 1;
 
-    if ($line =~ /Sum/){
-      my @sumLine = split(':', $line);
+      my @sim = split(':', $line);
+      my $queryStart = $sim[6];
+      my $queryEnd = $sim[7];
 
-      if($similarities{$line}->{subjectId} == $sumLine[1] && $similarities{$line}->{queryStart} == $sumLine[6]){
+      my $subjectStart = $sim[4];
+      my $subjectEnd = $sim[5];
 
-	if ($sumLine[7] > $similarities{$line}->{queryEnd}){
+      $subjectId = $sim[1];
+      my $blastResult = BlastResult->new($queryStart, $queryEnd, $subjectStart, $subjectEnd, $line);
+
+      push(@{$similarities->{$subjectId}}, $blastResult);
+    }
+    elsif($line =~ /HSP/) {
+      my $last = scalar(@{$similarities->{$subjectId}}) - 1;
+      $similarities->{$subjectId}->[$last]->addHsp($line);
+    }
+    else {
+      die "Invalid line $line: $!";
+    }
+  }
+  close(TEMP);
+  return($similarities);
+}
+
+
+sub _removeDuplicates {
+  my ($similarities) = @_;
+
+  foreach my $subjects (keys(%$similarities)) {
+    next if(scalar(@{$similarities->{$subjects}}) == 1);
+    my $newBlastResults = [];
+
+    while(my $blatResult = pop(@{$similarities->{$subjects}})) {
+      if(&_isKeeper($blastResult, $similarities->{$subjects})) {
+        push(@{$newBlastResults}, $blastResult);
+      }
+
+
+
+
+
 
 	  delete $similarities{$line};
 	  $similarities{$line} = ({'subjectId' => $sumLine[1],
@@ -152,10 +196,40 @@ sub _removeDuplicates{
 #      print "I will be kept: $_\n";
     }
 
-  }
-
   close (TEMP);
 }
+
+sub _isKeeper {
+  my ($blastResult, $ar) = @_;
+
+  my $newAr = [];
+
+  foreach my $result (@$ar) {
+    my $rsQueryEnd = $result->getQueryEnd();
+    my $rsQueryStart = $result->getQueryStart();
+    my $rsSubjectStart = $result->getSubjectStart();
+    my $rsSubjectEnd = $result->getSubjectEnd();
+
+    my $brQueryEnd = $blastResult->getQueryEnd();
+    my $brQueryStart = $blastResult->getQueryStart();
+    my $brSubjectStart = $blastResult->getSubjectStart();
+    my $brSubjectEnd = $blastResult->getSubjectEnd();
+
+    if($brQueryStart == $rsQueryStart && $brSubjectStart == $rsSubjectStart) {
+      if($resultQueryEnd > $brQueryEnd) {
+        return(0);
+      }
+      else {
+        
+      }
+    }
+
+    
+  }
+  
+
+}
+
 
 
 sub usage {
@@ -169,9 +243,11 @@ sub usage {
 sub _getFh {
   my ($id, $prevId, $fh, $inputFile, $seen) = @_;
 
-  $inputFile =~ s/\/[a-zA-Z0-9_\.]+$/\/$id/;
+  my $newId = $id . ".bbf";
 
-  print STDERR "WARNING: FILE $id already exists\n" if(!$seen->{$id} && -e $inputFile);
+  $inputFile =~ s/\/[a-zA-Z0-9_\.]+$/\/$newId/;
+
+  print STDERR "WARNING: Appending to FILE $id which already exists\n" if(!$seen->{$id} && -e $inputFile);
 
   if(defined($prevId) && $id eq $prevId) {
     return($fh);
@@ -181,6 +257,44 @@ sub _getFh {
     open(FILE, ">> $inputFile") || die "Cannot open file $id for writing";
   }
   return(\*FILE);
+}
+
+#==================================================================
+
+package BlastResult;
+
+sub new {
+  my ($class, $queryStart, $queryEnd, $subjectStart, $subjectEnd, $sumline) = @_;
+
+  unless($queryStart || $queryEnd || $sumline) {
+    die "Invalad BlastResult: qs=$queryStart, qe=$queryEnd, sumline=$sumline";
+  }
+  my $args = { _query_start => $queryStart,
+               _query_end => $queryEnd,
+               _subj_start => $subjectStart,
+               _subj_end => $subjectEnd,
+               _sum_line => $sumline,
+               _hsps => [],
+               };
+
+  my $self = bless $args, $class;
+
+  return $self;
+}
+
+sub getQueryStart {$_[0]->{_query_start}}
+sub getQueryEnd   {$_[0]->{_query_end}}
+sub getSubjectStart {$_[0]->{_subj_start}}
+sub getSubjectEnd   {$_[0]->{_subj_end}}
+sub getSumLine    {$_[0]->{_sum_line}}
+sub getHsps       {$_[0]->{_hsps}}
+
+sub addHsp {
+  my ($self, $hspLine) = @_;
+
+  die "Invalid hsp line: $hspLine" unless($hspLine =~ /HSP/);
+
+  push(@{$self->getHsps}, $hspLine);
 }
 
 
