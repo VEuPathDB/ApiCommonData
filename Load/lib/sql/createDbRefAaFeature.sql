ac2ba53fe64e4dcd529792818c729afe86ec5818
John Iodice <iodice@pennmedicine.upenn.edu>
2006-10-09 13:09:17 +0000

    fix primary key; drop unneeded subqueries
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@13323 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createDbRefAaFeature.sql
+++ Load/lib/sql/createDbRefAaFeature.sql
@@ -21,14 +21,12 @@ INSERT INTO core.TableInfo
      other_read, other_write, row_user_id, row_group_id, row_project_id, 
      row_alg_invocation_id)
 SELECT core.tableinfo_sq.nextval, 'DbRefAaFeature',
-       'Standard', 'na_feature_id',
+       'Standard', 'db_ref_aa_feature_id',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
-     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'dots') d,
-     (SELECT table_id FROM core.TableInfo WHERE name = 'NAFeatureImp') t,
-     (SELECT table_id FROM core.TableInfo WHERE name = 'NAFeature') s;
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'dots') d;
 
 ------------------------------------------------------------------------------
 exit;
