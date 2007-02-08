42165e3d432b835c9b94e5a4d29feea36b73d7ad
Mark Heiges <mheiges@gmail.com>
2007-02-08 05:53:29 +0000

    initial import of bioperlSeqPreprocessor for GenBank and similar formats where genes are flatly followed by a subfeature. e.g.  gene  CDS  gene  tRNA
    Known bug: get_tag_values() returns array is being handled as scalar, so qualifiers are not being copied in some cases.
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15007 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/GeneAndCds2BioperlTree.pm
@@ -0,0 +1,73 @@
+package ApiCommonData::Load::GeneAndCds2BioperlTree;
+
+# Remove existing gene features, promote CDS, tRNA, etc to gene
+
+use strict;
+use Bio::SeqFeature::Generic;
+use Bio::Location::Simple;
+use ApiCommonData::Load::BioperlTreeUtils qw{makeBioperlFeature};
+use Data::Dumper;
+#input: CDS with join location (if multiple exons)
+#output: standard api tree: gene->transcript->exons
+#                                           ->CDS
+
+# (0) remove all seq features, add back the non-genes
+# (1) copy old gene qualifiers to cds/rna feature
+# (2) retype CDS into Gene
+# (3) remember its join locations
+# (4) create transcript, give it a copy of the gene's location
+# (5) add to gene
+# (6) create exons from sublocations
+# (7) add to transcript
+sub preprocess {
+  my ($bioperlSeq, $plugin) = @_;
+  my $tempTree;
+
+  my @seqFeatures = $bioperlSeq->remove_SeqFeatures;
+  foreach my $bioperlFeatureTree (@seqFeatures) {
+    my $type = $bioperlFeatureTree->primary_tag();
+    
+    if ($type eq 'gene') {
+      $tempTree = $bioperlFeatureTree;
+      next;
+    }
+    
+    copyQualifiers($tempTree, $bioperlFeatureTree) if ($tempTree);
+
+    $bioperlSeq->add_SeqFeature($bioperlFeatureTree);
+    
+    if (grep {$type eq $_} ("CDS", "tRNA", "rRNA", "snRNA", "snoRNA")) {
+      $type = "coding" if $type eq "CDS";
+      $bioperlFeatureTree->primary_tag("${type}_gene");
+      my $gene = $bioperlFeatureTree;
+      my $geneLoc = $gene->location();
+      my $transcript = &makeBioperlFeature("transcript", $geneLoc, $bioperlSeq);
+      $gene->add_SeqFeature($transcript);
+      my @exonLocations = $geneLoc->each_Location();
+      foreach my $exonLoc (@exonLocations) {
+        my $exon = &makeBioperlFeature("exon", $exonLoc, $bioperlSeq);
+        $transcript->add_SeqFeature($exon);
+      }
+    }
+  }
+}
+
+sub copyQualifiers {
+  my ($tempTree, $bioperlFeatureTree) = @_;
+  for my $qualifier ($tempTree->get_all_tags()) {
+    unless ($bioperlFeatureTree->has_tag($qualifier) 
+            and ($bioperlFeatureTree->get_tag_values($qualifier) 
+                 eq $tempTree->get_tag_values($qualifier)
+                )
+           ) {
+      $bioperlFeatureTree->add_tag_value(
+                             $qualifier, 
+                             $tempTree->get_tag_values($qualifier)
+                           );
+     }
+  }
+  undef $tempTree;
+
+}
+
+1;
