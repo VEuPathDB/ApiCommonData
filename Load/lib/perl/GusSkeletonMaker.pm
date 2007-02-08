a47779f16b66a5f65e4105a07ad620f6efc969b4
Mark Heiges <mheiges@gmail.com>
2007-02-08 04:38:56 +0000

    sno more exceptions when encountering snoRNA_gene
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15006 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/GusSkeletonMaker.pm
+++ Load/lib/perl/GusSkeletonMaker.pm
@@ -13,6 +13,7 @@ my $soTerms = { 'coding_gene'=>'protein_coding',
 		'tRNA_gene'=> 'tRNA_encoding',
 		'rRNA_gene'=> 'rRNA_encoding',
 		'snRNA_gene'=> 'snRNA_encoding',
+		'snoRNA_gene'=> 'snoRNA_encoding',
 		'transcript' => 'transcript',
 		'exon' => 'exon'
 	      };
@@ -85,7 +86,7 @@ sub makeGusGene {
 
   my $type = $bioperlGene->primary_tag();
 
-  $plugin->error("Trying to make gus skeleton from a tree rooted with an unexpected type: '$type'") unless (grep {$type eq $_} ("coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene"));
+  $plugin->error("Trying to make gus skeleton from a tree rooted with an unexpected type: '$type'") unless (grep {$type eq $_} ("coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene"));
 
   my $gusGene = $plugin->makeSkeletalGusFeature($bioperlGene, $genomicSeqId,
 						$dbRlsId, 
