076a75293783774bcbcfda0d980ac5819ec633ef
John Brestelli <jbrestell@gmail.com>
2007-02-26 20:37:23 +0000

    analysis result id
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15112 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createDifferentialExpression.sql
+++ Load/lib/sql/createDifferentialExpression.sql
@@ -36,7 +36,7 @@ INSERT INTO core.TableInfo
      other_read, other_write, row_user_id, row_group_id, row_project_id, 
      row_alg_invocation_id)
 SELECT core.tableinfo_sq.nextval, 'DifferentialExpression',
-       'Standard', 'na_feature_id',
+       'Standard', 'analysis_result_id',
        d.database_id, 0, 1, t.table_id, s.table_id, 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
