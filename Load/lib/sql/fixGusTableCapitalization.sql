ded8364a1da38bdb7016e971ac85e41c60a63c92
John Iodice <iodice@pennmedicine.upenn.edu>
2007-03-06 20:24:28 +0000

    change core.TableInfo.name values that are all all capitals to conform to GUS camel-caps convention
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15179 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/fixGusTableCapitalization.sql
@@ -0,0 +1,25 @@
+UPDATE core.TableInfo
+SET name = 'AAFeatureComment'
+WHERE name = 'AAFEATURECOMMENT';
+
+UPDATE core.TableInfo
+SET name = 'AAFeatureDbRef'
+WHERE name = 'AAFEATUREDBREF';
+
+UPDATE core.TableInfo
+SET name = 'CompositeElementDbRref'
+WHERE name = 'COMPOSITEELEMENTDBREF';
+
+UPDATE core.TableInfo
+SET name = 'CompositeElementNASequence'
+WHERE name = 'COMPOSITEELEMENTNASEQUENCE';
+
+UPDATE core.TableInfo
+SET name = 'ElementDbRef'
+WHERE name = 'ELEMENTDBREF';
+
+UPDATE core.TableInfo
+SET name = 'ElementNASequence'
+WHERE name = 'ELEMENTNASEQUENCE';
+
+exit
