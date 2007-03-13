ee5b5802857d6ac936d2d2b8fe07b0ded2f3b3ee
John Iodice <iodice@pennmedicine.upenn.edu>
2007-03-13 19:18:34 +0000

    drop to go with the add
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15258 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/dropMassSpecSummary.sql
@@ -0,0 +1,9 @@
+DROP TABLE apidb.MassSpecSummary;
+
+DELETE FROM core.TableInfo
+WHERE lower(name) = lower('MassSpecSummary')
+  AND database_id = (SELECT database_id
+                     FROM core.DatabaseInfo 
+                     WHERE lower(name) = 'apidb');
+
+exit;
