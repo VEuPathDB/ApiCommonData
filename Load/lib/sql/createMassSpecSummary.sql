f6a0d505d8bbf042a51b2506236233e68487d899
John Iodice <iodice@pennmedicine.upenn.edu>
2007-03-13 19:06:55 +0000

    a new table requested by Steve
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15257 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createMassSpecSummary.sql
@@ -0,0 +1,61 @@
+CREATE TABLE apidb.MassSpecSummary (
+ mass_spec_summary_id         NUMBER(12) NOT NULL,
+ aa_sequence_id               NUMBER(12) NOT NULL,
+ prediction_algorithm_id      NUMBER(12) NOT NULL,
+ external_database_release_id NUMBER(12),
+ developmental_stage          VARCHAR2(20) NOT NULL,
+ is_expressed                 NUMBER(1) NOT NULL,
+ number_of_spans              NUMBER(12) NOT NULL,
+ sequence_count               NUMBER(12) NOT NULL,
+ spectrum_count               NUMBER(12) NOT NULL,
+ aa_seq_percent_covered       FLOAT(126) NOT NULL,
+ aa_seq_length                NUMBER(12) NOT NULL,
+ aa_seq_molecular_weight      NUMBER(12) NOT NULL,
+ aa_seq_pi                    FLOAT(126) NOT NULL,
+ modification_date            date NOT NULL,
+ user_read                    NUMBER(1) NOT NULL,
+ user_write                   NUMBER(1) NOT NULL,
+ group_read                   NUMBER(1) NOT NULL,
+ group_write                  NUMBER(1) NOT NULL,
+ other_read                   NUMBER(1) NOT NULL,
+ other_write                  NUMBER(1) NOT NULL,
+ row_user_id                  NUMBER(12) NOT NULL,
+ row_group_id                 NUMBER(3) NOT NULL,
+ row_project_id               NUMBER(4) NOT NULL,
+ row_alg_invocation_id        NUMBER(12) NOT NULL
+);
+
+ALTER TABLE apidb.MassSpecSummary
+ADD CONSTRAINT mss_pk PRIMARY KEY (mass_spec_summary_id);
+
+ALTER TABLE apidb.MassSpecSummary
+ADD CONSTRAINT mss_fk1 FOREIGN KEY (aa_sequence_id)
+REFERENCES dots.AaSequenceImp (aa_sequence_id);
+
+ALTER TABLE apidb.MassSpecSummary
+ADD CONSTRAINT mss_fk2 FOREIGN KEY (external_database_release_id)
+REFERENCES sres.ExternalDatabaseRelease (external_database_release_id);
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.MassSpecSummary TO gus_w;
+GRANT SELECT ON apidb.MassSpecSummary TO gus_r;
+
+------------------------------------------------------------------------------
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'MassSpecSummary',
+       'Standard', 'db_ref_aa_feature_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
+WHERE 'massspecsummary' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
+
+
+exit;
