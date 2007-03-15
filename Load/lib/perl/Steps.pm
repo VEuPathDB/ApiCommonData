c68e4ee946a8d807afa6ea8056e4ad5926edfd7d
Steve Fischer <sfischer@upenn.edu>
2007-03-15 21:25:55 +0000

    move init stuff from ToxoPlasmoSteps into Steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15300 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/Steps.pm
+++ Load/lib/perl/Steps.pm
@@ -8,14 +8,29 @@ use GUS::Pipeline::Manager;
 
 chomp(my $pipelineScript = `basename $0`);
 
+## NOTE: at the bottom of this file are the old "steps specific property
+## declarations.  we intend to incorporate them into the steps that need them
+
+
 sub init {
-  my ($propertiesFile, $printXML, $allSpecies, $taxId) = @_;
+  my ($propertiesFile, $inputOptionalArgs, $allSpecies, $taxId) = @_;
 
   $| = 1;
   umask 002;
 
+  my $optionalArgs = {printXML => 0,
+		      skipCleanup => 0};
+
   &usage unless -e $propertiesFile;
-  &usage if ($printXML && $printXML ne "-printXML");
+  if ($inputOptionalArgs){
+    &usage unless scalar(@$inputOptionalArgs <= scalar(keys(%$optionalArgs)));
+    foreach my $optionalArg (@$inputOptionalArgs) {
+      &usage unless $optionalArg =~ /\-(\w+)/;
+      my $arg = $1;
+      &usage unless defined($optionalArgs->{$arg});
+      $optionalArgs->{$arg} = 1;
+    }
+  }
 
   # [name, default (or null if reqd), comment]
   my @properties = 
@@ -56,7 +71,9 @@ sub init {
   my $mgr = GUS::Pipeline::Manager->new($myPipelineDir, $propertySet,
 					$propertiesFile, $cluster,
 					$propertySet->getProp('testNextPlugin'),
-					$printXML);
+					$optionalArgs->{printXml},
+					$optionalArgs->{skipCleanup},
+				       );
 
   # set up global variables
   $mgr->{propertiesFile} = $propertiesFile;
@@ -78,6 +95,46 @@ sub init {
   $mgr->{taxonHsh} = $taxonHsh;
 
   return ($mgr, $projectDir, $release);
+}
+
+sub initToxoAnalysis {
+  my ($propertyFile, $optionalArgs) = @_;
+
+  my $allSpecies = 'Tgondii';
+
+  my $taxId = ["Tgondii:5811","TgondiiApicoplast:5811","TgondiiGT1:5811","TgondiiVeg:398031","TgondiiRH:383379"];
+
+  my ($mgr, $projectDir, $release)
+    = &init($propertyFile, $optionalArgs,$allSpecies, $taxId);
+
+  return ($mgr, $projectDir, $release, $allSpecies);
+}
+
+sub initPlasmoAnalysis {
+  my ($propertyFile, $optionalArgs) = @_;
+
+  my $allSpecies = 'Pfalciparum,Pyoelii,Pvivax,Pberghei,Pchabaudi';
+
+  my $taxId = ["Pfalciparum:36329","PfalciparumPlastid:36329","PfalciparumMito:36329","Pyoelii:352914","Pvivax:126793","Pberghei:5823","Pchabaudi:31271","Pknowlesi:5851"];
+
+  my ($mgr, $projectDir, $release)
+    = &init($propertyFile, $optionalArgs, $allSpecies, $taxId);
+
+  return ($mgr, $projectDir, $release, $allSpecies);
+}
+
+
+sub initOrthomclAnalysis {
+  my ($propertyFile, $optionalArgs) = @_;
+
+  my $allSpecies = 'BAE_all';
+
+  my $taxId = [];
+
+  my ($mgr, $projectDir, $release)
+    = &init($propertyFile, $optionalArgs, $allSpecies, $taxId);
+
+  return ($mgr, $projectDir, $release, $allSpecies);
 }
 
 sub createDataDir {
@@ -2928,7 +2985,7 @@ sub copyFileOrDirFromComputeCluster {
 sub usage {
   my $prog = `basename $0`;
   chomp $prog;
-  print STDERR "usage: $prog propertiesfile [-printXML]\n";
+  print STDERR "usage: $prog propertiesfile [-printXML -skipCleanup]\n";
   exit 1;
 }
 
@@ -3321,5 +3378,61 @@ EOF
          $args,
          "Loading $dir Iprscan output");
 }
+
+
+## these are the old "steps specific" property
+## declarations.  we intend to incorporate them into the steps that need them
+
+  my @plasmoProps = (
+    # step specific properties
+     ["blastsimilarity.taskSize", "", "number of query sequences per blast similarity done on cluster"],
+     ["exportpredPath", "", "full path to exportpred software"],
+     ["genome.taskSize", "" ,"fill in"],
+     ["genome.path", "" ,"fill in"],
+     ["genome.options", "" ,"fill in"],
+     ["genome.version", "" ,"fill in"],
+     ["phrapDir", "", "fill in"],
+     ["fileOfRepeats", "", "fill in"],
+     ["wuBlastBinPathCluster", "", "path to find wu BLAST on cluster"],
+     ["signalP.version", "", "version number for the signalP package"],
+     ["signalP.path", "","full path of signalP executable"],
+     ["tmhmm.path", "","full path of tmhmm executable"],
+     ["tmhmm.version", "","version number for the TMHMM package"],
+     ["trfPath", "","path of directory that contains trf400"],
+     ["ncbiBlastPath", "", "path to find ncbi blast dir on server"],
+     ["psipredPath", "", "path to find the psipred executables"],
+     ["psipred.taskSize", "","number of seqs per subtask to be done on compute cluster"],
+     ["psipred.clusterpath", "","path to dir containing psipred script on cluster"],
+     ["trnascan.taskSize", "", "number of seqs per subtask to be done on compute cluster"],
+     ["trnascan.clusterpath", "", "path to dir containing tRNAscan script on cluster"]
+ );
+
+my @toxoProps = 
+(
+ ["wuBlastBinPathCluster", "", "path to find wu BLAST on cluster"],
+ ["wuBlastPath", "", "path to find wu BLAST locally"],
+ ["blastzPath", "", "path to find BLASTZ locally"],
+ ["signalP.version", "", "version number for the signalP package"],
+ ["signalP.path", "","full path of signalP executable"],
+ ["projectName", "", " project name from projectinfo.name"],
+ ["tmhmm.path", "","full path of tmhmm executable"],
+ ["tmhmm.version", "","version number for the TMHMM package"],
+ ["ncbiBlastPath", "", "path to find ncbi blast dir on server"],
+ ["trfPath","","path to find the trf software"]
+);
+
+my @orthomclProps = 
+(
+ ["wuBlastBinPathCluster", "", "path to find wu BLAST on cluster"],
+ ["wuBlastPath", "", "path to find wu BLAST locally"],
+ ["blastzPath", "", "path to find BLASTZ locally"],
+ ["projectName", "", " project name from projectinfo.name"],
+ ["ncbiBlastPath", "", "path to find ncbi blast dir on server"],
+);
+
+my @orthomclProps = 
+(
+
+);
 
 1;
