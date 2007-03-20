debdbaed2870352c622c6bfceb086818e6416fe4
Cristina Aurrecoechea <aurreco@uga.edu>
2007-03-20 17:09:18 +0000

    ApiDB adds project_id to the bfmv's.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15366 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createBfmvApiDB.sql
@@ -0,0 +1,96 @@
+---------------------------
+-- GENEs
+---------------------------
+
+DROP MATERIALIZED VIEW apidb.GeneAttributes;
+
+
+CREATE MATERIALIZED VIEW apidb.GeneAttributes AS
+(
+SELECT 'cryptodb' AS project_id, GeneAttributes.* FROM  
+apidb.GeneAttributes@CRDEV35A_LOQUAT
+UNION
+SELECT 'plasmodb' AS project_id, GeneAttributes.* FROM  
+apidb.GeneAttributes@PLASBLD
+UNION
+SELECT 'toxodb' AS project_id, GeneAttributes.* FROM  
+apidb.GeneAttributes@TOXOBLD
+);
+
+---------------------------
+-- SEQUENCEs
+---------------------------
+
+DROP MATERIALIZED VIEW apidb.SequenceAttributes;
+
+
+CREATE MATERIALIZED VIEW apidb.SequenceAttributes AS
+(
+SELECT 'cryptodb' AS project_id, SequenceAttributes.* FROM  
+apidb.SequenceAttributes@CRDEV35A_LOQUAT
+UNION
+SELECT 'plasmodb' AS project_id, SequenceAttributes.* FROM  
+apidb.SequenceAttributes@PLASBLD
+UNION
+SELECT 'toxodb' AS project_id, SequenceAttributes.* FROM  
+apidb.SequenceAttributes@TOXOBLD
+);
+
+---------------------------
+-- ESTs
+---------------------------
+
+DROP MATERIALIZED VIEW apidb.EstAttributes;
+
+
+CREATE MATERIALIZED VIEW apidb.EstAttributes AS
+(
+SELECT 'cryptodb' AS project_id, EstAttributes.* FROM  
+apidb.EstAttributes@CRDEV35A_LOQUAT
+UNION
+SELECT 'plasmodb' AS project_id, EstAttributes.* FROM  
+apidb.EstAttributes@PLASBLD
+UNION
+SELECT 'toxodb' AS project_id, EstAttributes.* FROM  
+apidb.EstAttributes@TOXOBLD
+);
+
+---------------------------
+-- ORFs
+---------------------------
+
+DROP MATERIALIZED VIEW apidb.OrfAttributes;
+
+
+CREATE MATERIALIZED VIEW apidb.OrfAttributes AS
+(
+SELECT 'cryptodb' AS project_id, OrfAttributes.* FROM  
+apidb.OrfAttributes@CRDEV35A_LOQUAT
+UNION
+SELECT 'plasmodb' AS project_id, OrfAttributes.* FROM  
+apidb.OrfAttributes@PLASBLD
+UNION
+SELECT 'toxodb' AS project_id, OrfAttributes.* FROM  
+apidb.OrfAttributes@TOXOBLD
+);
+
+---------------------------
+-- SNPs
+---------------------------
+
+DROP MATERIALIZED VIEW apidb.SnpAttributes;
+
+
+
+CREATE MATERIALIZED VIEW apidb.SnpAttributes AS
+(
+SELECT 'cryptodb' AS project_id, SnpAttributes.* FROM  
+apidb.SnpAttributes@CRDEV35A_LOQUAT
+UNION
+SELECT 'plasmodb' AS project_id, SnpAttributes.* FROM  
+apidb.SnpAttributes@PLASBLD
+UNION
+SELECT 'toxodb' AS project_id, SnpAttributes.* FROM  
+apidb.SnpAttributes@TOXOBLD
+);
+
