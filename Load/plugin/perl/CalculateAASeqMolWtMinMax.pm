bc49e75549844ec2841a594fbe4a611b5dd80b7d
Mark Heiges <mheiges@gmail.com>
2007-03-31 03:15:42 +0000

    svn propset keywords
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15535 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/CalculateAASeqMolWtMinMax.pm
+++ Load/plugin/perl/CalculateAASeqMolWtMinMax.pm
@@ -77,7 +77,7 @@ sub new {
   bless $self, $class;
 
   $self->initialize({ requiredDbVersion => 3.5,
-		      cvsRevision =>  '$Revision: 10093 $',
+		      cvsRevision =>  '$Revision$',
 		      name => ref($self),
 		      argsDeclaration   => $argsDeclaration,
 		      documentation     => $documentation
