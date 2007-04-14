5662060c786639759e6f5d8a11ab68ca18188d9d
Cristina Aurrecoechea <aurreco@uga.edu>
2007-04-14 18:20:07 +0000

    prompts added
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@15838 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createBfmvApiDB.sql
+++ Load/lib/sql/createBfmvApiDB.sql
@@ -1,10 +1,10 @@
 ---------------------------
 -- GENEs
 ---------------------------
-
+prompt *****DROP MATERIALIZED VIEW apidb.GeneAttributes;
 DROP MATERIALIZED VIEW apidb.GeneAttributes;
 
-
+prompt *****CREATE MATERIALIZED VIEW apidb.GeneAttributes
 CREATE MATERIALIZED VIEW apidb.GeneAttributes AS
 (
 SELECT 'cryptodb' AS project_id, GeneAttributes.* FROM  
@@ -17,18 +17,22 @@ SELECT 'toxodb' AS project_id, GeneAttributes.* FROM
 apidb.GeneAttributes@TOXOBLD
 );
 
+prompt *****select count(*) from apidb.geneattributes@CRYPTOB;
 select count(*) from apidb.geneattributes@CRYPTOB;
+prompt *****select count(*) from apidb.geneattributes@PLASBLD;
 select count(*) from apidb.geneattributes@PLASBLD;
+prompt *****select count(*) from apidb.geneattributes@TOXOBLD;
 select count(*) from apidb.geneattributes@TOXOBLD;
+prompt *****select count(*) from apidb.geneattributes;
 select count(*) from apidb.geneattributes;
 
 ---------------------------
 -- SEQUENCEs
 ---------------------------
-
+prompt *****DROP MATERIALIZED VIEW apidb.SequenceAttributes;
 DROP MATERIALIZED VIEW apidb.SequenceAttributes;
 
-
+prompt *****CREATE MATERIALIZED VIEW apidb.SequenceAttributes
 CREATE MATERIALIZED VIEW apidb.SequenceAttributes AS
 (
 SELECT 'cryptodb' AS project_id, SequenceAttributes.* FROM  
@@ -41,18 +45,28 @@ SELECT 'toxodb' AS project_id, SequenceAttributes.* FROM
 apidb.SequenceAttributes@TOXOBLD
 );
 
+prompt *****select count(*) from apidb.sequenceattributes@CRYPTOB;
 select count(*) from apidb.sequenceattributes@CRYPTOB;
+prompt *****select count(*) from apidb.sequenceattributes@PLASBLD;
 select count(*) from apidb.sequenceattributes@PLASBLD;
+prompt *****select count(*) from apidb.sequenceattributes@TOXOBLD;
 select count(*) from apidb.sequenceattributes@TOXOBLD;
+prompt *****select count(*) from apidb.sequenceattributes;
 select count(*) from apidb.sequenceattributes;
+
+
+
+
 
 ---------------------------
 -- ESTs
 ---------------------------
 
+prompt *****DROP MATERIALIZED VIEW apidb.EstAttributes;
 DROP MATERIALIZED VIEW apidb.EstAttributes;
 
 
+prompt *****CREATE MATERIALIZED VIEW apidb.EstAttributes
 CREATE MATERIALIZED VIEW apidb.EstAttributes AS
 (
 SELECT 'cryptodb' AS project_id, EstAttributes.* FROM  
@@ -65,18 +79,28 @@ SELECT 'toxodb' AS project_id, EstAttributes.* FROM
 apidb.EstAttributes@TOXOBLD
 );
 
+prompt *****select count(*) from apidb.estattributes@CRYPTOB;
 select count(*) from apidb.estattributes@CRYPTOB;
+prompt *****select count(*) from apidb.estattributes@PLASBLD;
 select count(*) from apidb.estattributes@PLASBLD;
+prompt *****select count(*) from apidb.estattributes@TOXOBLD;
 select count(*) from apidb.estattributes@TOXOBLD;
+prompt *****select count(*) from apidb.estattributes;
 select count(*) from apidb.estattributes;
+
+
+
+
 
 ---------------------------
 -- ORFs
 ---------------------------
 
+prompt *****DROP MATERIALIZED VIEW apidb.OrfAttributes;
 DROP MATERIALIZED VIEW apidb.OrfAttributes;
 
 
+prompt *****CREATE MATERIALIZED VIEW apidb.OrfAttributes
 CREATE MATERIALIZED VIEW apidb.OrfAttributes AS
 (
 SELECT 'cryptodb' AS project_id, OrfAttributes.* FROM  
@@ -89,19 +113,28 @@ SELECT 'toxodb' AS project_id, OrfAttributes.* FROM
 apidb.OrfAttributes@TOXOBLD
 );
 
+prompt *****select count(*) from apidb.orfattributes@CRYPTOB;
 select count(*) from apidb.orfattributes@CRYPTOB;
+prompt *****select count(*) from apidb.orfattributes@PLASBLD;
 select count(*) from apidb.orfattributes@PLASBLD;
+prompt *****select count(*) from apidb.orfattributes@TOXOBLD;
 select count(*) from apidb.orfattributes@TOXOBLD;
+prompt *****select count(*) from apidb.orfattributes;
 select count(*) from apidb.orfattributes;
+
+
+
+
 
 ---------------------------
 -- SNPs
 ---------------------------
 
+prompt *****DROP MATERIALIZED VIEW apidb.SnpAttributes;
 DROP MATERIALIZED VIEW apidb.SnpAttributes;
 
 
-
+prompt *****CREATE MATERIALIZED VIEW apidb.SnpAttributes
 CREATE MATERIALIZED VIEW apidb.SnpAttributes AS
 (
 SELECT 'cryptodb' AS project_id, SnpAttributes.* FROM  
@@ -114,7 +147,14 @@ SELECT 'toxodb' AS project_id, SnpAttributes.* FROM
 apidb.SnpAttributes@TOXOBLD
 );
 
+prompt *****select count(*) from apidb.snpattributes@CRYPTOB;
 select count(*) from apidb.snpattributes@CRYPTOB;
+prompt *****select count(*) from apidb.snpattributes@PLASBLD;
 select count(*) from apidb.snpattributes@PLASBLD;
+prompt *****select count(*) from apidb.snpattributes@TOXOBLD;
 select count(*) from apidb.snpattributes@TOXOBLD;
+prompt *****select count(*) from apidb.snpattributes;
 select count(*) from apidb.snpattributes;
+
+
+
