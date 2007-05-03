35b124a0e79699c9274095ba98bb7dd62bf6ba54
John Iodice <iodice@pennmedicine.upenn.edu>
2007-05-03 22:21:07 +0000

    add SyntenyAnchor table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@16221 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createSyntenyTables.sql
+++ Load/lib/sql/createSyntenyTables.sql
@@ -53,4 +53,50 @@ WHERE 'Synteny' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 ------------------------------------------------------------------------------
+
+CREATE TABLE ApiDB.SyntenyAnchor (
+ synteny_anchor_id            NUMBER(10),
+ synteny_id                   NUMBER(10),
+ syntenic_loc                 NUMBER(12),
+ prev_ref_loc                 NUMBER(12),
+ ref_loc                      NUMBER(12),
+ next_ref_loc                 NUMBER(12),
+ modification_date            DATE,
+ user_read                    NUMBER(1),
+ user_write                   NUMBER(1),
+ group_read                   NUMBER(1),
+ group_write                  NUMBER(1),
+ other_read                   NUMBER(1),
+ other_write                  NUMBER(1),
+ row_user_id                  NUMBER(12),
+ row_group_id                 NUMBER(3),
+ row_project_id               NUMBER(4),
+ row_alg_invocation_id        NUMBER(12),
+ FOREIGN KEY (synteny_id) REFERENCES apidb.Synteny (synteny_id),
+ PRIMARY KEY (synteny_anchor_id)
+);
+
+CREATE SEQUENCE ApiDB.SyntenyAnchor_sq;
+
+GRANT insert, select, update, delete ON ApiDB.SyntenyAnchor TO gus_w;
+GRANT select ON ApiDB.SyntenyAnchor TO gus_r;
+GRANT select ON ApiDB.SyntenyAnchor_sq TO gus_w;
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'SyntenyAnchor',
+       'Standard', 'synteny_anchor_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE LOWER(name) = 'apidb') d
+WHERE 'SyntenyAnchor' NOT IN (SELECT name FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
+
+------------------------------------------------------------------------------
 exit;
