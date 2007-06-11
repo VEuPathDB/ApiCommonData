b2265570870c33a8beb6be656361f0b9cbf5fa8a
John Iodice <iodice@pennmedicine.upenn.edu>
2007-06-11 20:26:13 +0000

    cache to speed up reporting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@16876 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createReportCache.sql
@@ -0,0 +1,17 @@
+CREATE TABLE apidb.GeneTable (
+ source_id  VARCHAR2(50),
+ table_name VARCHAR2(80),
+ row_count  NUMBER(4),
+ content    CLOB
+);
+
+CREATE INDEX apidb.gtab_ix
+       ON apidb.GeneTable (source_id, table_name, row_count);
+CREATE INDEX apidb.gtab_name_ix
+       ON apidb.GeneTable (table_name, source_id, row_count);
+
+GRANT insert, select, update, delete ON ApiDB.GeneTable TO gus_w;
+GRANT select ON ApiDB.GeneTable TO gus_r;
+
+------------------------------------------------------------------------------
+exit;
