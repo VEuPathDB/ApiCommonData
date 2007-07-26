073a358a2c76e857d8bb81b300711687b243dad2
Jennifer Dommer <jldommer@_.com>
2007-07-26 17:14:00 +0000

    reshaper for new gff files
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@17510 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/CdsAndRnasAndExons2BioperlTree.pm
@@ -0,0 +1,49 @@
+package ApiCommonData::Load::CdsAndRnasAndExons2BioperlTree;
+
+
+use strict;
+use Bio::SeqFeature::Generic;
+use Bio::Location::Simple;
+use ApiCommonData::Load::BioperlTreeUtils qw{makeBioperlFeature};
+
+
+#input: CDS with join location (if multiple exons)
+#output: standard api tree: gene->transcript->exons
+#                                           ->CDS
+
+sub preprocess {
+  my ($bioperlSeq, $plugin) = @_;
+
+  # (1) retype CDS into Gene
+  # (4) create transcript, give it a copy of the gene's location
+  # (5) add to gene
+  # (6) add exons to transcript
+  # (7) remove exons from gene
+  foreach my $bioperlFeatureTree ($bioperlSeq->get_SeqFeatures()) {
+    my @types = $bioperlFeatureTree->get_tag_values("GeneType");
+    die "Improperly formated tag. GeneType should only contain one value\n" if (scalar(@types) > 1);
+    my $type = $types[0];
+#print "TYPE: $type\n";
+    if (grep {$type eq $_} ("transcript","CDS", "tRNA", "rRNA", "snRNA","repeated_gene", "protein_coding")) {
+      $type = "coding" if ($type eq "CDS" || $type eq "transcript" || $type eq "protein_coding");
+      $type = "repeated" if ($type eq "repeated_gene");
+      $bioperlFeatureTree->primary_tag("${type}_gene");
+      my $gene = $bioperlFeatureTree;
+      my $geneLoc = $gene->location();
+      my $transcript = &makeBioperlFeature("transcript", $geneLoc, $bioperlSeq);
+      my @exons = $gene->get_SeqFeatures();
+      foreach my $exon (@exons) {
+	my $t = $exon->primary_tag();
+	die "expected bioperl exon but got '$t'" unless $t = "exon";
+	$transcript->add_SeqFeature($exon);
+      }
+      # we have to remove the exons before adding the transcript b/c
+      # remove_SeqFeatures() removes all subfeatures of the $gene
+      $gene->remove_SeqFeatures();
+      $gene->add_SeqFeature($transcript);
+    }
+  }
+}
+
+
+1;
