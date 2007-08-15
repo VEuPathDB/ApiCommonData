0323249a987931da0eced7289efe2c0cabefc7a0
Cristina Aurrecoechea <aurreco@uga.edu>
2007-08-15 16:25:38 +0000

    - updating dblinks to grab new views - we do not need to add projectid anymore since component views now have it.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@17818 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createBfmvApiDB.sql
+++ Load/lib/sql/createBfmvApiDB.sql
@@ -7,22 +7,22 @@ DROP MATERIALIZED VIEW apidb.GeneAttributes;
 prompt *****CREATE MATERIALIZED VIEW apidb.GeneAttributes
 CREATE MATERIALIZED VIEW apidb.GeneAttributes AS
 (
-SELECT 'cryptodb' AS project_id, GeneAttributes.* FROM  
-apidb.GeneAttributes@CRYPTOB
+SELECT GeneAttributes.* FROM  
+apidb.GeneAttributes@CRDEV35A_LOQUAT
 UNION
-SELECT 'plasmodb' AS project_id, GeneAttributes.* FROM  
-apidb.GeneAttributes@PLASBLD
+SELECT GeneAttributes.* FROM  
+apidb.GeneAttributes@PLAS54P
 UNION
-SELECT 'toxodb' AS project_id, GeneAttributes.* FROM  
-apidb.GeneAttributes@TOXOBLD
+SELECT GeneAttributes.* FROM  
+apidb.GeneAttributes@TOXO43P
 );
 
-prompt *****select count(*) from apidb.geneattributes@CRYPTOB;
-select count(*) from apidb.geneattributes@CRYPTOB;
-prompt *****select count(*) from apidb.geneattributes@PLASBLD;
-select count(*) from apidb.geneattributes@PLASBLD;
-prompt *****select count(*) from apidb.geneattributes@TOXOBLD;
-select count(*) from apidb.geneattributes@TOXOBLD;
+prompt *****select count(*) from apidb.geneattributes@CRDEV35A_LOQUAT;
+select count(*) from apidb.geneattributes@CRDEV35A_LOQUAT;
+prompt *****select count(*) from apidb.geneattributes@PLAS54P;
+select count(*) from apidb.geneattributes@PLAS54P;
+prompt *****select count(*) from apidb.geneattributes@TOXO43P;
+select count(*) from apidb.geneattributes@TOXO43P;
 prompt *****select count(*) from apidb.geneattributes;
 select count(*) from apidb.geneattributes;
 
@@ -35,22 +35,22 @@ DROP MATERIALIZED VIEW apidb.SequenceAttributes;
 prompt *****CREATE MATERIALIZED VIEW apidb.SequenceAttributes
 CREATE MATERIALIZED VIEW apidb.SequenceAttributes AS
 (
-SELECT 'cryptodb' AS project_id, SequenceAttributes.* FROM  
-apidb.SequenceAttributes@CRYPTOB
+SELECT SequenceAttributes.* FROM  
+apidb.SequenceAttributes@CRDEV35A_LOQUAT
 UNION
-SELECT 'plasmodb' AS project_id, SequenceAttributes.* FROM  
-apidb.SequenceAttributes@PLASBLD
+SELECT SequenceAttributes.* FROM  
+apidb.SequenceAttributes@PLAS54P
 UNION
-SELECT 'toxodb' AS project_id, SequenceAttributes.* FROM  
-apidb.SequenceAttributes@TOXOBLD
+SELECT SequenceAttributes.* FROM  
+apidb.SequenceAttributes@TOXO43P
 );
 
-prompt *****select count(*) from apidb.sequenceattributes@CRYPTOB;
-select count(*) from apidb.sequenceattributes@CRYPTOB;
-prompt *****select count(*) from apidb.sequenceattributes@PLASBLD;
-select count(*) from apidb.sequenceattributes@PLASBLD;
-prompt *****select count(*) from apidb.sequenceattributes@TOXOBLD;
-select count(*) from apidb.sequenceattributes@TOXOBLD;
+prompt *****select count(*) from apidb.sequenceattributes@CRDEV35A_LOQUAT;
+select count(*) from apidb.sequenceattributes@CRDEV35A_LOQUAT;
+prompt *****select count(*) from apidb.sequenceattributes@PLAS54P;
+select count(*) from apidb.sequenceattributes@PLAS54P;
+prompt *****select count(*) from apidb.sequenceattributes@TOXO43P;
+select count(*) from apidb.sequenceattributes@TOXO43P;
 prompt *****select count(*) from apidb.sequenceattributes;
 select count(*) from apidb.sequenceattributes;
 
@@ -69,22 +69,22 @@ DROP MATERIALIZED VIEW apidb.EstAttributes;
 prompt *****CREATE MATERIALIZED VIEW apidb.EstAttributes
 CREATE MATERIALIZED VIEW apidb.EstAttributes AS
 (
-SELECT 'cryptodb' AS project_id, EstAttributes.* FROM  
-apidb.EstAttributes@CRYPTOB
+SELECT EstAttributes.* FROM  
+apidb.EstAttributes@CRDEV35A_LOQUAT
 UNION
-SELECT 'plasmodb' AS project_id, EstAttributes.* FROM  
-apidb.EstAttributes@PLASBLD
+SELECT EstAttributes.* FROM  
+apidb.EstAttributes@PLAS54P
 UNION
-SELECT 'toxodb' AS project_id, EstAttributes.* FROM  
-apidb.EstAttributes@TOXOBLD
+SELECT EstAttributes.* FROM  
+apidb.EstAttributes@TOXO43P
 );
 
-prompt *****select count(*) from apidb.estattributes@CRYPTOB;
-select count(*) from apidb.estattributes@CRYPTOB;
-prompt *****select count(*) from apidb.estattributes@PLASBLD;
-select count(*) from apidb.estattributes@PLASBLD;
-prompt *****select count(*) from apidb.estattributes@TOXOBLD;
-select count(*) from apidb.estattributes@TOXOBLD;
+prompt *****select count(*) from apidb.estattributes@CRDEV35A_LOQUAT;
+select count(*) from apidb.estattributes@CRDEV35A_LOQUAT;
+prompt *****select count(*) from apidb.estattributes@PLAS54P;
+select count(*) from apidb.estattributes@PLAS54P;
+prompt *****select count(*) from apidb.estattributes@TOXO43P;
+select count(*) from apidb.estattributes@TOXO43P;
 prompt *****select count(*) from apidb.estattributes;
 select count(*) from apidb.estattributes;
 
@@ -103,22 +103,22 @@ DROP MATERIALIZED VIEW apidb.OrfAttributes;
 prompt *****CREATE MATERIALIZED VIEW apidb.OrfAttributes
 CREATE MATERIALIZED VIEW apidb.OrfAttributes AS
 (
-SELECT 'cryptodb' AS project_id, OrfAttributes.* FROM  
-apidb.OrfAttributes@CRYPTOB
+SELECT OrfAttributes.* FROM  
+apidb.OrfAttributes@CRDEV35A_LOQUAT
 UNION
-SELECT 'plasmodb' AS project_id, OrfAttributes.* FROM  
-apidb.OrfAttributes@PLASBLD
+SELECT OrfAttributes.* FROM  
+apidb.OrfAttributes@PLAS54P
 UNION
-SELECT 'toxodb' AS project_id, OrfAttributes.* FROM  
-apidb.OrfAttributes@TOXOBLD
+SELECT OrfAttributes.* FROM  
+apidb.OrfAttributes@TOXO43P
 );
 
-prompt *****select count(*) from apidb.orfattributes@CRYPTOB;
-select count(*) from apidb.orfattributes@CRYPTOB;
-prompt *****select count(*) from apidb.orfattributes@PLASBLD;
-select count(*) from apidb.orfattributes@PLASBLD;
-prompt *****select count(*) from apidb.orfattributes@TOXOBLD;
-select count(*) from apidb.orfattributes@TOXOBLD;
+prompt *****select count(*) from apidb.orfattributes@CRDEV35A_LOQUAT;
+select count(*) from apidb.orfattributes@CRDEV35A_LOQUAT;
+prompt *****select count(*) from apidb.orfattributes@PLAS54P;
+select count(*) from apidb.orfattributes@PLAS54P;
+prompt *****select count(*) from apidb.orfattributes@TOXO43P;
+select count(*) from apidb.orfattributes@TOXO43P;
 prompt *****select count(*) from apidb.orfattributes;
 select count(*) from apidb.orfattributes;
 
@@ -137,17 +137,17 @@ DROP MATERIALIZED VIEW apidb.SnpAttributes;
 prompt *****CREATE MATERIALIZED VIEW apidb.SnpAttributes
 CREATE MATERIALIZED VIEW apidb.SnpAttributes AS
 (
-SELECT 'plasmodb' AS project_id, SnpAttributes.* FROM  
-apidb.SnpAttributes@PLASBLD
+SELECT SnpAttributes.* FROM  
+apidb.SnpAttributes@PLAS54P
 UNION
-SELECT 'toxodb' AS project_id, SnpAttributes.* FROM  
-apidb.SnpAttributes@TOXOBLD
+SELECT SnpAttributes.* FROM  
+apidb.SnpAttributes@TOXO43P
 );
 
-prompt *****select count(*) from apidb.snpattributes@PLASBLD;
-select count(*) from apidb.snpattributes@PLASBLD;
-prompt *****select count(*) from apidb.snpattributes@TOXOBLD;
-select count(*) from apidb.snpattributes@TOXOBLD;
+prompt *****select count(*) from apidb.snpattributes@PLAS54P;
+select count(*) from apidb.snpattributes@PLAS54P;
+prompt *****select count(*) from apidb.snpattributes@TOXO43P;
+select count(*) from apidb.snpattributes@TOXO43P;
 prompt *****select count(*) from apidb.snpattributes;
 select count(*) from apidb.snpattributes;
 
