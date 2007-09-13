17e299c5381516512b006e1dcd451e26820ecd92
John Iodice <iodice@pennmedicine.upenn.edu>
2007-09-13 15:36:54 +0000

    two utility scripts for managing our materialized views
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@18204 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/4d.sql
@@ -0,0 +1,14 @@
+/*
+   4d.sql -- Show digits embedded in materialized-view names
+             (See createBfmv.sql for why we care).
+             Note that an mview named 'flank3prime2222' would generate the
+             output value '32222'.
+*/
+
+select distinct regexp_replace(mview_name, '[^0-9]*', '')
+       as "digits embedded in mview names"
+from all_mviews
+where regexp_replace(mview_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
+      like '%fournumbers';
+
+exit;
---
+++ Load/lib/sql/superfluous_mviews.sql
@@ -0,0 +1,27 @@
+
+/*
+   superfluous_mviews.sql
+
+   Finds materialized views whose names end in four digits and which have no
+   synonyms pointing to them.  These are probably obsolete.
+
+   Example: The synonym GeneAttributes points to the mview GeneAttributes1111.
+   We want to change its schema, so we create GeneAttributes2468 and point the
+   synonym at it.  GeneAttributes1111 is now obsolete, and can be dropped.
+
+   For convenience, the output is in the form of "drop materialized view"
+   statements.
+/*
+
+
+SELECT 'drop materialized view ' || owner || '.' || mview_name || ';' AS drops
+FROM all_mviews
+WHERE mview_name IN (SELECT mview_name
+                     FROM all_mviews
+                    MINUS
+                     SELECT table_name
+                     FROM all_synonyms)
+  AND REGEXP_REPLACE(mview_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
+      LIKE '%fournumbers';
+
+exit
