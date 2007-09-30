eb5e284215333f6628cdce1a4263b98c0bb0dbbe
John Brestelli <jbrestell@gmail.com>
2007-09-30 22:17:49 +0000

    allow for 2 channel data and fix the undo method
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@18455 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertExprProfileFromProcessedResult.pm
+++ Load/plugin/perl/InsertExprProfileFromProcessedResult.pm
@@ -82,7 +82,7 @@ my $argsDeclaration =
 	    descr => 'the type of processing used',
 	    reqd => 1,
 	    constraintFunc => undef,
-	    enum => "RMAExpress, AffymetrixMas5, MOID",
+	    enum => "RMAExpress, AffymetrixMas5, MOID, DataTransformationResult",
 	    isList => 0,
 	   }),
    booleanArg({name => 'loadProfileElement',
@@ -104,6 +104,12 @@ my $argsDeclaration =
                 name           => 'baseX',
                 isList         => 0,
                 reqd           => 0,
+                constraintFunc => undef,
+              }),
+   integerArg({ descr          => '1 or 2 channel data',
+                name           => 'numberOfChannels',
+                isList         => 0,
+                reqd           => 1,
                 constraintFunc => undef,
               }),
   ];
@@ -157,22 +163,55 @@ sub makeHeader {
   my ($self, $extDbRlsId, $protocolName) = @_;
 
  # sort by quantification_id to retain the desired order of quants
-  my $sql = "
+  my %sqlHash = (
+                 '1' => <<Sql,
 SELECT s.name, a.name, q.quantification_id
 FROM Study.Study s, RAD.Protocol p,
      RAD.StudyAssay sa, RAD.Acquisition ac, 
      Rad.Assay a, RAD.Quantification q
-WHERE s.external_database_release_id = $extDbRlsId
-  AND p.name = '$protocolName'
+WHERE s.external_database_release_id = ?
+  AND p.name = ?
   AND sa.study_id = s.study_id
   AND a.assay_id = sa.assay_id
   AND ac.assay_id = a.assay_id
   AND q.acquisition_id = ac.acquisition_id
   AND q.protocol_id = p.protocol_id
 ORDER BY s.name, a.name
-";
+Sql
+                 '2' => <<Sql,
+select distinct s.name, ap.value
+from SRes.EXTERNALDATABASERELEASE r, study.STUDY s, Rad.STUDYASSAY sa, Rad.ACQUISITION a,
+     Rad.QUANTIFICATION q, Core.TABLEINFO t, Rad.LogicalGroupLink ll,
+     Rad.ANALYSISINPUT ai, Rad.ANALYSIS an, Rad.PROTOCOLPARAM pp, 
+     Core.DATABASEINFO d, Rad.ANALYSISPARAM ap, Rad.Protocol p
+where r.external_database_release_id = ?
+ and p.name = ?
+ and d.name = 'RAD'
+ and t.name = 'Quantification'
+ and pp.name = 'Analysis Name'
+ and r.external_database_release_id = s.external_database_release_id
+ and s.study_id = sa.study_id
+ and sa.assay_id = a.assay_id
+ and a.acquisition_id = q.acquisition_id
+ and q.quantification_id = ll.row_id
+ and d.database_id = t.database_id
+ and t.table_id = ll.table_id
+ and ll.logical_group_id = ai.logical_group_id
+ and ai.analysis_id = an.analysis_id
+ and an.protocol_id = p.protocol_id
+ and p.protocol_id = pp.protocol_id
+ and pp.protocol_param_id = ap.protocol_param_id
+ and ap.analysis_id = an.analysis_id
+ORDER BY s.name, ap.value
+Sql
+                );
 
-  my $stmt = $self->prepareAndExecute($sql);
+  my $numberOfChannels = $self->getArg('numberOfChannels');
+  my $sql = $sqlHash{$numberOfChannels};
+
+  my $dbh = $self->getQueryHandle();
+  my $stmt = $dbh->prepare($sql);
+  $stmt->execute($extDbRlsId, $protocolName);
   my $studyCount = 0;
   my $prevStudyName = 'pooh';
   my $header = [];
@@ -198,22 +237,25 @@ sub getProfileRows {
   $self->log("Getting the profile data from database");
 
   my $resultViewName = $self->getArg('processingType');
-  my $resultColumnHash = {RMAExpress => 'RMA_EXPRESSION_MEASURE',
-			  AffymetrixMas5 => '',
-			  MOID => ''};
+  my $resultColumnHash = { RMAExpress => 'RMA_EXPRESSION_MEASURE',
+			   AffymetrixMas5 => 'SIGNAL',
+                           MOID => '',
+                           DataTransformationResult => 'FLOAT_VALUE',
+                         };
   my $resultColumn = $resultColumnHash->{$resultViewName};
   $self->error("can't find resultColumn") unless $resultColumn;
 
-  my $sql = "
+  my %sqlHash = (
+                 '1' => <<Sql,
 SELECT g.source_id, a.name, cer.$resultColumn
 FROM Study.Study s, RAD.ArrayDesign ad, RAD.Protocol p,
      RAD.StudyAssay sa, RAD.Assay a, RAD.Acquisition ac, RAD.Quantification q,
      RAD.ShortOligoFamily  ce, RAD.$resultViewName cer,
      DoTS.GeneFeature g, RAD.CompositeElementNaSequence ces,
      DoTS.Transcript t
-WHERE s.external_database_release_id = $extDbRlsId
-  AND ad.name = '$arrayDesignName'
-  AND p.name = '$protocolName'
+WHERE s.external_database_release_id = ?
+  AND ad.name = ?
+  AND p.name = ?
   AND sa.study_id = s.study_id
   AND a.assay_id = sa.assay_id
   AND ac.assay_id = a.assay_id
@@ -226,18 +268,59 @@ WHERE s.external_database_release_id = $extDbRlsId
   AND ces.na_sequence_id = t.na_sequence_id
   AND g.na_feature_id = t.parent_id
   ORDER BY g.source_id
-";
+Sql
+                 '2' => <<Sql,
+SELECT distinct gf.source_id, ap.value, cer.$resultColumn
+FROM Study.Study s, RAD.StudyAssay sa, RAD.ACQUISITION ac, 
+     RAD.QUANTIFICATION q, Core.TABLEINFO ti, Rad.LogicalGroupLink lgl,
+     Rad.ANALYSISINPUT ai, Rad.ANALYSIS a, Rad.PROTOCOLPARAM pp, 
+     Core.DATABASEINFO di, Rad.ANALYSISPARAM ap, Rad.Protocol p,
+     RAD.ArrayDesign ad, RAD.ShortOligoFamily  ce, RAD.$resultViewName cer,
+     DoTS.GeneFeature gf, RAD.CompositeElementNaSequence ces, DoTS.Transcript t
+WHERE s.external_database_release_id = ?
+  AND ad.name = ?
+  AND p.name = ?
+  and di.name = 'RAD'
+  and ti.name = 'Quantification'
+  and pp.name = 'Analysis Name'
+  and s.study_id = sa.study_id
+  and sa.assay_id = ac.assay_id
+  and ac.acquisition_id = q.acquisition_id
+  and q.quantification_id = lgl.row_id
+  and lgl.table_id = ti.table_id
+  and ti.database_id = di.database_id
+  and lgl.logical_group_id = ai.logical_group_id
+  and ai.analysis_id = a.analysis_id
+  and a.protocol_id = p.protocol_id
+  and p.protocol_id = pp.protocol_id
+  and pp.protocol_param_id = ap.protocol_param_id
+  and ap.analysis_id = a.analysis_id
+  and a.analysis_id = cer.analysis_id
+  and ad.array_design_id = ce.array_design_id
+  and ce.composite_element_id = cer.row_id
+  AND ce.composite_element_id = ces.composite_element_id
+  AND ces.na_sequence_id = t.na_sequence_id
+  AND gf.na_feature_id = t.parent_id
+order by gf.source_id
+Sql
+                );
+
+  my $numberOfChannels = $self->getArg('numberOfChannels');
+  my $sql = $sqlHash{$numberOfChannels};
+
+  my $dbh = $self->getQueryHandle();
+  my $stmt = $dbh->prepare($sql);
+
+  $stmt->execute($extDbRlsId, $arrayDesignName, $protocolName);
 
   # transform individual result rows to a single row with all results for a
   # sourceId
-  my $stmt = $self->prepareAndExecute($sql);
   my $rows = [];
   my $sourceIdRowHash = {};
   my $count = 0;
   my $prevSourceId = "first";
-  my ($sourceId, $quantName, $result);
 
-  while (($sourceId, $quantName, $result) = $stmt->fetchrow_array()) {
+  while(my ($sourceId, $quantName, $result) = $stmt->fetchrow_array()) {
     if ($sourceId ne $prevSourceId && $prevSourceId ne 'first') {
       if ($count++ != 0) {
 	my $row = $self->makeSourceIdRow($prevSourceId, $sourceIdRowHash,
@@ -281,9 +364,12 @@ sub makeSourceIdRow {
 sub undoTables {
   my ($self) = @_;
 
-  return ('DoTS.ExternalNASequence',
-	  'RAD.ElementNASequence',
+  return ('ApiDB.ProfileElement',
+	  'ApiDB.Profile',
+	  'ApiDB.ProfileElementName',
+	  'ApiDB.ProfileSet',
 	 );
 }
+
 
 1;
