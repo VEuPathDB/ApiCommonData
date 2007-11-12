589b4e0552f941edbbdc39d6f7428b6f04587e37
John Brestelli <jbrestell@gmail.com>
2007-11-12 16:19:16 +0000

    refactored long methods, added perldoc, hack fix for paralogs in synteny anchors... (want Gbrowse Window to return exactly 2 rows
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@19070 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertSyntenySpans.pm
+++ Load/plugin/perl/InsertSyntenySpans.pm
@@ -105,38 +105,75 @@ sub new {
     return $self;
 }
 
+#--------------------------------------------------------------------------------
+
 sub run {
   my ($self) = @_;
 
   my $file = $self->getArg('inputFile');
-
   my $extDbRlsIdA = $self->getExtDbRlsId($self->getArg('extDbRlsSpecA'));
-
   my $extDbRlsIdB = $self->getExtDbRlsId($self->getArg('extDbRlsSpecB'));
-
   my $synDbRlsId = $self->getExtDbRlsId($self->getArg('syntenyDbRlsSpec'));
 
   my $count = 0;
 
   open(IN, "<$file") or $self->error("Couldn't open file '$file': $!\n");
+
   while (<IN>) {
     chomp;
-    $self->_handleSyntenySpan($_, $extDbRlsIdA, $extDbRlsIdB, $synDbRlsId);
-    $count+=2;
 
-    $self->log("Inserted $count ApiDB::Synteny") if($count % 500 == 0 || $count-1 % 500 == 0);
+    $self->_handleSyntenySpan($_, $extDbRlsIdA, $extDbRlsIdB, $synDbRlsId);
+    $count++;
+
+    if($count % 500) {
+      $self->log("Read $count lines... Inserted " . $count*2 . " ApiDB::Synteny");
+    }
+
+    $self->undefPointerCache();
   }
   close(IN);
 
-  my $anchorCount = $self->insertAnchors($synDbRlsId, $extDbRlsIdA,
-					 $self->getArg('extDbRlsSpecA'));
-  $anchorCount += $self->insertAnchors($synDbRlsId, $extDbRlsIdB,
-				       $self->getArg('extDbRlsSpecB'));
+  $self->insertAnchors($synDbRlsId, $extDbRlsIdA, $self->getArg('extDbRlsSpecA'));
+  $self->insertAnchors($synDbRlsId, $extDbRlsIdB, $self->getArg('extDbRlsSpecB'));
 
-  $self->log("Inserted $count ApiDB::SyntenyAnchors") if($anchorCount % 50 == 0 || $anchorCount-1 % 50 == 0);
+  my $anchorCount = $self->getAnchorCount();
 
   return "inserted $count synteny spans and $anchorCount anchors ";
 }
+
+#--------------------------------------------------------------------------------
+
+sub getAnchorCount {$_[0]->{anchor_count}}
+
+sub countAnchor {
+  my ($self) = @_;
+
+  my $count = $self->{anchor_count}++;
+
+  $self->log("Inserted $count rows into ApiDb::SyntenyAnchor") if ($count && $count % 1000 == 0);
+}
+
+#--------------------------------------------------------------------------------
+
+=head2 Subroutines
+
+=over 4
+
+=item I<_handleSyntenySpan>
+
+B<Parameters:>
+
+ $self(_PACKAGE_):
+ $line(STRING): ex: "MAL13   ctg_7202        111895  790019  115060  856803  +"
+ $extDbRlsIdA(NUMBER): SRes::ExternalDatabaseRelease id for the Genome in the first column
+ $extDbRlsIdB(NUMBER): SRes::ExternalDatabaseRelease id for the Genome in the second column
+ $synDbRlsId(NUBER): SRes::ExternalDatabaseRelease id for the results
+
+B<Return Type:> ARRAY
+
+ 2 Elements, Both are ApiDB.Synteny Objects... the second being the opposite of the first
+
+=cut
 
 sub _handleSyntenySpan {
   my ($self, $line, $extDbRlsIdA, $extDbRlsIdB, $synDbRlsId) = @_;
@@ -146,46 +183,78 @@ sub _handleSyntenySpan {
       $b_start, $b_len,
       $strand) = split(" ", $line);
 
-  my ($a_pk) = $self->getDbHandle()->selectrow_array(<<EOSQL, undef, $a_id, $extDbRlsIdA);
-  SELECT na_sequence_id
-  FROM   @{[$self->getArg('seqTableA')]}
-  WHERE  source_id = ?
-    AND  external_database_release_id = ?  
-EOSQL
-  $self->error("Couldn't find primary key for $a_id with external database release id $extDbRlsIdA\n") unless $a_pk;
-  
-  my ($b_pk) = $self->getDbHandle()->selectrow_array(<<EOSQL, undef, $b_id, $extDbRlsIdB);
-  SELECT na_sequence_id
-  FROM   @{[$self->getArg('seqTableB')]}
-  WHERE  source_id = ?
-    AND  external_database_release_id = ?  
-EOSQL
-  $self->error("Couldn't find primary key for $b_id with external database release id $extDbRlsIdB\n") unless $b_pk;
-  
+  my $a_pk = $self->getNaSequenceId($a_id, $extDbRlsIdA, $self->getArg('seqTableA'));
+  my $b_pk = $self->getNaSequenceId($b_id, $extDbRlsIdB, $self->getArg('seqTableB'));
+
+  my $a_end = $a_start + $a_len - 1;
+  my $b_end = $b_start + $b_len - 1;
+  my $isReversed = $strand eq "-" ? 1 : 0;
+
+  my $synteny = $self->makeSynteny($a_pk, $b_pk, $a_start, $b_start, $a_end, $b_end, $isReversed, $synDbRlsId);
+  my $reverse = $self->makeSynteny($b_pk, $a_pk, $b_start, $a_start, $b_end, $a_end, $isReversed, $synDbRlsId);
+
+  return($synteny, $reverse);
+}
+
+#--------------------------------------------------------------------------------
+
+=item I<makeSynteny>
+
+B<Parameters:>
+
+ $self(_PACKAGE_):
+ $a_pk(NUMBER): na_sequence_id
+ $b_pk(NUMBER): na_sequence_id
+ $a_start(NUMBER): Genomic coordinate where synteny begins
+ $b_start(NUMBER): Genomic coordinate where synteny begins
+ $a_end(NUMBER): Genomic coordinate where synteny ends
+ $b_end(NUMBER): Genomic coordinate where synteny ends
+ $isReversed(BOOLEAN): 1 for - strand; 0 for + strand
+ $synDbRlsId(NUMBER): SRes::ExternalDatabaseRelease Id for the Synteny object
+
+B<Return Type:> ARRAY
+
+ 2 Elements, Both are ApiDB.Synteny Objects... the second being the opposite of the first
+
+=cut
+
+sub makeSynteny {
+  my ($self, $a_pk, $b_pk, $a_start, $b_start, $a_end, $b_end, $isReversed, $synDbRlsId) = @_;
+
   my $synteny = GUS::Model::ApiDB::Synteny->new({ a_na_sequence_id => $a_pk,
 						  b_na_sequence_id => $b_pk,
 						  a_start => $a_start,
 						  b_start => $b_start,
-						  a_end   => $a_start + $a_len - 1,
-						  b_end   => $b_start + $b_len - 1,
-						  is_reversed => $strand eq "-"? 1 : 0,
+						  a_end   => $a_end,
+						  b_end   => $b_end,
+						  is_reversed => $isReversed,
 						  external_database_release_id => $synDbRlsId,
 						});
   $synteny->submit();
 
-  # add row w/ opposite organism as ref
-  my $synteny = GUS::Model::ApiDB::Synteny->new({ a_na_sequence_id => $b_pk,
-						  b_na_sequence_id => $a_pk,
-						  a_start => $b_start,
-						  b_start => $a_start,
-						  a_end   => $b_start + $b_len - 1,
-						  b_end   => $a_start + $a_len - 1,
-						  is_reversed => $strand eq "-"? 1 : 0,
-						  external_database_release_id => $synDbRlsId,
-						});
-  $synteny->submit();
-  $self->undefPointerCache();
+  return $synteny;
 }
+
+#--------------------------------------------------------------------------------
+
+sub getNaSequenceId {
+  my ($self, $sourceId, $extDbRlsId, $seqTable) = @_;
+
+  my $dbh = $self->getQueryHandle();
+  my $sql = "SELECT na_sequence_id FROM $seqTable 
+             WHERE  source_id = ? AND external_database_release_id = ?";
+
+  my $sh = $dbh->prepare($sql);
+
+  my @ids = $self->sqlAsArray( Handle => $sh, Bind => [$sourceId, $extDbRlsId] );
+
+  if(scalar @ids != 1) {
+    $self->error("Sql Should return only one value: $sql\n for values: $sourceId and $extDbRlsId");
+  }
+  return $ids[0];
+}
+
+#--------------------------------------------------------------------------------
 
 sub insertAnchors {
   my ($self, $synDbRlsId, $extDbRlsIdA, $extDbSpec) = @_;
@@ -195,9 +264,9 @@ sub insertAnchors {
   my ($gene2orthologGroup, $orthologGroup2refGenes)
     = $self->findOrthologGroups($extDbRlsIdA);
 
-  my $findGenesStmt = $self->getFindGenesStmt();
+  my $genesLocStmt = $self->prepareGenesLocStmt();
 
-  my $sql = "
+  my $retrieveSyntenySql = "
 select syn.*
 from apidb.Synteny syn, dots.ExternalNaSequence seq
 where syn.external_database_release_id = $synDbRlsId
@@ -205,19 +274,15 @@ and seq.na_sequence_id = syn.a_na_sequence_id
 and seq.external_database_release_id = $extDbRlsIdA
 ";
 
-  my $stmt = $self->getDbHandle()->prepareAndExecute($sql);
+  my $syntenyStmt = $self->getDbHandle()->prepareAndExecute($retrieveSyntenySql);
 
-  my $anchorCount = 0;
-
-  while(my $syntenyRow = $stmt->fetchrow_hashref) {
-  my $rev = $syntenyRow->{IS_REVERSED};
-
-    my $refGenes = $self->findGenes($findGenesStmt,
+  while(my $syntenyRow = $syntenyStmt->fetchrow_hashref) {
+    my $refGenes = $self->findGenes($genesLocStmt,
 				    $syntenyRow->{A_NA_SEQUENCE_ID},
 				    $syntenyRow->{A_START},
 				    $syntenyRow->{A_END});
 
-    my $synGenes = $self->findGenes($findGenesStmt,
+    my $synGenes = $self->findGenes($genesLocStmt,
 				    $syntenyRow->{B_NA_SEQUENCE_ID},
 				    $syntenyRow->{B_START},
 				    $syntenyRow->{B_END});
@@ -227,69 +292,69 @@ and seq.external_database_release_id = $extDbRlsIdA
 					     $gene2orthologGroup,
 					     $orthologGroup2refGenes);
 
-    my $syntenyObj = 
-      GUS::Model::ApiDB::Synteny->new({synteny_id => $syntenyRow->{SYNTENY_ID}});
+    $self->createSyntenyAnchors($syntenyRow, $genePairs);
 
-    $syntenyObj->retrieveFromDB();
-
-    my $anchors = [];
-
-    my $anchorsCursor = 0;
-
-    $anchorCount += $self->addAnchor($syntenyObj,
-				     $syntenyRow->{A_START},
-				     $syntenyRow->{$rev? 'B_END':'B_START'},
-				     $anchors, $anchorsCursor++,
-				     $anchorCount,
-				     0);
-
-    foreach my $genePair (@$genePairs) {
-
-      if ($genePair->{refStart} > $syntenyRow->{A_START}) {
-	$anchorCount += $self->addAnchor($syntenyObj, 
-					 $genePair->{refStart},
-					 $genePair->{$rev? 'synEnd':'synStart'},
-					 $anchors, $anchorsCursor++,
-					 $anchorCount,
-					 0);
-      }
-
-      if ($genePair->{refEnd} < $syntenyRow->{A_END}) {
-	$anchorCount += $self->addAnchor($syntenyObj,
-					 $genePair->{refEnd},
-					 $genePair->{$rev? 'synStart':'synEnd'},
-					 $anchors, $anchorsCursor++,
-					 $anchorCount,
-					 0);
-      }
-    }
-
-    $anchorCount += $self->addAnchor($syntenyObj,
-				     $syntenyRow->{A_END},
-				     $syntenyRow->{$rev? 'B_START':'B_END'},
-				     $anchors, $anchorsCursor++,
-				     $anchorCount,
-				     1);
-
-    $syntenyObj->submit();
     $self->undefPointerCache();
   }
-  return $anchorCount;
+  return 1;
 }
 
-sub getFindGenesStmt {
+#--------------------------------------------------------------------------------
+
+
+sub createSyntenyAnchors {
+  my ($self, $syntenyRow, $genePairs) = @_;
+
+  my $rev = $syntenyRow->{IS_REVERSED};
+  my $syntenyId =  $syntenyRow->{SYNTENY_ID};
+
+  my $syntenyObj = GUS::Model::ApiDB::Synteny->new({synteny_id => $syntenyId});
+  $syntenyObj->retrieveFromDB();
+
+  my $anchors = [];
+
+  my $anchorsCursor = 0;
+
+  $self->addAnchor($syntenyObj,
+                   $syntenyRow->{A_START},
+                   $syntenyRow->{$rev? 'B_END':'B_START'},
+                   $anchors, $anchorsCursor++,
+                   0);
+
+  foreach my $genePair (@$genePairs) {
+
+    if ($genePair->{refStart} > $syntenyRow->{A_START}) {
+      $self->addAnchor($syntenyObj, 
+                       $genePair->{refStart},
+                       $genePair->{$rev? 'synEnd':'synStart'},
+                       $anchors, $anchorsCursor++,
+                       0);
+    }
+
+    if ($genePair->{refEnd} < $syntenyRow->{A_END}) {
+      $self->addAnchor($syntenyObj,
+                       $genePair->{refEnd},
+                       $genePair->{$rev? 'synStart':'synEnd'},
+                       $anchors, $anchorsCursor++,
+                       0);
+    }
+  }
+
+  $self->addAnchor($syntenyObj,
+                   $syntenyRow->{A_END},
+                   $syntenyRow->{$rev? 'B_START':'B_END'},
+                   $anchors, $anchorsCursor++,
+                   1);
+
+  $syntenyObj->submit();
+}
+
+#--------------------------------------------------------------------------------
+
+sub prepareGenesLocStmt {
   my ($self) = @_;
 
-#  my $sql = "
-#select g.na_feature_id, l.start_min, l.end_max
-#from dots.GeneFeature g, dots.NaLocation l
-#where g.na_sequence_id = ?
-#and l.na_feature_id = g.na_feature_id
-#and l.end_max > ?
-#and l.start_min < ?
-#";
-
-my $sql = "select na_feature_id, start_min, end_max
+  my $sql = "select na_feature_id, start_min, end_max
 from apidb.FEATURELOCATION
 where feature_type = 'GeneFeature'
 and na_sequence_id = ?
@@ -299,13 +364,32 @@ and start_min < ?";
   return $self->getDbHandle()->prepare($sql);
 }
 
+#--------------------------------------------------------------------------------
+
+=item I<findOrthologGroups>
+
+B<Parameters:>
+
+ $self(_PACKAGE_):
+ $extDbRlsIdA(NUMBER): SRes::ExternalDatabaseRelease id for the Reference Genome
+
+B<Return Type:> ARRAY
+
+2 elements, each are HASHREFS.  
+   The first maps GeneFeature na_feature_id to SequenceSequenceGroup sequence_group_id
+   The second tracks which na_feature_ids are from the reference.
+
+=cut
+
 sub findOrthologGroups {
   my ($self, $extDbRlsIdA) = @_;
 
 my $sql = "
 select ssg.sequence_id, ssg.sequence_group_id, g.external_database_release_id
-from dots.SequenceSequenceGroup ssg, dots.genefeature g
-where g.na_feature_id = ssg.sequence_id
+from dots.SequenceSequenceGroup ssg, dots.genefeature g, Core.TableInfo t
+where t.name = 'GeneFeature' 
+ and g.na_feature_id = ssg.sequence_id
+ and t.table_id = ssg.source_table_id
 ";
   my $stmt = $self->getDbHandle()->prepareAndExecute($sql);
 
@@ -320,6 +404,24 @@ where g.na_feature_id = ssg.sequence_id
   return ($gene2orthologGroup, $orthologGroup2refGenes);
 }
 
+#--------------------------------------------------------------------------------
+
+=item I<findGenes>
+
+B<Parameters:>
+
+ $self(_PACKAGE_):
+ $stmt(prepared dbi statement handle): Find all genes (and their start/end) for given genomic coordinates
+ $na_sequence_id(NUMBER): Dots::NaSequence pk (contig/chromosome where we're looking)
+ $start(NUMBER): Where the synteny begins
+ $end(NUMBER): Where the synteny ends
+
+B<Return Type:> ARRAYREF
+
+ Each element is a HASH of genes with keys id, start, end. (id is the na_feature_id)
+
+=cut
+
 sub findGenes {
   my ($self, $stmt, $na_sequence_id, $start, $end) = @_;
 
@@ -327,28 +429,46 @@ sub findGenes {
 
   my @genes;
   while (my ($naFeatId, $geneStart, $geneEnd) = $stmt->fetchrow_array()) {
-    push(@genes, {id=>$naFeatId, start=>$geneStart, end=>$geneEnd});
+    push(@genes, {id => $naFeatId, start => $geneStart, end => $geneEnd});
   }
+
   return \@genes;
 }
 
-# each pair is a hash, with these keys:
-#  refStart
-#  synStart 
-#  refEnd
-#  synEnd
+#--------------------------------------------------------------------------------
+
+=item I<findOrthologPairs>
+
+B<Parameters:>
+
+ $self(_PACKAGE_)
+ $refGenes(ARRAYREF): ARRAYREF, Each element is a hash of genes (id, start, end)
+ $synGenes(HASHREF): ARRAYREF, Each element is a hash of genes (id, start, end)
+ $gene2orthologGroup(HASHREF): na_feature_id to SequenceSequenceGroup sequence_group_id
+ $orthologGroup2refGenes(HASHREF): second tracks which na_feature_ids are from the reference.
+
+B<Return Type:> ARRAYREF
+
+ ArrayRef of GenePairs for a Syntenic Region.  Each pair is a hash with keys: refStart, synStart, refEnd, synEnd
+
+=cut
+
 sub findOrthologPairs {
   my ($self, $refGenes, $synGenes, $gene2orthologGroup, $orthologGroup2refGenes) = @_;
 
   my @genePairs;
   foreach my $synGene (@$synGenes) {
     my $ssgId = $gene2orthologGroup->{$synGene->{id}};
+
     foreach my $refGene (@$refGenes) {
-      if ($orthologGroup2refGenes->{$ssgId}->{$refGene->{id}}) {
+      my $geneFeatureId = $refGene->{id};
+
+      if ($orthologGroup2refGenes->{$ssgId}->{$geneFeatureId}) {
 	my $genePair = {refStart=>$refGene->{start},
 			refEnd=>$refGene->{end},
 			synStart=>$synGene->{start},
 			synEnd=>$synGene->{end}};
+
 	push(@genePairs, $genePair);
       }
     }
@@ -358,24 +478,35 @@ sub findOrthologPairs {
   return \@sortedPairs;
 }
 
+#--------------------------------------------------------------------------------
+
 sub addAnchor {
-  my ($self, $syntenyObj, $refLoc, $synLoc, $anchors, $anchorsCursor, $anchorCount, $addFinalToo) = @_;
+  my ($self, $syntenyObj, $refLoc, $synLoc, $anchors, $anchorsCursor, $addFinalToo) = @_;
 
-  $anchors->[$anchorsCursor] = {prev_ref_loc=> -9999999999,
-				ref_loc=> $refLoc,
-				next_ref_loc=> 9999999999,
-				syntenic_loc=> $synLoc};
+  my $anchor = {prev_ref_loc=> -9999999999,
+                ref_loc=> $refLoc,
+                next_ref_loc=> 9999999999,
+                syntenic_loc=> $synLoc};
 
+  $anchors->[$anchorsCursor] = $anchor;
   my $prevAnchor = $anchors->[$anchorsCursor - 1];
-  my $anchor = $anchors->[$anchorsCursor];
 
   if ($anchorsCursor > 0) {
-    $prevAnchor->{next_ref_loc} = $refLoc;
-    $anchor->{prev_ref_loc} = $prevAnchor->{ref_loc};
-    $self->addAnchorToGusObj($prevAnchor, $syntenyObj, $anchorCount);
+
+    # Hack to avoid returning multiple rows in GBrowse for paralogs
+    if($prevAnchor->{ref_loc} == $anchor->{ref_loc}) {
+      $prevAnchor->{next_ref_loc} = -9999999999;
+      $anchor->{prev_ref_loc} = 9999999999;
+    }
+    else {
+      $prevAnchor->{next_ref_loc} = $refLoc;
+      $anchor->{prev_ref_loc} = $prevAnchor->{ref_loc}; 
+   }
+ 
+    $self->addAnchorToGusObj($prevAnchor, $syntenyObj);
 
     if ($addFinalToo) {
-      $self->addAnchorToGusObj($anchor, $syntenyObj, $anchorCount + 1);
+      $self->addAnchorToGusObj($anchor, $syntenyObj);
       return 2;
     }
     return 1;
@@ -384,14 +515,17 @@ sub addAnchor {
   return 0;
 }
 
+#--------------------------------------------------------------------------------
+
 sub addAnchorToGusObj {
-  my ($self, $anchor, $syntenyObj, $anchorCount) = @_;
+  my ($self, $anchor, $syntenyObj) = @_;
 
   my $anchorObj = GUS::Model::ApiDB::SyntenyAnchor->new($anchor);
   $syntenyObj->addChild($anchorObj);
-  $self->log("added $anchorCount anchors")
-    if ($anchorCount && $anchorCount % 1000 == 0);
+  $self->countAnchor();
 }
+
+#--------------------------------------------------------------------------------
 
 sub undoTables {
   return qw(ApiDB.SyntenyAnchor ApiDB.Synteny);
