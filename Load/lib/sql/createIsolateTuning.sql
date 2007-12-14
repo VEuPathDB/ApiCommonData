d7072a824297604a683efe70d02b7b2848dae6e6
Haiming Wang <hwang@uga.edu>
2007-12-14 18:25:50 +0000

    created a materialized view to concatenate multi-product name into one string delimited by '|'.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@19626 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createIsolateTuning.sql
@@ -0,0 +1,75 @@
+DROP MATERIALIZED VIEW apidb.IsolateAttributes; 
+DROP INDEX apidb.IsolateAttr_sourceId_idx;
+
+CREATE MATERIALIZED VIEW apidb.IsolateAttributes AS 
+(
+SELECT etn.na_sequence_id, 
+       etn.external_database_release_id,
+       etn.source_id,
+       src.organism as organism,
+			 src.strain || ' ' || src.isolate as strain,
+			 src.specific_host || src.lab_host specific_host,
+			 src.isolation_source,
+			 src.country,
+			 src.note,
+			 etn.description,
+			 src.pcr_primers,
+			 etn.sequence,
+			 aln.query_name,
+			 aln.target_name,
+			 aln.min_subject_start,
+			 aln.max_subject_end,
+			 aln.map
+FROM   DoTS.ExternalNASequence etn,
+			 DoTS.IsolateSource src,
+			 SRes.ExternalDatabaseRelease edr,
+			 SRes.ExternalDatabase edb, 
+       ( SELECT extq.source_id,
+			          extq.source_id query_name,
+			          extt.source_id target_name,
+							  sim.min_subject_start - 1500 as min_subject_start,
+								sim.max_subject_end + 1500 as max_subject_end,
+								extt.source_id || ':' || sim.min_subject_start || '..' || sim.max_subject_end as map
+				 FROM   dots.SIMILARITY sim,
+								dots.EXTERNALNASEQUENCE extt,
+								dots.EXTERNALNASEQUENCE extq,
+								SRes.ExternalDatabaseRelease edr,
+								SRes.ExternalDatabase edb
+				 WHERE  edr.external_database_id = edb.external_database_id 
+				    AND edr.external_database_release_id = extq.external_database_release_id 
+						AND edb.name = 'Isolates Data Test9' 
+						AND edr.version = '2007-12-10' 
+						AND sim.query_id = extq.na_sequence_id 
+						AND sim.subject_id = extt.na_sequence_id) aln 
+WHERE  aln.source_id(+) = etn.source_id  and 
+       etn.na_sequence_id = src.na_sequence_id
+			 AND edr.external_database_id = edb.external_database_id
+			 AND edr.external_database_release_id = etn.external_database_release_id
+			 AND edb.name = 'Isolates Data Test9'
+			 AND edr.version = '2007-12-10'
+);
+
+CREATE INDEX apidb.IsolateAttr_sourceId_idx ON apidb.IsolateAttributes (source_id);
+
+----------------------- Product Materialized View -------------------
+DROP MATERIALIZED VIEW apidb.IsolateProductAttributes; 
+DROP INDEX apidb.IsoProductAttr_idx;
+
+CREATE MATERIALIZED VIEW apidb.IsolateProductAttributes AS 
+(
+SELECT etn.source_id,
+       apidb.tab_to_string(cast(collect(gf.product) as apidb.varchartab), ' | ') as prod
+FROM   DoTS.ExternalNASequence etn,
+			 DoTS.Genefeature gf,
+			 SRes.ExternalDatabaseRelease edr,
+			 SRes.ExternalDatabase edb
+WHERE  etn.na_sequence_id = gf.na_sequence_id
+	 AND edr.external_database_id = edb.external_database_id
+	 AND edr.external_database_release_id = etn.external_database_release_id
+	 AND edb.name = 'Isolates Data Test9'
+	 AND edr.version = '2007-12-10'
+	 AND gf.product is not null 
+GROUP BY etn.source_id
+);
+
+CREATE INDEX apidb.IsoProductAttr_idx ON apidb.IsolateProductAttributes (source_id);
