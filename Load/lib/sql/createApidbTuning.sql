07805543eeb04f23540bc8ab257a87531565c731
John Iodice <iodice@pennmedicine.upenn.edu>
2007-12-19 21:19:28 +0000

    Moved isolate mviews into createApidbTuning.sql, so they can be with their friends.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@19687 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createApidbTuning.sql
+++ Load/lib/sql/createApidbTuning.sql
@@ -1555,6 +1555,102 @@ CREATE INDEX polymorphism_ix&1 ON apidb.Polymorphism&1(na_sequence_id, ref_strai
 
 CREATE OR REPLACE SYNONYM apidb.Polymorphism FOR apidb.Polymorphism&1;
 
+-------------------------------------------------------------------------------
+CREATE MATERIALIZED VIEW apidb.IsolateAttributes&1 AS 
+(
+SELECT etn.na_sequence_id, 
+       etn.external_database_release_id,
+       etn.source_id,
+       src.organism as organism,
+       src.strain || ' ' || src.isolate as strain,
+       src.specific_host || src.lab_host specific_host,
+       src.isolation_source,
+       src.country,
+       src.note,
+       etn.description,
+       src.pcr_primers,
+       etn.sequence,
+       aln.query_name,
+       aln.target_name,
+       aln.min_subject_start,
+       aln.max_subject_end,
+       aln.map,
+       'CryptoDB' as project_id
+FROM   DoTS.ExternalNASequence etn,
+       DoTS.IsolateSource src,
+       SRes.ExternalDatabaseRelease edr,
+       SRes.ExternalDatabase edb, 
+       ( SELECT extq.source_id,
+                extq.source_id query_name,
+                extt.source_id target_name,
+                sim.min_subject_start - 1500 as min_subject_start,
+                sim.max_subject_end + 1500 as max_subject_end,
+                extt.source_id || ':' || sim.min_subject_start || '..' || sim.max_subject_end as map
+         FROM   dots.SIMILARITY sim,
+                dots.EXTERNALNASEQUENCE extt,
+                dots.EXTERNALNASEQUENCE extq,
+                SRes.ExternalDatabaseRelease edr,
+                SRes.ExternalDatabase edb
+         WHERE  edr.external_database_id = edb.external_database_id 
+            AND edr.external_database_release_id = extq.external_database_release_id 
+            AND edb.name = 'Isolates Data' 
+            AND edr.version = '2007-12-12' 
+            AND sim.query_id = extq.na_sequence_id 
+            AND sim.subject_id = extt.na_sequence_id) aln 
+WHERE  aln.source_id(+) = etn.source_id  and 
+       etn.na_sequence_id = src.na_sequence_id
+       AND edr.external_database_id = edb.external_database_id
+       AND edr.external_database_release_id = etn.external_database_release_id
+       AND edb.name = 'Isolates Data'
+       AND edr.version = '2007-12-12'
+);
+
+GRANT SELECT ON apidb.IsolateAttributes&1 TO gus_r;
+
+CREATE INDEX apidb.IsolateAttr_sourceId_idx&1 ON apidb.IsolateAttributes&1 (source_id);
+
+CREATE OR REPLACE SYNONYM apidb.IsolateAttributes FOR apidb.IsolateAttributes&1;
+
+-------------------------------------------------------------------------------
+
+CREATE MATERIALIZED VIEW apidb.IsolateProductAttributes&1 AS 
+(
+SELECT etn.source_id,
+       'CryptoDB' as project_id,
+       apidb.tab_to_string(cast(collect(gf.product) as apidb.varchartab), ' | ') as product
+FROM   DoTS.ExternalNASequence etn,
+       DoTS.Genefeature gf,
+       SRes.ExternalDatabaseRelease edr,
+       SRes.ExternalDatabase edb
+WHERE  etn.na_sequence_id = gf.na_sequence_id
+       AND edr.external_database_id = edb.external_database_id
+       AND edr.external_database_release_id = etn.external_database_release_id
+       AND edb.name = 'Isolates Data'
+       AND edr.version = '2007-12-12'
+GROUP BY etn.source_id
+UNION
+SELECT etn.source_id,
+       'CryptoDB' as project_id,
+       dbms_lob.substr(com.comment_string, 4000, 1) as product 
+FROM   DoTS.Repeats rpt,
+       DoTS.externalnasequence etn,
+       DoTS.nafeaturecomment com,
+       SRes.ExternalDatabaseRelease edr,
+       SRes.ExternalDatabase edb
+Where  rpt.na_sequence_id = etn.na_sequence_id
+   AND com.na_feature_id = rpt.na_feature_id
+   AND edr.external_database_id = edb.external_database_id
+   AND edr.external_database_release_id = rpt.external_database_release_id
+   AND edb.name = 'Isolates Data'
+   AND edr.version = '2007-12-12'
+);
+
+CREATE INDEX apidb.IsoProductAttr_idx&1 ON apidb.IsolateProductAttributes&1 (source_id);
+
+GRANT SELECT ON apidb.IsolateProductAttributes&1 TO gus_r;
+
+CREATE OR REPLACE SYNONYM apidb.IsolateProductAttributes FOR apidb.IsolateProductAttributes&1;
+
 ---------------------------
 -- cleanup
 ---------------------------
