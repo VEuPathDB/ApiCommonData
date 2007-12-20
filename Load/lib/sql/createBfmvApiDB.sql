0d5102c9a027aee04c4ee30f78b3bd52309bf6b9
Cary Pennington <carypen@_.com>
2007-12-20 15:02:25 +0000

    updated to include Trich and Giardia
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@19691 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createBfmvApiDB.sql
+++ Load/lib/sql/createBfmvApiDB.sql
@@ -15,6 +15,9 @@ apidb.GeneAttributes@PLASMO54
 UNION
 SELECT GeneAttributes.* FROM  
 apidb.GeneAttributes@TOXO43UGA
+UNION
+SELECT GeneAttributes.* FROM  
+apidb.GeneAttributes@AMITO10
 );
 
 prompt *****select count(*) from apidb.geneattributes@CRYPTO36UGA;
@@ -23,6 +26,8 @@ prompt *****select count(*) from apidb.geneattributes@PLASMO54;
 select count(*) from apidb.geneattributes@PLASMO54;
 prompt *****select count(*) from apidb.geneattributes@TOXO43UGA;
 select count(*) from apidb.geneattributes@TOXO43UGA;
+prompt *****select count(*) from apidb.geneattributes@AMITO10;
+select count(*) from apidb.geneattributes@AMITO10;
 prompt *****select count(*) from apidb.geneattributes;
 select count(*) from apidb.geneattributes;
 
@@ -43,6 +48,51 @@ apidb.SequenceAttributes@PLASMO54
 UNION
 SELECT SequenceAttributes.* FROM  
 apidb.SequenceAttributes@TOXO43UGA
+UNION
+SELECT SequenceAttributes.* FROM  
+apidb.SequenceAttributes@AMITO10
+UNION
+SELECT 
+     CAST('ApiDB' as VARCHAR2(47)) as PROJECT_ID, 
+     CAST(enas.SOURCE_ID as VARCHAR2(50)) as SOURCE_ID,
+     CAST(null as NUMBER(12)) as A_COUNT,
+     CAST(null as NUMBER(12)) as C_COUNT,
+     CAST(null as NUMBER(12)) as G_COUNT,
+     CAST(null as NUMBER(12)) as T_COUNT,
+     CAST(null as NUMBER) as OTHER_COUNT,
+     enas.LENGTH,
+     CAST(null as VARCHAR2(6)) as AT_PERCENT,
+     CAST('Theileria parva strain Muguga' as VARCHAR2(40)) as ORGANISM,
+     CAST(333668 as NUMBER(10)) as NCBI_TAX_ID,
+     CAST(enas.DESCRIPTION as VARCHAR2(400)) as SEQUENCE_DESCRIPTION,
+     CAST(enas.SOURCE_ID as VARCHAR2(20)) as GENBANK_ACCESSION,
+     CAST(null as VARCHAR2(30)) as DATABASE_VERSION,
+     CAST(null as VARCHAR2(80)) as DATABASE_NAME,
+     CAST(null as VARCHAR2(20)) as CHROMOSOME ,
+     CAST(null as NUMBER(12)) as CHROMOSOME_ORDER_NUM
+FROM dots.ExternalNASequence enas
+WHERE enas.taxon_id = '88839'
+UNION
+SELECT
+ CAST('ApiDB' as VARCHAR2(47)) as PROJECT_ID, 
+     CAST(enas.SOURCE_ID as VARCHAR2(50)) as SOURCE_ID,
+     CAST(null as NUMBER(12)) as A_COUNT,
+     CAST(null as NUMBER(12)) as C_COUNT,
+     CAST(null as NUMBER(12)) as G_COUNT,
+     CAST(null as NUMBER(12)) as T_COUNT,
+     CAST(null as NUMBER) as OTHER_COUNT,
+     enas.LENGTH,
+     CAST(null as VARCHAR2(6)) as AT_PERCENT,
+     CAST('Theileria annulata strain Ankara' as VARCHAR2(40)) as ORGANISM,
+     CAST(353154 as NUMBER(10)) as NCBI_TAX_ID,
+     CAST(enas.DESCRIPTION as VARCHAR2(400)) as SEQUENCE_DESCRIPTION,
+     CAST(enas.SOURCE_ID as VARCHAR2(20)) as GENBANK_ACCESSION,
+     CAST(null as VARCHAR2(30)) as DATABASE_VERSION,
+     CAST(null as VARCHAR2(80)) as DATABASE_NAME,
+     CAST(null as VARCHAR2(20)) as CHROMOSOME ,
+     CAST(null as NUMBER(12)) as CHROMOSOME_ORDER_NUM
+FROM dots.ExternalNASequence enas
+WHERE enas.taxon_id = '88899'
 );
 
 prompt *****select count(*) from apidb.sequenceattributes@CRYPTO36UGA;
@@ -51,6 +101,8 @@ prompt *****select count(*) from apidb.sequenceattributes@PLASMO54;
 select count(*) from apidb.sequenceattributes@PLASMO54;
 prompt *****select count(*) from apidb.sequenceattributes@TOXO43UGA;
 select count(*) from apidb.sequenceattributes@TOXO43UGA;
+prompt *****select count(*) from apidb.sequenceattributes@AMITO10;
+select count(*) from apidb.sequenceattributes@AMITO10;
 prompt *****select count(*) from apidb.sequenceattributes;
 select count(*) from apidb.sequenceattributes;
 
@@ -77,6 +129,17 @@ apidb.EstAttributes@PLASMO54
 UNION
 SELECT EstAttributes.* FROM  
 apidb.EstAttributes@TOXO43UGA
+UNION
+SELECT EstAttributes.* FROM  
+apidb.EstAttributes@TOXO43UGA
+UNION
+SELECT EstAttributes.*, 
+	CAST(null as NUMBER(5)) as LIBRARY_ID,
+	CAST(null as VARCHAR2(120)) as LIBRARY_DBEST_NAME,
+	CAST(null as NUMBER(10)) as ASSEMBLY_NA_SEQUENCE_ID,
+	CAST(null as VARCHAR2(255)) as ASSEMBLY_SOURCE_ID,
+	CAST(null as NUMBER(12)) as ASSEMBLY_EST_COUNT 
+FROM  apidb.EstAttributes@AMITO10
 );
 
 prompt *****select count(*) from apidb.estattributes@CRYPTO36UGA;
@@ -85,6 +148,8 @@ prompt *****select count(*) from apidb.estattributes@PLASMO54;
 select count(*) from apidb.estattributes@PLASMO54;
 prompt *****select count(*) from apidb.estattributes@TOXO43UGA;
 select count(*) from apidb.estattributes@TOXO43UGA;
+prompt *****select count(*) from apidb.estattributes@AMITO10;
+select count(*) from apidb.estattributes@AMITO10;
 prompt *****select count(*) from apidb.estattributes;
 select count(*) from apidb.estattributes;
 
@@ -111,6 +176,9 @@ apidb.OrfAttributes@PLASMO54
 UNION
 SELECT OrfAttributes.* FROM  
 apidb.OrfAttributes@TOXO43UGA
+UNION
+SELECT OrfAttributes.* FROM  
+apidb.OrfAttributes@AMITO10
 );
 
 prompt *****select count(*) from apidb.orfattributes@CRYPTO36UGA;
@@ -119,6 +187,8 @@ prompt *****select count(*) from apidb.orfattributes@PLASMO54;
 select count(*) from apidb.orfattributes@PLASMO54;
 prompt *****select count(*) from apidb.orfattributes@TOXO43UGA;
 select count(*) from apidb.orfattributes@TOXO43UGA;
+prompt *****select count(*) from apidb.orfattributes@AMITO10;
+select count(*) from apidb.orfattributes@AMITO10;
 prompt *****select count(*) from apidb.orfattributes;
 select count(*) from apidb.orfattributes;
 
@@ -145,6 +215,9 @@ apidb.SnpAttributes@PLASMO54
 UNION
 SELECT SnpAttributes.* FROM  
 apidb.SnpAttributes@TOXO43UGA
+UNION
+SELECT SnpAttributes.* FROM  
+apidb.SnpAttributes@AMITO10
 );
 
 prompt *****select count(*) from apidb.snpattributes@CRYPTO36UGA;
@@ -153,6 +226,8 @@ prompt *****select count(*) from apidb.snpattributes@PLASMO54;
 select count(*) from apidb.snpattributes@PLASMO54;
 prompt *****select count(*) from apidb.snpattributes@TOXO43UGA;
 select count(*) from apidb.snpattributes@TOXO43UGA;
+prompt *****select count(*) from apidb.snpattributes@AMITO10;
+select count(*) from apidb.snpattributes@AMITO10;
 prompt *****select count(*) from apidb.snpattributes;
 select count(*) from apidb.snpattributes;
 
