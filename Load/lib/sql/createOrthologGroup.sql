80409bce6459830ec7977ddd3d030e3c7f5a2f64
John Iodice <iodice@pennmedicine.upenn.edu>
2008-01-24 21:33:43 +0000

    create table for OrthologGroupAaSequence; index on OrthologGroup.name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@19947 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createOrthologGroup.sql
+++ Load/lib/sql/createOrthologGroup.sql
@@ -36,6 +36,7 @@ REFERENCES sres.ExternalDatabaseRelease;
 GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.OrthologGroup TO gus_w;
 GRANT SELECT ON apidb.OrthologGroup TO gus_r;
 
+CREATE INDEX apidb.og_name_ix ON apidb.OrthologGroup (name, ortholog_group_id);
 ------------------------------------------------------------------------------
 
 CREATE SEQUENCE apidb.OrthologGroup_sq;
@@ -677,5 +678,71 @@ FROM dual,
 WHERE 'grouptaxonmatrix' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     where database_id = d.database_id);
 
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+CREATE TABLE apidb.OrthologGroupAaSequence (
+ ortholog_group_aa_sequence_id NUMBER(12) NOT NULL,
+ ortholog_group_id             NUMBER(12) NOT NULL,
+ aa_sequence_id                NUMBER(12) NOT NULL,
+ connectivity                  FLOAT(126),
+ modification_date             DATE NOT NULL,
+ user_read                     NUMBER(1) NOT NULL,
+ user_write                    NUMBER(1) NOT NULL,
+ group_read                    NUMBER(1) NOT NULL,
+ group_write                   NUMBER(1) NOT NULL,
+ other_read                    NUMBER(1) NOT NULL,
+ other_write                   NUMBER(1) NOT NULL,
+ row_user_id                   NUMBER(12) NOT NULL,
+ row_group_id                  NUMBER(3) NOT NULL,
+ row_project_id                NUMBER(4) NOT NULL,
+ row_alg_invocation_id         NUMBER(12) NOT NULL
+);
+
+ALTER TABLE apidb.OrthologGroupAaSequence
+ADD CONSTRAINT ogas_pk PRIMARY KEY (ortholog_group_id);
+
+ALTER TABLE apidb.OrthologGroupAaSequence
+ADD CONSTRAINT ogas_fk1 FOREIGN KEY (ortholog_group_id)
+REFERENCES apidb.OrthologGroup;
+
+ALTER TABLE apidb.OrthologGroupAaSequence
+ADD CONSTRAINT ogas_fk2 FOREIGN KEY (aa_sequence_id)
+REFERENCES dots.AaSequence;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.OrthologGroupAaSequence TO gus_w;
+GRANT SELECT ON apidb.OrthologGroupAaSequence TO gus_r;
+
+CREATE INDEX apidb.ogas_ogas_ix
+ON apidb.OrthologGroupAaSequence (ortholog_group_id, aa_sequence_id);
+
+CREATE INDEX apidb.ogas_asog_ix
+ON apidb.OrthologGroupAaSequence (aa_sequence_id, ortholog_group_id);
+------------------------------------------------------------------------------
+
+CREATE SEQUENCE apidb.OrthologGroupAaSequence_sq;
+
+GRANT SELECT ON apidb.OrthologGroupAaSequence_sq TO gus_r;
+GRANT SELECT ON apidb.OrthologGroupAaSequence_sq TO gus_w;
+
+------------------------------------------------------------------------------
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'OrthologGroupAaSequence',
+       'Standard', 'ORTHOLOG_GROUP_ID',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
+WHERE 'orthologgroupaasequence' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    where database_id = d.database_id);
+
+------------------------------------------------------------------------------
 
 exit;
