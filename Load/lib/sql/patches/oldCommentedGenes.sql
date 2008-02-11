12d59e928496efb589e4df7de2ffcdfb01e91b5c
John Iodice <iodice@pennmedicine.upenn.edu>
2008-02-11 21:21:31 +0000

    script to handle three gene renamings (and help look for more)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/plasmo_5_4_1@20294 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/patches/oldCommentedGenes.sql
@@ -0,0 +1,14 @@
+
+-- update (so-called) stable_id field for renamed P.f. genes
+update comments2.comments set stable_id = 'mal_mito_3' where stable_id = 'coxI';
+update comments2.comments set stable_id = 'MAL7P1.231' where stable_id = 'MAL7P1.321';
+update comments2.comments set stable_id = 'PFC0355c' where stable_id = 'PFC0358c';
+
+-- generate (most of) a query for comments whose stable_id is not a valid gene
+-- must drop last "union" and append "minus select source_id from apidb.GeneAttributes"
+spool commentedGenes.sql
+select distinct 'select ''' || stable_id || ''' from dual union'
+from comments2.comments
+where project_name = 'PlasmoDB' and comment_target_id = 'gene';
+spool off
+
