eb0049164348f699ec6ceeac4e55a8f5cdd7da36
Brian Brunk <brunkb@upenn.edu>
2008-02-12 18:46:19 +0000

    added link to TDR targets db in external links
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@20316 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
+++ Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
@@ -39,7 +39,7 @@ sub new {
 
   $self->initialize({
                      requiredDbVersion => 3.5,
-                     cvsRevision       => '$Revision: 17596 $',
+                     cvsRevision       => '$Revision: 19690 $',
                      name              => ref($self),
                      argsDeclaration   => declareArgs(),
                      documentation     => getDocumentation(),
