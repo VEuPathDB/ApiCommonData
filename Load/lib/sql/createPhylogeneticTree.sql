95ae36befd6468930760056d154be338921fb2ca
Deborah Pinney <pinney@upenn.edu>
2008-03-14 19:21:59 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@20909 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createPhylogeneticTree.sql
@@ -0,0 +1,9 @@
+DROP TABLE apidb.PhylogeneticTree;
+
+CREATE TABLE apidb.PhylogeneticTree (
+   source_id varchar2(30),
+   atv clob,
+   con clob);
+
+GRANT SELECT ON apidb.PhylogeneticTree TO gus_r;
+GRANT INSERT, UPDATE, DELETE ON apidb.PhylogeneticTree TO gus_w;
