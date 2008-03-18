3df06ee8f9b437e407bebb7a715c848b0dd693f4
Cristina Aurrecoechea <aurreco@uga.edu>
2008-03-18 15:53:16 +0000

    adding Other Apicomplexan ESTs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@20933 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/createBfmvApiDBESTs.sql
+++ Load/lib/sql/createBfmvApiDBESTs.sql
@@ -26,6 +26,27 @@ SELECT EstAttributes.*,
 	CAST(null as VARCHAR2(255)) as ASSEMBLY_SOURCE_ID,
 	CAST(null as NUMBER(12)) as ASSEMBLY_EST_COUNT 
 FROM  apidb.EstAttributes@AMITO
+UNION
+SELECT
+     CAST('ApiDB' as VARCHAR2(47)) as PROJECT_ID,
+     CAST(enas.SOURCE_ID as VARCHAR2(50)) as SOURCE_ID,
+     CAST(null as NUMBER(12)) as A_COUNT,
+     CAST(null as NUMBER(12)) as C_COUNT,
+     CAST(null as NUMBER(12)) as G_COUNT,
+     CAST(null as NUMBER(12)) as T_COUNT,
+     CAST(null as NUMBER) as OTHER_COUNT,
+     enas.LENGTH,
+     CAST(null as VARCHAR2(6)) as AT_PERCENT,
+     CAST('Other Apicomplexan (no Cmp,Pbfvy,Tg)' as VARCHAR2(40)) as ORGANISM,
+     CAST(333668 as NUMBER(10)) as NCBI_TAX_ID,
+     CAST(enas.DESCRIPTION as VARCHAR2(400)) as SEQUENCE_DESCRIPTION,
+     CAST(enas.SOURCE_ID as VARCHAR2(20)) as GENBANK_ACCESSION,
+     CAST(null as VARCHAR2(30)) as DATABASE_VERSION,
+     CAST(null as VARCHAR2(80)) as DATABASE_NAME,
+     CAST(null as VARCHAR2(20)) as CHROMOSOME ,
+     CAST(null as NUMBER(12)) as CHROMOSOME_ORDER_NUM
+FROM dots.ExternalNASequence enas
+WHERE enas.taxon_id = '88165'
 );
 
 prompt *****select count(*) from apidb.estattributes@CRYPTO;
