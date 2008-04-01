338d648d555b936525d8270f8dde01bedce16af9
Ryan Thibodeau <ryanthib@_.com>
2008-04-01 21:20:50 +0000

    Alpha version of SQL to create tables for announcements system.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21045 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createMessagesTables.sql
@@ -0,0 +1,47 @@
+DROP TABLE ryanthib.messages; 
+DROP TABLE ryanthib.projects;
+DROP TABLE ryanthib.categories;
+DROP TABLE ryanthib.message_projects;
+
+
+CREATE TABLE ryanthib.messages
+(
+  message_id NUMBER(4) NOT NULL,
+  message_text VARCHAR(4000) NOT NULL,
+  message_category NUMBER(2),
+  start_date DATE,
+  stop_date  DATE,
+  admin_comments VARCHAR(4000),
+  time_submitted TIMESTAMP,
+  CONSTRAINT messages_pkey PRIMARY KEY (message_id)  
+);
+
+CREATE TABLE ryanthib.projects
+(
+  project_id NUMBER(3) NOT NULL,
+  project_name VARCHAR(150) NOT NULL,
+  CONSTRAINT projects_pkey PRIMARY KEY (project_id)
+);
+
+CREATE TABLE ryanthib.category
+(
+  category_id NUMBER(3) NOT NULL, 
+  category_name VARCHAR(150) NOT NULL,
+  CONSTRAINT category_pkey PRIMARY KEY (category_id)
+);
+
+CREATE TABLE ryanthib.message_projects
+(
+  message_id NUMBER(3) NOT NULL;
+  project_id NUMBER(3) NOT NULL
+  CONSTRAINT message_id_fkey FOREIGN KEY (message_id) REFERENCES ryanthib.messages(message_id),
+  CONSTRAINT project_id_fkey FOREIGN KEY (project_id) REFERENCES ryanthib.projects(project_id),
+);
+
+CREATE SEQUENCE ryanthib.messages_id_pkseq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE SEQUENCE ryanthib.projects_id_pkseq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE SEQUENCE ryanthib.category_id_pkseq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+exit
