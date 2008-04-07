1555da2b85bbe1059c8544ba4444b98d79119202
John Iodice <iodice@pennmedicine.upenn.edu>
2008-04-07 17:47:52 +0000

    use tables instead of materialized views
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21062 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/4d.sql
+++ Load/lib/sql/4d.sql
@@ -1,15 +1,15 @@
 /*
-   4d.sql -- Show digits embedded in materialized-view names
-             (See createBfmv.sql for why we care).
-             Note that an mview named 'flank3prime2222' would generate the
+   4d.sql -- Show digits embedded in table names (including materialized views)
+             (See updateApidbTuning.sql for why we care).
+             Note that a table named 'flank3prime2222' would generate the
              output value '32222'.
 */
 
-select distinct regexp_replace(mview_name, '[^0-9]*', '')
-       as "digits embedded in mview names"
-from all_mviews
-where regexp_replace(mview_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
+select distinct regexp_replace(table_name, '[^0-9]*', '')
+       as "digits embedded in table names"
+from all_tables
+where regexp_replace(table_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
       like '%fournumbers'
-order by regexp_replace(mview_name, '[^0-9]*', '');
+order by regexp_replace(table_name, '[^0-9]*', '');
 
 exit;
