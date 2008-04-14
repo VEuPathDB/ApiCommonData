4a13ca83c62ca0f3ed0ad0e2200ca953e47c0902
John Iodice <iodice@pennmedicine.upenn.edu>
2008-04-14 21:14:46 +0000

    check all_tables as well as all_mviews
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21155 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/mviewCleanup.sql
+++ Load/lib/sql/mviewCleanup.sql
@@ -26,4 +26,20 @@ WHERE mview_name IN (SELECT mview_name
   AND REGEXP_REPLACE(mview_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
       LIKE '%fournumbers';
 
+prompt These tables appear superfluous (their names end in four digits but no synonym points at them).
+prompt Consider dropping them if all synonyms are OK.
+
+SELECT 'drop table ' || owner || '.' || table_name || ';' AS "drop tables"
+FROM all_tables
+WHERE table_name IN (SELECT table_name
+                     FROM all_tables
+                    MINUS
+                     SELECT table_name
+                     FROM all_synonyms
+                    MINUS
+                     SELECT mview_name
+                     FROM all_mviews)
+  AND REGEXP_REPLACE(table_name, '[0-9][0-9][0-9][0-9]', 'fournumbers')
+      LIKE '%fournumbers';
+
 exit
