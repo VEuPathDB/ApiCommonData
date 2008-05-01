e3e90e5eaafdc428e89fd1dd30bd9c60efd7d5ca
Steve Fischer <sfischer@upenn.edu>
2008-05-01 17:35:47 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21432 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createOrthomclEngine.sql
@@ -0,0 +1,53 @@
+CREATE TABLE apidb.SimilarSequences (
+ QUERY_ID                 NUMBER(10) 
+ SUBJECT_ID               NUMBER(10)
+ QUERY_TAXON_ID           NUMBER(10)
+ SUBJECT_TAXON_ID         NUMBER(10)
+ EVALUE_MANT              FLOAT(126)
+ EVALUE_EXP               NUMBER
+ PERCENT_IDENTITY         NUMBER
+ PERCENT_MATCH            NUMBER  
+)
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.SimilarSequences TO gus_w;
+GRANT SELECT ON apidb.SimilarSequences TO gus_r
+
+CREATE INDEX apidb.ss_qtaxexp_ix ON apidb.SimilarSequences(query_id, subject_taxon_id, evalue_exp, evalue_mant, query_taxon_id, subject_id);
+
+CREATE INDEX apidb.ss_seqs_ix on apidb.SimilarSequences(query_id, subject_id, evalue_exp, evalue_mant);
+
+-----------------------------------------------------------
+
+CREATE TABLE apidb.Inparalog (
+ SEQUENCE_ID_A           NUMBER(10)
+ SEQUENCE_ID_B           NUMBER(10)
+ TAXON_ID                NUMBER(10)
+ UNNORMALIZED_SCORE      NUMBER    
+ NORMALIZED_SCORE        NUMBER    
+);
+
+
+------------------------------------------------------------
+
+CREATE TABLE apidb.Ortholog (
+ SEQUENCE_ID_A           NUMBER(10)
+ SEQUENCE_ID_B           NUMBER(10)
+ TAXON_ID_A              NUMBER(10)
+ TAXON_ID_B              NUMBER(10)
+ UNNORMALIZED_SCORE      NUMBER    
+ NORMALIZED_SCORE        NUMBER    
+);
+
+CREATE INDEX apidb.ortholog_seq_a_ix on apidb.ortholog(sequence_id_a);
+
+CREATE INDEX apidb.ortholog_seq_b_ix on apidb.ortholog(sequence_id_b);
+
+------------------------------------------------------------
+ 
+CREATE TABLE apidb.CoOrtholog (
+ SEQUENCE_ID_A           NUMBER(10)
+ SEQUENCE_ID_B           NUMBER(10)
+ UNNORMALIZED_SCORE      NUMBER    
+ NORMALIZED_SCORE        NUMBER    
+);
+
