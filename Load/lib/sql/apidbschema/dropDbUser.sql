c80747a597c5a8502e4d61cf7575e30c4759f77d
John Iodice <iodice@pennmedicine.upenn.edu>
2008-05-14 21:16:27 +0000

    complement to new script that creates multiple db users
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21623 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropDbUser.sql
@@ -0,0 +1,10 @@
+DROP USER amitodbwww;
+DROP USER amitodbwwwdev;
+DROP USER giardiadbwww;
+DROP USER giardiadbwwwdev;
+DROP USER plasmodbwww;
+DROP USER plasmodbwwwdev;
+DROP USER toxodbwww;
+DROP USER toxodbwwwdev;
+
+exit
