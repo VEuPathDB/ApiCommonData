8077d41673ff3e389f90388b6dc43e05a98901a7
John Iodice <iodice@pennmedicine.upenn.edu>
2008-05-15 02:50:14 +0000

    reorganizing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@21628 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropDbTuning.sql
@@ -0,0 +1,13 @@
+DROP INDEX dots.aaseq_subclassdbrel_ix;
+DROP INDEX dots.NaFeat_alleles_ix;
+DROP INDEX dots.AaSequenceImp_string2_ix;
+DROP INDEX dots.AaSequenceImp_classtax_ix;
+DROP INDEX dots.NaFeat_SubclassIds_ix;
+DROP INDEX dots.NaFeat_SubclassParent_ix;
+DROP INDEX dots.loc_feat_ix;
+DROP INDEX dots.sim_pval_ix;
+DROP INDEX dots.nasequenceimp_string1_idx;
+DROP INDEX dots.aafeat_subclassparent_ix;
+DROP INDEX dots.aafeature_subclasssource_ix;
+
+exit
