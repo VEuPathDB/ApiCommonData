4b829309dccd913b135f12566b272ca8642ca4ac
Vishal Nayak <vishalkn@_.com>
2008-06-19 03:00:00 +0000

    added regex arguments for contig and gene ids
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@22067 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeZffFromGff.pl
+++ Load/bin/makeZffFromGff.pl
@@ -4,16 +4,18 @@ use strict;
 
 use Getopt::Long;
 
-my ($help, $fn);
+my ($help, $fn, $contigIdRegex, $geneIdRegex);
 
 &GetOptions('help|h' => \$help,
             'file=s' => \$fn,
+            'contigIdRegex=s' => \$contigIdRegex,
+            'geneIdRegex=s' => \$geneIdRegex,
             );
 
 
 unless(-e $fn) {
-  print STDERR "Error File $fn not found";
-  print STDERR "usage:  makeZff <GFF>\n";
+  print STDERR "Error File $fn not found\n";
+  print STDERR "usage:  makeZff --file <GFF> --contigIdRegex <ContigIdRegEx> --geneIdRegex <GeneIdRegEx>\n";
   exit(1);
 }
 
@@ -24,7 +26,15 @@ my %H;
 
 while (<FILE>) {
   my @f = split;
-  push @{$H{$f[0]}{$f[8]}}, {
+   my $contig = $f[0];
+   my $gene = $f[8];
+  if (defined $contigIdRegex){
+    ($contig) = $f[0] =~ /$contigIdRegex/;
+  }
+  if (defined $geneIdRegex){
+    ($gene) = $f[8] =~ /$geneIdRegex/;
+  }
+  push @{$H{$contig}{$gene}}, {
                              start  => $f[3],
                              end    => $f[4],
                              strand => $f[6],
@@ -36,7 +46,7 @@ close FILE;
 foreach my $contig (sort keys %H) {
   print ">$contig\n";
   foreach my $gene (sort keys %{$H{$contig}}) {
-    my ($name) = $gene =~ /\|(rna.+)$/;
+
     
     foreach my $exon (@{$H{$contig}{$gene}}) {
       print 'Exon', "\t";
@@ -45,7 +55,7 @@ foreach my $contig (sort keys %H) {
       } else {
         print $exon->{end}, "\t", $exon->{start};
       }
-      print "\t", $name, "\n";
+      print "\t", $gene, "\n";
     }
   }
 }
