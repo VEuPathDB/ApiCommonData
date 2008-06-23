bb6c773a1ecfb9752a9ca941dea8812f93a6c422
Bindu Gajria <bindu.gajria@gmail.com>
2008-06-23 19:40:11 +0000

    create and drop for table with Plasmo MAPP data (from Kevin Brick)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@22081 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/createPlasmoMapp.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ApiDB.PlasmoMapp (
+ na_sequence_id  NUMBER(10) NOT NULL,
+ strand          NUMBER(3)  NOT NULL,
+ location        NUMBER(10) NOT NULL,
+ value	         FLOAT(10) NOT NULL
+);
+
+GRANT insert, select, update, delete ON ApiDB.PlasmoMapp TO gus_w;
+GRANT select ON ApiDB.PlasmoMapp  TO gus_r;
