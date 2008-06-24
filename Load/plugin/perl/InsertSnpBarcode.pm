3beeecdf516a98cc5a2ad4d53ae3f79463e5087f
Haiming Wang <hwang@uga.edu>
2008-06-24 20:22:55 +0000

    process barcode featuress, including snp id#, possible alleles, major and minor allele. e.g.Pf_01_000130573 C/T C T. save them to the dots.isolatefeature table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@22090 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertSnpBarcode.pm
+++ Load/plugin/perl/InsertSnpBarcode.pm
@@ -8,6 +8,7 @@ use GUS::PluginMgr::Plugin;
 use GUS::Model::DoTS::ExternalNASequence;
 use GUS::Model::DoTS::NALocation;
 use GUS::Model::DoTS::IsolateSource;
+use GUS::Model::DoTS::IsolateFeature;
 
 use Data::Dumper;
 
@@ -87,7 +88,7 @@ sub new {
   bless($self,$class); 
 
   $self->initialize({requiredDbVersion => 3.5,
-                     cvsRevision => '$Revision$', # cvs fills this in!
+                     cvsRevision => '$Revision: 0 $', # cvs fills this in!
                      name => ref($self),
                      argsDeclaration => $argsDeclaration,
                      documentation => $documentation
@@ -107,31 +108,59 @@ sub run {
   open(FILE, $inputFile) || $self->error("couldn't open file '$inputFile' for reading");
 
   my $count = 0;
+	my @snpFeats;
+	my $flag = 0;
+
   while(<FILE>) {
     chomp;
     next if /^\s*$/;
-    next if /^#/;
+    push @snpFeats, $_ and next if /^#/;
+
+		my %alleleHash = $self->processAlleles(@snpFeats) if($flag == 0);
+
+		#print Dumper(%alleleHash);
+
     my ($strain, $origin, $source, $barcode, $call, $match, @snp) = split /\t/, $_;
     map { s/\s+$// } @snp;  # trim off extra spaces
 
     my $objArgs = {
                     strain                       => $strain,
-                    name                         => 'dummy name',
+                    name                         => $strain,  # name for barcode ?
                     isolate                      => $strain,
                     country                      => $origin,
                     collected_by                 => $source,
                     external_database_release_id => $extDbRlsId,
                   };
 
-    my $extNASeq = $self->buildSequence($barcode, $extDbRlsId);
-
     my $isolateSource = GUS::Model::DoTS::IsolateSource->new($objArgs);
+
+		# foreach barcode nucleotide, find SNP ID#, major and minor allele
+
+		my @nuc = split //, $barcode;
+		for(my $i=0; $i<24; $i++) {
+		  my ($name, $possible_allele, $major_allele, $minor_allele) = split /\s/, $alleleHash{$i};
+			my ($species, $chr, $location) = split /_/, $name;
+
+			print "#### $name\n";
+
+      my $featArgs = { allele                       => $nuc[$i],
+                       name                         => 'dummy name',
+                       map                          => $location,
+                       external_database_release_id => $extDbRlsId,
+                     };
+      my $isolateFeature = GUS::Model::DoTS::IsolateFeature->new($featArgs);
+      $isolateSource->addChild($isolateFeature);
+		}
+
+    my $extNASeq = $self->buildSequence($barcode, $extDbRlsId);
 
     $extNASeq->addChild($isolateSource);
 
     $extNASeq->submit();
     $count++;
     $self->log("processed $count") if ($count % 1000) == 0;
+
+		$flag = 1;
   }
 
   return "Inserted $count rows.";
@@ -146,6 +175,25 @@ sub buildSequence {
   $extNASeq->setSequence($seq);
 
   return $extNASeq;
+}
+
+sub processAlleles {
+  my ($self, @lines) = @_;
+
+	# SNP format: SNP ID#,Possible Allels, Major Allele, Minor Allele
+	# e.g. Pf_10_000082376  A/T A  T
+	my @snps;
+	my %hash;
+	foreach(@lines) {
+	  push @snps, [split /\t/, $_];
+	}
+
+	for(my $i=1; $i<25; $i++) {
+	  my $j = $i-1;
+	  $hash{$j} = $snps[0]->[$i]. ' '. $snps[1]->[$i]. ' '. $snps[2]->[$i]. ' '. $snps[3]->[$i];
+	}
+
+  return %hash;
 }
 
 sub undoTables {
