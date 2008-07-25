04c2397255e929a5c33b7894749ffa3a11beb798
John Brestelli <jbrestell@gmail.com>
2008-07-25 21:15:42 +0000

    simple plugin to load rad.analysis and rad.differentialexpression
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@22424 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/plugin/perl/InsertDifferentialExpression.pm
@@ -0,0 +1,271 @@
+package ApiCommonData::Load::Plugin::InsertDifferentialExpression;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+use strict;
+
+use GUS::PluginMgr::Plugin;
+
+use GUS::Model::Study::OntologyEntry;
+use GUS::Model::RAD::DifferentialExpression;
+use GUS::Model::RAD::Analysis;
+use GUS::Model::RAD::Protocol;
+
+$| = 1;
+
+# ---------------------------------------------------------------------------
+# Load Arguments
+# ---------------------------------------------------------------------------
+
+sub getArgumentsDeclaration{
+  my $argsDeclaration =
+    [
+
+     fileArg({name           => 'inputDir',
+              descr          => 'Directory in which to find input files',
+              reqd           => 1,
+              mustExist      => 1,
+              format         => '',
+              constraintFunc => undef,
+              isList         => 0, 
+             }),
+
+     fileArg({ name           => 'configFile',
+               descr          => 'tab-delimited file with differential expression stats',
+               reqd           => 1,
+               mustExist      => 1,
+               format         => '',
+               constraintFunc => undef,
+               isList         => 0,
+             }),
+
+    ];
+  return $argsDeclaration;
+}
+
+# ----------------------------------------------------------------------
+# Documentation
+# ----------------------------------------------------------------------
+
+sub getDocumentation {
+  my $purposeBrief = "Inserts Rad.Analysis and Rad.DifferentiaExpression to store fold change and optionally confidence.  Creates the associated Rad.Protocol if it doesn't exist";
+
+  my $purpose = "Inserts Rad.Analysis and Rad.DifferentiaExpression to store fold change and optionally confidence.  Creates the associated Rad.Protocol if it doesn't exist";
+
+  my $tablesAffected = [['RAD::Analysis', 'One Row to Identify this experiment'],['RAD::DifferentialExpression', 'one row per line in the data file'],['RAD::Protocol', 'Will Create generic row if the specified protocol does not already exist']];
+
+  my $tablesDependedOn = [['Study::OntologyEntry',  'new protocols will be assigned unknown_protocol_type'],
+                          ['DoTS::GeneFeature', 'The id in the data file must ge an existing Gene Feature']];
+
+  my $howToRestart = "No restart";
+
+  my $failureCases = "";
+
+  my $notes = "Each Data file has the following columns (WITH HEADER): GeneFeature.source_id,FoldChange,Confidence(OPTIONAL).  The Config file has the following columns (no header):file analysis_name protocol_name protocol_type(OPTOINAL)";
+
+  my $documentation = {purpose=>$purpose, purposeBrief=>$purposeBrief, tablesAffected=>$tablesAffected, tablesDependedOn=>$tablesDependedOn, howToRestart=>$howToRestart, failureCases=>$failureCases,notes=>$notes};
+
+  return $documentation;
+}
+
+#--------------------------------------------------------------------------------
+
+sub new {
+  my ($class) = @_;
+  my $self = {na_sequences => []
+             };
+  bless($self,$class);
+
+  my $documentation = &getDocumentation();
+  my $argumentDeclaration = &getArgumentsDeclaration();
+
+
+  $self->initialize({requiredDbVersion => 3.5,
+		     cvsRevision => '$Revision: 15195 $',
+                     name => ref($self),
+                     revisionNotes => '',
+                     argsDeclaration => $argumentDeclaration,
+                     documentation => $documentation});
+
+  return $self;
+}
+
+#--------------------------------------------------------------------------------
+
+sub run {
+  my ($self) = @_;
+
+  my $config = $self->readConfig();
+
+  my $totalLines;
+
+  foreach my $configRow (@$config) {
+    my $dataFile = $configRow->[0];
+    my $analysisName = $configRow->[1];
+    my $protocolName = $configRow->[2];
+    my $protocolType = $configRow->[3];
+
+    my $protocol = $self->getProtocol($protocolName, $protocolType);
+
+    my $analysis = $self->createAnalysis($protocol, $analysisName);
+
+    my $count = $self->processDataFile($analysis, $dataFile);
+
+    $totalLines = $totalLines + $count;
+  }
+
+  my $totalInserts = $self->getTotalInserts();
+
+  return "Processed $totalLines lines from data files and $totalInserts Total Inserts";
+}
+
+#--------------------------------------------------------------------------------
+
+sub readConfig {
+  my ($self) = @_;
+
+  my $inputDir = $self->getArg('inputDir');
+  my $fn = $self->getArg('configFile');
+
+  open(CONFIG, "$inputDir/$fn") or $self->error("Cannot open file $inputDir/$fn for reading: $!");
+
+  my @rv;
+
+  while(<CONFIG>) {
+    chomp;
+
+    next unless($_);
+    next if /^#/;
+
+    my @a = split(/\t/, $_);
+
+    push @rv, \@a;
+  }
+
+  close CONFIG;
+
+  return \@rv;
+}
+
+
+#--------------------------------------------------------------------------------
+
+sub processDataFile {
+  my ($self, $analysis, $fn) = @_;
+
+  my $inputDir = $self->getArg('inputDir');
+
+  open(FILE, "inputDir/$fn") or $self->error("Cannot open file $inputDir/$fn for reading: $!");
+
+  my $tableId = $self->getTableId();
+
+  # remove the header;
+  <FILE>;
+
+  my $count;
+
+  while(<FILE>) {
+    chomp;
+
+    my ($sourceId, $foldChange, $confidence) = split(/\t/, $_);
+
+    my $naFeatureId = $self->getNaFeatureId($sourceId);
+
+    my $differentialExpression = GUS::Model::RAD::DifferentialExpression->new({table_id => $tableId,
+                                                                               row_id => $naFeatureId,
+                                                                               confidence => $confidence,
+                                                                               fold_change => $foldChange
+                                                                              });
+    $differentialExpression->setParent($analysis);
+
+    $differentialExpression->submit();
+    $count++;
+
+    if($count % 500 == 0) {
+      $self->undefPointerCache();
+      $self->log("Processed $count rows from the input file");
+    }
+  }
+
+  close FILE;
+
+  return $count;
+}
+
+#--------------------------------------------------------------------------------
+
+sub getNaFeatureId {
+  my ($self, $sourceId) = @_;
+
+  my @naFeatures = $self->sqlAsArray( Sql => "select na_feature_id from dots.geneFeature where source_id = '$sourceId'" );
+
+  if(scalar @naFeatures != 1) {
+    $self->error("Dots.GeneFeature na_feature_id not found for sourceId $sourceId");
+  }
+  return $naFeatures[0];
+
+}
+
+#--------------------------------------------------------------------------------
+
+sub getTableId {
+  my ($self) =  @_;
+
+  my $table = 'GeneFeature';
+  my @tableIds = $self->sqlAsArray( Sql => "select table_id from core.tableinfo where name = '$table'" );
+
+  if(scalar @tableIds != 1) {
+    $self->error("Core::TableInfo not found for $table");
+  }
+  return $tableIds[0];
+}
+
+#--------------------------------------------------------------------------------
+
+sub createAnalysis {
+  my ($self, $protocol, $analysisName) = @_;
+
+  my $analysis = GUS::Model::RAD::Analysis->new({name => $analysisName});
+
+  $analysis->setParent($protocol);
+
+  return $analysis;
+}
+
+#--------------------------------------------------------------------------------
+
+sub getProtocol {
+  my ($self, $protocolName, $protocolType) = @_;
+
+  my $protocol = GUS::Model::RAD::Protocol->new({name => $protocolName });
+
+  unless($protocol->retrieveFromDB()) {
+    my $protocolTypeId = $self->getProtocolTypeId($protocolType);
+
+    # Can't set parent because there are many fk to Study::OntologyEntry
+    $protocol->setProtocolTypeId($protocolTypeId);
+  }
+
+  return $protocol;
+
+}
+
+#--------------------------------------------------------------------------------
+
+sub getProtocolTypeId {
+  my ($self, $protocolType) = @_;
+
+  $protocolType = 'unknown_protocol_type' unless($protocolType);
+  my $category = 'ExperimentalProtocolType';
+
+  my $oe = GUS::Model::Study::OntologyEntry->new({value => $protocolType,
+                                                  category => $category,
+                                                 });
+
+  unless($oe->retrieveFromDB()) {
+    $self->error("Could not retrieve Study::OntologyEntry for value $protocolType");
+  }
+
+  return $oe->getId();
+}
+
+1;
