7588b60d27f1e70b97119d4f7b0d5814b3c78581
Brian Brunk <brunkb@upenn.edu>
2008-09-08 13:27:38 +0000

    fixed problem with create statement
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@23317 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/makeToxoVersion4Genes.sql
+++ Load/lib/sql/makeToxoVersion4Genes.sql
@@ -2,8 +2,9 @@
 
 drop table apidb.toxoversion4genes;
 
+copy from apidb/po34weep@toxo43p to apidb/po34weep@toxo440n -
 create apidb.toxoversion4genes using -
-select source_id,sequence_id,start_min,end_max,strand,context_start,context_end -   
+select source_id,sequence_id,start_min,end_max,strand,context_start,context_end,product -   
 from apidb.geneattributes;
 
 grant select on apidb.toxoversion4genes to gus_r;
