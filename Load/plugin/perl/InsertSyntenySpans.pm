7de5c837350ea3eb5cf641987c3c6e39a97d9b39
Vishal Nayak <vishalkn@_.com>
2008-09-17 15:18:48 +0000

    Modified Toxo query to get ortholog groups
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@23644 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertSyntenySpans.pm
+++ Load/plugin/perl/InsertSyntenySpans.pm
@@ -425,7 +425,14 @@ my $sql;
     $sql = "
     select g.na_feature_id as sequence_id, to_char(gi.gene_id) as sequence_group_id, g.external_database_release_id
     from dots.GENEINSTANCE gi, Dots.GENEFEATURE g
-    where g.na_feature_id = gi.na_feature_id";
+    where g.na_feature_id = gi.na_feature_id
+    UNION
+    select ssg.sequence_id, to_char(ssg.sequence_group_id), g.external_database_release_id
+    from dots.SequenceSequenceGroup ssg, dots.genefeature g, Core.TableInfo t
+    where t.name = 'GeneFeature'
+    and g.na_feature_id = ssg.sequence_id
+    and t.table_id = ssg.source_table_id
+    ";
   }elsif($self->getArg('organism') eq 'Cryptosporidium'){
     $sql = "select g.na_feature_id as sequence_id, to_char(ssg.group_id) as sequence_group_id, g.external_database_release_id
     from apidb.CHROMOSOME6ORTHOLOGY ssg, dots.genefeature g
