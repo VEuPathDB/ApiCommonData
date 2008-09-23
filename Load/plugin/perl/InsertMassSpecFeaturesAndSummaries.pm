1f2d40068680d4af04a98053d219f596ecd00cac
Wei Li <weili1@pennmedicine.upenn.edu>
2008-09-23 19:02:57 +0000

    revision problems
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@23813 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
+++ Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
@@ -39,7 +39,7 @@ sub new {
 
   $self->initialize({
                      requiredDbVersion => 3.5,
-                     cvsRevision       => '$Revision: 19690 $',
+                     cvsRevision       => '$Revision: 22807 $',
                      name              => ref($self),
                      argsDeclaration   => declareArgs(),
                      documentation     => getDocumentation(),
