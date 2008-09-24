a6c12873c639be5c89b41259627c0cc3e403fa5f
John Iodice <iodice@pennmedicine.upenn.edu>
2008-09-24 20:52:36 +0000

    new procedure "apidb_unanalyzed_stats"
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@23832 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createStoredProcedures.sql
+++ Load/lib/sql/apidbschema/createStoredProcedures.sql
@@ -92,4 +92,43 @@ show errors;
 GRANT execute ON apidb.tab_to_string TO gus_r;
 GRANT execute ON apidb.tab_to_string TO gus_w;
 
+-------------------------------------------------------------------------------
+create or replace procedure
+apidb.apidb_unanalyzed_stats
+is
+
+tab_count   SMALLINT;
+ind_count   SMALLINT;
+
+begin
+    for s in (select distinct(owner) schema from all_tables where owner in ('APIDB') )
+	loop
+		dbms_output.put_line('Checking stats for the '||s.schema||' schema');
+		for t in (select distinct(table_name) from
+					(select distinct(table_name) table_name from all_tables where owner = s.schema and last_analyzed is null
+				  			union
+				  	 select distinct(table_name) table_name from all_indexes where owner = s.schema and last_analyzed is null))
+		loop
+			dbms_stats.unlock_table_stats(ownname => s.schema, tabname => t.table_name);
+			select count(table_name) into tab_count from all_tables
+				where owner = s.schema and table_name = t.table_name and last_analyzed is null;
+			if tab_count >0 then
+			   dbms_output.put_line( '  Updating stats for table '||t.table_name );
+			   dbms_stats.gather_table_stats(ownname => s.schema, tabname => t.table_name, estimate_percent => 100, degree => 2);
+			end if;
+			for i in (select distinct(index_name) index_name from all_indexes where owner = s.schema and table_name = t.table_name and last_analyzed is null and INDEX_TYPE != 'DOMAIN')
+		    loop
+			    dbms_output.put_line( '    Updating stats for index '||i.index_name );
+			    dbms_stats.gather_index_stats(ownname => s.schema, indname => i.index_name, estimate_percent => 100, degree => 2);
+		    end loop;
+			dbms_stats.lock_table_stats(ownname => s.schema, tabname => t.table_name);
+		end loop;
+	end loop;
+end;
+/
+
+show errors
+GRANT execute ON apidb.apidb_unanalyzed_stats TO gus_r;
+GRANT execute ON apidb.apidb_unanalyzed_stats TO gus_w;
+
 exit;
