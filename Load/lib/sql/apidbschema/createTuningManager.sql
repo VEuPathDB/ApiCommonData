dda861dfc846f0e0a796d8787b31c562572de2c2
John Iodice <iodice@pennmedicine.upenn.edu>
2008-10-14 18:28:57 +0000

    grant privileges
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24173 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createTuningManager.sql
+++ Load/lib/sql/apidbschema/createTuningManager.sql
@@ -3,12 +3,20 @@ create table apidb.TuningTable (
    timestamp  date not null,
    definition clob not null);
 
+grant select on apidb.TuningTable to gus_r;
+grant insert, update, delete on apidb.TuningTable to gus_w;
+
 create table apidb.ObsoleteTuningTable (
    name      varchar2(65) primary key,
    timestamp date not null);
 
+grant select on apidb.ObsoleteTuningTable to gus_r;
+grant insert, update, delete on apidb.ObsoleteTuningTable to gus_w;
+
 create sequence apidb.TuningManager_sq
    start with 1111;
+
+grant select on apidb.TuningManager_sq to gus_w;
 
 create table apidb.TuningMgrExternalDependency (
    name         varchar2(65) primary key,
@@ -16,11 +24,16 @@ create table apidb.TuningMgrExternalDependency (
    timestamp    date not null,
    row_count    number not null);
 
+grant select on apidb.TuningMgrExternalDependency to gus_r;
+grant insert, update, delete on apidb.TuningMgrExternalDependency to gus_w;
+
 create table apidb.InstanceMetaInfo as
 select substr(sys_context ('USERENV', 'SERVICE_NAME'), 1, instr(sys_context ('USERENV', 'SERVICE_NAME'), '.')-1) as instance_name,
        sys_context ('USERENV', 'SERVICE_NAME') as service_name,
        cast(null as varchar2(50)) as validation_key,
        cast(null as varchar2(50)) as current_updater
 from dual;
+
+grant select on apidb.InstanceMetaInfo to gus_r;
 
 exit
