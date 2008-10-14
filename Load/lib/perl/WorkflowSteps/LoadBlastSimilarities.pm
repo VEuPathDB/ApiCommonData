ee3e11406f13765d8be03985c62326e616eed8f8
Steve Fischer <sfischer@upenn.edu>
2008-10-14 20:04:14 +0000

    first draft, mostly stub
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24181 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/LoadBlastSimilarities.pm
@@ -0,0 +1,68 @@
+package ApiCommonData::Load::Steps::WorkflowSteps::LoadBlastSimilarities;
+
+@ISA = (GUS::Pipeline::WorkflowStep);
+
+sub run {
+  my ($self) = @_;
+
+  my $dataDir = $self->getParam('dataDir');
+  my $inputFile = $self->getParam('inputFile');
+  my $queryTable = $self->getParam('queryTable');
+  my $queryTableIdCol = $self->getParam('queryTableSrcIdCol');
+  my $queryExtDbRlsSpec = $self->getParam('queryExtDbRlsSpec');
+  my $subjectTable = $self->getParam('subjectTable');
+  my $subjectTableIdCol = $self->getParam('subjectTableSrcIdCol');
+  my $subjectExtDbRlsSpec = $self->getParam('subjectExtDbRlsSpec');
+
+  my $queryColArg = "--queryTableSrcIdCol $queryTableSrcIdCol" if $queryTableSrcIdCol;
+
+  my $subjectColArg = "--subjectTableSrcIdCol $subjectTableSrcIdCol" if $subjectTableSrcIdCol;
+
+  my $queryExtDbArg = "";
+  if ($queryExtDbRlsSpec) {
+      $queryDbName = $self->getDbName($queryExtDbRlsSpec);
+      $queryDbRlsVer = $self->getDbName($queryExtDbRlsSpec);
+      $queryExtDbArg = " --queryExtDbName '$queryDbName' --queryExtDbRlsVer '$queryDbRlsVer'";
+  }
+
+  my $subjectExtDbArg = "";
+  if ($subjectExtDbRlsSpec) {
+      $subjectDbName = $self->getDbName($subjectExtDbRlsSpec);
+      $subjectDbRlsVer = $self->getDbName($subjectExtDbRlsSpec);
+      $subjectExtDbArg = " --subjectExtDbName '$subjectDbName' --subjectExtDbRlsVer '$subjectDbRlsVer'";
+  }
+
+  my $sbjDbNameArg = " --subjectExtDbName '$subjectDbName'" if $subjectDbName ne '';
+  my $sbjRlsArg = " --subjectExtDbRlsVer '$subjectDbRlsVer'" if $subjectDbRlsVer ne '';
+  
+  my $args = "--file $file $restartAlgInvs --queryTable $queryTable $queryColArg$queryExtDbArg  --subjectTable $subjectTable $subjectColArg $subjectExtDbArg $addedArgs";
+
+  $self->runPlugin("GUS::Supported::Plugin::InsertBlastSimilarities", $args);
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getParamsDeclaration {
+    return (
+	'dataDir',
+	'inputFile',
+	'queryTable',
+	'queryTableSrcIdCol',
+	'queryExtDbRlsSpec',
+	'subjectTable',
+	'subjectTableSrcIdCol',
+	'subjectExtDbRlsSpec',
+	);
+}
+
+sub getConfigurationDeclaration {
+    return ();
+}
+
+sub getDocumentation {
+}
