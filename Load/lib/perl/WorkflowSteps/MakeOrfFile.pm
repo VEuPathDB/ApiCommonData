a55b82d36d9f64ed9d223061709651eb877ffc85
Wei Li <weili1@pennmedicine.upenn.edu>
2008-10-23 16:17:46 +0000

    initial version
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24403 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/MakeOrfFile.pm
@@ -0,0 +1,66 @@
+package ApiCommonData::Load::WorkflowSteps::ExtractNaSeq;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $seqFile = $self->getParamValue('seqFile');
+  
+  my $minPepLength = $self->getParamValue('minPepLength');
+
+  my $seqfilesDir = $self->getParamValue('seqfilesDir');
+  
+  my $outFile = $self->getParamValue('outputFile');
+
+  my $dataDir = $self->getGlobalConfig('dataDir');
+
+  my $projectName = $self->getGlobalConfig('projectName');
+ 
+  my $projectVersion = $self->getGlobalConfig('projectVersion');
+
+  $seqfilesDir = "$dataDir/$projectName/$projectVersion/data/$seqfilesDir";
+  
+  $outFile = "$seqfilesDir/$outFile";
+ 
+   my $cmd = <<"EOF";
+orfFinder --dataset  $seqfilesDir/$seqFile \\
+--minPepLength $minPepLength \\
+--outFile $outFile
+EOF
+
+  if ($test) {
+
+      $self->runCmd($test,$cmd);
+  } else {
+      $self->runCmd(0,"test > $outFile");
+  }
+
+}
+
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['seqFile', "", ""],
+     ['minPepLength', "", ""],
+     ['seqfilesDir', "", ""],
+     ['outputFile', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
