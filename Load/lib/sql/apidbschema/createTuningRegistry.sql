e0d25c314bc20fd99d942fb68b6e1028302637e1
John Iodice <iodice@pennmedicine.upenn.edu>
2008-10-24 16:33:28 +0000

    new columns for last update/r, last check/er also removed "drop table" statements at the top; those could be dangerous.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24438 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createTuningRegistry.sql
+++ Load/lib/sql/apidbschema/createTuningRegistry.sql
@@ -1,6 +1,3 @@
-drop table apidb.TuningInstance;
-drop table apidb.TuningFamily;
-
 create table apidb.TuningFamily (
    family_name    varchar2(32) primary key,
    subversion_url varchar2(200) not null,
@@ -12,7 +9,11 @@ grant insert, update, delete on apidb.TuningFamily to gus_w;
 create table apidb.TuningInstance (
    instance_name varchar2(32) primary key,
    family_name   varchar2(32) not null references apidb.TuningFamily,
-   service_name  varchar2(85) not null);
+   service_name  varchar2(85) not null,
+   last_update   date,
+   last_updater  varchar2(50),
+   last_check    date,
+   last_checker  varchar2(50));
 
 grant select on apidb.TuningInstance to gus_r;
 grant insert, update, delete on apidb.TuningInstance to gus_w;
