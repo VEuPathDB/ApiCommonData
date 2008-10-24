d860c1d964bf0614fbe8f007a321577e74fc67cc
Wei Li <weili1@pennmedicine.upenn.edu>
2008-10-24 20:02:06 +0000

    added flag for one or multiple fasta files
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24447 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/ExtractNaSeq.pm
+++ Load/lib/perl/WorkflowSteps/ExtractNaSeq.pm
@@ -14,48 +14,45 @@ use GUS::Workflow::WorkflowStepInvoker;
 sub run {
   my ($self, $test) = @_;
 
-  my $genomeName = $self->getParamValue('genomeName');
-  
-  my $seqType = $self->getParamValue('seqType');
-
   my $table = $self->getParamValue('table');
   
   my $identifier = $self->getParamValue('identifier');
 
-  my $ncbiTaxId = $self->getParamValue('ncbiTaxId');
+  my $taxonId = $self->getTaxonId($self->getParamValue('ncbiTaxId'));
 
   my $dbRlsId = $self->getExtDbRlsId($self->getParamValue('genomeExtDbRlsSpec'));
 
-  my $outFile = $self->getParamValue('outputFile');
+  my $outputFileOrDir = $self->getParamValue('outputFileOrDir');
+
+  my $separateFastaFiles = $self->getParamValue('separateFastaFiles');
 
   my $seqfilesDir = $self->getParamValue('seqfilesDir');
-
-  my $dataDir = $self->getGlobalConfig('dataDir');
-
-  my $projectName = $self->getGlobalConfig('projectName');
- 
-  my $projectVersion = $self->getGlobalConfig('projectVersion');
-
-  $seqfilesDir = "$dataDir/$projectName/$projectVersion/primary/data/$genomeName/$seqfilesDir";
   
-  $outFile = "$seqfilesDir/$outFile";
-
-  my $logFile = "$dataDir/$projectName/$projectVersion/logs/$genomeName/Extract$genomeName-$seqType.log";
-
   $self->runCmd(0, "mkdir -p $seqfilesDir") if $seqfilesDir;
   
   my $sql = "select x.$identifier, x.description,
             'length='||x.length,x.sequence
              from dots.$table x
              where x.external_database_release_id = $dbRlsId";
+  my $cmd;
 
-  my $taxonId = $self->getTaxonId($self->getParamValue('ncbiTaxId'));
+  if ($separateFastaFiles) {
 
-  my $cmd = "gusExtractSequences --outputFile $outFile --idSQL \"$sql\" --verbose 2>> $logFile";
-  if ($test) {
-      $self->runCmd(0,"test > $outFile");
+      $cmd="gusExtractIndividualSequences --outputDir $outputFileOrDir --idSQL \"$sql\" --verbose";
+
   } else {
+
+      $cmd = "gusExtractSequences --outputFile $outputFileOrDir --idSQL \"$sql\" --verbose";
+  }
+  
+  if ($test) {
+
+      $self->runCmd(0,"test > $outputFileOrDir");
+
+  } else {
+
       $self->runCmd($test,$cmd);
+
   }
 
 }
