8621bfa66cae1ebb3f622a300b1237497a47c5af
Wei Li <weili1@pennmedicine.upenn.edu>
2008-11-03 18:24:35 +0000

    initial version
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24614 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/InsertSplign.pm
@@ -0,0 +1,50 @@
+package ApiCommonData::Load::WorkflowSteps::InsertSplign;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $queryExtDbRlsSpecc = $self->getParamValue('queryExtDbRlsSpecc');
+
+  my $subjectExtDbRlsSpec = $self->getParamValue('subjectExtDbRlsSpec');
+
+  my $inputFile = $self->getParamValue('inputFile');
+  
+  my $queryTable = $self->getParamValue('queryTable');
+
+  my $subjectTable = $self->getParamValue('subjectTable');
+
+  my $args = "--inputFile $inputFile --estTable '$queryTable' --seqTable '$subjectTable' --estExtDbRlsSpec '$queryExtDbRlsSpec' --seqExtDbRlsSpec '$subjectExtDbRlsSpec'";
+ 
+  $self -> runPlugin ("ApiCommonData::Load::Plugin::InsertSplignAlignments", $args);
+}
+
+
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['queryExtDbRlsSpecc', "", ""],
+     ['subjectExtDbRlsSpec', "", ""],
+     ['inputFile', "", ""],
+     ['queryTable', "", ""],
+     ['subjectTable', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
