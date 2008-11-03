e5fd39501e080c7f3410fd727a866e2fc844295f
Wei Li <weili1@pennmedicine.upenn.edu>
2008-11-03 21:40:22 +0000

    initial development
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24637 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/ExtractIdsFromBlastResult.pm
@@ -0,0 +1,53 @@
+package ApiCommonData::Load::WorkflowSteps::ExtractIdsFromBlastResult;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $inputFile = $self->getParamValue('inputFile');
+
+  my $idType = $self->getParamValue('idType');
+
+  my $outputFile = $self->getParamValue('outputFile');
+
+  $self->runCmd(0,"gunzip $inputFile");
+
+  my $cmd = "makeIdFileFromBlastSimOutput --$idType --subject --blastSimFile $inputFile --outFile $outputFile";
+
+  if ($test) {
+
+      $self->runCmd(0,"test > $outputFile");
+
+  } else {
+
+      $self->runCmd($test,$cmd);
+
+  }
+
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['idType', "", ""],
+     ['outputFile', "", ""],
+     ['inputFile', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
---
+++ Load/lib/perl/WorkflowSteps/LoadFastaSubset.pm
@@ -0,0 +1,47 @@
+package ApiCommonData::Load::WorkflowSteps::LoadFastaSubset;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $extDbRlsSpec = $self->getParamValue('extDbRlsSpec');
+
+  my ($extDbName, $extDbRlsVer) = split('|',$extDbRlsSpec);
+
+  my $fastaFile = $self->getParamValue('fastaFile');
+
+  my $idsFile = $self->getParamValue('idsFile');
+
+  $self->runCmd(0,"gunzip $fastaFile") if (-e "${fastaFile}.gz");
+
+  my $args = "--externalDatabaseName $extDbName --externalDatabaseVersion $extDbRlsVer --sequenceFile $fastaFile --sourceIdsFile  $idsFile --regexSourceId  '>gi\\|(\\d+)\\|' --regexDesc '^>(.+)' --tableName DoTS::ExternalAASequence";
+
+  $self->runPlugin("GUS::Supported::Plugin::LoadFastaSequences",$args);
+
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['idsFile', "", ""],
+     ['extDbRlsSpec', "", ""],
+     ['fastaFile', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
