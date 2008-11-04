c17268f1387efcfab3389fb0718159b997909466
Wei Li <weili1@pennmedicine.upenn.edu>
2008-11-04 21:38:55 +0000

    initial development
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24661 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/CalculateProteinMolWt.pm
@@ -0,0 +1,53 @@
+package ApiCommonData::Load::WorkflowSteps::CalculateProteinMolWt;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $extDbRlsSpec = $self->getParamValue('genomeExtDbRlsSpec');
+
+  my ($extDbName, $extDbRlsVer);
+
+  if ($extDbRlsSpec =~ /(.+)\|(.+)/) {
+
+      $extDbName = $1;
+
+      $extDbRlsVer= $2
+
+    } else {
+
+      die "Database specifier '$extDbRlsSpec' is not in 'name|version' format";
+  }
+
+  my $table = $self->getParamValue('table');
+
+  my $args = "--extDbRlsName '$extDbName' --extDbRlsVer '$extDbRlsVer' --seqTable $table";
+
+  $self->runPlugin("GUS::Supported::Plugin::CalculateAASequenceMolWt",$args);
+
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['genomeExtDbRlsSpec', "", ""],
+     ['table', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
---
+++ Load/lib/perl/WorkflowSteps/CalculateTranslatedProteinSequence.pm
@@ -0,0 +1,53 @@
+package ApiCommonData::Load::WorkflowSteps::CalculateTranslatedProteinSequence;
+
+@ISA = (GUS::Workflow::WorkflowStepInvoker);
+
+use strict;
+use GUS::Workflow::WorkflowStepInvoker;
+
+
+sub run {
+  my ($self, $test) = @_;
+
+  my $extDbRlsSpec = $self->getParamValue('genomeExtDbRlsSpec');
+
+  my ($extDbName, $extDbRlsVer);
+
+  if ($extDbRlsSpec =~ /(.+)\|(.+)/) {
+
+      $extDbName = $1;
+
+      $extDbRlsVer = $2
+
+    } else {
+
+      die "Database specifier '$extDbRlsSpec' is not in 'name|version' format";
+  }
+
+  my $soVersion = $self->getParamValue('soVersion');
+
+  my $args = "--sqlVerbose --extDbRlsName '$extDbName' --extDbRlsVer '$extDbRlsVer' --soCvsVersion $soVersion";
+
+  $self->runPlugin("GUS::Supported::Plugin::CalculateTranslatedAASequences",$args);
+
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getConfigDeclaration {
+  my @properties = 
+    (
+     # [name, default, description]
+     ['genomeExtDbRlsSpec', "", ""],
+     ['soVersion', "", ""],
+    );
+  return @properties;
+}
+
+sub getDocumentation {
+}
