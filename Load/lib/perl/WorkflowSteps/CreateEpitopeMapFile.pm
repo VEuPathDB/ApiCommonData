61b8a0552ee21af3dc02e5f1721f2da2cf07a3c2
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2008-11-11 20:56:36 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24786 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/CreateEpitopeMapFile.pm
@@ -0,0 +1,53 @@
+package ApiCommonData::Load::WorkflowSteps::CreateEpitopeMapFile;
+
+@ISA = (ApiCommonData::Load::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
+
+sub run {
+ 	my ($self, $test) = @_;
+
+	my $ncbiBlastPath = $self->getConfig('ncbiBlastPath');
+
+	my $inputDir = $self->getParamValue('inputDirRelativeToDownloadsDir');
+	my $querydir = $self->getParamValue('proteinsFile');
+	my $blastDir = $self->getParamValue('blastDbDir');
+	my $speciesKey = $self->getParamValue('organismTwoLetterAbbrev');
+	my $outputDir = $self->getParamValue('outputDir');
+
+  	my $cmd = "createEpitopeMappingFile  --ncbiBlastPath $ncbiBlastPath --inputDir $inputDir --queryDir $queryDir --outputDir $outputDir --blastDatabase $blastDir";
+  	$cmd .= " --speciesKey $speciesKey" if ($speciesKey);
+
+    	$self->runCmd($test,$cmd);
+}
+
+
+
+sub getParamsDeclaration {
+  return ('inputDirRelativeToDownloadsDir',
+	  'blastDbDir',
+	  'organismTwoLetterAbbrev',
+	  'proteinsFile',
+	  'outputDir'
+	 );
+}
+
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+ 	    ['ncbiBlastPath', "", ""],
+	 );
+}
+
+sub getDocumentation {
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
