faddaa3078b5823c1fea5b6af7781c964c000931
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2008-11-12 16:14:56 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24799 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/FixMercatorOffsetsInGFF.pm
@@ -0,0 +1,42 @@
+package ApiCommonData::Load::WorkflowSteps::FixMercatorOffsetsInGFF;
+
+@ISA = (ApiCommonData::Load::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
+
+sub run {
+	my ($self, $test) = @_;
+
+	my $inGFFFile = self->getParamValue('inputFile');
+	my $inFastaFile = self->getParamValue('fsaFile');
+	my $outFile = self->getParamValue('outputFile');
+
+	my $args = "--f $inFastaFile --g $inGFFFile --o $outFile";
+
+}
+
+
+sub getParamsDeclaration {
+  return ('inputFile',
+	  'fsaFile',
+	  'outputFile'
+	 );
+}
+
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+ 	 );
+}
+
+sub getDocumentation {
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
