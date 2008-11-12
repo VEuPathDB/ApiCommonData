aac25f9d394d3d6ef75952564d1351cf583a5c33
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2008-11-12 16:39:43 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24800 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/FixMercatorOffsetsInGFF.pm
+++ Load/lib/perl/WorkflowSteps/FixMercatorOffsetsInGFF.pm
@@ -8,14 +8,18 @@ use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 sub run {
 	my ($self, $test) = @_;
 
-	my $inGFFFile = self->getParamValue('inputFile');
-	my $inFastaFile = self->getParamValue('fsaFile');
-	my $outFile = self->getParamValue('outputFile');
-
+	my $inGFFFile = $self->getParamValue('inputFile');
+	my $inFastaFile = $self->getParamValue('fsaFile');
+	my $outFile = $self->getParamValue('outputFile');
+	
 	my $args = "--f $inFastaFile --g $inGFFFile --o $outFile";
 
+	 if ($test){
+	      $self->runCmd(0,'echo test > $outFile');
+	  }else{
+	      $self->runCmd($test,"fixMercatorOffsetsInGFF.pl $args");
+	  }
 }
-
 
 sub getParamsDeclaration {
   return ('inputFile',
