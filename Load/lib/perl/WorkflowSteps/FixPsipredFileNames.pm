ce25e221a013b699f363162931d5d077c2a989de
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2008-11-13 23:12:12 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24855 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/FixPsipredFileNames.pm
+++ Load/lib/perl/WorkflowSteps/FixPsipredFileNames.pm
@@ -1,51 +1,55 @@
 package ApiCommonData::Load::WorkflowSteps::FixPsipredFileNames;
 
 @ISA = (ApiCommonData::Load::WorkflowSteps::WorkflowStep);
+
 use strict;
 use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
-TEMPLATE
 sub run {
-  my ($self, $test) = @_;
+    my ($self, $test) = @_;
+	
+    my $inputDir = $self->getParamValue('inputDir');
+    my $outputDir = $self->getParamValue('outputDir');
+    my @files;
+	
+    $self->runCmd($test,"cp -r $inputDir  $outputDir");
 
-  # get parameters
-  my $inputDir = $self->getParamValue('inputDir');
-  my $outputDir = $self->getParamValue('outputDir');
 
-  # get global properties
-  my $ = $self->getGlobalConfig('');
-
-  # get step properties
-  my $ = $self->getConfig('');
-
-  if ($test) {
-  } else {
-  }
-
-  $self->runPlugin($test, '', $args);
-
+    if (-d $inputDir){
+        opendir(DIR, $inputDir) || die "Can't open directory '$inputDir'";
+        my @noDotFiles = grep { $_ ne '.' && $_ ne '..' } readdir(DIR);
+        @files = map { "$inputDir/$_" } @noDotFiles;
+    } else {
+        $files[0] = $inputDir;
+    }
+	
+	
+    foreach my $file (@files){
+        my $original = $file;
+        $file =~ s/(\S+)_(\d)/$1-$2/g;
+        $self->runCmd($test,"mv $original $file");
+    }
 }
 
 sub getParamsDeclaration {
-  return (
-          'inputDir',
-          'outputDir',
-         );
+    return ('inputDir',
+            'outputDir'
+           );
 }
 
+
 sub getConfigDeclaration {
-  return (
-         # [name, default, description]
-         # ['', '', ''],
-         );
+    return (
+            # [name, default, description]
+           );
+}
+
+sub getDocumentation {
 }
 
 sub restart {
 }
 
 sub undo {
-
 }
 
-sub getDocumentation {
-}
