b821099b7da92d4eba1b3473d4e03252533e4459
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2008-11-14 19:23:00 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@24879 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/FixMercatorOffsetsInGFF.pm
+++ Load/lib/perl/WorkflowSteps/FixMercatorOffsetsInGFF.pm
@@ -6,36 +6,38 @@ use strict;
 use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
 sub run {
-  my ($self, $test) = @_;
+    my ($self, $test) = @_;
 
-  my $inGFFFile = $self->getParamValue('inputFile');
-  my $inFastaFile = $self->getParamValue('fsaFile');
-  my $outputFile = $self->getParamValue('outputFile');
+    my $inputFile = $self->getParamValue('inputFile');
+    my $fsaFile = $self->getParamValue('fsaFile');
+    my $outputFile = $self->getParamValue('outputFile');
+	
+    my $localDataDir = $self->getLocalDataDir();
 
-  my $localDataDir = $self->getLocalDataDir();
 
-  my $args = "--f $localDataDir/$inFastaFile --g $localDataDir/$inGFFFile --o $outFile";
+    my $args = "--f $localDataDir/$fsaFile --g $localDataDir/$inputFile --o $localDataDir/$outputFile";
 
-  if ($test){
-    $self->runCmd(0,'echo test > $localDataDir/$outputFile');
-  }else{
-    $self->runCmd($test,"fixMercatorOffsetsInGFF.pl $args");
-  }
+    if ($test){
+        $self->runCmd(0,'echo test > $localDataDir/$outFile');
+    }else{
+        $self->runCmd($test,"fixMercatorOffsetsInGFF.pl $args");
+    }
+
 
 }
 
 sub getParamsDeclaration {
-  return ('inputFile',
-	  'fsaFile',
-	  'outputFile'
-	 );
+    return ('inputFile',
+            'fsaFile',
+            'outputFile'
+           );
 }
 
 
 sub getConfigDeclaration {
-  return (
-	  # [name, default, description]
- 	 );
+    return (
+            # [name, default, description]
+           );
 }
 
 sub getDocumentation {
