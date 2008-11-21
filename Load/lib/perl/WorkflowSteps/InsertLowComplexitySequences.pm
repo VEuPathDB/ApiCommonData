38a723ae1aac7b4548b3c3905e78b04d210ba4de
Wei Li <weili1@pennmedicine.upenn.edu>
2008-11-21 20:11:38 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@25032 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/InsertLowComplexitySequences.pm
+++ Load/lib/perl/WorkflowSteps/InsertLowComplexitySequences.pm
@@ -1,4 +1,4 @@
-package ApiCommonData::Load::WorkflowSteps::LoadLowComplexitySequences;
+package ApiCommonData::Load::WorkflowSteps::InsertLowComplexitySequences;
 
 @ISA = (ApiCommonData::Load::WorkflowSteps::WorkflowStep);
 
