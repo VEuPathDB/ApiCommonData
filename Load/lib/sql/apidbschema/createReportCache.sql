38a7a58c32b39469ef38673319e05352c142a9a2
John Iodice <iodice@pennmedicine.upenn.edu>
2008-12-02 20:21:27 +0000

    new table wdkIsolateTable; new primary key column wdk_table_id
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@25234 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createReportCache.sql
+++ Load/lib/sql/apidbschema/createReportCache.sql
@@ -1,9 +1,11 @@
 CREATE TABLE apidb.GeneTable (
+ wdk_table_id number(10) not null,
  source_id  VARCHAR2(50),
  project_id  VARCHAR2(50),
  table_name VARCHAR2(80),
  row_count  NUMBER(4),
- content    CLOB
+ content    CLOB,
+ primary key (wdk_table_id)
 );
 
 CREATE INDEX apidb.gtab_ix
@@ -13,6 +15,28 @@ CREATE INDEX apidb.gtab_name_ix
 
 GRANT insert, select, update, delete ON ApiDB.GeneTable TO gus_w;
 GRANT select ON ApiDB.GeneTable TO gus_r;
+
+------------------------------------------------------------------------------
+CREATE TABLE apidb.wdkIsolateTable (
+ wdk_table_id number(10) not null,
+ source_id  VARCHAR2(50),
+ project_id  VARCHAR2(50),
+ table_name VARCHAR2(80),
+ row_count  NUMBER(4),
+ content    CLOB,
+ primary key (wdk_table_id)
+);
+
+CREATE INDEX apidb.itab_ix
+       ON apidb.WdkIsolateTable (source_id, table_name, row_count);
+CREATE INDEX apidb.itab_name_ix
+       ON apidb.WdkIsolateTable (table_name, source_id, row_count);
+
+GRANT insert, select, update, delete ON ApiDB.WdkIsolateTable TO gus_w;
+GRANT select ON ApiDB.WdkIsolateTable TO gus_r;
+
+------------------------------------------------------------------------------
+CREATE SEQUENCE apidb.wdkTable_sq;
 
 ------------------------------------------------------------------------------
 exit;
