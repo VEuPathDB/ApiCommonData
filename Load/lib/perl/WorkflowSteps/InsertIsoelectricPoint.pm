6b0914c2b0d209fc27504f257a3c163bfa62f849
Steve Fischer <sfischer@upenn.edu>
2009-01-12 15:25:21 +0000

    correct plugin name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@25987 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/InsertIsoelectricPoint.pm
+++ Load/lib/perl/WorkflowSteps/InsertIsoelectricPoint.pm
@@ -16,7 +16,7 @@ sub run {
 
   my $args = "--extDbRlsName '$extDbName' --extDbRlsVer '$extDbRlsVer' --seqTable $table";
 
-  $self->runPlugin($test, "GUS::Supported::Plugin::CalculateAASequenceIsoelectricPoint",$args);
+  $self->runPlugin($test, "ApiCommonData::Load::Plugin::CalculateAASequenceIsoelectricPoint",$args);
 
 }
 
