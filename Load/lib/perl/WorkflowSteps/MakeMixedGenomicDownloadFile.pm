36522d8e3d69d00b688313920d237b22c68786bb
Wei Li <weili1@pennmedicine.upenn.edu>
2009-01-14 14:50:24 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26039 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/WorkflowSteps/MakeMixedGenomicDownloadFile.pm
@@ -0,0 +1,69 @@
+package ApiCommonData::Load::WorkflowSteps::MakeMixedGenomicDownloadFile;
+
+@ISA = (ApiCommonData::Load::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test) = @_;
+
+  # get parameters
+  my @extDbNames = map{"'$_'"} split (/,/,$self->getParamValue('extDbName'));
+  my @extDbRlss = map{"'$_'"} split (/,/,$self->getParamValue('extDbRls'));
+  my $outputFile = $self->getParamValue('outputFile');
+  my $dataSource = $self->getParamValue('DataSource');
+
+  my $extDbName = join(",", @extDbNames);
+  my $extDbRls = join(",", @extDbRlss);
+
+  my $sql = " SELECT '$dataSource'
+                ||'|'||
+               sa.source_id
+                ||' | organism='||
+               replace(sa.organism, ' ', '_')
+                ||' | version='||
+               sa.database_version
+                ||' | length=' ||
+               sa.length
+               as defline,
+               ns.sequence
+           FROM dots.nasequence ns,
+                apidb.sequenceattributes sa
+          WHERE ns.na_sequence_id = sa.na_sequence_id
+            AND sa.database_name in ($extDbName) AND sa.database_version in ($extDbRls)
+            AND sa.is_top_level = 1;
+
+  my $cmd = " gusExtractSequences --outputFile $outputFile  --idSQL \"$sql\"";
+
+  if ($test) {
+      $self->runCmd(0, "echo test > $outputFile");
+  }else{
+       $self->runCmd($test, $cmd);
+   }
+
+}
+
+sub getParamsDeclaration {
+  return (
+          'outputFile',
+          'extDbName',
+          'extDbRls',
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
+sub restart {
+}
+
+sub undo {
+
+}
+
+sub getDocumentation {
+}
