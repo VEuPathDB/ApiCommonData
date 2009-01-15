fe976b6932567de22adf8197d802a246bb446724
Deborah Pinney <pinney@upenn.edu>
2009-01-15 19:01:00 +0000

    revise prior to testing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26072 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/MakeInterproDownloadFile.pm
+++ Load/lib/perl/WorkflowSteps/MakeInterproDownloadFile.pm
@@ -4,30 +4,61 @@ package ApiCommonData::Load::WorkflowSteps::MakeInterproDownloadFile;
 use strict;
 use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
-TEMPLATE
+
 sub run {
   my ($self, $test) = @_;
 
-  # get parameters
   my $outputFile = $self->getParamValue('outputFile');
-  my $genomeExtDbRlsSpec = $self->getParamValue('genomeExtDbRlsSpec');
-  my $interproExtDbRlsSpec = $self->getParamValue('interproExtDbRlsSpec');
-  my $projectDB = $self->getParamValue('projectDB');
+  my $genomeDbRlsId = $self->getExtDbRlsId($test,$self->getParamValue('genomeExtDbRlsSpec'));
+  my $interproDbRlsId = $self->getExtDbRlsId($test,$self->getParamValue('interproExtDbRlsSpec'));
+  my $apiSiteFilesDir = $self->getGlobalConfig('apiSiteFilesDir');
 
-  # get global properties
-  my $ = $self->getGlobalConfig('');
+  my $sql = <<"EOF";
+  SELECT gf.source_id
+           || chr(9) ||
+         xd1.name
+           || chr(9) ||
+         dr.primary_identifier
+           || chr(9) ||
+         dr.secondary_identifier
+           || chr(9) ||
+         al.start_min
+           || chr(9) ||
+         al.end_min
+           || chr(9) ||
+         to_char(df.e_value,'9.9EEEE')
+  FROM
+    dots.aalocation al,
+    sres.dbref dr,
+    dots.DbRefAAFeature draf, 
+    dots.domainfeature df, 
+    dots.genefeature gf,
+    dots.transcript t, 
+    dots.translatedaafeature taf,
+    dots.translatedaasequence tas 
+  WHERE
+   gf.external_database_release_id = $genomeDbRlsId
+     AND gf.na_feature_id = t.parent_id 
+     AND t.na_feature_id = taf.na_feature_id 
+     AND taf.aa_sequence_id = tas.aa_sequence_id 
+     AND tas.aa_sequence_id = df.aa_sequence_id 
+     AND df.aa_feature_id = draf.aa_feature_id  
+     AND draf.db_ref_id = dr.db_ref_id 
+     AND df.aa_feature_id = al.aa_feature_id 
+     AND df.external_database_release_id =  $interproDbRlsId
+EOF
 
-  # get step properties
-  my $ = $self->getConfig('');
 
-  my $localDataDir = $self->getLocalDataDir();
+  my $cmd = <<"EOF";
+     makeFileWithSql --outFile $apiSiteFilesDir/$outputFile \\
+     --sql \"$sql\"
+EOF
 
   if ($test) {
-  } else {
+      $self->runCmd(0,"echo test > $apiSiteFilesDir/$outputFile");
   }
 
-  $self->runPlugin($test, '', $args);
-
+  $self->runCmd($test,$cmd);
 }
 
 sub getParamsDeclaration {
