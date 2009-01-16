b6532941e1df305656789d542f7b40ed7d2a6c24
Deborah Pinney <pinney@upenn.edu>
2009-01-16 14:54:22 +0000

    revise prior to testing
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26092 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/MakeOrfDownloadFile.pm
+++ Load/lib/perl/WorkflowSteps/MakeOrfDownloadFile.pm
@@ -4,36 +4,77 @@ package ApiCommonData::Load::WorkflowSteps::MakeOrfDownloadFile;
 use strict;
 use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
-TEMPLATE
+
 sub run {
   my ($self, $test) = @_;
 
   # get parameters
   my $outputFile = $self->getParamValue('outputFile');
-  my $genomeExtDbRlsSpec = $self->getParamValue('genomeExtDbRlsSpec');
-  my $projectDB = $self->getParamValue('projectDB');
 
-  # get global properties
-  my $ = $self->getGlobalConfig('');
+  my @extDbRlsIds;
+  push(@extDbRlsIds,$self->getExtDbRlsId($test, $self->getParamValue('genomeExtDbRlsSpec'))) if $self->getParamValue('genomeExtDbRlsSpec');
+  push(@extDbRlsIds,$self->getExtDbRlsId($test, $self->getParamValue('genomeVirtualSeqsExtDbRlsSpec'))) if $self->getParamValue('genomeVirtualSeqsExtDbRlsSpec');
 
-  # get step properties
-  my $ = $self->getConfig('');
+  my $length = $self->getParamValue('minOrfLength');
 
-  my $localDataDir = $self->getLocalDataDir();
+  my $apiSiteFilesDir = $self->getGlobalConfig('apiSiteFilesDir');
+
+  my $dbRlsIds = join(",", @extDbRlsIds);
+
+   my $sql = <<"EOF";
+    SELECT
+       m.source_id
+        ||' | organism='||
+       replace(tn.name, ' ', '_')
+        ||' | location='||
+       fl.sequence_source_id
+        ||':'||
+       fl.start_min
+        ||'-'||
+       fl.end_max
+        ||'('||
+       decode(fl.is_reversed, 1, '-', '+')
+        ||') | length='||
+       taas.length as defline,
+       taas.sequence
+       FROM dots.miscellaneous m,
+            dots.translatedaafeature taaf,
+            dots.translatedaasequence taas,
+            sres.taxonname tn,
+            sres.sequenceontology so,
+            apidb.featurelocation fl,
+            dots.nasequence enas
+      WHERE m.na_feature_id = taaf.na_feature_id
+        AND taaf.aa_sequence_id = taas.aa_sequence_id
+        AND m.na_feature_id = fl.na_feature_id
+        AND fl.is_top_level = 1
+        AND enas.na_sequence_id = fl.na_sequence_id 
+        AND enas.taxon_id = tn.taxon_id
+        AND tn.name_class = 'scientific name'
+        AND m.sequence_ontology_id = so.sequence_ontology_id
+        AND so.term_name = 'ORF'
+        AND taas.length >= $length
+        AND m.external_database_release_id in ($dbRlsIds)
+EOF
+
+    my $cmd = <<"EOF";
+      gusExtractSequences --outputFile $apiSiteFilesDir/$outputFile \\
+      --idSQL \"$sql\" \\
+      --verbose
+EOF
 
   if ($test) {
-  } else {
+      $self->runCmd(0,"echo test > $apiSiteFilesDir/$outputFile");
   }
 
-  $self->runPlugin($test, '', $args);
-
+  $self->runCmd($test,$cmd);
 }
 
 sub getParamsDeclaration {
   return (
           'outputFile',
           'genomeExtDbRlsSpec',
-          'projectDB',
+          'genomeVirtualSeqsExtDbRlsSpec'
          );
 }
 
