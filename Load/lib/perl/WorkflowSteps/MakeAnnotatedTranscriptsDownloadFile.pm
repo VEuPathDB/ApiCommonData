5d307ebff30e71679cb8187d0b38fe637eaf996c
Deborah Pinney <pinney@upenn.edu>
2009-02-06 20:56:41 +0000

    add undo and get rid of unused subs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26659 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/MakeAnnotatedTranscriptsDownloadFile.pm
+++ Load/lib/perl/WorkflowSteps/MakeAnnotatedTranscriptsDownloadFile.pm
@@ -6,7 +6,7 @@ use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
 
 sub run {
-  my ($self, $test) = @_;
+  my ($self, $test, $undo) = @_;
 
   # get parameters
   my $outputFile = $self->getParamValue('outputFile');
@@ -66,7 +66,12 @@ my $cmd = "gusExtractSequences --outputFile $apiSiteFilesDir/$outputFile  --idSQ
       $self->runCmd(0,"echo test > $apiSiteFilesDir/$outputFile");
   }
 
-  $self->runCmd($test,$cmd);
+   if ($undo) {
+    $self->runCmd(0, "rm -f $apiSiteFilesDir/$outputFile");
+  } else {
+    $self->runCmd($test,$cmd);
+  }
+
 }
 
 
@@ -87,12 +92,3 @@ sub getConfigDeclaration {
          );
 }
 
-sub restart {
-}
-
-sub undo {
-
-}
-
-sub getDocumentation {
-}
