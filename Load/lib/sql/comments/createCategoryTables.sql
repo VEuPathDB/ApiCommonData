74ae5a8a1bb2da89da6c04a26f21f44348dc81e9
Haiming Wang <hwang@uga.edu>
2009-02-10 22:08:26 +0000

    alter the column name for comments2.commentReference table to handle both genbank accessions and pubmed ids.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26769 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/createCategoryTables.sql
+++ Load/lib/sql/comments/createCategoryTables.sql
@@ -1,6 +1,6 @@
 DROP TABLE comments2.CommentTargetCategory;
 DROP TABLE comments2.TargetCategory;
-DROP TABLE comments2.CommentBibliographicReference;
+DROP TABLE comments2.CommentReference;
 
 DROP SEQUENCE comments2.commentTargetCategory_pkseq; 
 DROP SEQUENCE comments2.commentReference_pkseq; 
@@ -29,7 +29,7 @@ CREATE TABLE comments2.CommentTargetCategory
 	comment_id NUMBER(10) NOT NULL,
   target_category_id NUMBER(10) NOT NULL,
 	CONSTRAINT comment_target_category_key PRIMARY KEY (comment_target_category_id),
-	CONSTRAINT comment_id_fkey FOREIGN KEY (comment_id)
+	CONSTRAINT comment_id_category_fkey FOREIGN KEY (comment_id)
 	   REFERENCES comments2.comments (comment_id),
 	CONSTRAINT target_category_id_fkey FOREIGN KEY (target_category_id)
 	   REFERENCES comments2.TargetCategory (target_category_id)
@@ -39,13 +39,15 @@ GRANT insert, update, delete on comments2.CommentTargetCategory to GUS_W;
 GRANT select on comments2.CommentTargetCategory to GUS_R;
 
 CREATE SEQUENCE comments2.commentTargetCategory_pkseq START WITH 1 INCREMENT BY 1;
+
 GRANT select on comments2.commentTargetCategory_pkseq to GUS_W;
 GRANT select on comments2.commentTargetCategory_pkseq to GUS_R;
 
 CREATE TABLE comments2.CommentReference
 (
   comment_reference_id NUMBER(10) NOT NULL,
-	reference_id VARCHAR2(15) NOT NULL,
+	source_id VARCHAR2(15) NOT NULL,
+	database_name VARCHAR2(15) NOT NULL,
   comment_id NUMBER(10) NOT NULL,
 	CONSTRAINT comment_reference_key PRIMARY KEY (comment_reference_id),
 	CONSTRAINT comment_id_ref_fkey FOREIGN KEY (comment_id)
