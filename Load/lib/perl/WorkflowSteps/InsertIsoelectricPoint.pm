6888fd315ca5bc26b6440b6693281a590298db88
Deborah Pinney <pinney@upenn.edu>
2009-02-10 16:05:13 +0000

    pass 0 as undo arg to runPlugin and get rid of unused subs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26735 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/InsertIsoelectricPoint.pm
+++ Load/lib/perl/WorkflowSteps/InsertIsoelectricPoint.pm
@@ -16,7 +16,7 @@ sub run {
 
   my $args = "--extDbRlsName '$extDbName' --extDbRlsVer '$extDbRlsVer' --seqTable $table";
 
-  $self->runPlugin($test, "ApiCommonData::Load::Plugin::CalculateAASequenceIsoelectricPoint",$args);
+  $self->runPlugin($test,0, "ApiCommonData::Load::Plugin::CalculateAASequenceIsoelectricPoint",$args);
 
 }
 
@@ -33,12 +33,4 @@ sub getConfigDeclaration {
 	 );
 }
 
-sub restart {
-}
 
-sub undo {
-
-}
-
-sub getDocumentation {
-}
