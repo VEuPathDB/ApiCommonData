0544abaa83ffbeba46d8b21e634a4c4b78c12a60
Wei Li <weili1@pennmedicine.upenn.edu>
2009-02-11 15:59:23 +0000

    fix cvsRevision
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26795 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/CalculateAASeqMolWtMinMaxAndIsoelectricPoint.pm
+++ Load/plugin/perl/CalculateAASeqMolWtMinMaxAndIsoelectricPoint.pm
@@ -79,7 +79,7 @@ sub new {
   bless $self, $class;
 
   $self->initialize({ requiredDbVersion => 3.5,
-		      cvsRevision =>  '$Revision: $',
+		      cvsRevision =>  '$Revision: 26795$',
 		      name => ref($self),
 		      argsDeclaration   => $argsDeclaration,
 		      documentation     => $documentation
