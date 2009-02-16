d49f97ddd2eabc8d27e81f241b537f87caad374d
Wei Li <weili1@pennmedicine.upenn.edu>
2009-02-16 21:10:21 +0000

    fix cvsRevision issue
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@26934 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertNrdbXrefs.pm
+++ Load/plugin/perl/InsertNrdbXrefs.pm
@@ -87,7 +87,7 @@ sub new {
 
 
     $self->initialize({requiredDbVersion => 3.5,
-		       cvsRevision => '$Revision: 26449 $', # cvs fills this in!
+		       cvsRevision => '$Revision:  $', # cvs fills this in!
 		       name => ref($self),
 		       argsDeclaration => $argsDeclaration,
 		       documentation => $documentation
