65d97da3efa277fb6681d21a84d0d39da1790f51
Deborah Pinney <pinney@upenn.edu>
2009-03-02 19:58:57 +0000

    add undo and test, get rid of unused subs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@27184 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/FixPsipredFileNames.pm
+++ Load/lib/perl/WorkflowSteps/FixPsipredFileNames.pm
@@ -6,24 +6,34 @@ use strict;
 use ApiCommonData::Load::WorkflowSteps::WorkflowStep;
 
 sub run {
-    my ($self, $test) = @_;
+    my ($self, $test, $undo) = @_;
 
     my $inputDir = $self->getParamValue('inputDir');
     my $outputDir = $self->getParamValue('outputDir');
 
     my $localDataDir = $self->getLocalDataDir();
 
+    $self->runCmd(0,"mkdir -p $localDataDir/$outputDir");
+
     $self->runCmd(0,"cp -r $localDataDir/$inputDir  $localDataDir/$outputDir");
 
     opendir(DIR, "$localDataDir/$outputDir") || die "Can't open directory '$localDataDir/$outputDir'";
-    
-    
-    my @files = readdir(DIR);
-    foreach my $file (@files) {
+
+    if ($test) {
+      $self->runCmd(0,"echo test > $localDataDir/$outputDir/testFile");
+    }
+
+
+    if ($undo) {
+      $self->runCmd(0, "rm -rf $localDataDir/$outputDir");
+    } else {
+      my @files = readdir(DIR);
+      foreach my $file (@files) {
 	next if $file=~ /^\.\.?$/;  # skip . and ..
         my $original = $file;
         $file =~ s/(\S+)_(\d)/$1-$2/g;
         $self->runCmd($test, "mv $localDataDir/$outputDir/$original $localDataDir/$outputDir/$file");
+      }
     }
 }
 
@@ -40,12 +50,4 @@ sub getConfigDeclaration {
            );
 }
 
-sub getDocumentation {
-}
-
-sub restart {
-}
-
-sub undo {
-}
 
