e03895328243e758bcd50d44589d934ca514d117
Haiming Wang <hwang@uga.edu>
2009-04-01 15:54:37 +0000

    updates for multiple-uploads with descriptoin
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@27804 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/createCategoryTables.sql
+++ Load/lib/sql/comments/createCategoryTables.sql
@@ -13,8 +13,8 @@ CREATE TABLE comments2.TargetCategory
 (
   target_category_id NUMBER(10) NOT NULL,
   category VARCHAR2(100) NOT NULL,
-	comment_target_id varchar(20) NOT NULL,
-	CONSTRAINT target_category_key PRIMARY KEY (target_category_id)
+  comment_target_id varchar(20) NOT NULL,
+  CONSTRAINT target_category_key PRIMARY KEY (target_category_id)
 );
 
 GRANT insert, update, delete on comments2.TargetCategory to GUS_W;
@@ -63,13 +63,13 @@ INSERT INTO comments2.TargetCategory (target_category_id, category, comment_targ
 CREATE TABLE comments2.CommentTargetCategory
 (
   comment_target_category_id NUMBER(10) NOT NULL,
-	comment_id NUMBER(10) NOT NULL,
+  comment_id NUMBER(10) NOT NULL,
   target_category_id NUMBER(10) NOT NULL,
-	CONSTRAINT comment_target_category_key PRIMARY KEY (comment_target_category_id),
-	CONSTRAINT comment_id_category_fkey FOREIGN KEY (comment_id)
-	   REFERENCES comments2.comments (comment_id),
-	CONSTRAINT target_category_id_fkey FOREIGN KEY (target_category_id)
-	   REFERENCES comments2.TargetCategory (target_category_id)
+  CONSTRAINT comment_target_category_key PRIMARY KEY (comment_target_category_id),
+  CONSTRAINT comment_id_category_fkey FOREIGN KEY (comment_id)
+     REFERENCES comments2.comments (comment_id),
+  CONSTRAINT target_category_id_fkey FOREIGN KEY (target_category_id)
+     REFERENCES comments2.TargetCategory (target_category_id)
 );
 
 GRANT insert, update, delete on comments2.CommentTargetCategory to GUS_W;
@@ -83,12 +83,12 @@ GRANT select on comments2.commentTargetCategory_pkseq to GUS_R;
 CREATE TABLE comments2.CommentReference
 (
   comment_reference_id NUMBER(10) NOT NULL,
-	source_id VARCHAR2(15) NOT NULL,
-	database_name VARCHAR2(15) NOT NULL,
+  source_id VARCHAR2(15) NOT NULL,
+  database_name VARCHAR2(15) NOT NULL,
   comment_id NUMBER(10) NOT NULL,
-	CONSTRAINT comment_reference_key PRIMARY KEY (comment_reference_id),
-	CONSTRAINT comment_id_ref_fkey FOREIGN KEY (comment_id)
-	   REFERENCES comments2.comments (comment_id)
+  CONSTRAINT comment_reference_key PRIMARY KEY (comment_reference_id),
+  CONSTRAINT comment_id_ref_fkey FOREIGN KEY (comment_id)
+     REFERENCES comments2.comments (comment_id)
 );
 
 GRANT insert, update, delete on comments2.CommentReference to GUS_W;
@@ -101,11 +101,12 @@ GRANT select on comments2.commentReference_pkseq to GUS_R;
 CREATE TABLE comments2.CommentFile
 (
   file_id NUMBER(10) NOT NULL,
-	uri VARCHAR2(500) NOT NULL,
+  name VARCHAR2(500) NOT NULL,
+  notes VARCHAR2(4000) NOT NULL,
   comment_id NUMBER(10) NOT NULL,
-	CONSTRAINT file_id_key PRIMARY KEY (file_id),
-	CONSTRAINT comment_id_file_fkey FOREIGN KEY (comment_id)
-	   REFERENCES comments2.comments (comment_id)
+  CONSTRAINT file_id_key PRIMARY KEY (file_id),
+  CONSTRAINT comment_id_file_fkey FOREIGN KEY (comment_id)
+     REFERENCES comments2.comments (comment_id)
 );
 
 GRANT insert, update, delete on comments2.CommentFile to GUS_W;
@@ -120,9 +121,9 @@ CREATE TABLE comments2.CommentStableId
   comment_stable_id NUMBER(10) NOT NULL,
   stable_id VARCHAR2(200) NOT NULL,
   comment_id NUMBER(10) NOT NULL,
-	CONSTRAINT comment_stable_id_key PRIMARY KEY (comment_stable_id),
-	CONSTRAINT comment_stable_id_fkey FOREIGN KEY (comment_id)
-	   REFERENCES comments2.comments (comment_id)
+  CONSTRAINT comment_stable_id_key PRIMARY KEY (comment_stable_id),
+  CONSTRAINT comment_stable_id_fkey FOREIGN KEY (comment_id)
+     REFERENCES comments2.comments (comment_id)
 );
 
 GRANT insert, update, delete on comments2.CommentStableId to GUS_W;
