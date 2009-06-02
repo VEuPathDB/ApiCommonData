c887ed42962bb49dec96614491746d2e045197cb
Deborah Pinney <pinney@upenn.edu>
2009-06-02 17:31:21 +0000

    added comment to explain step
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@29276 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/WorkflowSteps/LoadNrdbXrefs.pm
+++ Load/lib/perl/WorkflowSteps/LoadNrdbXrefs.pm
@@ -35,3 +35,11 @@ sub getConfigDeclaration {
          );
 }
 
+
+
+##this step loads the results of mapping an NR protein record to an annotated protein based on their sequences
+##the protein gi numbers and source_ids from the nrdb file record are inserted into sres.dbref
+##and are linked via dots.dbrefnafeature rows to the dots.genefeature row corresponding to the annotated protein
+##need to avoid mapping proteins (from nr record)  from organisms in the same project
+##will alternative splicing cause a problem with this?
+
