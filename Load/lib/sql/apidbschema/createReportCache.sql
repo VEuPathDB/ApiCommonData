30f8c2d64325b03038e8c88abe0b29fe1909dda9
John Iodice <iodice@pennmedicine.upenn.edu>
2009-06-12 21:37:42 +0000

    Make new friends but kep the old.  Oh wait, not friends.  Data-dump tables.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@29479 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createReportCache.sql
+++ Load/lib/sql/apidbschema/createReportCache.sql
@@ -59,4 +59,47 @@ GRANT insert, select, update, delete ON apidb.SequenceDetail TO gus_w;
 GRANT select ON apidb.SequenceDetail TO gus_r;
 
 ------------------------------------------------------------------------------
+-- for now, include the old style as well.
+
+CREATE TABLE apidb.GeneTable (
+ wdk_table_id number(10) not null,
+ source_id  VARCHAR2(50),
+ project_id  VARCHAR2(50),
+ table_name VARCHAR2(80),
+ row_count  NUMBER(4),
+ content    CLOB,
+ primary key (wdk_table_id)
+);
+
+CREATE INDEX apidb.gtab_ix
+       ON apidb.GeneTable (source_id, table_name, row_count);
+CREATE INDEX apidb.gtab_name_ix
+       ON apidb.GeneTable (table_name, source_id, row_count);
+
+GRANT insert, select, update, delete ON ApiDB.GeneTable TO gus_w;
+GRANT select ON ApiDB.GeneTable TO gus_r;
+
+------------------------------------------------------------------------------
+CREATE TABLE apidb.wdkIsolateTable (
+ wdk_table_id number(10) not null,
+ source_id  VARCHAR2(50),
+ project_id  VARCHAR2(50),
+ table_name VARCHAR2(80),
+ row_count  NUMBER(4),
+ content    CLOB,
+ primary key (wdk_table_id)
+);
+
+CREATE INDEX apidb.itab_ix
+       ON apidb.WdkIsolateTable (source_id, table_name, row_count);
+CREATE INDEX apidb.itab_name_ix
+       ON apidb.WdkIsolateTable (table_name, source_id, row_count);
+
+GRANT insert, select, update, delete ON ApiDB.WdkIsolateTable TO gus_w;
+GRANT select ON ApiDB.WdkIsolateTable TO gus_r;
+
+------------------------------------------------------------------------------
+CREATE SEQUENCE apidb.wdkTable_pkseq;
+
+------------------------------------------------------------------------------
 exit;
