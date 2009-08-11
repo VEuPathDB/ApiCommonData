9211c837e4efd3e87dae455b7931b11b78dc133b
John Brestelli <jbrestell@gmail.com>
2009-08-11 19:25:57 +0000

    update to isolatevocabulary table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@30772 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createIsolateVocabulary.sql
+++ Load/lib/sql/apidbschema/createIsolateVocabulary.sql
@@ -1,26 +1,13 @@
 CREATE TABLE ApiDB.IsolateVocabulary (
  term                        varchar(200) NOT NULL,
+ original_term               varchar(200) NOT NULL,
  parent                      varchar(200),
- type                        varchar(50) NOT NULL
+ type                        varchar(50) NOT NULL,
+ source                      varchar(50) NOT NULL
 );
 
 GRANT insert, select, update, delete ON ApiDB.IsolateVocabulary TO gus_w;
 GRANT select ON ApiDB.IsolateVocabulary TO gus_r;
-
---------------------------------------------------------------------------------
-
-
-CREATE TABLE ApiDB.IsolateVocabularyLink (
- old_term                        varchar(200) NOT NULL,
- new_term                        varchar(200) NOT NULL,
- type                            varchar(50) NOT NULL
-);
-
-GRANT insert, select, update, delete ON ApiDB.IsolateVocabularyLink TO gus_w;
-GRANT select ON ApiDB.IsolateVocabularyLink TO gus_r;
-
-
-
 
 
 exit;
