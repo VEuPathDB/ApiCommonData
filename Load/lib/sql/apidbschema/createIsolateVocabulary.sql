59c7c3c525cd96ca47b50d8ed5db4d503ab70951
John Brestelli <jbrestell@gmail.com>
2009-08-11 14:47:10 +0000

    add linking table for isolate vocabulary
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@30756 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createIsolateVocabulary.sql
+++ Load/lib/sql/apidbschema/createIsolateVocabulary.sql
@@ -7,5 +7,20 @@ CREATE TABLE ApiDB.IsolateVocabulary (
 GRANT insert, select, update, delete ON ApiDB.IsolateVocabulary TO gus_w;
 GRANT select ON ApiDB.IsolateVocabulary TO gus_r;
 
+--------------------------------------------------------------------------------
+
+
+CREATE TABLE ApiDB.IsolateVocabularyLink (
+ old_term                        varchar(200) NOT NULL,
+ new_term                        varchar(200) NOT NULL,
+ type                            varchar(50) NOT NULL
+);
+
+GRANT insert, select, update, delete ON ApiDB.IsolateVocabularyLink TO gus_w;
+GRANT select ON ApiDB.IsolateVocabularyLink TO gus_r;
+
+
+
+
 
 exit;
