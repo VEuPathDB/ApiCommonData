6e10fbc8f82db2ffa7ffc3d5fc64981fc70b4f60
Wei Li <weili1@pennmedicine.upenn.edu>
2009-08-11 19:35:13 +0000

    script to simple execute an idSQL passed in
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@30774 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/executeIdSQL.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+
+use lib "$ENV{GUS_HOME}/lib/perl";
+
+use CBIL::Util::PropertySet;
+use Getopt::Long;
+use DBI;
+use DBD::Oracle;
+
+my ($verbose,$gusConfigFile,$idSQL);
+
+&GetOptions("verbose!"=> \$verbose,
+            'gus_config_file=s' => \$gusConfigFile,
+            "idSQL=s" => \$idSQL,
+            );
+
+die "usage: executeIdSQL.pl --idSQL 'sql query' --configFile gusConfigFile\n" unless $idSQL;
+
+#============================================
+
+$gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config" unless($gusConfigFile);
+
+my @properties = ();
+my $gusconfig = CBIL::Util::PropertySet->new($gusConfigFile, \@properties, 1);
+
+my $u = $gusconfig->{props}->{databaseLogin};
+my $pw = $gusconfig->{props}->{databasePassword};
+my $dsn = $gusconfig->{props}->{dbiDsn};
+
+my $dbh = DBI->connect($dsn, $u, $pw) or die DBI::errstr;
+$dbh->{RaiseError} = 1;
+$dbh->{AutoCommit} = 0;
+
+my $stmt = $dbh->prepareAndExecute($idSQL);
+$stmt->finish();
+$dbh->commit;
+print "Deleted rows from table.\n";
+
+$dbh->disconnect();
+
+1;
