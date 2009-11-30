4407de74f3559be34b8d3ed5dd04288c6952d38d
Wei Li <weili1@pennmedicine.upenn.edu>
2009-11-30 21:07:23 +0000

    add apidb.siRNA to apidbschema
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@32367 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/createsiRNA.sql
@@ -0,0 +1,19 @@
+CREATE TABLE apidb.siRNA (
+ gene                         VARCHAR2(50) NOT NULL,
+ go_id                        VARCHAR2(50),
+ go_term                      VARCHAR2(100),
+ pato_id                      VARCHAR2(50),
+ pato_term                    VARCHAR2(100),
+ rel_time                     VARCHAR2(100),
+ plo_id                       VARCHAR2(50),
+ plo_term                     VARCHAR2(100),
+ evid_id                      VARCHAR2(50),
+ evid_desc                    VARCHAR2(200),
+ species                      VARCHAR2(200),
+ db_xref                      VARCHAR2(100),
+ annotator                    VARCHAR2(200)
+);
+GRANT SELECT ON apidb.siRNA TO gus_r;
+GRANT SELECT ON apidb.siRNA TO gus_w;
+
+exit;
