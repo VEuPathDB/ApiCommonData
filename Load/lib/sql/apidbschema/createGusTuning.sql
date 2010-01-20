55268fca03057bcbd451b8713dc601ea594e1915
John Iodice <iodice@pennmedicine.upenn.edu>
2010-01-20 16:37:00 +0000

    creation of indexes on GUS tables is now handled by createGusTuning, so it can be offloaded from the tuning manager
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@32997 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGusTuning.sql
+++ Load/lib/sql/apidbschema/createGusTuning.sql
@@ -1,7 +1,67 @@
 -- indexes on GUS tables
 
-create index dots.AaSeq_source_ix on dots.AaSequenceImp (lower(source_id)) tablespace INDX;
-create index dots.AaSeq_2ary_ix on dots.AaSequenceImp (string1, aa_sequence_id) tablespace INDX;
+create index dots.AaSeq_source_ix
+  on dots.AaSequenceImp (lower(source_id)) tablespace INDX;
+
+create index dots.aaseq_subclassdbrel_ix
+  on dots.AaSequenceImp (external_database_release_id, subclass_view, aa_sequence_id)
+  tablespace INDX;
+
+create index dots.NaFeat_alleles_ix
+  on dots.NaFeatureImp (subclass_view, number4, number5, na_sequence_id, na_feature_id)
+  tablespace INDX;
+
+create index dots.AaSequenceImp_string2_ix
+  on dots.AaSequenceImp (string2, aa_sequence_id)
+  tablespace INDX;
+
+create index dots.AaSequenceImp_class_tax_ix
+  on dots.AaSequenceImp (subclass_view, taxon_id, aa_sequence_id)
+  tablespace INDX;
+
+create index dots.NaFeat_SubclassIds_ix
+  on dots.NaFeatureImp (subclass_view, source_id, na_feature_id, na_sequence_id)
+  tablespace INDX;
+
+create index dots.NaFeat_SubclassParent_ix
+  on dots.NaFeatureImp (subclass_view, parent_id, na_feature_id)
+  tablespace INDX;
+
+create index dots.loc_feat_ix
+  on dots.NaLocation (na_feature_id, start_min, end_max, is_reversed)
+  tablespace INDX;
+
+create index dots.sim_pval_ix
+  on dots.Similarity (query_id, pvalue_exp, pvalue_mant, subject_id)
+  tablespace INDX;
+
+create index dots.nasequenceimp_string1_seq_idx
+  on dots.NaSequenceImp (string1, external_database_release_id, na_sequence_id)
+  tablespace INDX;
+
+create index dots.nasequenceimp_string1_idx
+  on dots.NaSequenceImp (string1, na_sequence_id)
+  tablespace INDX;
+
+create index dots.aafeat_subclassparent_ix
+  on dots.AaFeatureImp (subclass_view, parent_id, aa_feature_id)
+  tablespace INDX;
+
+create index aafeature_subclasssource_ix
+  on dots.AaFeatureImp (subclass_view, source_id, aa_feature_id)
+  tablespace INDX; 
+
+create index AnalRes_subclassRow_ix
+  on rad.AnalysisResultImp (subclass_view, row_id)
+  tablespace INDX; 
+
+create index ExonOrder_ix
+  on dots.NaFeatureImp (subclass_view, parent_id, number3, na_feature_id)
+   tablespace INDX; 
+
+create index sres.so_term_ix
+  on sres.SequenceOntology (term_name, sequence_ontology_id)
+  tablespace INDX; 
 
 -- GUS table shortcomings
 
