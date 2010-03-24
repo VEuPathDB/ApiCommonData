8fa3bc3dc2e6512c504ac8e150b852fd9da98eb2
Steve Fischer <sfischer@upenn.edu>
2010-03-24 19:57:01 +0000

    drop constraint properly
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@34105 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropWorkflow.sql
+++ Load/lib/sql/apidbschema/dropWorkflow.sql
@@ -4,6 +4,8 @@ DROP SEQUENCE apidb.WorkflowStepDependency_sq;
 DROP TABLE apidb.WorkflowStepAlgInvocation;
 DROP SEQUENCE apidb.WorkflowStepAlgInvocation_sq;
 
+ALTER TABLE apidb.Workflow DROP CONSTRAINT workflow_fk1;
+
 DROP TABLE apidb.WorkflowStep;
 DROP SEQUENCE apidb.WorkflowStep_sq;
 
