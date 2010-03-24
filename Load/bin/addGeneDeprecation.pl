823e255ad2c7da9623a0f244cfccd1a30af916b9
Bindu Gajria <bindu.gajria@gmail.com>
2010-03-24 16:21:59 +0000

    script to populate ApiDB.GeneDeprecation on GiardiaDB database
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@34103 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/addGeneDeprecation.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Getopt::Long;
+use DBI;
+use DBD::Oracle;
+use CBIL::Util::PropertySet;
+
+my ($help, $gusConfig, $file, $act, $date);
+&GetOptions('help|h' => \$help,
+            'gus_config=s' => \$gusConfig,
+            'file=s' => \$file,
+	    'action=s' => \$act,
+	    'date=s' => \$date,
+            );
+
+unless(-e $file && $act && ($date eq 'deprecated' || $date eq 'undeprecated')) {
+  print STDERR "usage:  perl addGeneDeprecation.pl --file <INPUT_FILE> --action <deprecated|undeprecated> --date <18-November-2008> [--gus_config <FILE>]\n";
+  exit;
+}
+
+unless(-e $gusConfig) {
+  print STDERR "gus.config not found... using default\n";
+  $gusConfig = $ENV{GUS_HOME} ."/config/gus.config";
+}
+
+my @properties = ();
+my $gusconfig = CBIL::Util::PropertySet->new($gusConfig, \@properties, 1);
+
+my $u = $gusconfig->{props}->{databaseLogin};
+my $pw = $gusconfig->{props}->{databasePassword};
+my $dsn = $gusconfig->{props}->{dbiDsn};
+
+my $dbh = DBI->connect($dsn, $u, $pw) or die DBI::errstr;
+my @gene_id;
+my $count = 0;
+my $id;
+
+
+my $sql = "INSERT INTO ApiDB.GeneDeprecation (source_id, action, action_date) 
+    VALUES (?, '$act', (SELECT TO_DATE ( '$date', 'DD-MONTH-YYYY') from dual))";
+my $sh = $dbh->prepare($sql);
+
+
+open(FILE, $file) or die "Cannot open file $file for reading: $!";
+while(<FILE>) {
+  chomp;
+
+  $id = $_;
+  $sh->execute($id);
+  $count++;
+}
+
+print STDERR "Inserted $count rows\n";
+$sh->finish();
+close(FILE);
+$dbh->disconnect();
+
+
+## RUNS on giar-inc:
+## > perl  addGeneDeprecation.pl --file /files/cbil/data/cbil/giardiaDB/manualDelivery/deprecatedGenes/GeneDeprecationTable/deprecated_v11 --action deprecated --date 18-November-2008 --gus_config ~/gusApps/gus_home/config/gus.config
+## Inserted 4778 rows
+## > perl  addGeneDeprecation.pl --file /files/cbil/data/cbil/giardiaDB/manualDelivery/deprecatedGenes/GeneDeprecationTable/undeprecated_v20  --action deprecated --date 16-November-2009 --gus_config ~/gusApps/gus_home/config/gus.config
+## Inserted 1012 rows
