dc46c50e252d218d04a283d4748afa91fb98b2d3
Bindu Gajria <bindu.gajria@gmail.com>
2010-03-25 03:13:34 +0000

    add "reason" column
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@34116 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGeneDeprecation.sql
+++ Load/lib/sql/apidbschema/createGeneDeprecation.sql
@@ -1,7 +1,8 @@
 CREATE TABLE ApiDB.GeneDeprecation (
  source_id     VARCHAR2(30) NOT NULL,
- action        VARCHAR2(10) NOT NULL,
- action_date   DATE NOT NULL
+ action        VARCHAR2(30) NOT NULL,
+ action_date   DATE NOT NULL,
+ reason        VARCHAR2(300)
 );
 
 GRANT insert, select, update, delete ON ApiDB.GeneDeprecation TO gus_w;
