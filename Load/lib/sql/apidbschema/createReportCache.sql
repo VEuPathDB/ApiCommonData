cab090b4818874648a8908812225516ba8ad830f
John Iodice <iodice@pennmedicine.upenn.edu>
2010-04-28 16:28:00 +0000

    rename GeneTable to GeneGff add modification_date column new trigger to keep modification_date up to date drop disused wdkIsolateTable
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@34715 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createReportCache.sql
+++ Load/lib/sql/apidbschema/createReportCache.sql
@@ -86,44 +86,34 @@ GRANT insert, select, update, delete ON apidb.SequenceDetail TO gus_w;
 GRANT select ON apidb.SequenceDetail TO gus_r;
 
 ------------------------------------------------------------------------------
--- for now, include the old style as well.
+-- GeneGff (formerly GeneTable) holds data used for the GFF download
 
-CREATE TABLE apidb.GeneTable (
+CREATE TABLE apidb.GeneGff (
  wdk_table_id number(10) not null,
  source_id  VARCHAR2(50),
  project_id  VARCHAR2(50),
  table_name VARCHAR2(80),
  row_count  NUMBER(4),
  content    CLOB,
- primary key (wdk_table_id)
+ primary key (wdk_table_id),
+ modification_date date
 );
 
 CREATE INDEX apidb.gtab_ix
-       ON apidb.GeneTable (source_id, table_name, row_count);
+       ON apidb.GeneGff (source_id, table_name, row_count);
 CREATE INDEX apidb.gtab_name_ix
-       ON apidb.GeneTable (table_name, source_id, row_count);
+       ON apidb.GeneGff (table_name, source_id, row_count);
 
-GRANT insert, select, update, delete ON ApiDB.GeneTable TO gus_w;
-GRANT select ON ApiDB.GeneTable TO gus_r;
+GRANT insert, select, update, delete ON ApiDB.GeneGff TO gus_w;
+GRANT select ON ApiDB.GeneGff TO gus_r;
 
-------------------------------------------------------------------------------
-CREATE TABLE apidb.wdkIsolateTable (
- wdk_table_id number(10) not null,
- source_id  VARCHAR2(50),
- project_id  VARCHAR2(50),
- table_name VARCHAR2(80),
- row_count  NUMBER(4),
- content    CLOB,
- primary key (wdk_table_id)
-);
-
-CREATE INDEX apidb.itab_ix
-       ON apidb.WdkIsolateTable (source_id, table_name, row_count);
-CREATE INDEX apidb.itab_name_ix
-       ON apidb.WdkIsolateTable (table_name, source_id, row_count);
-
-GRANT insert, select, update, delete ON ApiDB.WdkIsolateTable TO gus_w;
-GRANT select ON ApiDB.WdkIsolateTable TO gus_r;
+CREATE OR REPLACE TRIGGER apidb.GeneGff_md_tg
+before update or insert on apidb.GeneGff
+for each row
+begin
+  :new.modification_date := sysdate;
+end;
+/
 
 ------------------------------------------------------------------------------
 CREATE SEQUENCE apidb.wdkTable_pkseq;
