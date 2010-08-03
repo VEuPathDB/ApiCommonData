de76d3f99b187ebca61df21c207af567f9b59517
Vishal Nayak <vishalkn@_.com>
2010-08-03 15:20:58 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@36253 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/LoadArrayElementFeature.pm
+++ Load/plugin/perl/LoadArrayElementFeature.pm
@@ -191,7 +191,7 @@ sub processFile {
     next if ($processedLines >= $row);
 
 
-    my $orient = $feature->strand eq '-' ? 1 : 0;
+    my $orient = $feature->strand;
 
     my $naSeqId = $naSequenceIds->{$feature->seq_id};
     $self->error("No NaSequenceId found for ".$feature->seq_id) unless($naSeqId);
