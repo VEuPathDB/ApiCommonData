5d3e922ce4d5c2f327f9e3956114c777cffd467a
John Iodice <iodice@pennmedicine.upenn.edu>
2010-08-18 04:14:18 +0000

    some results are large
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@36459 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/increaseColumnWidth.sql
+++ Load/lib/sql/apidbschema/increaseColumnWidth.sql
@@ -1,4 +1,4 @@
-alter table Core.AlgorithmInvocation MODIFY result VARCHAR2(350);
+alter table Core.AlgorithmInvocation MODIFY result VARCHAR2(4000);
 
 alter table Core.AlgorithmParam MODIFY string_value VARCHAR2(3000);
 
