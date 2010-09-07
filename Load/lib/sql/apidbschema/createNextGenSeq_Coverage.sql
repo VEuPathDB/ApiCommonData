cdba97cf56fe9209602ad7d8a51aaeb93a3c08e1
John Iodice <iodice@pennmedicine.upenn.edu>
2010-09-07 18:41:07 +0000

    remove underscore to conform to table naming convention
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@36728 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createNextGenSeq_Coverage.sql
+++ Load/lib/sql/apidbschema/createNextGenSeq_Coverage.sql
@@ -1,5 +1,5 @@
-create table Apidb.NextGenSeq_Coverage (
-nextGenSeq_coverage_id NUMBER(10),
+create table Apidb.NextGenSeqCoverage (
+next_gen_seq_coverage_id NUMBER(10),
 external_database_release_id number(10) not null,
 sample varchar(30) not null,
 na_sequence_id number(10) not null,
@@ -18,18 +18,18 @@ multiple number(1),
  ROW_GROUP_ID          NUMBER(3),
  ROW_PROJECT_ID        NUMBER(4),
 ROW_ALG_INVOCATION_ID NUMBER(12) NOT NULL,
- PRIMARY KEY (nextGenSeq_coverage_id)
+ PRIMARY KEY (next_gen_seq_coverage_id)
 );
 
 
-CREATE SEQUENCE ApiDB.NextGenSeq_Coverage_sq;
+CREATE SEQUENCE ApiDB.NextGenSeqCoverage_sq;
 
-grant insert, select, update, delete on Apidb.NextGenSeq_Coverage to gus_w;
-grant select on Apidb.NextGenSeq_Coverage to gus_r;
-GRANT select ON ApiDB.NextGenSeq_Coverage_sq TO gus_w;
+grant insert, select, update, delete on Apidb.NextGenSeqCoverage to gus_w;
+grant select on Apidb.NextGenSeqCoverage to gus_r;
+GRANT select ON ApiDB.NextGenSeqCoverage_sq TO gus_w;
 
-create index apidb.nextgenseq_cov_indx
-on Apidb.NextGenSeq_Coverage (external_database_release_id,sample,na_sequence_id,mapping_start,mapping_end,coverage,multiple)
+create index apidb.nextgenseqcov_indx
+on Apidb.NextGenSeqCoverage (external_database_release_id,sample,na_sequence_id,mapping_start,mapping_end,coverage,multiple)
 tablespace indx;
 
 INSERT INTO core.TableInfo
@@ -38,14 +38,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write, 
      other_read, other_write, row_user_id, row_group_id, row_project_id, 
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'NextGenSeq_Coverage',
-       'Standard', 'nextGenSeq_coverage_id ',
+SELECT core.tableinfo_sq.nextval, 'NextGenSeqCoverage',
+       'Standard', 'next_gen_seq_coverage_id ',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'NextGenSeq_Coverage' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'NextGenSeqCoverage' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 exit;
