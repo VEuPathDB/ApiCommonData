e698be706efb61133a8b2decbd40d069664bf2d7
Wei Li <weili1@pennmedicine.upenn.edu>
2010-10-05 15:25:40 +0000

    drop multi-users to remove gus/apidb schema from a new instance
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@37132 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropMultiDbUsers.sql
@@ -0,0 +1,18 @@
+drop user APIDB cascade;
+drop user CORE cascade;
+drop user COREVER cascade;
+drop user DOTS cascade;
+drop user DOTSVER cascade;
+drop user PROT cascade;
+drop user PROTVER cascade;
+drop user RAD cascade;
+drop user RADVER cascade;
+drop user SRES cascade;
+drop user SRESVER cascade;
+drop user STUDY cascade;
+drop user STUDYVER cascade;
+drop user TESS cascade;
+drop user TESSVER cascade;
+
+
+exit
