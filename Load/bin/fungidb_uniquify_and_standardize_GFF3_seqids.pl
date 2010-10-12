3c4660b54579f741af5af04737b32b03ff8c5b5f
Todd Harris <tharris@_.com>
2010-10-12 15:42:49 +0000

    expanded to handle other annotations; fasta IDs, etc
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/fungiDB-1.0@37252 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/fungidb_uniquify_and_standardize_GFF3_seqids.pl
+++ Load/bin/fungidb_uniquify_and_standardize_GFF3_seqids.pl
@@ -17,6 +17,8 @@ Uniquify chromosomes/scaffolds by appending a short prefix
 corresponding to the Genus species + strain of the organism
 to the original IDs.
 
+Can process fasta files, GFF files, or GFF with embedded fasta.
+
 eg, for Saccharomyces:
   Chr1 -> Scer_s288c:Chr1
 
@@ -35,7 +37,7 @@ More generally:
   supercont_1   -> supercontig1
   scaffold_1    -> scaffold1
 
-Usage: $0 --input [gff_file] --output [gff_file] --prefix [prefix]"
+Usage: $0 --input [gff_file] --output [gff_file] --prefix [prefix]
 END
 ;
 }
@@ -45,44 +47,89 @@ $out->open("> $output") or die "Couldn't open the output file: $output $!";
 
 my $in = new IO::File;
 if ($in->open($input)) {
-    while (<$in>) {
-        next if $_ =~ /^#/;
+    
+    my $fasta_seen;
 
-	my ($id,$source,$type,$start,$end,$score,$strand,$phase,$attributes) = split("\t");
-	
-        # Save the original id for later post-processing.
-        my $original_id = $id;
-
-	# Turn Chromosome into Chr
-	$id =~ s/Chromosome/Chr/;
-
-        # Supercontig/contig all lowercase
-        $id = lc($id) if $id =~ /contig/i;
-
-        # Chr are ucfirst
-        $id = ucfirst($id) if $id =~ /chr/;
-
-        # make sure supercontig is spelled out
-        $id =~ s/supercont([_|\d])/supercontig$1/;
-       
-        # Scaffolds becomes SC; a stajichism.
-        $id =~ s/scaffold/SC/;
-	
-	# Strip underscores
-	$id =~ s/_//;
-
-        # Finally, insert an underscore between the feature
-        # type and the feature ID.
-        $id =~ s/([Chr|contig|supercontig]*)(.*)/$1_$2/;
-	
-	# Append the prefix
-        my $new_id = "$prefix:$id";
-
-        $attributes =~ s/$original_id/$new_id/g;
-        
-        # Replace the old ref with the new universally.
-        print $out join("\t",$new_id,$source,$type,$start,$end,$score,$strand,$phase,$attributes);
-  }
+    while (<$in>) {              
+        # Comments
+        if ($_ =~ /^\#/) {
+	    print $out $_ if $_ =~ /^\#\#/;  # Need to retain some comments for splitting fasta
+            next;
+	    
+        # FASTA might be embedded
+        } elsif ($fasta_seen || $_ =~ /^>/) {
+	    
+	    $fasta_seen++;  # We've seen fasta. Start dumping.
+	    my ($id) = $_ =~ />(.*)/;
+	    if ($id) {
+		chomp $id;		
+		my $new_id = transmogrify($id);
+		print $out ">$new_id\n";
+	    } else {
+		# Just dump the fasta
+		print $out $_;
+	    }
+	    
+	# Plain 'ol annotations.
+	} else {
+	    process_annotations($_);
+	}
+    }
 }
 $in->close;
 $out->close;
+
+
+
+sub transmogrify {
+    my $id = shift;
+    
+    # Turn Chromosome into Chr
+    $id =~ s/Chromosome/Chr/;
+    
+    # Supercontig/contig all lowercase
+    $id = lc($id) if $id =~ /contig/i;
+    
+    # Chr are ucfirst
+    $id = ucfirst($id) if $id =~ /chr/;
+    
+    # make sure supercontig is spelled out
+    $id =~ s/supercont([_|\d])/supercontig$1/;
+    
+    # Scaffolds becomes SC; a stajichism.
+    $id =~ s/scaffold/SC/;
+    
+    # Strip underscores
+    $id =~ s/_//;
+    
+    # Insert an underscore between the feature
+    # type and the feature ID unless this is 
+    # something weird like 2-micron. There may be
+    # other per-organism cases to deal with later.
+
+    unless ($id eq '2-micron') {	
+	$id =~ s/([Chr|contig|supercontig]*)(.*)/$1_$2/;
+    }
+
+    # Append the prefix
+    my $new_id = "$prefix:$id";
+    return $new_id;
+}
+
+
+
+sub process_annotations {
+    my $line = shift;
+    
+    my ($id,$source,$type,$start,$end,$score,$strand,$phase,$attributes) = split("\t",$line);
+    
+    # Save the original id for later post-processing.
+    my $original_id = $id;
+    
+    my $new_id = transmogrify($id);
+    
+    # Fix attributes to match the new id
+    $attributes =~ s/$original_id/$new_id/g;
+    
+    print $out join("\t",$new_id,$source,$type,$start,$end,$score,$strand,$phase,$attributes);
+}
