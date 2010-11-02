7b0ac30da86fec3a0ed7e44b81721ccc7c550bc1
Deborah Pinney <pinney@upenn.edu>
2010-11-02 17:53:51 +0000

    add die if script does not have all the values for a line in the output file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@37533 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/generateCoveragePlotInputFile.pl
+++ Load/bin/generateCoveragePlotInputFile.pl
@@ -57,6 +57,8 @@ $dbh->disconnect();
     chomp;
     my ($source_id,$start,$end,$coverage) = split("\t",$_);
 
+    die "Can't reformat coverage plot file, values missing:\n $RNASeqExtDbRlsId\t$sample\t$naSeqHash{$source_id}\t$start\t$end\t$coverage\t$multipleVal" unless ($source_id && $start && $end &&$coverage && $multipleVal);
+
     if($naSeqHash{$source_id}){
 	print "$RNASeqExtDbRlsId\t$sample\t$naSeqHash{$source_id}\t$start\t$end\t$coverage\t$multipleVal\t\n";
     }
@@ -70,7 +72,7 @@ sub getExtDbRlsId {
     my $sth = $dbh->prepareAndExecute($sql);
     my $extDbRlsId = $sth->fetchrow_array();
     die "Can't retrieve an ext db rls id with $extDbSpecs" unless $extDbRlsId;
-      
+
     $sth->finish();
     return $extDbRlsId;
 }
