3f066d6c089d4cac5834b61408356a25e5aa697c
Wei Li <weili1@pennmedicine.upenn.edu>
2011-02-25 14:39:14 +0000

    debug,insert a row in core.TableInfo and create sequence
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@39205 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGFF3Table.sql
+++ Load/lib/sql/apidbschema/createGFF3Table.sql
@@ -1,6 +1,6 @@
 GRANT references ON DoTS.NaSequenceImp TO ApiDB;
 GRANT references ON SRes.ExternalDatabaseRelease TO ApiDB;
-
+GRANT references ON Sres.SequenceOntology TO ApiDB;
 ------------------------------------------------------------------------------
 
 CREATE TABLE ApiDB.GFF3 (
@@ -8,8 +8,8 @@ CREATE TABLE ApiDB.GFF3 (
  na_sequence_id        NUMBER(10),  
  source                VARCHAR2(20),  
  sequence_ontology_id  NUMBER(10),  
- start                 NUMBER(8),
- end                   NUMBER(8),
+ mapping_start                 NUMBER(8),
+ mapping_end                   NUMBER(8),
  score                 FLOAT,
  is_reversed           NUMBER(3),
  phase                 NUMBER(3),
@@ -33,4 +33,27 @@ CREATE TABLE ApiDB.GFF3 (
 );
 
 CREATE INDEX apidb.gff3_feature_id_idx
-ON apidb.GFF3 (gff3_feature_id);
+ON ApiDB.GFF3 (gff3_feature_id);
+
+CREATE SEQUENCE apidb.GFF3_sq;
+
+GRANT SELECT ON apidb.GFF3_sq TO gus_r;
+GRANT SELECT ON apidb.GFF3_sq TO gus_w;
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'GFF3',
+       'Standard', 'gff3_feature_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
+WHERE 'gff3' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
+
+exit;
--- Load/lib/sql/apidbschema/dropGFF3Table.sql
+++ Load/lib/sql/apidbschema/dropGFF3Table.sql
@@ -1,5 +1,10 @@
 drop table ApiDB.GFF3;
-drop sequence ApiDB.gff3_feature_id_idx;
-delete from Core.TableInfo where name = 'GFF3';
+drop sequence apidb.GFF3_sq;
+
+DELETE FROM core.TableInfo
+WHERE lower(name) = lower('GFF3')
+  AND database_id = (SELECT database_id
+                     FROM core.DatabaseInfo 
+                     WHERE lower(name) = 'apidb');
 
 exit;
