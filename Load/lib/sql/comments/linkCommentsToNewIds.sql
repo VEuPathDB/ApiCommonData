b627b96807753fa92a618cb7e42677af3ac1c3fe
John Iodice <iodice@pennmedicine.upenn.edu>
2011-03-16 14:50:24 +0000

    add query to allow the vetting of newly-relinked queries
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@39670 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/linkCommentsToNewIds.sql
+++ Load/lib/sql/comments/linkCommentsToNewIds.sql
@@ -6,6 +6,13 @@
 -- comments2.CommentStableId, so they are linked. This allows comments to come
 -- along when gene IDs change.
 
+promt inserting records to link changed gene IDs to existing comments
+prompt
+
+prompt checking CommentStableId primary key before insert
+select comments2.CommentStableId_pkseq.nextval@apidb.login_comment "comment_stable_id BEFORE insert"
+from dual;
+
 insert into comments2.CommentStableId@apidb.login_comment
             (stable_id, comment_id, comment_stable_id) -- shouldn't that be "comment_stable_id_id"?
 select gene, comment_id, comments2.CommentStableId_pkseq.nextval@apidb.login_comment
@@ -24,3 +31,11 @@ from (  select gi.gene, comment_id
         (  select stable_id, comment_id from comments2.comments@apidb.login_comment
          union
            select stable_id, comment_id from comments2.commentStableId@apidb.login_comment));
+
+prompt to examine linked comments, run this query in the apicomm instance, substituting the comment_stable_id value from the BEFORE query above:
+select 'select csi.stable_id as "new gene ID", headline, comment_date, content' || chr(10) ||
+       'from comments2.comments c, comments2.commentStableId csi' || chr(10) ||
+       'where csi.comment_stable_id between <BEFORE comment_stable_id> and ' || comments2.CommentStableId_pkseq.nextval@apidb.login_comment || chr(10) ||
+       '  and csi.comment_id = c.comment_id' || chr(10) ||
+       'order by c.comment_id;' as "test query"
+from dual;
