3c00a63c91b86fdf92abee6a00e64f3dacae3caa
John Iodice <iodice@pennmedicine.upenn.edu>
2011-03-23 03:14:47 +0000

    initial checkin
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@39783 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/updateTaxonNames.sql
@@ -0,0 +1,25 @@
+update sres.TaxonName
+set name_class = 'scientific name'
+where name = 'Aspergillus nidulans';
+
+update sres.TaxonName
+set name_class = 'genbank anamorph'
+where name = 'Emericella nidulans';
+
+update sres.TaxonName
+set name_class = 'scientific name'
+where name = 'Fusarium graminearum';
+
+update sres.TaxonName
+set name_class = 'genbank anamorph'
+where name = 'Gibberella zeae';
+
+update sres.TaxonName
+set name_class = 'scientific name'
+where name = 'Cryptococcus neoformans';
+
+update sres.TaxonName
+set name_class = 'genbank anamorph'
+where name = 'Filobasidiella neoformans';
+
+exit
