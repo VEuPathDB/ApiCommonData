41c11248f9e19896fdf1c01c20c503d83d3488a1
Steve Fischer <sfischer@upenn.edu>
2011-05-11 19:42:26 +0000

    rename ApiDB.OrganismProject to ApiDB.Organism
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/portToxoToFlow@40695 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createOrganismProject.sql
+++ Load/lib/sql/apidbschema/createOrganismProject.sql
@@ -1,9 +1,15 @@
-CREATE TABLE apidb.OrganismProject (
- organism_project_id          NUMBER(12) NOT NULL,
- organism                VARCHAR2(100) NOT NULL,
- project                      VARCHAR2(20) NOT NULL,
+CREATE TABLE apidb.Organism (
+ organism_id                  NUMBER(12) NOT NULL,
+ taxon_id                     number(12) not null,
+ project_name                 VARCHAR2(20) NOT NULL,
+ abbrev                       VARCHAR2(20) NOT NULL,
+ abbrev_public                VARCHAR2(20) NOT NULL,
+ abbrev_for_filenames         VARCHAR2(20) NOT NULL,
+ abbrev_orthomcl              VARCHAR2(20) NOT NULL,
+ abbrev_strain                VARCHAR2(20) NOT NULL,
+ is_reference_genome          NUMBER(1) NOT NULL,
+ is_draft_genome              NUMBER(1) NOT NULL,
  modification_date            DATE NOT NULL,
- user_read                    NUMBER(1) NOT NULL,
  user_write                   NUMBER(1) NOT NULL,
  group_read                   NUMBER(1) NOT NULL,
  group_write                  NUMBER(1) NOT NULL,
@@ -15,26 +21,30 @@ CREATE TABLE apidb.OrganismProject (
  row_alg_invocation_id        NUMBER(12) NOT NULL
 );
 
-ALTER TABLE apidb.OrganismProject
-ADD CONSTRAINT organism_project_pk PRIMARY KEY (organism_project_id);
+ALTER TABLE apidb.Organism
+ADD CONSTRAINT organism_pk PRIMARY KEY (organism_id);
 
-ALTER TABLE apidb.OrganismProject
-ADD CONSTRAINT organism_project_uniq
-UNIQUE (organism, project);
+ALTER TABLE apidb.Organism
+ADD CONSTRAINT organism_uniq
+UNIQUE (taxon_id, project);
 
-GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.OrganismProject TO gus_w;
-GRANT SELECT ON apidb.OrganismProject TO gus_r;
+ALTER TABLE apidb.Organism
+ADD CONSTRAINT organism_fk1 FOREIGN KEY (taxon_id)
+REFERENCES sres.taxon (taxon_id);
 
-CREATE SEQUENCE apidb.OrganismProject_sq;
+GRANT INSERT, SELECT, UPDATE, DELETE ON apidb.Organism TO gus_w;
+GRANT SELECT ON apidb.Organism TO gus_r;
+
+CREATE SEQUENCE apidb.Organism_sq;
 
 INSERT INTO core.TableInfo
     (table_id, name, table_type, primary_key_column, database_id, is_versioned,
      is_view, view_on_table_id, superclass_table_id, is_updatable, 
      modification_date, user_read, user_write, group_read, group_write, 
-     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     other_read, other_write, row_user_id, row_group_id, row_id, 
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'OrganismProject',
-       'Standard', 'organism_project_id',
+SELECT core.tableinfo_sq.nextval, 'Organism',
+       'Standard', 'organism_id',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
