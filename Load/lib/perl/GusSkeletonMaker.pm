4b87103128a4f7b70d4955249e0b79de5557b63e
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2011-08-04 15:54:19 +0000

    added 'haplotype_block' to the list of allowed so terms. Haplotype Blocks are being stored as features for the Llinas-Ferdig eQTL dataset.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@41812 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/GusSkeletonMaker.pm
+++ Load/lib/perl/GusSkeletonMaker.pm
@@ -14,6 +14,7 @@ use GUS::Model::DoTS::GeneInstance;
 
 my $soTerms = { 'coding_gene'=>'protein_coding',
 		'repeated_gene'=>'repeat_region',
+		'haplotype_block'=>'haplotype_block',
 		'tRNA_gene'=> 'tRNA_encoding',
 		'rRNA_gene'=> 'rRNA_encoding',
                 'pseudo_gene'=>'pseudogene',                 
@@ -122,7 +123,7 @@ sub makeGusGene {
   my $type = $bioperlGene->primary_tag();
 
   $plugin->error("Trying to make gus skeleton from a tree rooted with an unexpected type: '$type'") 
-     unless (grep {$type eq $_} ("coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene","scRNA_gene"));
+     unless (grep {$type eq $_} ("haplotype_block","coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene","scRNA_gene"));
 
   my $gusGene = $plugin->makeSkeletalGusFeature($bioperlGene, $genomicSeqId,
 						$dbRlsId, 
