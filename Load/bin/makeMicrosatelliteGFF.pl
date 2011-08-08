a8d1e8dc31289d942933852ee1649b870394d221
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2011-08-08 16:23:18 +0000

    modified the microsatellite gff script to: print name of the microsatellites in the attributes tag. print the fwd and rvrse primer fasta file in the same dir instead if special dir foe BLAST. added some remarks about the command etc.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@41871 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeMicrosatelliteGFF.pl
+++ Load/bin/makeMicrosatelliteGFF.pl
@@ -4,7 +4,7 @@ use strict;
 use IO::File; 
 use Getopt::Long;
 
-my ($help, $stsFile, $genomeFasta, $mapFile);
+my ($help, $stsFile, $genomeFasta, $mapFile, $outputGff);
 
 &GetOptions('help|h' => \$help,
             'stsFile=s' => \$stsFile,
@@ -14,13 +14,15 @@ my ($help, $stsFile, $genomeFasta, $mapFile);
             );
 
 unless(-e $stsFile && $mapFile && $genomeFasta) {
-  print STDERR "usage:  perl makeMicrosatelliteGFF.pl --stsFile <Microsatellite sts file>  --genomeFasta <xdformatted fasta file of Genome> --mapFile  <Microsatellites-Centimorgan mapping file> \n";
+  print STDERR "usage:  perl      makeMicrosatelliteGFF.pl      --stsFile <Microsatellite sts file>       --genomeFasta <xdformatted fasta file of Genome>     --mapFile  <Microsatellites-Centimorgan mapping file>      --outputGff  Output file to print the GFF features to\n";
   exit;
 }
 
+
+
 open(sts,$stsFile );
-open(fwdFasta,">BLAST_FILES/forwardPrimers.fasta");
-open(rvsFasta,">BLAST_FILES/reversePrimers.fasta");
+open(fwdFasta,">forwardPrimers.fasta");
+open(rvsFasta,">reversePrimers.fasta");
 
 while (<sts>) {
   chomp;
@@ -31,9 +33,10 @@ while (<sts>) {
 }
 
 
-my $cmd1 = `blastn $genomeFasta  BLAST_FILES/forwardPrimers.fasta -sort_by_pvalue -mformat=2 E=1000 W=7 M=1 N=-3 Q=5 R=2 B=14 hspmax=1 warnings >forwardHits.txt`;
+#The command is optimized with parameters to search for similarity for very short sequences. Additionally B=14 has been set because Pfalciparum has 14 chromosomes and along with hspmax=1, only one best hit per chromosome will be reported. This is then correlated with the centimorgan-microsatellite map provided to determine which chromosome is the correct match.
+my $cmd1 = `blastn $genomeFasta  forwardPrimers.fasta -sort_by_pvalue -mformat=2 E=1000 W=7 M=1 N=-3 Q=5 R=2 B=14 hspmax=1 warnings >forwardHits.txt`;
 
-my $cmd2 = `blastn $genomeFasta  BLAST_FILES/reversePrimers.fasta -sort_by_pvalue -mformat=2 E=1000 W=7 M=1 N=-3 Q=5 R=2 B=14 hspmax=1 warnings  >reverseHits.txt`;
+my $cmd2 = `blastn $genomeFasta reversePrimers.fasta -sort_by_pvalue -mformat=2 E=1000 W=7 M=1 N=-3 Q=5 R=2 B=14 hspmax=1 warnings  >reverseHits.txt`;
 
 
 eval $cmd1;
@@ -41,7 +44,7 @@ eval $cmd2;
 
 
 
-my (%IdChrMap,%fwdCood,%revCood,%strand,%cMorgan);
+my (%IdChrMap,%fwdCood,%revCood,%strand,%cMorgan,%stsName);
 
 open (mapFile,$mapFile);
 
@@ -50,10 +53,12 @@ while (<mapFile>) {
    my @elements = split(/\t/,$_);
    chomp(@elements);
 
-   next unless ($elements[1] =~ /G/);
-   next unless ($elements[3] ne ".");
-   $cMorgan{$elements[1]} = $elements[3];
+   next unless (($elements[1] =~ /G/) || ($elements[3] ne "."));
 
+   $cMorgan{$elements[1]} = $elements[3];
+   $stsName{$elements[1]} = $elements[0];
+
+#map chromosome number to chromosome Id in the DB
   if ($elements[4] < 10) {
         $IdChrMap{$elements[1]} = "Pf3D7_0".$elements[4];
      } else {
@@ -75,7 +80,7 @@ while (<fwdHits>) {
   my $strand = $hitElements[16];
 
   next unless (exists $IdChrMap{$Id});
-  next unless $chromId =~ m/$IdChrMap{$Id}/;
+  next unless $chromId =~ m/$IdChrMap{$Id}/; #proceed only if the chromosome reported as hit is the same as provided in the Map.
 
   if ($strand eq "+1") {
     $fwdCood{$chromId}{$Id} = $hitElements[20];
@@ -115,7 +120,7 @@ open (gffFile, ">$outputGff");
 foreach my $chromosome (sort keys %fwdCood){
   foreach my $Id (sort {$fwdCood{$chromosome}{$a} <=> $fwdCood{$chromosome}{$b} } %{ $fwdCood{$chromosome} }) {
     next unless (exists $revCood{$chromosome}{$Id});
-    print  gffFile "$chromosome\tSu\tmicrosatellite\t$fwdCood{$chromosome}{$Id}\t$revCood{$chromosome}{$Id}\t.\t$strand{$chromosome}{$Id}\t.\tID $Id\; Name\n";
+    print  gffFile "$chromosome\tSu\tmicrosatellite\t$fwdCood{$chromosome}{$Id}\t$revCood{$chromosome}{$Id}\t.\t$strand{$chromosome}{$Id}\t.\tID $Id\; Name\t$stsName{$Id}\n";
   }
 }
 close(gffFile);
