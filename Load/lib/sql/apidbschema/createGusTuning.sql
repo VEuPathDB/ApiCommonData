d26c33666bb8aac8ac347d7d49e2ff25494f444a
John Iodice <iodice@pennmedicine.upenn.edu>
2011-08-12 16:18:34 +0000

    added SeqvarStrain_ix
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@41975 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGusTuning.sql
+++ Load/lib/sql/apidbschema/createGusTuning.sql
@@ -11,17 +11,21 @@ create index dots.AaSequenceImp_string2_ix
   on dots.AaSequenceImp (string2, aa_sequence_id)
   tablespace INDX;
 
-create index dots.nasequenceimp_string1_seq_idx
+create index dots.nasequenceimp_string1_seq_ix
   on dots.NaSequenceImp (string1, external_database_release_id, na_sequence_id)
   tablespace INDX;
 
-create index dots.nasequenceimp_string1_idx
+create index dots.nasequenceimp_string1_ix
   on dots.NaSequenceImp (string1, na_sequence_id)
   tablespace INDX;
 
 create index ExonOrder_ix
   on dots.NaFeatureImp (subclass_view, parent_id, number3, na_feature_id)
-   tablespace INDX; 
+  tablespace INDX; 
+
+create index SeqvarStrain_ix
+  on dots.NaFeatureImp (subclass_view, external_database_release_id, string9, na_feature_id)
+  tablespace INDX; 
 
 -- schema changes for GUS tables
 
