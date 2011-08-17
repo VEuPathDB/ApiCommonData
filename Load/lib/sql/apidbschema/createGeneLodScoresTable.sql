c14b5168a9b986f67d41bdefe296e749467353ef
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2011-08-17 16:28:18 +0000

    corrected typos in table name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@42056 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGeneLodScoresTable.sql
+++ Load/lib/sql/apidbschema/createGeneLodScoresTable.sql
@@ -3,7 +3,7 @@ GRANT references ON SRes.ExternalDatabaseRelease TO ApiDB;
 GRANT references ON DoTS.ChromosomeElementFeature TO ApiDB;
 ------------------------------------------------------------------------------
 
-CREATE TABLE ApiDB.GeneFeatureLodsScore (
+CREATE TABLE ApiDB.GeneFeatureLodScore (
  GENE_HAPBLOCK_SCORE_ID               NUMBER(10) not null,
  NA_FEATURE_ID                        NUMBER(10) not null,
  HAPLOTYPE_BLOCK_NAME                 VARCHAR2(50) not null,
@@ -16,14 +16,14 @@ CREATE TABLE ApiDB.GeneFeatureLodsScore (
  PRIMARY KEY (GENE_HAPBLOCK_SCORE_ID)
  );
 
-GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.GeneFeatureLodsScore TO gus_w;
-GRANT SELECT ON ApiDB.GeneFeatureLodsScore TO gus_r;
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.GeneFeatureLodScore TO gus_w;
+GRANT SELECT ON ApiDB.GeneFeatureLodScore TO gus_r;
 
 CREATE INDEX apidb.lodScore_idx
-ON ApiDB.GeneFeatureLodsScore (GENE_HAPBLOCK_SCORE_ID);
+ON ApiDB.GeneFeatureLodScore (GENE_HAPBLOCK_SCORE_ID);
 
 CREATE INDEX apidb.geneHapBlock_idx
-ON ApiDB.GeneFeatureLodsScore (NA_FEATURE_ID,HAPLOTYPE_BLOCK_NAME);
+ON ApiDB.GeneFeatureLodScore (NA_FEATURE_ID,HAPLOTYPE_BLOCK_NAME);
 
 INSERT INTO core.TableInfo
     (table_id, name, table_type, primary_key_column, database_id, is_versioned,
@@ -31,14 +31,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'GeneFeatureLodsScore',
+SELECT core.tableinfo_sq.nextval, 'GeneFeatureLodScore',
        'Standard', 'GENE_HAPBLOCK_SCORE_ID',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'GeneFeatureLodsScore' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'GeneFeatureLodScore' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 
--- Load/lib/sql/apidbschema/dropGeneLodScoresTable.sql
+++ Load/lib/sql/apidbschema/dropGeneLodScoresTable.sql
@@ -1,7 +1,7 @@
-drop table ApiDB.GeneFeatureLodsScore;
+drop table ApiDB.GeneFeatureLodScore;
 
 DELETE FROM core.TableInfo
-WHERE lower(name) = lower('GeneFeatureLodsScore')
+WHERE lower(name) = lower('GeneFeatureLodScore')
   AND database_id = (SELECT database_id
                      FROM core.DatabaseInfo
                      WHERE lower(name) = 'apidb');
