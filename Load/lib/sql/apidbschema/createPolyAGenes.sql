8884eabfa205edab0636fa2c54ca8e9ad19f3c45
Bindu Gajria <bindu.gajria@gmail.com>
2011-08-21 19:24:14 +0000

    refs #5022
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@42097 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createPolyAGenes.sql
+++ Load/lib/sql/apidbschema/createPolyAGenes.sql
@@ -40,7 +40,7 @@ INSERT INTO core.TableInfo
      other_read, other_write, row_user_id, row_group_id, row_project_id, 
      row_alg_invocation_id)
 SELECT core.tableinfo_sq.nextval, 'PolyAGenes',
-       'Standard', '',
+       'Standard', 'splice_site_feature_id',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
