9179992507ad83f6be0b6822521d063d57fdc06f
John Iodice <iodice@pennmedicine.upenn.edu>
2011-09-01 15:05:10 +0000

    ApidbTuning schema is strictly for tuning tables (i.e. those created by the tuning manager).
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@42257 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createIsolateSnpTable.sql
+++ Load/lib/sql/apidbschema/createIsolateSnpTable.sql
@@ -1,10 +1,10 @@
-drop table ApidbTuning.IsolateSNPs;
+drop table Apidb.IsolateSNPs;
 
-create table ApidbTuning.IsolateSNPs as
+create table Apidb.IsolateSNPs as
 select s.na_sequence_id as is_na_sequence_id,atr.source_id as is_source_id, s.name,s.isolate, 
 f.allele,snpf.source_id as snp_source_id,d.name as snp_db_name,snpf.na_sequence_id as snp_na_sequence_id,snpl.start_min as snp_start_min
 from dots.ISOLATEFEATURE f, dots.ISOLATESOURCE s, dots.NALOCATION il, dots.SNPFEATURE snpf, 
-dots.NALOCATION snpl,ApidbTuning.IsolateAttributes atr, sres.externaldatabaserelease rel, sres.externaldatabase d
+dots.NALOCATION snpl,Apidb.IsolateAttributes atr, sres.externaldatabaserelease rel, sres.externaldatabase d
 where s.na_feature_id = f.parent_id
 and s.na_sequence_id = atr.NA_SEQUENCE_ID
 and il.na_feature_id = f.na_feature_id
@@ -17,13 +17,13 @@ and d.name in ('PlasmoDB combined SNPs','Broad SNPs','Sanger falciparum SNPs','S
 
 commit;
 
-GRANT SELECT ON ApidbTuning.IsolateSNPs TO gus_r;
-GRANT SELECT ON ApidbTuning.IsolateSNPs TO gus_w;
+GRANT SELECT ON Apidb.IsolateSNPs TO gus_r;
+GRANT SELECT ON Apidb.IsolateSNPs TO gus_w;
 
-create index ApidbTuning.IsolateSNPs_is_sid_idx on ApidbTuning.IsolateSNPs (is_source_id);
-create index ApidbTuning.IsolateSNPs_snp_db_id_idx on ApidbTuning.IsolateSNPs (snp_db_name,snp_source_id,allele);
-create index ApidbTuning.IsolateSNPs_snp_id_idx on ApidbTuning.IsolateSNPs (snp_source_id);
-create index ApidbTuning.IsolateSNPs_snp_seq_start_idx on ApidbTuning.IsolateSNPs (snp_na_sequence_id,snp_start_min);
+create index Apidb.IsolateSNPs_is_sid_idx on Apidb.IsolateSNPs (is_source_id);
+create index Apidb.IsolateSNPs_snp_db_id_idx on Apidb.IsolateSNPs (snp_db_name,snp_source_id,allele);
+create index Apidb.IsolateSNPs_snp_id_idx on Apidb.IsolateSNPs (snp_source_id);
+create index Apidb.IsolateSNPs_snp_seq_start_idx on Apidb.IsolateSNPs (snp_na_sequence_id,snp_start_min);
 
 commit;
 
