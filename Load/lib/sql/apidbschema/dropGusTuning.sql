072762bbcc2a5e610ee756aa6f5800f8a995f543
Steve Fischer <sfischer@upenn.edu>
2011-09-28 22:06:38 +0000

    change the drop indexes to exactly those that are in the create script
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@42843 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropGusTuning.sql
+++ Load/lib/sql/apidbschema/dropGusTuning.sql
@@ -1,13 +1,9 @@
-DROP INDEX dots.aaseq_subclassdbrel_ix;
-DROP INDEX dots.NaFeat_alleles_ix;
-DROP INDEX dots.AaSequenceImp_string2_ix;
-DROP INDEX dots.AaSequenceImp_classtax_ix;
-DROP INDEX dots.NaFeat_SubclassIds_ix;
-DROP INDEX dots.NaFeat_SubclassParent_ix;
-DROP INDEX dots.loc_feat_ix;
-DROP INDEX dots.sim_pval_ix;
-DROP INDEX dots.nasequenceimp_string1_idx;
-DROP INDEX dots.aafeat_subclassparent_ix;
-DROP INDEX dots.aafeature_subclasssource_ix;
+drop index dots.AaSeq_source_ix
+drop index dots.NaFeat_alleles_ix
+drop index dots.AaSequenceImp_string2_ix
+drop index dots.nasequenceimp_string1_seq_ix
+drop index dots.nasequenceimp_string1_ix
+drop index ExonOrder_ix
+drop index SeqvarStrain_ix
 
 exit
