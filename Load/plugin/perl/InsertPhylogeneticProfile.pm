4999be5bf62a0011680e8bed403d9261e72ede4c
Steve Fischer <sfischer@upenn.edu>
2011-10-07 00:51:30 +0000

    fix missing single quotes in sql statement
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/portToxoToFlow@43005 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertPhylogeneticProfile.pm
+++ Load/plugin/perl/InsertPhylogeneticProfile.pm
@@ -103,7 +103,7 @@ sub run {
 
   if ($projectName) {
       my $sql = "select orthomclAbbrev from ApiDB.Organism
-                 where projectName = $projectName";
+                 where projectName = '$projectName'";
       my $sth = $self->prepareAndExecute($sql);
       while (my ($orthomclAbbrev) = $sth->fetchrow_array()) {
 	  push(@$taxaToLoad, $orthomclAbbrev);
