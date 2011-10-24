f7075aa2a31c94328d1b05ca44176504e4461611
Steve Fischer <sfischer@upenn.edu>
2011-10-24 17:55:33 +0000

    add indexes needed by orthomcl
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/portToxoToFlow@43297 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropGusTuning.sql
+++ Load/lib/sql/apidbschema/dropGusTuning.sql
@@ -5,6 +5,9 @@ drop index dots.nasequenceimp_string1_seq_ix;
 drop index dots.nasequenceimp_string1_ix;
 drop index dots.ExonOrder_ix;
 drop index dots.SeqvarStrain_ix;
+DROP INDEX dots.aasequenceimp_ind_desc;
+DROP INDEX sres.dbref_ind_id2;
+DROP INDEX sres.dbref_ind_rmk;
 
 alter table dots.sequencePiece DROP ( start_position, end_position );
 
