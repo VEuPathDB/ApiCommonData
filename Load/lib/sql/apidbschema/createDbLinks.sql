36e1e5991e76821375b22da7c253b7a93cb2e87b
Steve Fischer <sfischer@upenn.edu>
2011-10-24 17:51:08 +0000

    lose unneeded apidb.cbilprod link
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/portToxoToFlow@43290 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDbLinks.sql
+++ Load/lib/sql/apidbschema/createDbLinks.sql
@@ -1,8 +1,3 @@
-CREATE PUBLIC DATABASE LINK apidb.cbilprod
-CONNECT TO plasmodbwww
-IDENTIFIED BY po34weep
-USING 'apicomm';
-
 CREATE PUBLIC DATABASE LINK apidb.login_comment
 CONNECT TO plasmodbwww
 IDENTIFIED BY po34weep
