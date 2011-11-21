d37f708ea4f318a6210d49f770f3c8bf47a9907d
Mark Heiges <mheiges@gmail.com>
2011-11-21 17:40:28 +0000

    add additional dblinks
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@43987 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDbLinks.sql
+++ Load/lib/sql/apidbschema/createDbLinks.sql
@@ -1,6 +1,22 @@
 CREATE PUBLIC DATABASE LINK apidb.login_comment
 CONNECT TO plasmodbwww
 IDENTIFIED BY po34weep
-USING 'apicomm';
+USING 'apicommS';
+
+CREATE PUBLIC DATABASE LINK alt.login_comment
+CONNECT TO plasmodbwww
+IDENTIFIED BY po34weep
+USING 'apicommN';
+
+CREATE PUBLIC DATABASE LINK devS.login_comment
+CONNECT TO plasmodbwww
+IDENTIFIED BY po34weep
+USING 'apicommDevS';
+
+CREATE PUBLIC DATABASE LINK devN.login_comment
+CONNECT TO plasmodbwww
+IDENTIFIED BY po34weep
+USING 'apicommDevN';
+
 
 exit
