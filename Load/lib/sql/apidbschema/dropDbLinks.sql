be24379ef6bacc030be8b7c40b89847b99ee1077
Mark Heiges <mheiges@gmail.com>
2011-11-21 17:38:11 +0000

    add additional dblinks
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/portToxoToFlow2@43986 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropDbLinks.sql
+++ Load/lib/sql/apidbschema/dropDbLinks.sql
@@ -1,3 +1,7 @@
 DROP PUBLIC DATABASE LINK apidb.login_comment;
+DROP PUBLIC DATABASE LINK alt.login_comment;
+
+DROP PUBLIC DATABASE LINK devS.login_comment;
+DROP PUBLIC DATABASE LINK devN.login_comment;
 
 exit;
