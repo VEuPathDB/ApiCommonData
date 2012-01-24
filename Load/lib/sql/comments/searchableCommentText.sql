72ad1bee1554aa08126904d6f96ef0483d1dc062
John Iodice <iodice@pennmedicine.upenn.edu>
2012-01-24 18:53:01 +0000

    trigger changes to capture updates of comments, CommentReference, or CommentStableId (#3663)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@45285 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/searchableCommentText.sql
+++ Load/lib/sql/comments/searchableCommentText.sql
@@ -1,29 +1,66 @@
-create or replace function apidb.searchable_comment_text (p_comment_id number)
-return clob
+-- these integrity constraints are assumed:
+--
+-- unique IDs: userlogins3.users.user_id
+--             comments2.comments.comment_id
+--             comments2.CommentStableId.(comment_id, stable_id)
+--
+-- foreign keys: comments2.Comments.user_id (references userlogins3.users)
+--               comments2.CommentStableId.comment_id (references comments2.comments)
+--               comments2.CommentReference.comment_id (references comments2.comments)
+--
+-- here's the same thing in Oracle-speak:
+--
+-- alter table userlogins3.users
+-- add (constraint pk_users primary key (user_id) validate);
+-- 
+-- alter table comments2.comments
+-- add (constraint comment_pk primary key (comment_id) validate);
+-- 
+-- create unique index comments2.csi_pairs
+--    on comments2.CommentStableId (comment_id, stable_id);
+-- 
+-- alter table comments2.comments
+--    add (constraint user_fk foreign key (user_id)
+--         references userlogins3.users validate);
+-- 
+-- alter table comments2.CommentReference
+--    add (constraint cr_fk foreign key (comment_id)
+--         references comments2.comments);
+-- 
+-- alter table comments2.CommentStableId
+--    add (constraint csi_fk foreign key (comment_id)
+--         references comments2.comments);
+
+create or replace function apidb.author_list (p_comment_id number)
+return varchar2
 is
-    smooshed clob;
+    authors varchar2(4000);
 begin
-    select c.headline || '|' || c.content || '|' || 
-           u.first_name || ' ' || u.last_name || '(' || u.organization || ')' || authors.authors into smooshed
-    from comments2.comments c, userlogins3.users u,
-         (select comment_id, apidb.tab_to_string(set(CAST(COLLECT(source_id) AS apidb.varchartab)), ', ') as authors
-          from comments2.CommentReference
-          where database_name = 'author'
-          group by comment_id) authors
-    where c.user_id = u.user_id(+)
-      and c.comment_id = authors.comment_id(+)
-      and c.comment_id = p_comment_id;
-    return smooshed;
+    select apidb.tab_to_string(set(CAST(COLLECT(source_id) AS apidb.varchartab)), ', ')
+    into authors
+    from comments2.CommentReference
+    where database_name = 'author'
+      and comment_id = p_comment_id;
+
+    return authors;
 end;
 /
 
-grant execute on apidb.searchable_comment_text to public;
+grant execute on apidb.author_list to public;
 
 drop table apidb.TextSearchableComment;
-create table apidb.TextSearchableComment as
-select comment_id, stable_id as source_id, project_name as project_id, organism,
-       apidb.searchable_comment_text(comment_id) as content
-from comments2.comments;
+
+create table apidb.TextSearchableComment nologging as
+select c.comment_id, ci.stable_id as source_id, c.project_name as project_id, c.organism,
+       c.headline || '|' || c.content || '|' ||  u.first_name || ' '
+       || u.last_name || '(' || u.organization || ')' || apidb.author_list(c.comment_id) as content
+from comments2.comments c, userlogins3.users u,
+     (select comment_id, stable_id from comments2.comments
+       union
+       select comment_id, stable_id from comments2.commentStableId) ci
+where c.comment_target_id = 'gene'
+  and c.comment_id = ci.comment_id
+  and c.user_id = u.user_id(+);
 
 grant select,insert,update,delete on apidb.TextSearchableComment to public;
 
@@ -32,97 +69,135 @@ on apidb.TextSearchableComment(content)
 indextype is ctxsys.context
 parameters('DATASTORE CTXSYS.DEFAULT_DATASTORE SYNC (ON COMMIT)');
 
-create or replace procedure apidb.move_comments
-is
-begin
-  insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content)
-  select c.comment_id, ci.stable_id, c.project_name, c.organism,
-         apidb.searchable_comment_text(c.comment_id)
-  from comments2.comments c,
-       (select comment_id, stable_id from comments2.comments
-       union
-       select comment_id, stable_id from comments2.commentStableId) ci
-  where c.comment_target_id = 'gene'
-    and c.comment_id = ci.comment_id
-    and (c.comment_id, ci.stable_id) not in (select comment_id, source_id from apidb.TextSearchableComment);
-end;
-/
-
-grant execute on apidb.move_comments to public;
-
 create or replace trigger comments2.comments_insert
-after insert on comments2.comments
+  after insert on comments2.comments
+  for each row
+declare
+  userinfo varchar2(1000);
 begin
-  apidb.move_comments;
-end;
-/
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = :new.user_id;
 
-create or replace trigger comments2.csi_insert
-after insert on comments2.commentStableId
-begin
-  apidb.move_comments;
-end;
-/
-
--- this update trigger won't do the right thing if the comment_id itself is updated
-create or replace trigger comments2.comments_update
-after update on comments2.comments
-for each row
-begin
-  update apidb.TextSearchableComment
-  set content = apidb.searchable_comment_text(:new.comment_id)
-  where comment_id = :new.comment_id;
+  insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content)
+  values (:new.comment_id, :new.stable_id, :new.project_name, :new.organism,
+          :new.headline || '|' || :new.content || '|' || userinfo || author_list(:new.comment_id));
 end;
 /
 
 create or replace trigger comments2.comments_delete
-after delete on comments2.comments
+  after delete on comments2.comments
+  for each row
 begin
   delete from apidb.TextSearchableComment
-  where comment_id not in (select comment_id from comments2.comments);
+  where comment_id = :old.comment_id;
 end;
 /
 
-BEGIN
-  dbms_ddl.set_trigger_firing_property('comments2', 'comments_insert', FALSE);
-  dbms_ddl.set_trigger_firing_property('comments2', 'comments_update', FALSE);
-  dbms_ddl.set_trigger_firing_property('comments2', 'comments_delete', FALSE);
-END;
+create or replace trigger comments2.comments_update
+  after update on comments2.comments
+  for each row
+declare
+  userinfo varchar2(1000);
+  authorinfo varchar2(4000);
+begin
+  delete from apidb.TextSearchableComment
+  where comment_id = :old.comment_id;
+
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = :new.user_id;
+
+  select apidb.author_list(:new.comment_id)
+  into authorinfo
+  from dual;
+
+  insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content)
+  values (:new.comment_id, :new.stable_id, :new.project_name, :new.organism,
+          :new.headline || '|' || :new.content || '|' || userinfo);
+end;
 /
 
-BEGIN
-DBMS_SCHEDULER.DROP_JOB(job_name => 'apidb.comment_mover');
-END;
+create or replace trigger comments2.csi_insert
+  after insert on comments2.commentStableId
+  for each row
+declare
+  userinfo varchar2(1000);
+begin
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = (select user_id from comments2.comments where comment_id = :new.comment_id);
+
+  insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content)
+  select comment_id, :new.stable_id, project_name, organism,
+          headline || '|' || content || '|' || userinfo || apidb.author_list(comment_id)
+  from comments2.comments
+  where comment_id = :new.comment_id;
+end;
 /
 
-BEGIN
-DBMS_SCHEDULER.CREATE_JOB(
-job_name => 'apidb.comment_mover',
-job_type => 'PLSQL_BLOCK',
-job_action => 'begin apidb.move_comments(); end',
-start_date => sysdate+ 1/(24*60*12), -- five seconds
-repeat_interval => 'FREQ=DAILY'
-);
-END;
+create or replace trigger comments2.csi_delete
+  after delete on comments2.CommentStableId
+  for each row
+begin
+  delete from apidb.TextSearchableComment
+  where comment_id = :old.comment_id
+    and source_id = :old.stable_id;
+end;
 /
 
-BEGIN
-DBMS_SCHEDULER.DROP_JOB(
-  job_name => 'apidb.optimize_index'
-);
-END;
+create or replace trigger comments2.csi_update
+  after update on comments2.commentStableId
+  for each row
+declare
+  userinfo varchar2(1000);
+begin
+  delete from apidb.TextSearchableComment
+  where comment_id = :old.comment_id
+    and source_id = :old.stable_id;
+
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = (select user_id from comments2.comments where comment_id = :new.comment_id);
+
+  insert into apidb.TextSearchableComment (comment_id, source_id, project_id, organism, content)
+  select comment_id, :new.stable_id, project_name, organism,
+          headline || '|' || content || '|' || userinfo || apidb.author_list(comment_id)
+  from comments2.comments
+  where comment_id = :new.comment_id;
+end;
 /
 
-BEGIN
-DBMS_SCHEDULER.CREATE_JOB(
-job_name => 'optimize_index',
-job_type => 'PLSQL_BLOCK',
-job_action => 'begin CTX_DDL.OPTIMIZE_INDEX(''comments_text_ix'',''FULL''); apidb.move_comments; end',
-start_date => sysdate+ 1/(24*60*12), -- five seconds
-repeat_interval => 'FREQ=DAILY'
-);
+create or replace trigger comments2.author_trigger
+  after insert or update or delete on comments2.CommentReference
+  for each row
+declare
+  userinfo varchar2(1000);
+begin
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = (select user_id from comments2.comments where comment_id = :old.comment_id);
 
-DBMS_SCHEDULER.ENABLE('optimize_index');
-END;
+  update apidb.TextSearchableComment
+  set content = (select headline || '|' || content || '|' || userinfo || apidb.author_list(comment_id)
+                 from comments2.comments
+                 where comment_id = :old.comment_id)
+  where comment_id = :old.comment_id;
+
+  select first_name || ' ' || last_name || '(' || organization || ')'
+  into userinfo
+  from userlogins3.users
+  where user_id = (select user_id from comments2.comments where comment_id = :new.comment_id);
+
+  update apidb.TextSearchableComment
+  set content = (select headline || '|' || content || '|' || userinfo || apidb.author_list(comment_id)
+                 from comments2.comments
+                 where comment_id = :new.comment_id)
+  where comment_id = :new.comment_id;
+end;
 /
-
