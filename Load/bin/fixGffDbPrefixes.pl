2880afd4021d74f5d5a3c4125c431993bbebe564
Brian Brunk <brunkb@upenn.edu>
2012-01-24 21:20:00 +0000

    works now
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@45298 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/fixGffDbPrefixes.pl
+++ Load/bin/fixGffDbPrefixes.pl
@@ -1,48 +1,59 @@
 #!/usr/bin/perl
 
 # use in download directory for setting the GFF identifer prefixes = fasta prefixes
-# takes in directories on commandline ... will decend into each and fix the gff files
+# takes in organism directories on commandline ... will decend into each and fix the gff files
+# creates a .bak file of the original gff file
 
 use strict;
+use Getopt::Long;
+
+my $nb;
+
+&GetOptions("noBackup|n!" => \$nb
+            );
 
 
 foreach my $dir (@ARGV){
-  my $findCmd = "find $dir/fasta -name '*Genomic_*' -print | grep -v ^\.";
-  print STDERR "Processing $dir: $findCmd\n";
+  die "you must enter a directory (or list of directories) that contain fasta and gff subdirs\n" if scalar(@ARGV) == 0;
+  my $findCmd = "find $dir/fasta -name '*.fasta' -print";
+  print STDERR "Processing $dir\n";
   my @fasta = `$findCmd`;
   my %sub;
   foreach my $fasta (@fasta){
     chomp $fasta;
+    next unless $fasta =~ /(Annotated|Genomic)/;
     open(F,"$fasta");
     while(<F>){
-      if(/^\>(\w+)\|(\S+)/){
+      if(/^\>(\w+\|)(\S+)/){
         $sub{$2} = $1;
       }
     }
     close F;
   }
+  print STDERR "  found prefixes for ".scalar(keys%sub)." identifiers\n";
   ##now open the gff file ....
-  my $gffFind = "find $dir/gff -name '*.gff' -print | grep -v ^\.";
+  my $gffFind = "find $dir/gff -name '*.gff' -print";
+#  print STDERR "gffFindCmd: '$gffFind'\n";
   my @gffFiles = `$gffFind`;
   foreach my $gff (@gffFiles){
-    system("mv $gff $gff.bak") unless -e "$gff.bak";  ##don't move if already there
+    chomp $gff;
+    print STDERR "  Making substitutions in $gff\n";
+    my $mvCmd = "mv $gff $gff.bak";
+#    print STDERR "Move Cmd: '$mvCmd'\n";
+    system($mvCmd) unless -e "$gff.bak";  ##don't move if already there
     open(F,"$gff.bak");
     open(O,">$gff");
     while(<F>){
-      my @line = split("\t",$_);
-      if($line[0] =~ /apidb\|(\S+)/){
+      while($_ =~ m/apidb\|(.*?)(\;|\s)/g){
         my $id = $1;
-        if($sub{$id}){
-          $line[0] =~ s/apidb\|$id/$sub{$id}\|$id/g;
-        }else{
-          print STDERR "ERROR: unable to match id for $line[0]\n";
+        $_ =~ s/apidb\|$id/$sub{$id}$id/; ##note that this will strip the prefix if this is not an id in a fasta file.
       }
-      $line[1] =~ s/ApiDB/EuPathDB/;
-      $line[8] =~ s/apidb//g;
-      print O join("\t",@line);
+      $_ =~ s/ApiDB/EuPathDB/g;  ##get rid of old ApiDB references.
+      print O $_;
     }
     close F;
     close O;
+    unlink("$gff.bak") if $nb;
   }
 }
       
