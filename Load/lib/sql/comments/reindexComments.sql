4c7adc37d93e1ed4a7a6ad27b020c1299646b076
John Iodice <iodice@pennmedicine.upenn.edu>
2012-01-30 17:51:46 +0000

    stop using the moveComments() function, which is deprecated
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@45404 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/reindexComments.sql
+++ Load/lib/sql/comments/reindexComments.sql
@@ -1,18 +1,38 @@
 -- reindexComments.sql
 --
--- delete and reload all the records in apidb.TextSearchableComment. This may be
--- useful to capture updates to comment records. (Comments2.comments and 
--- comments2.CommentStableId have insert triggers to index new data, but not
--- update or delete triggers.)
+-- truncate and reload apidb.TextSearchableComment. This will ensure that it is
+-- in sync with the Comments / CommentStableId / CommentReference tables, which
+-- are the primary home of comments. (We can't simply point text search at those
+-- because Oracle Text wants to index and search a single column.)
+
+-- In the past, it was necessary to do this from time to time to bring
+-- TextSearchableComment back into sync because the triggers on the comments
+-- tables did not handle updates or deletes. We're installing triggers that do,
+-- but keeping this script around for the transition and as a fallback.
+
+-- Note that the expression SELECTed into TextSearchableComment must be kept in
+-- sync with those in the triggers, which live in searchableCommentText.sql
+
 
 select count(*) "starting" from apidb.TextSearchableComment;
 
-delete from apidb.TextSearchableComment;
+truncate table apidb.TextSearchableComment;
 
-select count(*) "after delete" from apidb.TextSearchableComment;
+select count(*) "after truncate" from apidb.TextSearchableComment;
 
-exec apidb.move_comments
+insert into apidb.TextSearchableComment
+            (comment_id, source_id, project_id, organism, content)
+select c.comment_id, ci.stable_id, c.project_name, c.organism,
+       c.headline || '|' || c.content || '|' ||  u.first_name || ' '
+       || u.last_name || '(' || u.organization || ')' || apidb.author_list(c.comment_id)
+from comments2.comments c, userlogins3.users u,
+     (select comment_id, stable_id from comments2.comments
+       union
+       select comment_id, stable_id from comments2.commentStableId) ci
+where c.comment_target_id = 'gene'
+  and c.comment_id = ci.comment_id
+  and c.user_id = u.user_id(+);
 
-select count(*) "after move_comments" from apidb.TextSearchableComment;
+select count(*) "after insert" from apidb.TextSearchableComment;
 
 exit
