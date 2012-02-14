ca299d8b9b707cafca57f4f75c197dda4e9ea6c2
John Iodice <iodice@pennmedicine.upenn.edu>
2012-02-14 02:55:57 +0000

    create table explicitly, not AS SELECT
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@45827 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/comments/createTextSearchableComment.sql
+++ Load/lib/sql/comments/createTextSearchableComment.sql
@@ -1,16 +1,12 @@
 drop table apidb.TextSearchableComment;
 
-create table apidb.TextSearchableComment nologging as
-select c.comment_id, ci.stable_id as source_id, c.project_name as project_id, c.organism,
-       c.headline || '|' || c.content || '|' ||  u.first_name || ' '
-       || u.last_name || '(' || u.organization || ')' || apidb.author_list(c.comment_id) as content
-from comments2.comments c, userlogins3.users u,
-     (select comment_id, stable_id from comments2.comments
-       union
-       select comment_id, stable_id from comments2.commentStableId) ci
-where c.comment_target_id = 'gene'
-  and c.comment_id = ci.comment_id
-  and c.user_id = u.user_id(+);
+create table apidb.TextSearchableComment (
+  comment_id number(10) not null,
+  source_id  varchar2(100),
+  project_id varchar2(40),
+  organism   varchar2(100),
+  content    clob
+);
 
 grant select,insert,update,delete on apidb.TextSearchableComment to public;
 
