890653736a651c3a72306dd82c7fcc04832ce0a5
Bindu Gajria <bindu.gajria@gmail.com>
2012-04-10 03:28:42 +0000

    tables for old (previous release) gene source_IDs and coding sequence OR annotation (go terms, ec nums and product) .
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@46834 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/createOldCodingSequence.sql
@@ -0,0 +1,42 @@
+CREATE TABLE apidb.OldCodingSequence (
+ old_coding_sequence_id      NUMBER(10),
+ coding_sequence             CLOB NOT NULL,
+ MODIFICATION_DATE           DATE,
+ USER_READ                   NUMBER(1),
+ USER_WRITE                  NUMBER(1),
+ GROUP_READ                  NUMBER(1),
+ GROUP_WRITE                 NUMBER(1),
+ OTHER_READ                  NUMBER(1),
+ OTHER_WRITE                 NUMBER(1),
+ ROW_USER_ID                 NUMBER(12),
+ ROW_GROUP_ID                NUMBER(3),
+ ROW_PROJECT_ID              NUMBER(4),
+ ROW_ALG_INVOCATION_ID       NUMBER(12),
+ PRIMARY KEY (old_coding_sequence_id)
+);
+
+CREATE SEQUENCE apidb.OldCodingSequence_sq;
+
+GRANT insert, select, update, delete ON apidb.OldCodingSequence TO gus_w;
+GRANT select ON apidb.OldCodingSequence TO gus_r;
+GRANT select ON apidb.OldCodingSequence_sq TO gus_w;
+
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'OldCodingSequence',
+       'Standard', 'old_coding_sequence_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
+WHERE 'OldCodingSequence' NOT IN (SELECT name FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
+
+------------------------------------------------------------------------------
+exit;
