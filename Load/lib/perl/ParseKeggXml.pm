c7dffe6986c6e054cb9886f2e390fc0aa6d38a8a
Ganesh Srinivasamoorthy <ganesh@uga.edu>
2012-06-07 16:48:15 +0000

    corrected package name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@48367 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/ParseKeggXml.pm
+++ Load/lib/perl/ParseKeggXml.pm
@@ -4,7 +4,7 @@
 # Package ParseKeggXml
 # =================================================
 
-package ErythronDBData::Network::ParseKeggXml;
+package ApiCommonData::Load::ParseKeggXml;
 
 # =================================================
 # Documentation
