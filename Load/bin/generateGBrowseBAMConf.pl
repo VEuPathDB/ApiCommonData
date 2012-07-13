852a3a9c9987c79222dff7754d2a04bde062a479
Haiming Wang <hwang@uga.edu>
2012-07-13 20:00:12 +0000

    generates bam conf files for gbrowse - use study and strain combination as database and track names. still need to work on the bam file location to automate the process.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@48795 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/generateGBrowseBAMConf.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/perl
+use strict;
+
+use lib "$ENV{GUS_HOME}/lib/perl";
+
+use Tie::IxHash;
+use GUS::Supported::GusConfig;
+use GUS::ObjRelP::DbiDatabase;
+
+my %hash;
+tie %hash, 'Tie::IxHash';
+my $gusConfigFile;
+
+open(BAM, '>bam.conf'); 
+open(BAMDB, '>bam_db.conf'); 
+
+my $usage = "generate_bam_conf.pl gus_config_file\n";
+
+my $gusConfigFile = shift or die $usage;
+
+my $gusconfig = GUS::Supported::GusConfig->new($gusConfigFile);
+
+my $db = GUS::ObjRelP::DbiDatabase->new($gusconfig->getDbiDsn(),
+                                        $gusconfig->getDatabaseLogin(),
+                                        $gusconfig->getDatabasePassword(),
+                                        0,0,1,
+                                        $gusconfig->getCoreSchemaName());
+
+my $dbh = $db->getQueryHandle(0);
+
+my $sql =<<EOL;
+SELECT distinct s.name, var.strain
+FROM   study.study s,
+       study.biomaterial b,
+       rad.studybiomaterial sb,
+       dots.seqvariation var
+WHERE  sb.study_id = s.study_id
+   AND sb.bio_material_id = b.bio_material_id
+   AND var.external_database_release_id = b.external_database_release_id
+ORDER BY s.name, var.strain
+EOL
+
+my $sth = $dbh->prepareAndExecute($sql);
+
+while(my ($study,$strain) = $sth->fetchrow_array()) {
+   push @{$hash{$study}}, $strain;
+}
+
+my $count = 0;
+while(my($study, $strains) = each %hash) {
+  if($count == 0) {
+    print BAMDB qq/category tables = 'Population Biology: $study' 'Coverage_Xyplot  Coverage_Density Alignment' '@$strains'\n/;
+  } else {
+    print BAMDB qq/   'Population Biology: $study' 'Coverage_Xyplot  Coverage_Density Alignment' '@$strains'\n/;
+  }
+  $count++;
+}
+
+print "\n";
+
+while(my($study, $strains) = each %hash) {
+
+  foreach my $strain(@$strains) {
+    print BAMDB <<EOL;
+[$study\_$strain:database]
+db_adaptor   = Bio::DB::Sam
+db_args      = -bam '/var/www/Common/apiSiteFilesMirror/webServices/PlasmoDB/release-9.1/bam/Sanger_HTS_Isolates/$strain/result.bam'
+
+EOL
+  }
+
+}
+
+while(my($study, $strains) = each %hash) {
+  my $category = "Population Biology: $study";
+
+  foreach my $strain(@$strains) {
+    print BAM <<EOL;
+[$study\_$strain\_CoverageXyplot]
+feature        = coverage
+database       = $study\_$strain
+glyph          = wiggle_xyplot
+height         = 50 
+fgcolor        = blue 
+bicolor_pivot  = 2  
+pos_color      = blue 
+neg_color      = red
+label          = 0  
+key            = $strain Coverage Xyplot
+category       = $category
+
+EOL
+  }
+
+  # print density conf
+  foreach my $strain(@$strains) {
+    print BAM <<EOL;
+[$study\_$strain\_CoverageDensity]
+feature        = coverage
+glyph          = wiggle_density
+database       = $study\_$strain
+height         = 30
+fgcolor        = blue
+bicolor_pivot  = 2
+pos_color      = blue
+neg_color      = red
+label          = 0
+key            = $strain Coverage Density
+category       = $category
+
+EOL
+  }
+
+  # print alignment conf
+  foreach my $strain(@$strains) {
+    print BAM <<EOL;
+[$study\_$strain\_Alignment]
+feature        = read_pair
+glyph          = segments
+database       = $study\_$strain
+draw_target    = 1
+show_mismatch  = 1
+mismatch_color = coral
+bgcolor        = cornflowerblue
+fgcolor        = black
+height         = 3
+label density  = 1
+bump           = fast
+connector      = dashed
+key            = $strain Alignment
+category       = $category
+
+[$study\_$strain\_Alignment:3000]
+hide           = 1
+
+EOL
+  }
+
+}
+
+
+## category|study|strain strain ...
+__DATA__
+Population Biology: D. High Throughput Sequencing (HTS) SNPs - Sanger Isolates|pfal3D7_Sanger_HTS_Isolates|7G8 CS2 GB4 T9_94 V1_S
+Population Biology: E. High Throughput Sequencing (HTS) SNPs - Broad Isolates|pvivSaI1_Broad_HTS_Isolates|Brazil_I India_VII Mauritania_I North_Korean
