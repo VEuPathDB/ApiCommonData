ab6acacdb7f2084d4669d2caac64669472e02303
Deborah Pinney <pinney@upenn.edu>
2012-07-17 14:39:02 +0000

    updated for trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/104@48820 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/IsolateVocabulary/InsertMappedValues.pm
+++ Load/lib/perl/IsolateVocabulary/InsertMappedValues.pm
@@ -7,6 +7,8 @@ use Data::Dumper;
 
 use ApiCommonData::Load::IsolateVocabulary::Utils;
 use ApiCommonData::Load::IsolateVocabulary::VocabularyTerm;
+
+use GUS::Model::ApiDB::VocabularyBiomaterial;
 use GUS::Model::ApiDB::IsolateMapping;
 
 sub getSqlTerms {$_[0]->{_sql_terms}}
@@ -15,13 +17,12 @@ sub setSqlTerms {$_[0]->{_sql_terms} = $_[1]}
 sub getXmlMapTerms {$_[0]->{_xml_map_terms}}
 sub setXmlMapTerms {$_[0]->{_xml_map_terms} = $_[1]}
 
-sub disconnect {$_[0]->{dbh}->disconnect()}
+sub setVocabulary {$_[0]->{_vocabulary} = $_[1]}
+sub getVocabulary {$_[0]->{_vocabulary}}
 
-sub getDbh {$_[0]->{dbh}}
-sub setDbh {$_[0]->{dbh} = $_[1]}
+sub getDbh {$_[0]->getPlugin()->getDbHandle() }
 
 sub getType {$_[0]->{_type}}
-
 sub setType {
   my ($self, $type) = @_;
 
@@ -36,19 +37,33 @@ sub getPlugin {$_[0]->{Plugin}}
 sub setPlugin {$_[0]->{Plugin} = $_[1]}
 
 sub new {
-  my ($class, $plugin, $dbh, $type, $xmlTerms, $sqlTerms) = @_;
+  my ($class, $plugin, $type, $xmlTerms, $sqlTerms, $vocabulary) = @_;
 
   my $args = {};
 
   my $self = bless $args, $class; 
 
-  $self->setDbh($dbh);
   $self->setType($type);
   $self->setXmlMapTerms($xmlTerms);
   $self->setSqlTerms($sqlTerms);
   $self->setPlugin($plugin);
+  $self->setVocabulary($vocabulary);
 
   return $self;
+}
+
+sub mapTableIdTypeLookup {
+  my ($self, $mapTable) = @_;
+
+  if($mapTable eq 'IsolateSource') {
+    return 'na_sequence_id';
+  }
+
+  if($mapTable eq 'OntologyEntry') {
+    return 'bio_material_id';
+  }
+
+  die "Map Table $mapTable must be either [IsolateSource or OntologyEntry]";
 }
 
 sub insert {
@@ -56,21 +71,21 @@ sub insert {
 
   my $dbh = $self->getDbh();
 
-  # create a data structure for all isolates in Dots Tables
-  #    @naSequenceIds = @{$hash{$table}->{$field}->{$original}}
-  my $dotsIsolatesNaSequences = $self->queryDotsIsolates();
+  # $isolateVocabularyId = $hash->{$type}->{$term}
+  my $isolateVocabularyIds =  $self->getVocabulary();
 
-  #    $isolateVocabularyId = $hash->{$type}->{$term}
-  my $isolateVocabularyIds =  ApiCommonData::Load::IsolateVocabulary::Utils::getAllOntologies($dbh);
+  # create a data structure for loaded isolates and their primary key
+  #    @identifiers = @{$hash{$idType}->{$type}->{$originalTerm}}
+  my $isolateTermIdentifiers = $self->queryLoadedIsolates();
 
   # insert mapping for terms which don't require manual mapping (ie. the value in the dots table == value in apidb table)
-  my $automaticCounts = $self->insertAutomaticTerms($dotsIsolatesNaSequences, $isolateVocabularyIds);
+  my $automaticCounts = $self->insertAutomaticTerms($isolateTermIdentifiers, $isolateVocabularyIds);
 
   # insert mapping for manual terms (from xml mapping file)
-  my $manualCounts = $self->insertManuallyMappedTerms($dotsIsolatesNaSequences, $isolateVocabularyIds);
+  my $manualCounts = $self->insertManuallyMappedTerms($isolateTermIdentifiers, $isolateVocabularyIds);
 
   # insert mapping for null/unknown terms
-  my $nullCounts = $self->insertNullMappedTerms($dotsIsolatesNaSequences, $isolateVocabularyIds);
+  my $nullCounts = $self->insertNullMappedTerms($isolateTermIdentifiers, $isolateVocabularyIds);
 
   my $mappingTotalCount = $automaticCounts + $manualCounts + $nullCounts;
 
@@ -88,13 +103,43 @@ sub insertNullMappedTerms {
 
   my $vocabTerm = ApiCommonData::Load::IsolateVocabulary::VocabularyTerm->new($term, '', '', $type, $type, 1);
 
-  my $isolateVocabularyId = $isolateVocabularyIds->{$term}->{$type};
+  my $isolateVocabularyId = $isolateVocabularyIds->{$type}->{$term};
 
   my $naSequenceIds = $self->queryForNullNaSequences($type);
 
-  $count = $count + $self->doMappingInsert($vocabTerm, $isolateVocabularyId, $naSequenceIds);
+  $count = $count + $self->doMappingInsert($vocabTerm, $isolateVocabularyId, $naSequenceIds, 'na_sequence_id');
+
+  # Check that every biomaterial has specific_host, source, and geographic_location
+  # These may be Unknown ... but we are requiring each biomaterial to put something for these
+  $self->checkForNullBioMaterials();
 
   return $count;
+}
+
+sub checkForNullBioMaterials {
+  my ($self) = @_;
+
+  my @bioMaterialIds;
+
+my $sql = "select bio_material_id
+from study.biomaterialcharacteristic bc, study.ontologyentry oe
+where oe.ontology_entry_id = bc.ontology_entry_id
+and oe.category in ('StrainOrLine', 'GeographicLocation', 'BioSourceType')
+group by bio_material_id
+having count(*) != 3";
+
+  my $dbh = $self->getDbh();
+  my $sh = $dbh->prepare($sql);
+  $sh->execute();
+
+  while(my ($b) = $sh->fetchrow_array()) {
+    push @bioMaterialIds, $b;
+  }
+  $sh->finish();
+
+  if(scalar @bioMaterialIds > 0) {
+    die "Not all biomaterials have been matched to vocabulary terms";
+  }
 }
 
 
@@ -127,7 +172,7 @@ where im.na_sequence_id is null";
 
 
 sub insertManuallyMappedTerms {
-  my ($self, $dotsIsolatesNaSequences, $isolateVocabularyIds) = @_;
+  my ($self, $allIsolateIds, $vocabularyIds) = @_;
 
   my $count;
 
@@ -139,19 +184,24 @@ sub insertManuallyMappedTerms {
     my $field = $xmlTerm->getField();
     my $type = $xmlTerm->getType();
 
-    # Get na_sequence_id for the orig, BUT isolate vocabulary id for the mapTerm
-    my $isolateVocabularyId = $isolateVocabularyIds->{$mapTerm}->{$type};
-    my $naSequenceIds = $dotsIsolatesNaSequences->{$table}->{uc($field)}->{$term};
-    next if (!$naSequenceIds);
-    $count = $count + $self->doMappingInsert($xmlTerm, $isolateVocabularyId, $naSequenceIds);
+    my $idType = $self->mapTableIdTypeLookup($table);
+
+    # Get na_sequence_id or bio_material_id for the orig, BUT isolate vocabulary id for the mapTerm
+    my $isolateVocabularyId = $vocabularyIds->{$type}->{$mapTerm};
+    my $isolateIds = $allIsolateIds->{$idType}->{lc($type)}->{$term};
+
+    next if (!$isolateIds);
+    $count = $count + $self->doMappingInsert($xmlTerm, $isolateVocabularyId, $isolateIds, $idType);
   }
   return $count;
 }
 
 sub insertAutomaticTerms {
-  my ($self, $dotsIsolatesNaSequences, $isolateVocabularyIds) = @_;
+  my ($self, $allIsolateIds, $vocabularyIds) = @_;
 
   my $count;
+
+# idType type term
 
   foreach my $sqlTerm (@{$self->getSqlTerms()}) {
     next unless($sqlTerm->getAlreadyMaps());
@@ -161,87 +211,99 @@ sub insertAutomaticTerms {
     my $field = $sqlTerm->getField();
     my $type = $sqlTerm->getType();
 
-    next if($type eq 'product');
+    my $idType = $self->mapTableIdTypeLookup($table);
 
-    # Get na_sequence_id for the orig, AND isolate vocabulary id for the mapTerm
-    my $isolateVocabularyId = $isolateVocabularyIds->{$term}->{$type};
-    my $naSequenceIds = $dotsIsolatesNaSequences->{$table}->{uc($field)}->{$term};
+    # Get na_sequence_id or bio_material_id && vocab_id for the orig
+    my $isolateVocabularyId = $vocabularyIds->{$type}->{$term};
+    my $isolateIds = $allIsolateIds->{$idType}->{lc($type)}->{$term};
 
-    $count = $count + $self->doMappingInsert($sqlTerm, $isolateVocabularyId, $naSequenceIds);
+    $count = $count + $self->doMappingInsert($sqlTerm, $isolateVocabularyId, $isolateIds, $idType);
 
   }
   return $count;
 }
 
 sub doMappingInsert {
-  my ($self, $term, $isolateVocabularyId, $naSequenceIds) = @_;
+  my ($self, $term, $isolateVocabularyId, $isolateIds, $idType) = @_;
 
   my $count;
 
-  my $plugin=$self->getPlugin();
+  my $plugin = $self->getPlugin();
 
-  unless($naSequenceIds) {
-    die "ERROR:  No NA_SEQUENCE_ID for Term " . $term->toString();
+  unless($isolateIds) {
+    die "ERROR:  No $idType found for Term " . $term->toString();
   }
 
   unless($isolateVocabularyId) {
     die "ERROR:  No ISOLATE_VOCABULARY_ID for Term " . $term->toString();
   }
 
-  foreach my $naSequenceId (@$naSequenceIds) {
-      my $mapping = GUS::Model::ApiDB::IsolateMapping -> new({NA_SEQUENCE_ID => $naSequenceId, 
-							      ISOLATE_VOCABULARY_ID => $isolateVocabularyId});
-      $mapping->submit();
-      $count++;
-      $plugin->undefPointerCache();
-  }
+  foreach my $isolateId (@$isolateIds) {
+    my $mapping;
 
+    if($idType eq 'bio_material_id') {
+      $mapping = GUS::Model::ApiDB::VocabularyBiomaterial -> new({BIO_MATERIAL_ID => $isolateId,
+                                                                  ISOLATE_VOCABULARY_ID => $isolateVocabularyId});
+    }
+
+    if($idType eq 'na_sequence_id') {
+      $mapping = GUS::Model::ApiDB::IsolateMapping -> new({NA_SEQUENCE_ID => $isolateId, 
+                                                           ISOLATE_VOCABULARY_ID => $isolateVocabularyId});
+    }
+
+    $mapping->submit();
+    $count++;
+    $plugin->undefPointerCache();
+  }
   return $count;
 }
 
 
-sub queryDotsIsolates {
+sub queryLoadedIsolates {
   my ($self) = @_;
-
   my $data = {};
 
-  my %all_sql = ('IsolateFeature' => <<Sql,
-select distinct na_sequence_id, source_id, product from dots.isolatefeature
+my $sql = <<Sql;
+select bc.bio_material_id as id, oe.value as term, 'bio_material_id' as id_type, 'specific_host' as type
+from STUDY.ontologyentry oe, STUDY.biomaterialcharacteristic bc
+where oe.ontology_entry_id = bc.ontology_entry_id
+and oe.category = 'Host'
+UNION
+select bc.bio_material_id as id, oe.value as term, 'bio_material_id' as id_type, 'geographic_location' as type
+from STUDY.ontologyentry oe, STUDY.biomaterialcharacteristic bc
+where oe.ontology_entry_id = bc.ontology_entry_id
+and oe.category = 'GeographicLocation'
+UNION
+select bc.bio_material_id as id, oe.value as term, 'bio_material_id' as id_type, 'isolation_source' as type
+from STUDY.ontologyentry oe, STUDY.biomaterialcharacteristic bc
+where oe.ontology_entry_id = bc.ontology_entry_id
+and oe.category = 'BioSourceType'
+UNION
+select i.na_sequence_id as id, i.country as term, 'na_sequence_id' as id_type, 'geographic_location' as type
+from dots.isolatesource i
+union
+select i.na_sequence_id as id, i.specific_host as term, 'na_sequence_id' as id_type, 'specific_host' as type
+from dots.isolatesource i
+union
+select i.na_sequence_id as id, i.isolation_source as term, 'na_sequence_id' as id_type, 'isolation_source' as type
+from dots.isolatesource i
 Sql
-             'IsolateSource' => <<Sql,
-select distinct na_sequence_id, country, specific_host, isolation_source from dots.isolatesource
-Sql
-# probably not needed anymore
-             'ExternalNaSequence' => <<Sql,
-select distinct s.na_sequence_id, s.source_id from dots.nasequence s, dots.isolatesource i where i.na_sequence_id = s.na_sequence_id
-Sql
-            );
-
-  my @tables = ('IsolateFeature', 'IsolateSource',
-		'ExternalNaSequence'
-      );
 
   my $dbh = $self->getDbh();
 
-  foreach my $table (@tables) {
-    my $sql = $all_sql{$table};
-    my $sh = $dbh->prepare($sql);
-    $sh->execute();
+  my $sh = $dbh->prepare($sql);
+  $sh->execute();
 
-    while(my $row = $sh->fetchrow_hashref) {
-#      my $identifier = $table eq 'IsolateFeature' ? $row->{NA_FEATURE_ID} : $row->{NA_SEQUENCE_ID};
-      my $identifier = $row->{NA_SEQUENCE_ID};
+  while(my $row = $sh->fetchrow_hashref) {
+    my $id = $row->{ID};
+    my $idType = $row->{ID_TYPE};
+    my $term = $row->{TERM};
+    my $type = $row->{TYPE};
 
-      foreach my $field (keys %$row) {
-        my $rowValue = $row->{$field};
-
-        unless($field eq 'NA_SEQUENCE_ID' || $field eq 'NA_FEATURE_ID') {
-          push @{$data->{$table}->{$field}->{$rowValue}}, $identifier;
-        }
-      }
-    }
-    $sh->finish();
+    push @{$data->{$idType}->{$type}->{$term}}, $id;
   }
+  $sh->finish();
+
   return $data;
 }
 
