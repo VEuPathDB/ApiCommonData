4706e7c999508b1001a77f3b966b1d8805b2fa3c
Brian Brunk <brunkb@upenn.edu>
2012-09-05 17:54:03 +0000

    added some help text
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@49842 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/testSRAFetch.pl
+++ Load/bin/testSRAFetch.pl
@@ -10,6 +10,10 @@ my($getFastq);
 &GetOptions("getFastq|q!" => \$getFastq 
             );
 
+if(scalar(@ARGV) == 0){
+  die "testSRAFetch.pl usage:\n\ttestSRAFetch.pl --getFastq (if present retrieves fastq sequences ... normally would not include this argument if just testing) 'strings to test'\n\tNOTE: it takes in test strings on the commandline.  You can pass in multiple strings for one sample as a comma delimited list ... string1,string2 or 'string1, string2' ... each argument will be evaluated as a sample\n";
+}
+
 my $ct = 0;
 foreach my $sampleId (@ARGV){
   $ct++;
