63f58c976d8e03fd084941cf4fcbae4ee0a9749e
Bindu Gajria <bindu.gajria@gmail.com>
2012-10-04 20:39:50 +0000

    sql to create/replace project_id function on portal database NOTE: it uses sequenceAttributes bfmv, so that needs to exists (whose `select` permissions need to be `public` - as i am setting in portalTuningManager.xml now)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@50591 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/patches/createPortalProjectIdFunction.sql
@@ -0,0 +1,22 @@
+-------------------------------------------------------------------------------
+-- To be used ONLY ON portal database instances 
+--   AFTER SequenceAttributes tuningTable exists on it
+-------------------------------------------------------------------------------
+create or replace function apidb.project_id (orgParam varchar2)
+return varchar2
+is
+   project varchar2(80);
+begin
+      select distinct sa.project_id 
+      into project
+      from apidbtuning.sequenceattributes sa
+      where SUBSTR(sa.organism,1,(INSTR(sa.organism,' ',1,1)-1)) = SUBSTR(orgParam,1,(INSTR(orgParam||' ',' ',1,1)-1));
+    return project;
+
+end project_id;
+/
+show errors;
+
+GRANT execute ON apidb.project_id TO public;
+
+-------------------------------------------------------------------------------
