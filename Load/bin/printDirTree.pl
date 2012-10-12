62d92728319c28a2ba0f66771b8b9dc4744af7e8
Wei Li <weili1@pennmedicine.upenn.edu>
2012-10-12 17:21:05 +0000

    new script to create a download directory report, ref #10713
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@50772 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/printDirTree.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+
+my ($dir) = @ARGV;
+$dir = "." unless $dir;
+&loopDir($dir, "");
+exit;
+
+sub loopDir {
+   my($dir, $margin) = @_;
+   chdir($dir) || die "Cannot chdir to $dir\n";
+   local(*DIR);
+   opendir(DIR, ".");
+   while (my $f=readdir(DIR)) {
+      next if ($f eq "." || $f eq "..");
+      my $filesize = getFileSize($f); 
+      if (-d $f) {
+	 print "$margin$f\n";
+         &loopDir($f,$margin."   ");
+     }else{
+	 print "$margin$f\t$filesize\n";
+     }
+   }
+   closedir(DIR);
+   chdir("..");
+}
+
+sub getFileSize
+    {
+         my $file = shift;
+     
+         my $size = (stat($file))[7];
+    
+         if ($size > 1099511627776)  #   TiB: 1024 GiB
+         {
+             return sprintf("%.1fT", $size / 1099511627776);
+         }
+         elsif ($size > 1073741824)  #   GiB: 1024 MiB
+         {
+            return sprintf("%.1fG", $size / 1073741824);
+         }
+         elsif ($size > 1048576)       #   MiB: 1024 KiB
+         {
+            return sprintf("%.1fM", $size / 1048576);
+         }
+         elsif ($size > 1024)            #   KiB: 1024 B
+         {
+            return sprintf("%.1fK", $size / 1024);
+         }
+         else                                    #   bytes
+         {
+            return sprintf("%.0f bytes", $size);
+         }
+    }
+
+1;
