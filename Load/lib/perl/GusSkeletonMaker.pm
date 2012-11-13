976030cb76a653053f7b0f1b3702b68dc8ee1a8a
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2012-11-13 20:43:00 +0000

    add miRNA_gene to makeGusGene
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@51569 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/GusSkeletonMaker.pm
+++ Load/lib/perl/GusSkeletonMaker.pm
@@ -30,6 +30,7 @@ my $soTerms = { 'coding_gene'=>'protein_coding',
 		'transcript' => 'transcript',
 		'exon' => 'exon',
 		'ORF' => 'ORF',
+		'miRNA_gene' => 'miRNA_encoding',
 	      };
 
 #--------------------------------------------------------------------------------
@@ -123,7 +124,7 @@ sub makeGusGene {
   my $type = $bioperlGene->primary_tag();
 
   $plugin->error("Trying to make gus skeleton from a tree rooted with an unexpected type: '$type'") 
-     unless (grep {$type eq $_} ("haplotype_block","coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene","scRNA_gene"));
+     unless (grep {$type eq $_} ("haplotype_block","coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene","scRNA_gene", "miRNA_gene"));
 
   my $gusGene = $plugin->makeSkeletalGusFeature($bioperlGene, $genomicSeqId,
 						$dbRlsId, 
