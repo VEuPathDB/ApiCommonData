f77e2261d743181c4898a45a644f8b5dd052d150
Mark Heiges <mheiges@gmail.com>
2012-11-27 19:22:03 +0000

    change alt/apidb dblink prefixes to prodn/prods
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@51867 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDbLinks.sql
+++ Load/lib/sql/apidbschema/createDbLinks.sql
@@ -1,11 +1,11 @@
-CREATE PUBLIC DATABASE LINK apidb.login_comment
-CONNECT TO plasmodbwww
-IDENTIFIED BY po34weep
+CREATE PUBLIC DATABASE LINK prodS.login_comment
+CONNECT TO uga_prod
+IDENTIFIED BY vcltum33r
 USING 'apicommS';
 
-CREATE PUBLIC DATABASE LINK alt.login_comment
-CONNECT TO plasmodbwww
-IDENTIFIED BY po34weep
+CREATE PUBLIC DATABASE LINK prodN.login_comment
+CONNECT TO uga_prod
+IDENTIFIED BY vcltum33r
 USING 'apicommN';
 
 CREATE PUBLIC DATABASE LINK devS.login_comment
--- Load/lib/sql/apidbschema/dropDbLinks.sql
+++ Load/lib/sql/apidbschema/dropDbLinks.sql
@@ -1,6 +1,9 @@
 DROP PUBLIC DATABASE LINK apidb.login_comment;
 DROP PUBLIC DATABASE LINK alt.login_comment;
 
+DROP PUBLIC DATABASE LINK prodS.login_comment;
+DROP PUBLIC DATABASE LINK prodN.login_comment;
+
 DROP PUBLIC DATABASE LINK devS.login_comment;
 DROP PUBLIC DATABASE LINK devN.login_comment;
 
