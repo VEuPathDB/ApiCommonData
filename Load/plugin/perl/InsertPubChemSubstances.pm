262b36a6d6daac1ec5b00f074fdfdbd79c7da0dd
Bindu Gajria <bindu.gajria@gmail.com>
2012-12-03 04:28:19 +0000

    big changes in plugin, specific change: using XML:Simple now
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@51957 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertPubChemSubstances.pm
+++ Load/plugin/perl/InsertPubChemSubstances.pm
@@ -9,15 +9,14 @@ package ApiCommonData::Load::Plugin::InsertPubChemSubstances;
 use strict;
 use warnings;
 
-use XML::Twig;
+use XML::Simple;
 use GUS::PluginMgr::Plugin;
 use GUS::Model::ApiDB::PubChemSubstance;
 
 
 my %subst;
 my $subst_id;
-my @compoundArr;
-my @idAndTypeArr;
+my %compoundH;
 
 # ----------------------------------------------------------
 # Load Arguments
@@ -43,7 +42,7 @@ sub getArgsDeclaration {
      stringArg({ name => 'compoundIdsFile',
 		 descr => 'file for compound IDs, with complete path',
 		 constraintFunc=> undef,
-		 reqd  => 1,
+		 reqd  => 0,
 		 isList => 0,
 		 mustExist => 0,
 	       })
@@ -122,36 +121,60 @@ sub new {
 
 sub run {
   my $self = shift;
-
-  my $roots = { 'PC-Substance_sid' => 1,
-		'PC-Compound' => 1,
-		'PC-Substance_source/PC-Source/PC-Source_db/PC-DBTracking' => 1,
-		'PC-Substance_synonyms' => 1
-	      };
-
-  my $handlers = { 'PC-Substance_sid/PC-ID' => \&get_ID,
-		   'PC-Compound' => \&get_CID,
-		   'PC-Substance_source/PC-Source/PC-Source_db/PC-DBTracking'  => \&get_KEGG,
-		   'PC-Substance_synonyms' => \&get_Syns
-		 };
-  my $twig = new XML::Twig(TwigRoots => $roots,
-			   TwigHandlers => $handlers);
-
   my $fileCount = 0;
   my $fileDir  = $self->getArg('fileDir');
   my @fileArray = @{$self->getArg('fileNames')};
   my $cidFile  = $self->getArg('compoundIdsFile');
 
-
+  my $simple = XML::Simple->new (ForceArray => 1, KeepRoot => 1);
   foreach my $file (@fileArray){
     $fileCount++;
     $file = $fileDir. "/" . $file;
-    $twig->parsefile($file);
+    my $data    = $simple->XMLin($file);
+    $self->parseFile($data);
   }
   $self->insertPubChemSubstance();
   $self->makeCidFile($cidFile) if $cidFile;
 
   return "Processed $fileCount files.";
+}
+
+sub parseFile {
+  my ($self, $data) = @_;
+  my $substances = $data->{'PC-Substances'}->[0]->{'PC-Substance'};
+  my @substArray = @{$substances};
+
+  foreach (@substArray){
+    # SID
+    my $subst_id = $_->{'PC-Substance_sid'}->[0]->{'PC-ID'}->[0]->{'PC-ID_id'}->[0];
+
+    # KEGG
+    my $dbName = $_->{'PC-Substance_source'}->[0]->{'PC-Source'}->[0]->{'PC-Source_db'}->[0]->{'PC-DBTracking'}->[0]->{'PC-DBTracking_name'}->[0];
+    my $dbValue = $_->{'PC-Substance_source'}->[0]->{'PC-Source'}->[0]->{'PC-Source_db'}->[0]->{'PC-DBTracking'}->[0]->{'PC-DBTracking_source-id'}->[0]->{'Object-id'}->[0]->{'Object-id_str'}->[0];
+   $subst{$subst_id}{$dbName} = $dbValue;
+
+    # synonms
+    my $syns =  $_->{'PC-Substance_synonyms'}->[0]->{'PC-Substance_synonyms_E'} ;
+    my @sArray = @{$syns};
+    $subst{$subst_id}{synonyms} = \@sArray;
+
+
+    # CIDs (compound IDs)
+    my $cArrayRef =  $_->{'PC-Substance_compound'}->[0]->{'PC-Compounds'}->[0]->{'PC-Compound'};
+    my @cArray = @{$cArrayRef};
+    my @idAndTypeArr;
+
+    foreach (@cArray){
+      my $type = $_->{'PC-Compound_id'}->[0]->{'PC-CompoundType'}->[0]->{'PC-CompoundType_type'}->[0]->{'value'};
+      my $id = $_->{'PC-Compound_id'}->[0]->{'PC-CompoundType'}->[0]->{'PC-CompoundType_id'}->[0]->{'PC-CompoundType_id_cid'}->[0];
+      if ($id) {
+	push (@idAndTypeArr, ($id  . "|type="  . $type) );
+	$compoundH{$id} = 1; # to capture CIDs for output file
+      }
+    }
+    $subst{$subst_id}{CID} = \@idAndTypeArr;
+
+  }
 }
 
 # get SIDs of already_loaded PubChem structures
@@ -170,6 +193,8 @@ sub getExistingSids {
   return \%sidHash;
 }
 
+
+# insert rows in table
 sub insertPubChemSubstance {
   my $self = shift;
   my $count;
@@ -177,7 +202,6 @@ sub insertPubChemSubstance {
   # get list (as hash) of substance (IDs) that are already in the database
   my $hashRef=$self->getExistingSids();
   my %loadedSids = %$hashRef;
-
 
   # process array of hashes (of hashes) for each substance
   my @data = keys(%subst);  # keys of outer hash are substance IDs
@@ -192,15 +216,9 @@ sub insertPubChemSubstance {
       my @props = keys(%y);   # keys of inner hash are various properties for each substance
 
       foreach my $p (@props) {
-	if ($p eq 'KEGG') {
-	  my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
-									property     => $p,
-									value        => $subst{$sid}{$p}
-								      });
-	  # add entry unless it already exists in the table
-	  $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
-	} elsif ($p eq 'CID'){
-	  my @cpds =  @{ $subst{$sid}{CID} }; #BB
+	if ($p eq 'CID'){
+	  my @cpds =  @{ $subst{$sid}{CID} };
+
 	  foreach my $c (@cpds) {
 	    my ($id, $type) = split(/\|type\=/, $c);  	    # break $c into cid and type
 	    my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
@@ -208,23 +226,27 @@ sub insertPubChemSubstance {
 									  value        => $id ,
 									  type         => $type
 									});
-	  # add entry unless it already exists in the table
-	  $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
+	    # add entry unless it already exists in the table
+	    $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
 	  }
-	} else {
+	} elsif($p eq 'synonyms') {
 	  my @syns = @{ $subst{$sid}{synonyms} };
 	  foreach my $s (@syns) {
-	    # skip KEGG_ID, as it is repeated as a synonym in the XML file
-	    # print "$sid, synonym, $s \n" if ($s ne $subst{$sid}{KEGG});
-	    if ($s ne $subst{$sid}{KEGG}) {
+
+	    if ($s ne $subst{$sid}{KEGG}) {    # skip KEGG_ID
 	      my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
 									    property     => $p,
 									    value        => $s
 									  });
-	      # add entry unless it already exists in the table
 	      $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
 	    }
 	  }
+	} else {
+	  my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
+									property     => $p,
+									value        => $subst{$sid}{$p}
+								      });
+	  $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
 	}
       }
       $count++;
@@ -240,68 +262,14 @@ sub insertPubChemSubstance {
 # make file of PubChem compound IDs (CIDs)
 sub makeCidFile {
   my ($self, $file) = @_;
-  my @cidArr;
 
   open(FILE, "> $file");
-  foreach my $c (@compoundArr) {
-    print FILE "$c\n";
+  for my $cid (keys %compoundH) {
+    print FILE "$cid\n";
   }
   close(FILE);
-
 }
 
-# get substance ID
-sub get_ID {
-  my ($twig, $ele) = @_;
-  my $id = $ele->first_child('PC-ID_id')->text;
-
-  $subst_id = $id;
-}
-
-# get the synonym list
-sub get_Syns {
-  my ($twig, $ele) = @_;
-
-  my @desc = $ele->find_by_tag_name('PC-Substance_synonyms_E');
-  my @synonyms;
-  foreach my $des (@desc) {
-    push (@synonyms, $des->text);
-  }
-  $subst{$subst_id}{synonyms} = \@synonyms;
-}
-
-#get the KEGG ID
-sub get_KEGG {
-  my ($twig, $ele) = @_;
-  my $name = $ele->first_child('PC-DBTracking_name')->text;
-  my $id = $ele->first_child('PC-DBTracking_source-id')->text;
-
-  $subst{$subst_id}{$name} = $id;
-}
-
-
-#get the compound ID (CID)
-sub get_CID {
-  my ($twig, $ele) = @_;
-  my @cArr = $ele->find_by_tag_name('PC-CompoundType');
-
-  foreach my $c (@cArr) {
-    # get the compound_id
-    my @cidArr = $c->find_by_tag_name('PC-CompoundType_id_cid');
-
-    foreach my $cid (@cidArr) {
-      my $compound_id = $cid->text;
-      my $compound_type;
-      # for each compound_id found, capture it's type
-      my @type = $c->find_by_tag_name('PC-CompoundType_type');
-      $compound_type = $type[0]->att( 'value' );
-
-      push (@compoundArr, $compound_id);
-      push (@idAndTypeArr, ($compound_id  . "|type="  . $compound_type) );
-    }
-    $subst{$subst_id}{CID} = \@idAndTypeArr if ($#idAndTypeArr + 1);
-  }
-}
 
 sub undoTables {
   my ($self) = @_;
