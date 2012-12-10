5a3e9de87cb8366aeca3cd0a9dec3966963aa8bf
Wei Li <weili1@pennmedicine.upenn.edu>
2012-12-10 15:59:54 +0000

    replace .bedgraph with .cov, and strip off the header row of cov file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/106@52081 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/normalizeCoverage.pl
+++ Load/bin/normalizeCoverage.pl
@@ -6,7 +6,7 @@ use CBIL::Util::Utils;
 use lib "$ENV{GUS_HOME}/lib/perl";
 
 # this script loops through each experiment output directory and sums the score under each experiment. 
-# Use sum_score / max_sum_core as normalization ratio and update bedgraph file 
+# Use sum_score / max_sum_core as normalization ratio and update coverage file 
 #
 #  ... Su_strand_specific/analyze_lateTroph/master/mainresult
 #  ... Su_strand_specific/analyze_schizont/master/mainresult
@@ -24,7 +24,7 @@ my ($inputDir, $strandSpecific, $topLevelSeqSizeFile);
 
 my $usage =<<endOfUsage;
 Usage:
-  normalizeBedGraph.pl --inputDir input_diretory --topLevelSeqSizeFile chrosome_size_file --strandSpecific 
+  normalizeCoverage.pl --inputDir input_diretory --topLevelSeqSizeFile chrosome_size_file --strandSpecific 
 
     intpuDir: top level directory, e.g. /eupath/data/EuPathDB/workflows/PlasmoDB/bigwig/data/pfal3D7/organismSpecificTopLevel/Su_strand_specific
     topLevelSeqSizeFile: chromosome size text file e.g. /eupath/data/EuPathDB/workflows/PlasmoDB/bigwig/data/pfal3D7/organismSpecificTopLevel/topLevelSeqSizes.txt
@@ -41,16 +41,15 @@ foreach my $d (@ds) {
   next unless $d =~ /^analyze_/;
   $inputDir =~ s/\/$//;
   my $exp_dir = "$inputDir/$d/master/mainresult";
-
   my $sum_coverage = get_sum_coverage($exp_dir);
   $hash{$exp_dir} = $sum_coverage;
 }
 
-update_bedgraph(\%hash);
+update_coverage(\%hash);
 
-merge_normalized_bedgraph(\%hash);
+merge_normalized_coverage(\%hash);
 
-sub merge_normalized_bedgraph {
+sub merge_normalized_coverage {
   my $hash = shift;
   while(my ($k, $v) = each %$hash) {  # $k is exp directory; $v is sum_coverage
     my $dir = "$k/normalized";
@@ -59,22 +58,22 @@ sub merge_normalized_bedgraph {
     }
 
     if($strandSpecific) {  # strand specific Unique (forward +, reverse -) | NonUnique (forward +, reverse -)
-      &runCmd("cat $k/normalized/RUM_NU_plus.bedgraph $k/normalized/RUM_NU_minus.bedgraph | sort -k1,1 -k2,2n > $k/normalized/final/RUM_NU.bedgraph");
-      &runCmd("cat $k/normalized/RUM_Unique_plus.bedgraph $k/normalized/RUM_Unique_minus.bedgraph | sort -k1,1 -k2,2n > $k/normalized/final/RUM_Unique.bedgraph");
+      &runCmd("cat $k/normalized/RUM_NU_plus.cov $k/normalized/RUM_NU_minus.cov | sort -k1,1 -k2,2n > $k/normalized/final/RUM_NU.cov");
+      &runCmd("cat $k/normalized/RUM_Unique_plus.cov $k/normalized/RUM_Unique_minus.cov | sort -k1,1 -k2,2n > $k/normalized/final/RUM_Unique.cov");
 
-      &runCmd("bedGraphToBigWig $k/normalized/final/RUM_Unique.bedgraph $topLevelSeqSizeFile $k/normalized/final/RUM_Unique.bw"); 
-      &runCmd("bedGraphToBigWig $k/normalized/final/RUM_NU.bedgraph $topLevelSeqSizeFile $k/normalized/final/RUM_NU.bw"); 
+      &runCmd("bedGraphToBigWig $k/normalized/final/RUM_Unique.cov $topLevelSeqSizeFile $k/normalized/final/RUM_Unique.bw"); 
+      &runCmd("bedGraphToBigWig $k/normalized/final/RUM_NU.cov $topLevelSeqSizeFile $k/normalized/final/RUM_NU.bw"); 
 
     } else {  # regular Unique +, Nonunique -
-      &runCmd("cat $k/normalized/RUM_Unique.bedgraph $k/normalized/RUM_NU.bedgraph | sort -k1,1 -k2,2n > $k/normalized/final/RUM.bedgraph");
-      &runCmd("bedGraphToBigWig $k/normalized/final/RUM.bedgraph $topLevelSeqSizeFile $k/normalized/final/RUM.bw"); 
+      &runCmd("cat $k/normalized/RUM_Unique.cov $k/normalized/RUM_NU.cov | sort -k1,1 -k2,2n > $k/normalized/final/RUM.cov");
+      &runCmd("bedGraphToBigWig $k/normalized/final/RUM.cov $topLevelSeqSizeFile $k/normalized/final/RUM.bw"); 
     }
   }
 }
 
 # updates bedgraph file - score * normalization_ratio
 # save updated bedgraph file to the new 'normalized' directory
-sub update_bedgraph {
+sub update_coverage {
   my $hash = shift;
 
   my $max_sum_coverage = get_max_sum_coverage($hash);
@@ -91,7 +90,7 @@ sub update_bedgraph {
     my $out_dir = "$k/normalized";
 
     foreach my $f (@fs) {
-      next if $f !~ /\.bedgraph/i;
+      next if $f !~ /\.cov/i;
       open(F, "$k/$f");
 
       my $indicator = 1;
@@ -102,6 +101,7 @@ sub update_bedgraph {
       }
 
       open OUT, ">$out_dir/$f";
+      <F>;
       while(<F>) {
         my($chr, $start, $stop, $score) = split /\t/, $_;
 
@@ -124,8 +124,9 @@ sub get_sum_coverage {
   my @fs = readdir(D);
 
   foreach my $f (@fs) {
-    next unless $f =~ /\.bedgraph/;
+    next unless $f =~ /\.cov/;
     open(F, "$d/$f");
+    <F>;
     while(<F>) {
       chomp;
       my($chr, $start, $stop, $score) = split /\t/, $_;
