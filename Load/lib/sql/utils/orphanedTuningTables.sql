e907b2c87f742d341b40c5ab8a06416fc9ca0c02
John Iodice <iodice@pennmedicine.upenn.edu>
2012-12-15 03:26:24 +0000

    remove section for orphaned tuning materialized views (from back in the day), and symbolic link exerciser
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@52225 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/utils/orphanedTuningTables.sql
+++ Load/lib/sql/utils/orphanedTuningTables.sql
@@ -1,38 +1,12 @@
--------------------------------------------------------------------------------
--- mviewcleanup.sql
---
--- partially redundant with createApidbTuning.sql
--- see that script for an explaination of our system of materialized views
--- and synonyms
+
+-- DROP TABLE statements for unneeded tuning tables
 
 set time on timing on pagesize 50000 linesize 100
 
-prompt Run these statements to test synonyms
-select 'select count(*) as ' || synonym_name || ' from ' || owner || '.' || synonym_name || ';'
-       as "synonym tests"
-from all_synonyms
-where owner='APIDBTUNING'
-order by synonym_name;
+# these look like tuning tables (their names end in four digits) but no synonym points at them.
 
-prompt These mviews appear superfluous (their names end in four digits but no synonym points at them).
-prompt Consider dropping them if all synonyms are OK.
-
-SELECT 'drop materialized view ' || owner || '.' || mview_name || ';' AS drops
-FROM all_mviews
-WHERE mview_name IN (SELECT mview_name
-                     FROM all_mviews
-                    MINUS
-                     SELECT table_name
-                     FROM all_synonyms)
-  AND REGEXP_REPLACE(mview_name, '[0-9][0-9][0-9][0-9]$', 'fournumbers')
-      LIKE '%fournumbers'
-  AND owner != 'SYS'
-ORDER BY mview_name;
-
-prompt These tables appear superfluous (their names end in four digits but no synonym points at them).
-prompt Consider dropping them if all synonyms are OK.
-
-SELECT 'drop table ' || owner || '.' || table_name || ';' AS "drop tables"
+SELECT 'drop table ' || owner || '.' || table_name || ';'
+       AS "orphaned tuning tables"
 FROM all_tables
 WHERE table_name IN (SELECT table_name
                      FROM all_tables
