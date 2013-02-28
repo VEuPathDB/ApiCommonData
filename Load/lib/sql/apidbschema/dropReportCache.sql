fe3f48ca915786301cd5e4e651e64433acd27552
John Iodice <iodice@pennmedicine.upenn.edu>
2013-02-28 17:43:39 +0000

    initial check-in
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@53728 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropReportCache.sql
@@ -0,0 +1,9 @@
+drop table apidb.GeneDetail;
+drop table apidb.IsolateDetail;
+drop table apidb.SequenceDetail;
+drop table apidb.OrthomclSequenceDetail;
+drop table apidb.CompoundDetail;
+
+drop sequence apidb.geneGff_pkseq;
+
+exit;
