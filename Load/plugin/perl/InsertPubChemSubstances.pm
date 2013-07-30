cc1d0deb850cda562c36ee3ee70df2d80767eefb
Bindu Gajria <bindu.gajria@gmail.com>
2013-07-30 18:46:19 +0000

    fixed in plugin so that the substance data (CID and Synonymns) can be loaded separately.
    -refs #13021
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@57201 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertPubChemSubstances.pm
+++ Load/plugin/perl/InsertPubChemSubstances.pm
@@ -16,7 +16,7 @@ use GUS::Model::ApiDB::PubChemSubstance;
 
 my %subst;
 my $subst_id;
-my %compoundH;
+my $property;
 
 # ----------------------------------------------------------
 # Load Arguments
@@ -37,7 +37,7 @@ sub getArgsDeclaration {
 		 constraintFunc=> undef,
 		 reqd  => 1,
 		 isList => 1,
-		 mustExist => 0,
+		 mustExist => 1,
 	       }),
      stringArg({ name => 'compoundIdsFile',
 		 descr => 'file for compound IDs, with complete path',
@@ -45,7 +45,15 @@ sub getArgsDeclaration {
 		 reqd  => 0,
 		 isList => 0,
 		 mustExist => 0,
+	       }),
+     stringArg({ name => 'property',
+		 descr => 'CID or Synonym',
+		 constraintFunc=> undef,
+		 reqd  => 1,
+		 isList => 0,
+		 mustExist => 1,
 	       })
+
     ];
 
   return $argsDeclaration;
@@ -58,7 +66,7 @@ sub getArgsDeclaration {
 sub getDocumentation {
 
   my $description = <<DESCR;
-Plugin to load PubChem Substances out of a single XML file, into ApiDB.PubChemSubstance, and to create a file of PubChem compounds IDs (that were in the XML file)
+Plugin to load PubChem Substances data (compound_id or synonymns) into ApiDB.PubChemSubstance, and to create a file of PubChem compounds IDs (ithat were in the data file).
 DESCR
 
   my $purpose = <<PURPOSE;
@@ -125,6 +133,7 @@ sub run {
   my $fileDir  = $self->getArg('fileDir');
   my @fileArray = @{$self->getArg('fileNames')};
   my $cidFile  = $self->getArg('compoundIdsFile');
+  $property = $self->getArg('property');
 
   my $simple = XML::Simple->new (ForceArray => 1, KeepRoot => 1);
   foreach my $file (@fileArray){
@@ -134,50 +143,26 @@ sub run {
     $self->parseFile($data);
   }
   $self->insertPubChemSubstance();
-  $self->makeCidFile($cidFile) if $cidFile;
+  $self->makeCidFile($cidFile) if ($cidFile && $property eq 'CID');
 
   return "Processed $fileCount files.";
 }
 
 sub parseFile {
   my ($self, $data) = @_;
-  my $substances = $data->{'eSummaryResult'}->[0]->{'DocSum'};
+  my $substances = $data->{'InformationList'}->[0]->{'Information'};
   my @substArray = @{$substances};
 
-  foreach (@substArray){  # for each substance
-   my $subst_id = $_->{'Id'}[0];      # SID
-    my $val;
-    my $prop;
+  foreach (@substArray){    # for each substance
+    my $subst_id = $_->{'SID'}[0];  # SID
 
-   foreach my $data ( @{$_->{'Item'}}) {
-     my %hsh = %{$data};
-     foreach my $attributes (keys %hsh){    
-       if ($attributes eq 'content' || $attributes eq 'Item') {
-	 $val = $hsh{$attributes};
-       } elsif ($attributes eq 'Name') {
-	 $prop = $hsh{$attributes};
-       }
+    if ($property eq 'CID') {
+      $subst{$subst_id}{CID} = $_->{'CID'}[0];
+    } else {    # for Synonym
+      $subst{$subst_id}{Synonym} = \@{$_->{'Synonym'}};
+    }
 
-     }
-     if ($prop eq 'SynonymList' ) {
-       my @list = @{$val};
-       $subst{$subst_id}{synonyms} = \@list;
-
-     } elsif ($prop eq 'CompoundIDList' ) {
-
-       my @list = @{$val}; 
-       foreach (@list) {
-	 my $comp = $_->{content};
-	 $compoundH{$comp} = 1; # to capture CIDs for output file
-       }
-       $subst{$subst_id}{CID} = \@list;
-
-     } elsif ($prop eq 'SourceID') {
-       $subst{$subst_id}{'KEGG'} = $val;
-     }
-   }
-
- }
+  }
 }
 
 # get SIDs of already_loaded PubChem structures
@@ -185,7 +170,11 @@ sub getExistingSids {
   my ($self) = @_;
   my %sidHash;
 
-  my $sql = "SELECT distinct substance_id from ApiDB.PubChemSubstance";
+  my $sql = <<EOSQL;
+  SELECT distinct substance_id 
+  FROM ApiDB.PubChemSubstance 
+  WHERE property= '$property'
+EOSQL
 
   my $dbh = $self->getQueryHandle();
   my $sth = $dbh->prepareAndExecute($sql);
@@ -212,42 +201,23 @@ sub insertPubChemSubstance {
 
     # load substance if SID is not in database
     if ($loadedSids{$sid}) {
-      $self->log("Ignoring SID $sid; it is already present in ApiDB.PubChemSubstance.");
+      $self->log("Ignoring SID $sid; it is already present in ApiDB.PubChemSubstance with property = $property.");
     } else {
 
       my %y = %{$subst{$sid}};
       my @props = keys(%y);   # keys of inner hash are various properties for each substance
 
       foreach my $p (@props) {
-	if ($p eq 'CID'){
-	  my @cpds =  @{ $subst{$sid}{CID} };
-
-	  my $type = 'standardized';
-	  foreach my $c (@cpds) {
-	    ##my ($id, $type) = split(/\|type\=/, $c);  	    # break $c into cid and type
-
+	if($p eq 'Synonym') {
+	  my @syns = @{ $subst{$sid}{Synonym} };
+	  foreach my $s (@syns) {
 	    my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
 									  property     => $p,
-									  value        => $c->{content} ,
-									  type         => $type
+									  value        => $s
 									});
-	    $type = 'component';
-	    # add entry unless it already exists in the table
 	    $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
 	  }
-	} elsif($p eq 'synonyms') {
-	  my @syns = @{ $subst{$sid}{synonyms} };
-	  foreach my $s (@syns) {
-
-	    if ($s->{content} ne $subst{$sid}{KEGG}) {    # skip KEGG_ID
-	      my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
-									    property     => $p,
-									    value        => $s->{content}
-									  });
-	      $pubChemSubst->submit() if (!$pubChemSubst->retrieveFromDB());
-	    }
-	  }
-	} else {
+	} elsif ($p eq 'CID'){
 	  my $pubChemSubst = GUS::Model::ApiDB::PubChemSubstance->new({ substance_id => $sid,
 									property     => $p,
 									value        => $subst{$sid}{$p}
@@ -264,13 +234,18 @@ sub insertPubChemSubstance {
 
 }
 
-
 # make file of PubChem compound IDs (CIDs)
 sub makeCidFile {
   my ($self, $file) = @_;
-
+  $self->log("Making file $file with CIDs.");
   open(FILE, "> $file");
-  for my $cid (keys %compoundH) {
+
+  my @data = keys(%subst);  # keys of outer hash are substance IDs
+  $self->log("Array [ @data ] has ($#data + 1) elements.");
+
+  foreach my $sid (@data) {
+    my $cid = $subst{$sid}{CID};
+    $self->log("sid is $sid AND cid is $cid");
     print FILE "$cid\n";
   }
   close(FILE);
