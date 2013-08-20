ced6c9f7b3bcdec6453dd98a5e57eea4715a6f19
Wei Li <weili1@pennmedicine.upenn.edu>
2013-08-20 20:06:03 +0000

    dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@57672 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertInterproscan5Results.pm
+++ Load/plugin/perl/InsertInterproscan5Results.pm
@@ -221,7 +221,7 @@ sub processProteinResults {
 
   my $tableName = $self->getArg('aaSeqTable');
   my $queryTable = "GUS::Model::DoTS::$tableName";
-  my $protein_id = ($proteins->[0] =~ m/^(\S+)/);
+  my $protein_id = $1 if($proteins->[0] =~ m/^(\S+)/);
   if (!$protein_id) {
   	$self->log("Skipping: Interproscan Results file has a match with empty source ID");
 	return;
