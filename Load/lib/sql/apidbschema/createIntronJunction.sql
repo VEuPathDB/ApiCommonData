e3e21059cdaf47b4f373b3ce96ab2bb911946102
John Brestelli <jbrestell@gmail.com>
2014-06-19 22:02:02 +0000

    rename the table and sequences
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@62877 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createIntronJunction.sql
+++ Load/lib/sql/apidbschema/createIntronJunction.sql
@@ -1,6 +1,6 @@
 
-CREATE TABLE apidb.RUMIntronFeature (
- RUM_intron_feature_id       NUMBER(10),
+CREATE TABLE apidb.IntronJunction (
+intron_junction_id       NUMBER(10),
  external_database_release_id NUMBER(10) NOT NULL,
  sample_name                  VARCHAR2(100) NOT NULL,
  na_sequence_id               NUMBER(10) NOT NULL,
@@ -27,14 +27,14 @@ CREATE TABLE apidb.RUMIntronFeature (
  ROW_PROJECT_ID               NUMBER(4),
  ROW_ALG_INVOCATION_ID        NUMBER(12),
  FOREIGN KEY (external_database_release_id) REFERENCES SRes.ExternalDatabaseRelease,
- PRIMARY KEY (RUM_intron_feature_id)
+ PRIMARY KEY (intron_junction_id)
 );
 
-CREATE SEQUENCE apidb.RUMIntronFeature_sq;
+CREATE SEQUENCE apidb.IntronJunction_sq;
 
-GRANT insert, select, update, delete ON apidb.RUMIntronFeature TO gus_w;
-GRANT select ON apidb.RUMIntronFeature TO gus_r;
-GRANT select ON apidb.RUMIntronFeature_sq TO gus_w;
+GRANT insert, select, update, delete ON apidb.IntronJunction TO gus_w;
+GRANT select ON apidb.IntronJunction TO gus_r;
+GRANT select ON apidb.IntronJunction_sq TO gus_w;
 
 INSERT INTO core.TableInfo
     (table_id, name, table_type, primary_key_column, database_id, is_versioned,
@@ -42,14 +42,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write, 
      other_read, other_write, row_user_id, row_group_id, row_project_id, 
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'RUMIntronFeature',
-       'Standard', 'RUM_intron_feature_id',
+SELECT core.tableinfo_sq.nextval, 'IntronJunctino',
+       'Standard', 'intron_junction_id',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'rumintronfeature' NOT IN (SELECT lower(name) FROM core.TableInfo
+WHERE 'intronjunction' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 ------------------------------------------------------------------------------
