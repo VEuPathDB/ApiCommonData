598d7d4af8654c9620d0ce4bf364789eda5a2b1f
John Brestelli <jbrestell@gmail.com>
2014-06-19 22:20:46 +0000

    rename table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@62881 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createNAFeatureHaploblock.sql
+++ Load/lib/sql/apidbschema/createNAFeatureHaploblock.sql
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 
-CREATE TABLE ApiDB.GeneFeatureLodScore (
- GENE_HAPBLOCK_SCORE_ID               NUMBER(10) not null,
+CREATE TABLE ApiDB.NAFeatureHaploblock (
+ na_feature_HAPBLOCK_ID               NUMBER(10) not null,
  NA_FEATURE_ID                        NUMBER(10) not null,
  HAPLOTYPE_BLOCK_NAME                 VARCHAR2(50) not null,
  LOD_SCORE_MANT                       FLOAT(126),
@@ -20,20 +20,20 @@ CREATE TABLE ApiDB.GeneFeatureLodScore (
  ROW_ALG_INVOCATION_ID                NUMBER(12),
  FOREIGN KEY (NA_FEATURE_ID) REFERENCES DoTS.NaFeatureImp (NA_FEATURE_ID),
  FOREIGN KEY (EXTERNAL_DATABASE_RELEASE_ID) REFERENCES SRes.ExternalDatabaseRelease (EXTERNAL_DATABASE_RELEASE_ID),
- PRIMARY KEY (GENE_HAPBLOCK_SCORE_ID)
+ PRIMARY KEY (na_feature_HAPBLOCK_ID)
  );
 
-GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.GeneFeatureLodScore TO gus_w;
-GRANT SELECT ON ApiDB.GeneFeatureLodScore TO gus_r;
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.NAFeatureHaploblock TO gus_w;
+GRANT SELECT ON ApiDB.NAFeatureHaploblock TO gus_r;
 
-CREATE INDEX apidb.geneHapBlock_idx
-ON ApiDB.GeneFeatureLodScore (NA_FEATURE_ID,HAPLOTYPE_BLOCK_NAME);
+CREATE INDEX apidb.NaFeatHapBlock_idx
+ON ApiDB.NAFeatureHaploblock (NA_FEATURE_ID,HAPLOTYPE_BLOCK_NAME);
 
 
-CREATE SEQUENCE apidb.GeneFeatureLodScore_sq;
+CREATE SEQUENCE apidb.NAFeatureHaploblock_sq;
 
-GRANT SELECT ON apidb.GeneFeatureLodScore_sq TO gus_r;
-GRANT SELECT ON apidb.GeneFeatureLodScore_sq TO gus_w;
+GRANT SELECT ON apidb.NAFeatureHaploblock_sq TO gus_r;
+GRANT SELECT ON apidb.NAFeatureHaploblock_sq TO gus_w;
 
 
 INSERT INTO core.TableInfo
@@ -42,14 +42,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'GeneFeatureLodScore',
-       'Standard', 'GENE_HAPBLOCK_SCORE_ID',
+SELECT core.tableinfo_sq.nextval, 'NAFeatureHaploblock',
+       'Standard', 'NA_FEATURE_HAPBLOCK_ID',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'GeneFeatureLodScore' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'NAFeatureHaploblock' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 
