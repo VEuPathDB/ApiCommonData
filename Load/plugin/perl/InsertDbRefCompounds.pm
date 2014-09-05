7ea5f6d67ec240209653a70bbbb31b9fbffa4bbb
John Brestelli <jbrestell@gmail.com>
2014-09-05 17:34:43 +0000

    review gus4 rules and update revision
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@64473 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertDbRefCompounds.pm
+++ Load/plugin/perl/InsertDbRefCompounds.pm
@@ -17,8 +17,7 @@ package ApiCommonData::Load::Plugin::InsertDbRefCompounds;
   # GUS4_STATUS | Simple Rename                  | auto   | absent
   # GUS4_STATUS | ApiDB Tuning Gene              | auto   | absent
   # GUS4_STATUS | Rethink                        | auto   | absent
-  # GUS4_STATUS | dots.gene                      | manual | unreviewed
-die 'This file has broken or unreviewed GUS4_STATUS rules.  Please remove this line when all are fixed or absent';
+  # GUS4_STATUS | dots.gene                      | manual | absent
 #^^^^^^^^^^^^^^^^^^^^^^^^^ End GUS4_STATUS ^^^^^^^^^^^^^^^^^^^^
 
 @ISA = qw(GUS::PluginMgr::Plugin);
@@ -124,7 +123,7 @@ sub new {
 
   my $documentation = &getDocumentation();
   my $args = &getArgsDeclaration();
-  my $configuration = { requiredDbVersion => 3.6,
+  my $configuration = { requiredDbVersion => 4.0,
                         cvsRevision => '$Revision$',
                         name => ref($self),
                         argsDeclaration => $args,
