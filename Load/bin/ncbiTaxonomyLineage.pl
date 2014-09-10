b77875b60ee80f8067be67780ee86284247ce22e
Jason Stajich <jason.stajich@ucr.edu>
2014-09-10 17:27:14 +0000

    made it more flexible for API changes in BioPerl, also include superkingdom column
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@64549 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/ncbiTaxonomyLineage.pl
+++ Load/bin/ncbiTaxonomyLineage.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/perl -w
 use strict;
-use Env qw(HOME);
+use Env qw(HOME USER);
+use Data::Dumper;
+use Bio::Root::Version;
+use version;
 use Bio::DB::Taxonomy;
 use Getopt::Long;
-my @tax_levels = qw(kingdom phylum class order family genus);
+my $VERSION = version->parse($Bio::Root::Version::VERSION);
+
+my @tax_levels = qw(superkingdom kingdom phylum class order family genus);
 my %wanted_rank = map { $_ => 1 } @tax_levels;
 my $nodesfile = "$HOME/lib/taxonomy/nodes.dmp";
 my $namefile = "$HOME/lib/taxonomy/names.dmp";
-my $indexdir = '/tmp/ncbi_taxonomy';
+my $indexdir = "/tmp/ncbi_taxonomy-$USER";
 my $infile = 'orthoOrgs.tab';
 GetOptions(
 'nodes:s' => \$nodesfile,
@@ -28,23 +33,46 @@ my @header = split('\t',$header);
 print join("\t", @tax_levels,@header),"\n";
 while (<$fh>) {
   chomp;
-  my ($name, $taxid,@rest) = split('\t',$_);
+  my ($name, $taxid,@rest) = split(/\t/,$_);
 
   if ( ! defined $name ) {
     warn("empty line $_");
     next;
   }
 
-  my $taxon = $taxdb->get_taxon(-taxonid => $taxid);
+  my $taxon;
+  if ( $VERSION == version->parse('1.4') ) {
+    # API 1.4
+    $taxon = $taxdb->get_Taxonomy_Node(-taxonid => $taxid);
+  } else {
+    # API 1.6+
+    $taxon = $taxdb->get_taxon(-taxonid => $taxid);
+  }
   my %taxonomy;
   if ( $taxon ) {
-    while ( my $ancestor = $taxon->ancestor ) {
-      my $rank = $taxon->rank;
-      if ( $wanted_rank{$rank} ) {
-	$taxonomy{$rank} = join(" ", @{$taxon->name('scientific')});
+#    my @class = $taxon->classification(@tax_levels);
+#    warn("class is @class\n");
+    if ( $VERSION == version->parse('1.4') ) {
+      my $ancestor = $taxon->parent_id;
+      # root node parent_id is 1
+      while ( $ancestor > 1  ) {
+	my $rank = $taxon->rank;
+	if ( $wanted_rank{$rank} ) {
+	  $taxonomy{$rank} = $taxon->node_name;
+	}
+	$taxon = $taxdb->get_Taxonomy_Node(-taxonid => $ancestor);
+	$ancestor = $taxon->parent_id;
       }
-      $taxon = $ancestor;
+    } else {
+      while ( my $ancestor = $taxon->ancestor ) {
+	my $rank = $taxon->rank;
+	if ( $wanted_rank{$rank} ) {
+	  $taxonomy{$rank} = join(" ", @{$taxon->name('scientific')});
+	}
+	$taxon = $ancestor;
+      }
     }
+
   } else {
     warn("cannot find taxon for $taxid ($name)\n");
   }
