44f85b571bbb95c3e6148c79c7a60811d91b4d2f
Bindu Gajria <bindu.gajria@gmail.com>
2014-10-03 14:51:25 +0000

    fix as per new schema - refs #17373
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@64852 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropPathwayTables.sql
+++ Load/lib/sql/apidbschema/dropPathwayTables.sql
@@ -1,3 +1,7 @@
+DROP TABLE ApiDB.ReactionXRefs;
+DROP TABLE ApiDB.ReactionRelationship;
+DROP TABLE ApiDB.Reaction;
+
 DROP TABLE ApiDB.PathwayRelationship;
 DROP TABLE ApiDB.PathwayNode;
 DROP TABLE ApiDB.Pathway;
@@ -6,9 +10,12 @@ DROP TABLE ApiDB.Pathway;
 DROP SEQUENCE ApiDB.Pathway_SQ;
 DROP SEQUENCE ApiDB.PathwayNode_SQ;
 DROP SEQUENCE ApiDB.PathwayRelationship_SQ;
+DROP SEQUENCE ApiDB.Reaction_SQ;
+DROP SEQUENCE ApiDB.ReactionRelationship_SQ;
+DROP SEQUENCE ApiDB.ReactionXRefs_SQ;
 
 DELETE FROM core.TableInfo
-WHERE lower(name) in ('pathway', 'pathwaynode', 'pathwayrelationship')
+WHERE lower(name) in ('pathway', 'pathwaynode', 'pathwayrelationship','reaction', 'reactionrelationship', 'reactionxrefs')
   AND database_id = (SELECT database_id
                      FROM core.DatabaseInfo
                      WHERE lower(name) = 'apidb');
