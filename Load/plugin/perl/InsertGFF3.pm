7dd4f2c29ece41f849b49cf43663213067d2d8dc
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2014-12-16 14:28:06 +0000

    Broke - rolled back to last good commit (65698)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@65728 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertGFF3.pm
+++ Load/plugin/perl/InsertGFF3.pm
@@ -158,7 +158,7 @@ sub new {
   my $args = &getArgsDeclaration();
 
   my $configuration = { requiredDbVersion => 4.0,
-                        cvsRevision => '$Revision: 65727 $',
+                        cvsRevision => '$Revision: 65728 $',
                         name => ref($self),
                         argsDeclaration => $args,
                         documentation => $documentation
