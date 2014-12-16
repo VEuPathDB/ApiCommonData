f20b59d0977fec0972ec582cd97436e3d769ff1b
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2014-12-16 11:56:06 +0000

    Fix cvs revision
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@65726 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertGFF3.pm
+++ Load/plugin/perl/InsertGFF3.pm
@@ -158,7 +158,7 @@ sub new {
   my $args = &getArgsDeclaration();
 
   my $configuration = { requiredDbVersion => 4.0,
-                        cvsRevision => '$Revision: 65918 $',
+                        cvsRevision => '$Revision: 65726 $',
                         name => ref($self),
                         argsDeclaration => $args,
                         documentation => $documentation
