ba84c8ffa8c62bc8692b16528b8fff6a3a69d8bf
John Brestelli <jbrestell@gmail.com>
2015-02-04 17:08:17 +0000

    Pathway appended to table names
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@66265 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createPathwayTables.sql
+++ Load/lib/sql/apidbschema/createPathwayTables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE ApiDB.Reaction (
-  REACTION_ID                   NUMBER(12)    NOT NULL,
+CREATE TABLE ApiDB.PathwayReaction (
+  PATHWAY_REACTION_ID                   NUMBER(12)    NOT NULL,
   SOURCE_ID                     VARCHAR(50)   NOT NULL,
   DESCRIPTION                   VARCHAR(255),
   EQUATION                    	VARCHAR(255),  
@@ -14,13 +14,13 @@ CREATE TABLE ApiDB.Reaction (
   ROW_GROUP_ID                  NUMBER(4),
   ROW_PROJECT_ID        	NUMBER(4),
   ROW_ALG_INVOCATION_ID		NUMBER(12),
-  PRIMARY KEY (REACTION_ID)
+  PRIMARY KEY (PATHWAY_REACTION_ID)
 );
 
 
-CREATE TABLE ApiDB.ReactionRelationship (
-  REACTION_RELATIONSHIP_ID       NUMBER(12)  NOT NULL,
-  REACTION_ID               	 NUMBER(10)  NOT NULL,
+CREATE TABLE ApiDB.PathwayReactionRel (
+  PATHWAY_REACTION_REL_ID       NUMBER(12)  NOT NULL,
+  PATHWAY_REACTION_ID               	 NUMBER(10)  NOT NULL,
   PATHWAY_RELATIONSHIP_ID 	 NUMBER(12)  NOT NULL,
   PATHWAY_ID 		  	 NUMBER(12),
   MODIFICATION_DATE              DATE,
@@ -34,15 +34,15 @@ CREATE TABLE ApiDB.ReactionRelationship (
   ROW_GROUP_ID                   NUMBER(3),
   ROW_PROJECT_ID                 NUMBER(4),
   ROW_ALG_INVOCATION_ID          NUMBER(12),
-  PRIMARY KEY (REACTION_RELATIONSHIP_ID),
-  FOREIGN KEY (REACTION_ID) REFERENCES ApiDB.Reaction (REACTION_ID),
+  PRIMARY KEY (PATHWAY_REACTION_REL_ID),
+  FOREIGN KEY (PATHWAY_REACTION_ID) REFERENCES ApiDB.PathwayReaction (PATHWAY_REACTION_ID),
   FOREIGN KEY (PATHWAY_RELATIONSHIP_ID) REFERENCES SRes.PathwayRelationship (PATHWAY_RELATIONSHIP_ID)
 );
 
 
-CREATE TABLE ApiDB.ReactionXRefs (
-  REACTION_XREFS_ID   		 NUMBER(12)  NOT NULL,
-  REACTION_ID               	 NUMBER(10)  NOT NULL,
+CREATE TABLE ApiDB.PathwayReactionXRef (
+  PATHWAY_REACTION_XREF_ID   		 NUMBER(12)  NOT NULL,
+  PATHWAY_REACTION_ID               	 NUMBER(10)  NOT NULL,
   ASSOCIATED_REACTION_ID 	 NUMBER(10)  NOT NULL,
   EXTERNAL_DATABASE_RELEASE_ID   NUMBER(10)  NOT NULL,
   MODIFICATION_DATE       	 DATE,
@@ -56,25 +56,25 @@ CREATE TABLE ApiDB.ReactionXRefs (
   ROW_GROUP_ID            	 NUMBER(4),
   ROW_PROJECT_ID          	 NUMBER(4),
   ROW_ALG_INVOCATION_ID   	 NUMBER(12),
-  PRIMARY KEY (REACTION_XREFS_ID),
-  FOREIGN KEY (REACTION_ID) REFERENCES ApiDB.Reaction (REACTION_ID),
-  FOREIGN KEY (ASSOCIATED_REACTION_ID) REFERENCES ApiDB.Reaction (REACTION_ID)
+  PRIMARY KEY (PATHWAY_REACTION_XREF_ID),
+  FOREIGN KEY (PATHWAY_REACTION_ID) REFERENCES ApiDB.PathwayReaction (PATHWAY_REACTION_ID),
+  FOREIGN KEY (ASSOCIATED_REACTION_ID) REFERENCES ApiDB.PathwayReaction (PATHWAY_REACTION_ID)
 );
 
 
-CREATE SEQUENCE ApiDB.Reaction_SQ;
-CREATE SEQUENCE ApiDB.ReactionRelationship_SQ;
-CREATE SEQUENCE ApiDB.ReactionXRefs_SQ;
+CREATE SEQUENCE ApiDB.PathwayReaction_SQ;
+CREATE SEQUENCE ApiDB.PathwayReactionRel_SQ;
+CREATE SEQUENCE ApiDB.PathwayReactionXRef_SQ;
 
 
-GRANT SELECT ON ApiDB.Reaction TO gus_r;
-GRANT SELECT ON ApiDB.ReactionRelationship TO gus_r;
-GRANT SELECT ON ApiDB.ReactionXRefs TO gus_r;
+GRANT SELECT ON ApiDB.PathwayReaction TO gus_r;
+GRANT SELECT ON ApiDB.PathwayReactionRel TO gus_r;
+GRANT SELECT ON ApiDB.PathwayReactionXRef TO gus_r;
 
 
-GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.Reaction TO gus_w;
-GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.ReactionRelationship TO gus_w;
-GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.ReactionXRefs TO gus_w;
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.PathwayReaction TO gus_w;
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.PathwayReactionRel TO gus_w;
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.PathwayReactionXRef TO gus_w;
 
 
 INSERT INTO core.TableInfo
@@ -83,14 +83,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'Reaction',
-       'Standard', 'REACTION_ID',
+SELECT core.tableinfo_sq.nextval, 'PathwayReaction',
+       'Standard', 'PATHWAY_REACTION_ID',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'Reaction' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'PathwayReaction' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 INSERT INTO core.TableInfo
@@ -99,14 +99,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'ReactionRelationship',
-       'Standard', 'REACTION_RELATIONSHIP_ID',
+SELECT core.tableinfo_sq.nextval, 'PathwayReactionRel',
+       'Standard', 'PATHWAY_REACTION_REL_ID',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'ReactionRelationship' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'PathwayReactionRel' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 INSERT INTO core.TableInfo
@@ -115,14 +115,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, 'ReactionXRefs',
-       'Standard', 'REACTION_XREFS_ID',
+SELECT core.tableinfo_sq.nextval, 'PathwayReactionXRef',
+       'Standard', 'PATHWAY_REACTION_XREF_ID',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-WHERE 'ReactionXRefs' NOT IN (SELECT name FROM core.TableInfo
+WHERE 'PathwayReactionXRef' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 
