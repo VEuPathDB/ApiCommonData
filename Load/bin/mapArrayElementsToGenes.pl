84695a16e56e8ac579b1f6d753d3097c88198b5f
John Brestelli <jbrestell@gmail.com>
2015-03-23 22:43:27 +0000

    gene to probe mapping for gus 4
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@66979 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/mapArrayElementsToGenes.pl
+++ Load/bin/mapArrayElementsToGenes.pl
@@ -27,17 +27,9 @@ use IO::File;
 use DBI;
 use CBIL::Util::PropertySet;
 
+use GUS::Community::GeneModelLocations;
 
-
-#----Usage/Example----
-# perl    mapArrayElementsToGenes.pl    --aefExtDbSpec     --geneExtDbSpec     --aefSense "sense"
-#---------------------
-
-
-#my $startTime = time();
-#print ("Started. Time: $startTime\n");
-
-#------- Uid , Password and DSN ..these are fetched from the gus.config file----------
+use Data::Dumper;
 
 my ($gusConfigFile);
 $gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config" unless($gusConfigFile);
@@ -57,7 +49,6 @@ my $dsn = $gusconfig->{props}->{dbiDsn};
 my $dbh = DBI->connect($dsn, $u, $pw) or die DBI::errstr;
 #----------------------------------------------------------------------
 
-
 my ($geneExtDbSpec,$aefExtDbSpec,$aefSense,$outputFile,$delimiter);
 
 &GetOptions('aefExtDbSpec=s' => \$aefExtDbSpec,
@@ -75,6 +66,38 @@ die "ERROR: Please provide a valid delimiter ('\\t', ',') for the Array Element 
 
 my $geneExtDbRlsId = getDbRlsId($geneExtDbSpec);
 my $aefExtDbRlsId = getDbRlsId($aefExtDbSpec);
+
+
+my $geneModelLocations = GUS::Community::GeneModelLocations->new($dbh, $geneExtDbRlsId, 1);
+
+
+my $geneSourceIds = $geneModelLocations->getAllGeneIds();
+
+my %exonLocations;
+
+foreach my $geneSourceId (@$geneSourceIds) {
+  my $geneModelHash = $geneModelLocations->getGeneModelHashFromGeneSourceId($geneSourceId);
+  my $sequenceSourceId = $geneModelHash->{na_sequence_id};
+
+  foreach my $exonSourceId (keys %{$geneModelHash->{exons}}) {
+
+    my $exon = $geneModelHash->{exons}->{$exonSourceId};    
+
+    my $isReversed = $exon->{strand} == -1 ? 1 : 0;
+    my $exonLocation = [$geneSourceId, $exon->{start}, $exon->{end}, $isReversed ];
+
+    push @{$exonLocations{$sequenceSourceId}}, $exonLocation;
+  }
+}
+
+my %sortedExonLocations;
+
+foreach my $sequenceSourceId (keys %exonLocations) {
+  my @sortedExons = sort {$a->[1] <=> $b->[1]} @{$exonLocations{$sequenceSourceId}};
+
+  push @{$sortedExonLocations{$sequenceSourceId}}, @sortedExons;
+}
+
 print STDERR "Extracting Array Element Features.....\n";
 
 my $aefSql = "select rl.na_sequence_id
@@ -95,88 +118,53 @@ my $sth = $dbh->prepare($aefSql);
 
 $sth->execute || die "Could not execute SQL statement!";
 
-my (%startAefHash,%endAefHash,%senseHash,%nameHash);
+my %reporters;
 
-while( my ($aefSeqId,$aefSourceId,$aefName,$aefStartMin,$aefEndMax,$reversed) = $sth->fetchrow_array() ){
-       $startAefHash{$aefSeqId}{$aefSourceId} = $aefStartMin;
-       $endAefHash{$aefSeqId}{$aefSourceId} =  $aefEndMax;
-       $senseHash{$aefSourceId} = $reversed;
-       $nameHash{$aefSourceId} = $aefName; 
+while( my ($naSeqId, $sourceId, $name, $start, $end, $isReversed) = $sth->fetchrow_array() ){
+  push @{$reporters{$naSeqId}}, [$name, $start, $end, $isReversed];
 } 
-
-print STDERR "Done extracting Array Element Features.\n";
-
-
-
-print STDERR "Extracting Exon Features.....\n";
- 
-my $exonSql = "select ef.na_sequence_id,
-                      ef.parent_id,
-                      ef.source_id,
-                      ef.coding_start,
-                      ef.coding_end 
-               from   dots.exonfeature ef 
-               where  ef.external_database_release_id = $geneExtDbRlsId";
-
-my $sth = $dbh->prepare($exonSql);
-
-$sth->execute || die "Could not execute SQL statement!";
-
-my (%startExonHash,%endExonHash,%geneExonHash);
-
-while( my ($exonSeqId,$exonParentId,$exonSourceId,$exonStart,$exonEnd) = $sth->fetchrow_array() ){
-    $startExonHash{$exonSeqId}{$exonSourceId} = $exonStart;
-    $endExonHash{$exonSeqId}{$exonSourceId} = $exonEnd; 
-    push @{ $geneExonHash{$exonParentId} },$exonSourceId;
-}
-
-print STDERR "Done extracting Exon Features.\n";
-
-
-
-my $geneSql = "select gf.source_id, 
-                      gf.na_sequence_id,
-                      nl.is_reversed,
-                      gf.na_feature_id 
-               from   dots.genefeature gf,
-                      dots.nalocation nl
-               where  gf.na_feature_id = nl.na_feature_id
-               and    gf.external_database_release_id = $geneExtDbRlsId";
-#quicktest     and    gf.source_id = 'PF13_0333'";
-
-my $sth = $dbh->prepare($geneSql);
-
-$sth->execute || die "Could not execute SQL statement!";
-
-print STDERR "Mapping Array Element Feature to Genes.....\n";
 
 open (mapFile, ">$outputFile");
 
 $delimiter = "\t" if ($delimiter ne ",");
 
-while( my ($geneSourceId,$geneSeqId,$geneReverse,$geneFeatureId) = $sth->fetchrow_array() ){
-        
-    my ($aefFeatureCount,@aefList,$exon,$aef);
+foreach my $sequenceSourceId (keys %sortedExonLocations) {
+  my %geneMap;
 
-    foreach $exon ( @{$geneExonHash{$geneFeatureId} }) {
-        foreach $aef ( keys %{ $startAefHash{$geneSeqId} } ) {
-            if ($geneReverse) {
-                next if ( ($aefSense eq 'sense' && $senseHash{$aef} == 0) || ($aefSense eq 'antisense' && $senseHash{$aef} == 1) );
-                if  ($startAefHash{$geneSeqId}{$aef} < $startExonHash{$geneSeqId}{$exon} && $endAefHash{$geneSeqId}{$aef} > $endExonHash{$geneSeqId}{$exon}) {
-                    $aefFeatureCount = $aefFeatureCount + 1;
-                    push @aefList, $nameHash{$aef};
-                } 
-            } else {
-                next if ( ($aefSense eq 'sense' && $senseHash{$aef} == 1) || ($aefSense eq 'antisense' && $senseHash{$aef} == 0) );
-                if  ($startAefHash{$geneSeqId}{$aef} < $endExonHash{$geneSeqId}{$exon} && $endAefHash{$geneSeqId}{$aef} > $startExonHash{$geneSeqId}{$exon}) {
-                    $aefFeatureCount = $aefFeatureCount + 1;
-                    push @aefList, $nameHash{$aef};
-                } 
-            }
-        }
+  foreach my $exonLoc (@{$sortedExonLocations{$sequenceSourceId}}) {
+    my $geneSourceId = $exonLoc->[0];
+    my $exonStart = $exonLoc->[1];
+    my $exonEnd = $exonLoc->[2];
+    my $exonIsReversed = $exonLoc->[3];
+
+    foreach my $reporter (@{$reporters{$sequenceSourceId}}) {
+      my $probeName = $reporter->[0];
+      my $probeStart = $reporter->[1];
+      my $probeEnd = $reporter->[2];
+      my $probeIsReversed = $reporter->[3];
+      
+      if ($exonIsReversed) {
+        next if(($aefSense eq 'sense' && $probeIsReversed == 0) || ($aefSense eq 'antisense' && $probeIsReversed == 1));
+      } 
+      else {
+        next if(($aefSense eq 'sense' && $probeIsReversed == 1) || ($aefSense eq 'antisense' && $probeIsReversed == 0));
+      }
+
+      if($probeStart < $exonEnd && $probeEnd > $exonStart) {
+        push @{$geneMap{$geneSourceId}}, $probeName;
+      }
     }
-    print (mapFile "$geneSourceId\t".join ("$delimiter",@aefList)."\n") unless (scalar @aefList <1);
+  }
+
+  foreach my $gene (keys %geneMap) {
+    my @probes = @{$geneMap{$gene}};
+    next if(scalar @probes < 1);
+    print mapFile "$gene\t" . join ("$delimiter", @probes) . "\n";
+  }
 }
+
+
+$dbh->disconnect();
 
 sub getDbRlsId {
 
