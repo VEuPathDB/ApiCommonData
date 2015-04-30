e44f904214ce7a3f5b9e481f8c7577ee5a23cf4c
John Iodice <iodice@pennmedicine.upenn.edu>
2015-04-30 21:08:23 +0000

    initial check-in
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@67585 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropTestTuningSchema.sql
@@ -0,0 +1,10 @@
+DROP USER TestTuning CASCADE;
+
+DELETE FROM core.TableInfo
+WHERE database_id IN (SELECT database_id
+                      FROM core.DatabaseInfo
+                      WHERE name = 'TestTuning');
+
+DELETE FROM core.DatabaseInfo WHERE name = 'TestTuning';
+
+exit;
