07907c690222c2d3c9a2722a6510aefea53e6294
John Iodice <iodice@pennmedicine.upenn.edu>
2015-07-21 18:56:52 +0000

    prefixed_project_id: use "like" to handle wildcards
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@68878 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createStoredProcedures.sql
+++ Load/lib/sql/apidbschema/createStoredProcedures.sql
@@ -413,7 +413,7 @@ begin
       execute immediate
          'select distinct project_id ' ||
          'from ApidbTuning.' || prefix || 'ProjectTaxon pt ' ||
-         'where pt.taxon = substr(lower(''' || organism || '''), 1, length(pt.taxon)) '
+         'where pt.taxon like substr(lower(''' || organism || '''), 1, length(pt.taxon)) '
       into project;
       exception
          when NO_DATA_FOUND then
