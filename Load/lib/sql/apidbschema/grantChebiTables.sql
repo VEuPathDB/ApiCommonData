e3dfb13ff5c856f25d89f92078443f78a3a24307
John Brestelli <jbrestell@gmail.com>
2015-07-28 14:30:54 +0000

    add Structures to grant tables
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@69016 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/grantChebiTables.sql
+++ Load/lib/sql/apidbschema/grantChebiTables.sql
@@ -202,4 +202,24 @@ WHERE 'Default_Structures' NOT IN (SELECT name FROM core.TableInfo
 GRANT INSERT, SELECT, UPDATE, DELETE ON chebi.autogen_structures TO gus_w;
 GRANT SELECT ON chebi.autogen_structures TO gus_r;
 
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable,
+     modification_date, user_read, user_write, group_read, group_write,
+     other_read, other_write, row_user_id, row_group_id, row_project_id,
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'Structures',
+       'Custom', 'id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE name = 'chEBI') d
+WHERE 'Structures' NOT IN (SELECT name FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
+GRANT INSERT, SELECT, UPDATE, DELETE ON chebi.structures TO gus_w;
+GRANT SELECT ON chebi.structures TO gus_r;
+
+
 exit;
