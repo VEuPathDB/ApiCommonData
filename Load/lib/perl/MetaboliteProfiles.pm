dc2c849e64ee2342d4c83f07932f1c6dd2f04bf7
John Brestelli <jbrestell@gmail.com>
2015-08-11 11:12:12 +0000

    override protocol name for metabolite profiles
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@69211 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/MetaboliteProfiles.pm
@@ -0,0 +1,12 @@
+package ApiCommonData::Load::MetaboloteProfiles;
+use base qw(CBIL::TranscriptExpression::DataMunger::Profiles);
+
+use strict;
+
+
+sub getProtocolName {
+  return "MetaboliteProfiles";
+}
+
+
+1;
