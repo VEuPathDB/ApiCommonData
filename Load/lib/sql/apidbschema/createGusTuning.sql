c62006fbb4033cd4cbc647f15320b8e427c503e2
John Brestelli <jbrestell@gmail.com>
2015-08-18 18:21:29 +0000

    add nafeaturehostresponse table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@69367 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGusTuning.sql
+++ Load/lib/sql/apidbschema/createGusTuning.sql
@@ -378,6 +378,56 @@ WHERE 'reporterintensity' NOT IN (SELECT lower(name) FROM core.TableInfo
 
 --------------------------------------------------------------------------------
 
+
+create table RESULTS.NAFeatureHostResponse
+  (
+    NA_FEATURE_HOST_RESPONSE_ID number(12) not null,
+    PROTOCOL_APP_NODE_ID   number(10) not null,
+    na_feature_ID            number(12) not null,
+    value float(126),
+    MODIFICATION_DATE     date not null,
+    USER_READ             number(1) not null,
+    USER_WRITE            number(1) not null,
+    GROUP_READ            number(1) not null,
+    GROUP_WRITE           number(1) not null,
+    OTHER_READ            number(1) not null,
+    OTHER_WRITE           number(1) not null,
+    ROW_USER_ID           number(12) not null,
+    ROW_GROUP_ID          number(4) not null,
+    ROW_PROJECT_ID        number(4) not null,
+    ROW_ALG_INVOCATION_ID number(12) not null,
+    foreign key (PROTOCOL_APP_NODE_ID) references STUDY.PROTOCOLAPPNODE,
+    foreign key (NA_FEATURE_ID) references DoTS.NAFeature,
+    primary key (NA_FEATURE_HOST_RESPONSE_ID)
+  );
+
+
+create sequence RESULTS.NAFEATUREHOSTRESPONSE_SQ;
+
+GRANT insert, select, update, delete ON  RESULTS.NAFEATUREHOSTRESPONSE TO gus_w;
+GRANT select ON RESULTS.NAFEATUREHOSTRESPONSE TO gus_r;
+GRANT select ON RESULTS.NAFEATUREHOSTRESPONSE_sq TO gus_w;
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     ROW_ALG_INVOCATION_ID)
+select CORE.TABLEINFO_SQ.NEXTVAL, 'NaFeatureHostResponse',
+       'Standard', 'na_feature_host_response_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (select DATABASE_ID from CORE.DATABASEINFO where name = 'Results') D
+WHERE 'nafeaturehostresponse' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    where DATABASE_ID = D.DATABASE_ID);
+
+
+--------------------------------------------------------------------------------
+
+
 create table Results.CompoundMassSpec
   (
     compound_mass_spec_id number(12) not null,
