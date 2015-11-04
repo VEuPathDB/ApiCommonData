f0127adb4d64a3d67ed2fe2310cb9c4c0473c0f8
Deborah Pinney <pinney@upenn.edu>
2015-11-04 20:46:50 +0000

    new script to create a gene product file that denotes the preferred product
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/triTryp_bld_18@70578 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/processProductAddPreferred.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+use lib "$ENV{GUS_HOME}/lib/perl";
+use strict;
+use Getopt::Long;
+
+my ($inputFile, $outputFile, $help);
+
+&GetOptions(
+            'help|h' => \$help,
+            'inputFile=s' => \$inputFile,
+            'outputFile=s' => \$outputFile,
+           );
+
+&usage() if($help);
+&usage("Missing a Required Argument") unless(defined $inputFile && $outputFile);
+
+my (%products, %isPreferred, %isPreferredProd, %assignedProduct);
+
+open (IN, $inputFile) || die "can not open inputFile to read\n";
+open(OUT,">$outputFile") || die "can not open outputFile to write\n";
+
+while (<IN>) {
+  chomp;
+  my @items= split (/\t/, $_);
+
+  ## check if the product name has been assigned to this gene
+  if (!$assignedProduct{$items[0]}{$items[1]}) {
+    push @{$products{$items[0]}}, $items[1];
+    $assignedProduct{$items[0]}{$items[1]} = 1;
+  }
+
+  if ($items[2] == 1 || $items[2] =~ /true/) {
+    $isPreferred{$items[0]} = 1;
+    $isPreferredProd{$items[0]}{$items[1]} = 1;
+  }
+}
+
+my $is_preferred;
+foreach my $k (sort keys %products) {
+#  print STDERR "$k, $#{$products{$k}}\n";
+  foreach my $i (0..$#{$products{$k}}) {
+    if ($#{$products{$k}} == 0 ) {
+      $is_preferred = 1;
+    } else {
+      if ($isPreferred{$k}) {
+	if ($isPreferredProd{$k}{$products{$k}[$i]}) {
+	  $is_preferred = 1;
+	} else {
+	  $is_preferred = 0;
+	}
+      } else {
+	$is_preferred = ($i == 0) ? 1 : 0;
+      }
+    }
+    print OUT "$k\t$products{$k}[$i]\t$is_preferred\n";
+  }
+}
+close IN;
+close OUT;
+
+
+sub usage {
+  die
+"
+Process product name file. If there is only one product per gene, the 3nd column add value 1.
+If there is more than one product name per gene, the 3nd column add value 1 if there is is_preferred = true or 1,
+otherwise the 3nd column add value 0.
+
+Usage:  perl processProductAddPreferred.pl --inputFile product.txt --outputFile isPreferredProduct.txt
+
+where
+  --inputFile:   the input file, two or three column 
+  --outputFile: the output file with the 3nd column always has value
+";
+}
+
+
+
+
+
