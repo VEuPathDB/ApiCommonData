5d785d8c718029b22f8e4a1ed0eb33e746a7216e
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2015-11-19 21:18:50 +0000

    new dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@70786 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/createSeqAliasBaseSeqIdentity.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+use Bio::SeqIO;
+use Getopt::Long;
+use strict;
+
+
+my ($help, $ifAlias, $oldSequenceFile, $oldSequenceFormat, $newSequenceFile, $newSequenceFormat);
+
+&GetOptions('help|h' => \$help,
+            'oldSequenceFile=s' => \$oldSequenceFile,
+            'oldSequenceFormat=s' => \$oldSequenceFormat,
+            'newSequenceFile=s' => \$newSequenceFile,
+            'newSequenceFormat=s' => \$newSequenceFormat,
+            'ifAlias' => \$ifAlias,
+           );
+
+&usage() if($help);
+
+my ($oldSeq, $newSeq);
+
+my $oldSeq = getSeqsFromFile ($oldSequenceFile, $oldSequenceFormat);
+my $newSeq = getSeqsFromFile ($newSequenceFile, $newSequenceFormat);
+
+my $outputFile ="seqAliases.txt";
+open (OUT, ">$outputFile") || die "can not open output file to write\n";
+foreach my $k (sort keys %{$newSeq}) {
+  foreach my $kk (sort keys %{$oldSeq}) {
+    if ($oldSeq->{$kk} eq $newSeq->{$k}) {
+      print OUT "$k\t$kk\n";
+    }
+  }
+}
+close OUT;
+
+
+###############
+sub getSeqsFromFile {
+  my ($inputFile, $format) = @_;
+  my (%seqs);
+
+  if ($format =~ /genbank/i) {
+    my $bioperlSeqIO = Bio::SeqIO->new(-format => 'genbank',
+				       -file => $inputFile);
+    while (my $seq = $bioperlSeqIO->next_seq() ) {
+      my $sId = ($seq->id) ? ($seq->id) : ($seq->accession());
+      $seqs{$sId} = uc($seq->seq()) if ($sId && $seq->seq());
+    }
+
+  } elsif ($format =~ /fasta/i) {
+    my $sId;
+    open (IN, "$inputFile") || die "can not open inputFile to read\n";
+    while (<IN>) {
+      chomp;
+      if ($_ =~ /^>(\S+?)\s+/) {
+	$sId = $1;
+	print STDERR "duplicated sourceId $sId in the file $inputFile\n" if ($seqs{$sId});
+      } else {
+	my $curr = $_;
+	$curr =~ s/\s+//g;
+	$curr =~ s/\n//g;
+	$seqs{$sId} .= uc($curr);
+      }
+    }
+    close IN;
+
+  } else {
+    print STDERR "file format has not been configured yet\n";
+  }
+  return (\%seqs);
+}
+
+sub usage {
+  die
+"
+A script to use generate sequence aliases file based on sequence similarity, 100% identity
+
+The standard output file is seqAliases.txt 
+
+Usage: perl createSeqAliasBaseSeqIdentity.pl --oldSequenceFile pre.genome.gbf --oldSequenceFormat genbank 
+                                             --newSequenceFile current.genome.gbf --newSequenceFormat genbank
+
+where
+            --ifAlias: optional, either ifAlias or ifReused
+            --oldSequenceFile: old sequence file name
+            --oldSequenceFormat: old sequence file format
+            --newSequenceFile: new sequence file name
+            --newSequenceFormat: new annotation file format
+
+";
+}
