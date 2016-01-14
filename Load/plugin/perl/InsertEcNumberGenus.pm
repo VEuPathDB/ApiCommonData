15160d852a92837b6605de8e14f67384f55a7b58
Wei Li <weili1@pennmedicine.upenn.edu>
2016-01-14 18:37:18 +0000

    reviewed GUS4_STATUS rules
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@71801 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertEcNumberGenus.pm
+++ Load/plugin/perl/InsertEcNumberGenus.pm
@@ -18,7 +18,6 @@ package ApiCommonData::Load::Plugin::InsertEcNumberGenus;
   # GUS4_STATUS | ApiDB Tuning Gene              | auto   | absent
   # GUS4_STATUS | Rethink                        | auto   | absent
   # GUS4_STATUS | dots.gene                      | manual | unreviewed
-die 'This file has broken or unreviewed GUS4_STATUS rules.  Please remove this line when all are fixed or absent';
 #^^^^^^^^^^^^^^^^^^^^^^^^^ End GUS4_STATUS ^^^^^^^^^^^^^^^^^^^^
 
 @ISA = qw(GUS::PluginMgr::Plugin);
@@ -82,7 +81,7 @@ sub new {
   my $self = {};
   bless($self,$class);
 
-  $self->initialize({ requiredDbVersion => 3.6,
+  $self->initialize({ requiredDbVersion => 4.0,
                       cvsRevision       => '$Revision$',
                       name              => ref($self),
                       argsDeclaration   => $argsDeclaration,
