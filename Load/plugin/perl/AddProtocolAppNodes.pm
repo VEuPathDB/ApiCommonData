861e11b19f847514e88afb1eba1f85ced583109e
Ja’Shon Cade <jcade@upenn.edu>
2016-02-05 08:34:56 +0000

    find type ext db rls id look up
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/prism_06252014@72271 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/AddProtocolAppNodes.pm
+++ Load/plugin/perl/AddProtocolAppNodes.pm
@@ -29,9 +29,9 @@ sub handleExistingProtocolAppNode {
   my ($self,$protocolAppNode) = @_;
   my $source_id = $protocolAppNode->findvalue('./source_id');
   my $type = $protocolAppNode->findvalue('./type');
-  my $type_ext_db_rls_id = $protocolAppNode->findvalue('./ext_db_rls');
+  my $type_ext_db_rls_id = $protocolAppNode->findvalue('./type_ext_db_rls_id');
   my $typeTerm = GUS::Model::SRes::OntologyTerm->new({name => $type,
-                                                           external_database_release_id => $type_ext_db_rls_id});
+                                                      external_database_release_id => $type_ext_db_rls_id});
    $self->userError("Input Type $type is not in the database for external database release id $type_ext_db_rls_id, please make sure that all input protocoal app nodes have been loaded") unless $typeTerm->retrieveFromDB();
   my $type_id = $typeTerm->getId();
   my $protocolAppNode = GUS::Model::Study::ProtocolAppNode->new({name => $source_id,
