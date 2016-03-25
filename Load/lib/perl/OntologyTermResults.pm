d97f17d9daa091ecf8921f050f1c28e805e52510
John Brestelli <jbrestell@gmail.com>
2016-03-25 19:19:01 +0000

    add module for Simple ontology term results
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@73532 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/OntologyTermResults.pm
@@ -0,0 +1,9 @@
+package ApiCommonData::Load::OntologyTermResults;
+use base qw(CBIL::TranscriptExpression::DataMunger::NoSampleConfigurationProfiles);
+
+sub getProtocolName {
+  return "simple_ontology_term_results";
+}
+
+1;
+
