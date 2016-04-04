32400ab0cf1ab0ac65af198baafbddec036c3822
Haiming Wang <hwang@uga.edu>
2016-04-04 20:21:49 +0000

    update sql to use dots tables instead of tm tables
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@73788 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/getGenesByTaxonAndEcNumber.pl
+++ Load/bin/getGenesByTaxonAndEcNumber.pl
@@ -23,41 +23,105 @@ my $db = GUS::ObjRelP::DbiDatabase->new($gusconfig->getDbiDsn(),
 
 my $dbh = $db->getQueryHandle(0);
 
-my $sql = "select source_id, ec_numbers,ortholog_number, paralog_number, orthomcl_name from apidbtuning.GENEATTRIBUTES order by  ORGANISM, species, source_id";
+my $sql =<<SQL;
+SELECT gf.source_id, ec.ec_numbers, orthologs.name as ortholog_name, tas.taxon_id
+FROM dots.TranslatedAaSequence tas, 
+     dots.GeneFeature gf, 
+     dots.Transcript t, 
+     dots.TranslatedAaFeature taf,
+     (SELECT aa_sequence_id,
+             SUBSTR(apidb.tab_to_string(set(cast(COLLECT(ec_number order by ec_number)
+                    as apidb.varchartab)), '; '), 1, 300) AS ec_numbers
+      FROM (SELECT DISTINCT asec.aa_sequence_id,
+                 ec.ec_number || ' (' || ec.description || ')' AS ec_number
+            FROM dots.AaSequenceEnzymeClass asec, sres.EnzymeClass ec
+            WHERE ec.enzyme_class_id = asec.enzyme_class_id
+             AND NOT asec.evidence_code = 'OrthoMCLDerived'
+            )
+      GROUP BY aa_sequence_id) ec,
+      (select gf.na_feature_id, sg.name
+       from dots.genefeature gf, dots.SequenceSequenceGroup ssg, 
+            dots.SequenceGroup sg, core.TableInfo ti
+       where gf.na_feature_id = ssg.sequence_id
+         and ssg.sequence_group_id = sg.sequence_group_id
+         and ssg.source_table_id = ti.table_id
+         and ti.name = 'GeneFeature') orthologs
+WHERE tas.aa_sequence_id = ec.aa_sequence_id(+)
+  AND t.na_feature_id = taf.na_feature_id
+  AND taf.aa_sequence_id = tas.aa_sequence_id
+  and gf.na_feature_id = t.parent_id
+  AND gf.na_feature_id = orthologs.na_feature_id(+)
+SQL
 
 my $sth = $dbh->prepare($sql);
 $sth->execute;
 
-open OUT, ">$outputGenesByTaxon";
-print OUT "[Gene ID] [EC Numbers] [Ortholog count] [Paralog count] [Ortholog Group]\n";
+my %hash;
 while (my $row = $sth -> fetchrow_arrayref) {
-  my ($source_id, $ec, $ortholog_number, $paralog_number, $orthomcl_name) = @$row;
-  print OUT "$source_id, $ec, $ortholog_number, $paralog_number, $orthomcl_name\n";
+  my ($source_id, $ec,  $orthomcl_name) = @$row;
+  $hash{$source_id} = { ec              => $ec, 
+                        orthomcl_name   => $orthomcl_name, 
+                        ortholog_number => 0, 
+                        paralog_number  => 0 };
 }
 
 $sth->finish;
-close OUT;
 
-open OUT, ">$outputGenesByEcNumber";
+# get ortholog_number
+my $sql_ortholog =<<SQL;
+WITH A as 
+( $sql )
+SELECT a1.source_id, count(*) as ortholog_number from A a1, A a2
+WHERE a1.ortholog_name = a2.ortholog_name
+  AND a1.taxon_id != a2.taxon_id
+GROUP BY a1.source_id
+SQL
 
-$sql =<<EOL;
-SELECT DISTINCT ga.source_id, ec_numbers,ortholog_number, paralog_number, orthomcl_name
-FROM dots.aaSequenceEnzymeClass asec,
-sres.enzymeClass ec, ApidbTuning.GeneAttributes ga
-WHERE ga.aa_sequence_id = asec.aa_sequence_id
-AND asec.enzyme_class_id = ec.enzyme_class_id
-AND asec.evidence_code != 'OrthoMCLDerived'
-order by ga.source_id
-EOL
-
-print OUT "[Gene ID] [EC Numbers] [Ortholog count] [Paralog count] [Ortholog Group]\n";
-
-$sth = $dbh->prepare($sql);
+$sth = $dbh->prepare($sql_ortholog);
 $sth->execute;
 
 while (my $row = $sth -> fetchrow_arrayref) {
-  my ($source_id, $ec, $ortholog_number, $paralog_number, $orthomcl_name) = @$row;
+  my ($source_id, $ortholog_number) = @$row;
+  $hash{$source_id}->{ortholog_number} = $ortholog_number;
+}
+
+$sth->finish;
+
+# get paralog_number
+my $sql_paralog =<<SQL;
+WITH A as 
+( $sql )
+SELECT a1.source_id, count(*) as paralog_number from A a1, A a2
+WHERE a1.ortholog_name = a2.ortholog_name
+  AND a1.taxon_id = a2.taxon_id
+  AND a1.source_id != a2.source_id
+GROUP BY a1.source_id
+SQL
+
+$sth = $dbh->prepare($sql_paralog);
+$sth->execute;
+
+while (my $row = $sth -> fetchrow_arrayref) {
+  my ($source_id, $paralog_number) = @$row;
+  $hash{$source_id}->{paralog_number} = $paralog_number;
+}
+
+$sth->finish;
+
+# get all data out
+open OUT, ">$outputGenesByTaxon";
+open OUT_EC, ">$outputGenesByEcNumber";
+print OUT "[Gene ID] [EC Numbers] [Ortholog count] [Paralog count] [Ortholog Group]\n";
+print OUT_EC "[Gene ID] [EC Numbers] [Ortholog count] [Paralog count] [Ortholog Group]\n";
+
+while(my ($k, $v) = each %hash) {
+  my $source_id = $k;
+  my $ec = $hash{$k}->{ec};
+  my $ortholog_number = $hash{$k}->{ortholog_number};
+  my $paralog_number = $hash{$k}->{paralog_number};
+  my $orthomcl_name = $hash{$k}->{orthomcl_name};
   print OUT "$source_id, $ec, $ortholog_number, $paralog_number, $orthomcl_name\n";
+  print OUT_EC "$source_id, $ec, $ortholog_number, $paralog_number, $orthomcl_name\n" if $ec; 
 }
 
 $dbh->disconnect; 
