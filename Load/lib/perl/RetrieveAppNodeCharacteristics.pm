3ed3c548a179ea8e7b9aa6d0ee64af76499a0376
Ja’Shon Cade <jcade@upenn.edu>
2016-04-08 01:05:00 +0000

    moved generateRecords.pl
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/prism_06252014@73860 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/RetrieveAppNodeCharacteristics.pm
+++ Load/lib/perl/RetrieveAppNodeCharacteristics.pm
@@ -113,6 +113,7 @@ sub cleanAttr {
   $attribute =~ s/^average_asexual_parasite_density_of_a_participant_with/avg_parasite_density_/;
   $attribute =~ s/^average_number_of_clinical_visits_with/avg_annual_visits_with_/;
   $attribute =~ s/^number_of_clinical_visits_resulting_in/num_visits_with_/;
+  $attribute =~ s/^number_of_/num_/;
 
   # my $old_parent_id_string = "parent_id";
   # my $parent_id_string = "parent_id";
