346e35e2d06dbd09606f515ad336ba14452a5eca
John Iodice <iodice@pennmedicine.upenn.edu>
2016-05-23 20:37:50 +0000

    GeneGff: add GUS columns, et c.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@74563 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createReportCache.sql
+++ Load/lib/sql/apidbschema/createReportCache.sql
@@ -149,9 +149,21 @@ CREATE TABLE apidb.GeneGff (
  table_name VARCHAR2(80),
  row_count  NUMBER(4),
  content    CLOB,
- primary key (wdk_table_id),
- modification_date date
+ modification_date date,
+ USER_READ             NUMBER(1),
+ USER_WRITE            NUMBER(1),
+ GROUP_READ            NUMBER(1),
+ GROUP_WRITE           NUMBER(1),
+ OTHER_READ            NUMBER(1),
+ OTHER_WRITE           NUMBER(1),
+ ROW_USER_ID           NUMBER(12),
+ ROW_GROUP_ID          NUMBER(3),
+ ROW_PROJECT_ID        NUMBER(4),
+ ROW_ALG_INVOCATION_ID NUMBER(12),
+ primary key (wdk_table_id)
 );
+
+CREATE SEQUENCE ApiDB.GeneGff_sq;
 
 CREATE INDEX apidb.ggff_ix
        ON apidb.GeneGff (source_id, table_name, row_count);
@@ -168,6 +180,22 @@ begin
   :new.modification_date := sysdate;
 end;
 /
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'GeneGff',
+       'Standard', 'WDK_TABLE_ID',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
+WHERE 'genegff' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    where database_id = d.database_id);
 
 ------------------------------------------------------------------------------
 CREATE SEQUENCE apidb.geneGff_pkseq;
