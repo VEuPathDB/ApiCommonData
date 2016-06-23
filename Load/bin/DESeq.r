71a2c2fb01af401417f48e414236e39c0052f864
Jane Pulman <jane.pulman@liverpool.ac.uk>
2016-06-23 16:01:40 +0000

    adding DESeq.r R script to run deseq2 using the DESeqAnalysis.pm a part of RnaSeqAnalysis.pm
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@75190 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/DESeq.r
@@ -0,0 +1,36 @@
+#!/usr/bin/Rscript
+# Command line arguments for R are not fun... so to make this easy create the args
+# by prepending them to this script directly before the R call:
+# [bash]$ echo 'arg1 = "value";arg2="someValue"' |cat - script.r | R --no-save
+# Variables dataFrame, inputDir, outputDir,  are required
+args <- commandArgs(TRUE)
+
+if (length(args) != 3) {
+    print("Usage: DESeq.r <dataFrame> <inputDir> <outputDir> <outputFile>")
+}
+
+dataFrame <- args[1]
+inputDir <- args[2]
+outputDir <-args[3]
+outputFile <-args[4]
+
+#biocLite("DESeq2");
+library(DESeq2);
+df = read.delim(dataFrame, header = TRUE, sep = "\t");
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = df, directory =inputDir, design=~ condition);
+ddsHTSeq$condition <-relevel(ddsHTSeq$condition, ref="reference");
+ddsHTSeq <- estimateSizeFactors(ddsHTSeq);
+ddsHTSeq <- estimateDispersions(ddsHTSeq);
+ddsHTSeq<- nbinomWaldTest(ddsHTSeq);
+rld<-rlogTransformation(ddsHTSeq, blind = FALSE);
+res<-results(ddsHTSeq);
+
+#can I do something like this for deseq2 so we only need one df for each experiment 
+#res = nbinomTest( cds, condition2, condition1); #this bit needs sorting
+
+#need to make this output relate to the display names etc 
+fullOutFile = paste(outputDir, outputFile, sep="/");
+write.csv( res, file=fullOutFile);
+
+
+
