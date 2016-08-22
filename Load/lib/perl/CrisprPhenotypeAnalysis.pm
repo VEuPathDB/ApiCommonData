f90f6e60a163f282481d83b7d3db998aeb8f95dd
John Brestelli <jbrestell@gmail.com>
2016-08-22 19:55:04 +0000

    write config for insertstudyresults for crisprphenotype data
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/gus4@76115 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/CrisprPhenotypeAnalysis.pm
@@ -0,0 +1,33 @@
+package ApiCommonData::Load::CrisprPhenotypeAnalysis;
+use base qw(CBIL::TranscriptExpression::DataMunger::Loadable);
+
+use strict;
+
+sub new {
+  my ($class, $args) = @_;
+
+  my $requiredParams = [
+                          'profileSetName',
+                          'inputFile',
+                          'sourceIdType',
+                         ];
+  my $self = $class->SUPER::new($args, $requiredParams);
+  return $self;
+}
+
+sub munge {
+  my ($self) = @_;
+
+  $self->setProtocolName("crispr_phenotype");
+
+  my $pan = $self->getProfileSetName();
+  $self->setNames([$pan]);
+
+  my $inputFile = $self->getInputFile();
+  $self->setFileNames([$inputFile]);
+
+  $self->createConfigFile();
+}
+
+
+1;
