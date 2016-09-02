c07717754db22604a01cecf3165ddd765b8fef1b
John Brestelli <jbrestell@gmail.com>
2016-09-02 19:54:07 +0000

    no analysis for rtpcr profiles.  just load profiles
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@76490 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/RtPCRAnalysis.pm
+++ Load/lib/perl/RtPCRAnalysis.pm
@@ -1,5 +1,5 @@
 package ApiCommonData::Load::RtPCRAnalysis;
-use base qw(CBIL::TranscriptExpression::DataMunger::Standardization);
+use base qw(CBIL::TranscriptExpression::DataMunger::NoSampleConfigurationProfiles);
 
 sub getProtocolName {
   return "rtpcr";
