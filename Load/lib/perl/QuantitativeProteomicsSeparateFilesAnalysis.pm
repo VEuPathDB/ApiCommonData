bfb9b7b21b5667f317375abb8655826e95298331
John Brestelli <jbrestell@gmail.com>
2016-12-02 21:52:04 +0000

    quant proteomics from separate file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@78587 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/QuantitativeProteomicsSeparateFilesAnalysis.pm
+++ Load/lib/perl/QuantitativeProteomicsSeparateFilesAnalysis.pm
@@ -1,5 +1,35 @@
 package ApiCommonData::Load::QuantitativeProteomicsSeparateFilesAnalysis;
-use base qw(CBIL::TranscriptExpression::DataMunger::ProfileFromSeparateFiles);
+use base qw(CBIL::TranscriptExpression::DataMunger::Loadable);
+
+use strict;
+
+sub getSample { $_[0]->{sample}}
+sub setSample { $_[0]->{sample} = $_ }
+
+sub new {
+  my ($class, $args) = @_;
+
+  my $requiredParams = [ 'outputFile',
+                         'sample',
+                         'profileSetName',
+                       ];
+
+  my $self = $class->SUPER::new($args, $requiredParams);
+
+  $self->setSourceIdType("gene");
+  $self->setNames([$args->{sample}]);
+  $self->setFileNames([$args->{outputFile}]);
+
+  return $self;
+}
+
+
+sub munge {
+  my $self = shift;
+
+  $self->createConfigFile();
+
+}
 
 sub getProtocolName {
   return "Quantitative Proteomics";
