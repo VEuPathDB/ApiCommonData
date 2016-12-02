1bd98c2c6e307fd540f972a52346599164fdc44d
John Brestelli <jbrestell@gmail.com>
2016-12-02 18:08:45 +0000

    separate files quant prot
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@78578 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/QuantitativeProteomicsSeparateFilesAnalysis.pm
@@ -0,0 +1,9 @@
+package ApiCommonData::Load::QuantitativeProteomicsSeparateFilesAnalysis;
+use base qw(CBIL::TranscriptExpression::DataMunger::ProfileFromSeparateFiles);
+
+sub getProtocolName {
+  return "Quantitative Proteomics";
+}
+
+1;
+
