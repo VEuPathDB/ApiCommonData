cab1c2ba99a1969b811046b61cdb407e24d540b3
Steve Fischer <sfischer@upenn.edu>
2016-12-15 17:11:01 +0000

    fix password key
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@78893 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createApidbUserDatasetsSchema.sql
+++ Load/lib/sql/apidbschema/createApidbUserDatasetsSchema.sql
@@ -1,5 +1,5 @@
 CREATE USER ApiDBUserDatasets
-IDENTIFIED BY VALUES 'BBA8A2E0BB2D7072'  -- encoding of standard password, same as apidbtuning
+IDENTIFIED BY VALUES 'DCB802868A9EF9F9'  -- encoding of standard password, same as apidbtuning
 QUOTA UNLIMITED ON users 
 QUOTA UNLIMITED ON gus
 QUOTA UNLIMITED ON indx
