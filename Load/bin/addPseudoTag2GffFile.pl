5781d0ff319509f291e8198b5c30790bce9c562c
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2017-04-26 19:32:17 +0000

    new dev for adding pseudo tag to pseudo-gff3 file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@81452 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/addPseudoTag2GffFile.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl5.8.0
+
+### usage: addPseudoTag2GffFile.pl whole_genome.gff.woPseudo internalStopCodonGeneList.txt > whole_genome.gff
+
+use strict;
+
+my ($gffFile, $pseudoFile) = @ARGV;
+
+my (%isPseudo);
+open (IN, $pseudoFile) || die "can not open pseudoFile file to read.\n";
+while (<IN>) {
+  chomp;
+  next if ($_ =~ /^\s*$/);
+  $isPseudo{$_} = 1;
+}
+close IN;
+
+open (INN, $gffFile) || die "can not open gffFile to read\n";
+while (<INN>) {
+  chomp;
+  my @items = split (/\t/, $_);
+  if ($items[2] eq 'mRNA') {
+    if ($items[8] =~ /ID \"(\S+?)\"/) {
+      if ($isPseudo{$1}) {
+	$items[8] .= "pseudo \"\"\;";
+      }
+    }
+  }
+
+  foreach my $i (0..8) {
+    ($i == 8) ? print "$items[$i]\n" : print "$items[$i]\t";
+  }
+}
+close INN;
+
+
+
