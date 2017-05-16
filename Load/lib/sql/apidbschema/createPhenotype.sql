40a3948843ee557a40c627a4b582b332183e6e3a
Jane Pulman <jane.pulman@liverpool.ac.uk>
2017-05-16 14:02:14 +0000

    adding nafeaturephenotypemodel table into core.tableinfo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@81747 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createPhenotype.sql
+++ Load/lib/sql/apidbschema/createPhenotype.sql
@@ -103,14 +103,6 @@ FROM dual,
 WHERE 'phenotyperesult' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
-create table apidb.NAFeaturePhenotypeModel( 
-na_feature_phenotype_model_id		    NUMBER(10) NOT NULL,
-PHENOTYPE_MODEL_ID			    NUMBER(10) NOT NULL,
-na_feature_id				    NUMBER(10),
-FOREIGN KEY (phenotype_model_id) REFERENCES apidb.phenotypemodel,
-FOREIGN KEY (na_feature_id) REFERENCES dots.nafeatureimp,
-PRIMARY KEY (na_feature_phenotype_model_id)
-);
 
 
 ------------------------------------------------------------------------------
@@ -125,6 +117,21 @@ create table apidb.NaFeaturePhenotypeModel
 
 GRANT insert, select, update, delete ON apidb.NaFeaturePhenotypeModel TO gus_w;
 GRANT select ON apidb.NaFeaturePhenotypeModel TO gus_r;
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'NaFeaturePhenotypeModel',
+       'Standard', 'na_feature_phenotype_model_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
+WHERE 'NaFeaturePhenotypeModel' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
 
 ------------------------------------------------------------------------------
 exit;
