e5770165b6a36c552575735c39bf0f579fc9bb16
Jane Pulman <jane.pulman@liverpool.ac.uk>
2017-05-19 13:27:40 +0000

    changing to make each phenotypeModel have a seperate naFeaturePhenotypeModel for each knock-down to allow for double knock-downs to be properly shown on the gene pages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@81832 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertPhenotype.pm
+++ Load/plugin/perl/InsertPhenotype.pm
@@ -1,4 +1,3 @@
-
 package ApiCommonData::Load::Plugin::InsertPhenotype;
 @ISA = qw(GUS::PluginMgr::Plugin);
 
@@ -7,260 +6,307 @@ use GUS::PluginMgr::Plugin;
 use GUS::Supported::Util;
 use GUS::Model::ApiDB::PhenotypeModel;
 use GUS::Model::ApiDB::PhenotypeResult;
+use GUS::Model::ApiDB::NaFeaturePhenotypeModel;
 use GUS::Model::SRes::OntologyTerm;
 use Data::Dumper;
 sub getArgsDeclaration {
-my $argsDeclaration  =
-[
-
-     stringArg({ name => 'inputFile',
-		 descr => 'TAB file that the plugin has to be run on',
-		 constraintFunc=> undef,
-		 reqd  => 1,
-		 isList => 0,
-		 mustExist => 1,
-	       }),
-     stringArg({ name => 'extDbName',
-		 descr => 'externaldatabase name',
-		 constraintFunc=> undef,
-		 reqd  => 1,
-		 isList => 0
-	       }),
-     stringArg({ name => 'extDbVer',
-		 descr => 'externaldatabaserelease version',
-		 constraintFunc=> undef,
-		 reqd  => 1,
-		 isList => 0
-	       }),
-
-   stringArg({name => 'organismAbbrev',
-	      descr => 'if supplied, use a prefix to use for tuning manager tables',
-	      reqd => 0,
-	      constraintFunc => undef,
-	      isList => 0,
-	     }),
-
-];
-
-return $argsDeclaration;
+    my $argsDeclaration  =
+	[
+	 
+	 stringArg({ name => 'inputFile',
+		     descr => 'TAB file that the plugin has to be run on',
+		     constraintFunc=> undef,
+		     reqd  => 1,
+		     isList => 0,
+		     mustExist => 1,
+		   }),
+	 stringArg({ name => 'extDbName',
+		     descr => 'externaldatabase name',
+		     constraintFunc=> undef,
+		     reqd  => 1,
+		     isList => 0
+		   }),
+	 stringArg({ name => 'extDbVer',
+		     descr => 'externaldatabaserelease version',
+		     constraintFunc=> undef,
+		     reqd  => 1,
+		     isList => 0
+		   }),
+	 
+	 stringArg({name => 'organismAbbrev',
+		    descr => 'if supplied, use a prefix to use for tuning manager tables',
+		    reqd => 0,
+		    constraintFunc => undef,
+		    isList => 0,
+		   }),
+	 
+	];
+    
+    return $argsDeclaration;
 }
 
 
 sub getDocumentation {
-
-  my $description = <<NOTES;
+    
+    my $description = <<NOTES;
 NOTES
-
-  my $purpose = <<PURPOSE;
+	
+	my $purpose = <<PURPOSE;
 PURPOSE
-
-  my $purposeBrief = <<PURPOSEBRIEF;
+	
+	my $purposeBrief = <<PURPOSEBRIEF;
 PURPOSEBRIEF
-
-  my $syntax = <<SYNTAX;
+	
+	my $syntax = <<SYNTAX;
 SYNTAX
-
-  my $notes = <<NOTES;
+	
+	my $notes = <<NOTES;
 NOTES
-
-  my $tablesAffected = <<AFFECT;
+	
+	my $tablesAffected = <<AFFECT;
 AFFECT
-
-  my $tablesDependedOn = <<TABD;
+	
+	my $tablesDependedOn = <<TABD;
 TABD
-
-  my $howToRestart = <<RESTART;
+	
+	my $howToRestart = <<RESTART;
 RESTART
-
-  my $failureCases = <<FAIL;
+	
+	my $failureCases = <<FAIL;
 FAIL
-
-  my $documentation = {purpose=>$purpose, purposeBrief=>$purposeBrief,tablesAffected=>$tablesAffected,tablesDependedOn=>$tablesDependedOn,howToRestart=>$howToRestart,failureCases=>$failureCases,notes=>$notes};
-
-  return ($documentation);
+	
+	my $documentation = {purpose=>$purpose, purposeBrief=>$purposeBrief,tablesAffected=>$tablesAffected,tablesDependedOn=>$tablesDependedOn,howToRestart=>$howToRestart,failureCases=>$failureCases,notes=>$notes};
+    
+    return ($documentation);
 }
 
 
 
 sub new {
-  my $class = shift;
-  my $self = {};
-  bless($self, $class);
-
-  my $documentation = &getDocumentation();
-
-  my $args = &getArgsDeclaration();
-
-  $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision$',
-		      name => ref($self),
-		     argsDeclaration   => $args,
-		     documentation     => $documentation
-		    });
-  return $self;
+    my $class = shift;
+    my $self = {};
+    bless($self, $class);
+    
+    my $documentation = &getDocumentation();
+    
+    my $args = &getArgsDeclaration();
+    
+    $self->initialize({requiredDbVersion => 4.0,
+		       cvsRevision => '$Revision$',
+		       name => ref($self),
+		       argsDeclaration   => $args,
+		       documentation     => $documentation
+		      });
+    return $self;
 }
 
 sub run {
-  my $self = shift;
-
-  my $extDbReleaseId = $self->getExtDbRlsId($self->getArg('extDbName'),$self->getArg('extDbVer'))
-    || $self->error("Cannot find external_database_release_id for the data source");
-
-
-  my $file = $self->getArg('inputFile');
-  my $notOntologyTerm = GUS::Model::SRes::OntologyTerm->new({name=>'NOT',source_id=>'EuPathUserDefined_NOT'});
-  $notOntologyTerm->retrieveFromDB();
-  $notOntologyTerm->submit();
-  my $ontologyTermIds = $self->queryForOntologyTermIds();
+    my $self = shift;
+    
+    my $extDbReleaseId = $self->getExtDbRlsId($self->getArg('extDbName'),$self->getArg('extDbVer'))
+	|| $self->error("Cannot find external_database_release_id for the data source");
+    
+    
+    my $file = $self->getArg('inputFile');
+    my $notOntologyTerm = GUS::Model::SRes::OntologyTerm->new({name=>'NOT',source_id=>'EuPathUserDefined_NOT'});
+    $notOntologyTerm->retrieveFromDB();
+    $notOntologyTerm->submit();
+    my $ontologyTermIds = $self->queryForOntologyTermIds();
 #  my $chebiTermIds = $self->queryForChebiTermIds();
-  open(FILE, $file) or die "Cannot open file $file for reading: $!";
-
-  my $count;
-
-  while(<FILE>) {
-    chomp;
-    my @a = split(/\t/, $_);
-
-    my $geneSourceId = $a[0];
-    my $modelSourceId = $a[1];
-    my $name = $a[2];
-    my $pubmedId = $a[3];
-    my $modType = $a[4];
-
-    my $isSuccessful = $a[5] eq 'yes'? 1 : 0;
-    my $organism = $a[6];
-    my $qualityTerm = $a[7];
-    my $entityTerm = $a[8];
-    my $timing = $a[9];
-    my $lifeCycleTerm = $a[10];
-    $lifeCycleTerm =~ s/^\s+//;
-    $lifeCycleTerm =~ s/\s+$//;
-    my $phenotypeString = $a[11];
-    my $evidenceTerm = $a[12];
-    my $note = $a[13];
-    my $experimentType = $a[14];
-    my $allele = $a[15];
-    my $chebiAnnotationExtension= $a[16];
-    my $proteinAnnotationExtension = $a[17];
-    my $mutation_desc = $a[18];
-
-    my $naFeatureId =  GUS::Supported::Util::getGeneFeatureId($self, $geneSourceId, 0, $self->getArg('organismAbbrev')) ;
+    open(FILE, $file) or die "Cannot open file $file for reading: $!";
+    
+    my $count;
+    my @naFeatureArray;
+    
+    while(<FILE>) {
+	chomp;
+	my @a = split(/\t/, $_);
+	
+	my $geneSourceId = $a[0];
+	my $modelSourceId = $a[1];
+	my $name = $a[2];
+	my $pubmedId = $a[3];
+	my $modType = $a[4];
+	
+	my $isSuccessful = $a[5] eq 'yes'? 1 : 0;
+	my $organism = $a[6];
+	my $qualityTerm = $a[7];
+	my $entityTerm = $a[8];
+	my $timing = $a[9];
+	my $lifeCycleTerm = $a[10];
+	$lifeCycleTerm =~ s/^\s+//;
+	$lifeCycleTerm =~ s/\s+$//;
+	my $phenotypeString = $a[11];
+	my $evidenceTerm = $a[12];
+	my $note = $a[13];
+	my $experimentType = $a[14];
+	my $allele = $a[15];
+	my $chebiAnnotationExtension= $a[16];
+	my $proteinAnnotationExtension = $a[17];
+	my $mutation_desc = $a[18];
+	my $multipleMutations = $a[19];
+	my $naFeatureId =  GUS::Supported::Util::getGeneFeatureId($self, $geneSourceId, 0, $self->getArg('organismAbbrev')) ;
 #    my $ontologyProteinExtensionNaFeatureId = GUS::Supported::Util::getGeneFeatureId($self, $proteinAnnotationExtension, 0, $self->getArg('organismAbbrev')) ;
 #    print Dumper "na feature id\n";
- #   print Dumper $naFeatureId;
-    my $phenotypeModel = $self->lookupModel($modelSourceId, $naFeatureId, $pubmedId);
- #   print Dumper "the model source id is \n";
- #   print Dumper $modelSourceId;
-    unless($phenotypeModel) {
-      $phenotypeModel = GUS::Model::ApiDB::PhenotypeModel->new({external_database_release_id => $extDbReleaseId, 
-                                                                na_feature_id => $naFeatureId,
-                                                                source_id => $modelSourceId,
-                                                                name => $name,
-                                                                pubmed_id => $pubmedId,
-                                                                modification_type => $modType,
-                                                                is_successful => $isSuccessful,
-                                                                organism => $organism,
-								experiment_type => $experimentType,
-								allele => $allele,
-								mutation_description => $mutation_desc,
-                                                               });
-  #    print Dumper "the stuff in the model is\n";
-  #    print Dumper $naFeatureId;
-  #    print Dumper $modelSourceId;
-  #    print Dumper $name;
-  #   print Dumper $modType;
-  #    print Dumper $mutation_desc;
-
-      push @{$self->{_models}}, $phenotypeModel;
-    }
-
-    my ($qualityTermId, $entityTermId, $lifeCycleTermId, $evidenceTermId, $chebiTermId, $proteinTermId);
-    if($qualityTerm) {
-      $qualityTermId = $ontologyTermIds->{$qualityTerm};
-      $self->userError("quality Term ${qualityTerm} specified but not found in database") unless($qualityTermId);
-    }
-    if($entityTerm) {
-      $entityTermId = $ontologyTermIds->{$entityTerm};
-      $self->userError("entity Term $entityTerm specified but not found in database") unless($entityTermId);
-    }
-    if($lifeCycleTerm) {
-      $lifeCycleTermId = $ontologyTermIds->{$lifeCycleTerm};
-      $self->userError("lifeCycle Term $lifeCycleTerm specified but not found in database") unless($lifeCycleTermId);
-    }
-    if($evidenceTerm) {
-      $evidenceTermId = $ontologyTermIds->{$evidenceTerm};
-      $self->userError("evidence Term $evidenceTerm specified but not found in database") unless($evidenceTermId);
-    }
+	#   print Dumper $naFeatureId;
+	my $phenotypeModel = $self->lookupModel($modelSourceId, $pubmedId); #removed naFeatureId
+	#   print Dumper "the model source id is \n";
+	#   print Dumper $modelSourceId;
+	unless($phenotypeModel) {
+	    $phenotypeModel = GUS::Model::ApiDB::PhenotypeModel->new({external_database_release_id => $extDbReleaseId, 
+								      #na_feature_id => $naFeatureId,
+								      source_id => $modelSourceId,
+								      name => $name,
+								      pubmed_id => $pubmedId,
+								      modification_type => $modType,
+								      is_successful => $isSuccessful,
+								      organism => $organism,
+								      experiment_type => $experimentType,
+								      allele => $allele,
+								      mutation_description => $mutation_desc,
+								      has_multiple_mutations =>$multipleMutations,
+								     });
+	    #      print Dumper "the stuff in the model is\n";
+	    #    print Dumper $naFeatureId;
+	    #     print Dumper $modelSourceId;
+	    #     print Dumper $name;
+	    #    print Dumper $modType;
+	    #     print Dumper $mutation_desc;
+	    
+	    push @{$self->{_models}}, $phenotypeModel;
+	    undef (@naFeatureArray);
+	}
+	
+	my ($qualityTermId, $entityTermId, $lifeCycleTermId, $evidenceTermId, $chebiTermId, $proteinTermId);
+	if($qualityTerm) {
+	    $qualityTermId = $ontologyTermIds->{$qualityTerm};
+	    $self->userError("quality Term ${qualityTerm} specified but not found in database") unless($qualityTermId);
+	}
+	if($entityTerm) {
+	    $entityTermId = $ontologyTermIds->{$entityTerm};
+	    $self->userError("entity Term $entityTerm specified but not found in database") unless($entityTermId);
+	}
+	if($lifeCycleTerm) {
+	    $lifeCycleTermId = $ontologyTermIds->{$lifeCycleTerm};
+	    $self->userError("lifeCycle Term $lifeCycleTerm specified but not found in database") unless($lifeCycleTermId);
+	}
+	if($evidenceTerm) {
+	    $evidenceTermId = $ontologyTermIds->{$evidenceTerm};
+	    $self->userError("evidence Term $evidenceTerm specified but not found in database") unless($evidenceTermId);
+	}
 #check against chebi table - seperate hash  
- #  if($chebiAnnotationExtension) {
-  #    $chebiTermId = $chebiTermIds->{$chebiAnnotationExtension};
-   #   $self->userError("chebi annotation extension id  $chebiAnnotationExtension specified but not found in database") unless($chebiTermId);
-   # }
+	#  if($chebiAnnotationExtension) {
+	#    $chebiTermId = $chebiTermIds->{$chebiAnnotationExtension};
+	#   $self->userError("chebi annotation extension id  $chebiAnnotationExtension specified but not found in database") unless($chebiTermId);
+	# }
+	
+	
+	my $phenotypeResult = GUS::Model::ApiDB::PhenotypeResult->new({phenotype_quality_term_id => $qualityTermId,
+								       phenotype_entity_term_id => $entityTermId,
+								       timing => $timing,
+								       life_cycle_stage_term_id => $lifeCycleTermId,
+								       phenotype_post_composition => $phenotypeString,
+								       phenotype_comment => $note,
+								       evidence_term_id => $evidenceTermId,
+								       chebi_annotation_extension => $chebiAnnotationExtension,
+								       protein_annotation_extension => $proteinAnnotationExtension,
+								      });
+	
+	$phenotypeResult->setParent($phenotypeModel);
+	# print Dumper "RESULT\n\n\n";
+	# print Dumper $lifeCycleTermId;
+	# print Dumper $phenotypeString;
+	
+	$phenotypeResult->submit();
+	
+	my $NAFeaturePhenotypeModel = $self->lookupNaFeatureModel($naFeatureId,@naFeatureArray);
+	#   print Dumper "the model source id is \n";
+	#   print Dumper $modelSourceId;
+	unless($NAFeaturePhenotypeModel) {
+	    
+	    my $NAFeaturePhenotypeModel = GUS::Model::ApiDB::NaFeaturePhenotypeModel->new({na_feature_id=>$naFeatureId});
+	    $NAFeaturePhenotypeModel->setParent($phenotypeModel);
+	    $NAFeaturePhenotypeModel->submit();
+	    push @naFeatureArray, $naFeatureId;
+	print Dumper "the na feature model is";
+	    print Dumper $naFeatureId;
+	}
+	$self->undefPointerCache() if $count++ % 500 == 0;
+	
+	
+    }
     
-
-    my $phenotypeResult = GUS::Model::ApiDB::PhenotypeResult->new({phenotype_quality_term_id => $qualityTermId,
-                                                                   phenotype_entity_term_id => $entityTermId,
-                                                                   timing => $timing,
-                                                                   life_cycle_stage_term_id => $lifeCycleTermId,
-                                                                   phenotype_post_composition => $phenotypeString,
-                                                                   phenotype_comment => $note,
-                                                                   evidence_term_id => $evidenceTermId,
-								   chebi_annotation_extension => $chebiAnnotationExtension,
-								   protein_annotation_extension => $proteinAnnotationExtension,
-                                                                  });
-
-    $phenotypeResult->setParent($phenotypeModel);
-   # print Dumper "RESULT\n\n\n";
-   # print Dumper $lifeCycleTermId;
-   # print Dumper $phenotypeString;
-
-    $phenotypeResult->submit();
-
-    $self->undefPointerCache() if $count++ % 500 == 0;
-  }
-
-  return "Inserted $count PhenotypeResults";
+    return "Inserted $count PhenotypeResults";
 }
 
 
 sub lookupModel {
-  my ($self, $sourceId, $naFeatureId, $pubmed) = @_;
+    my ($self, $sourceId, $pubmed) = @_;
 #  print Dumper "getting into look up model\n";
-  foreach my $model(@{$self->{_models}}) {
-    if(($sourceId && $model->getSourceId() eq $sourceId)&& ($model->getNaFeatureId() eq $naFeatureId && $model->getPubmedId() eq $pubmed)) {
-#	print Dumper "look up model source id model get source if\n";
+#    print Dumper "the models array is";
+#    my @array = @{$self->{_models}};
+#    print Dumper $self->{_models};
+    foreach my $model(@{$self->{_models}}) {
+	if(($sourceId && $model->getSourceId() eq $sourceId) && ($model->getPubmedId() eq $pubmed)) {
+#    if(($sourceId && $model->getSourceId() eq $sourceId)&& ($model->getNaFeatureId() eq $naFeatureId && $model->getPubmedId() eq $pubmed)) {
+#	print Dumper "look up model source id model get source if\n";#
+	    
 #	print Dumper $sourceId;
 #	print Dumper $model->getSourceId();
-	return $model;
-    }
-
+	    return $model;
+	}
+	
 #   if($model->getNaFeatureId() eq $naFeatureId && $model->getPubmedId() eq $pubmed) {
 #	print Dumper "in second of of look up model";
- #     return $model;
-
-  #  }
-  }
-
-  return undef;
+	#     return $model;
+	
+	#  }
+    }
+    
+    return undef;
 }
 
+sub lookupNaFeatureModel {
+    my ($self, $naFeatureId, @naFeatureArray) = @_;
+  print Dumper "getting into look up model\n";
+    print Dumper "the models array is";
+#    my @array = @{$self->{_models}};
+    print Dumper @naFeatureArray;
+    foreach my $model(@naFeatureArray) {
+	if($naFeatureId eq $model) {
+#    if(($sourceId && $model->getSourceId() eq $sourceId)&& ($model->getNaFeatureId() eq $naFeatureId && $model->getPubmedId() eq $pubmed)) {
+	print Dumper "matching Na FEATUREs\n";#
+	    
+#	print Dumper $sourceId;
+#	print Dumper $model->getSourceId();
+	    return $model;
+	}
+#   if($model->getNaFeatureId() eq $naFeatureId && $model->getPubmedId() eq $pubmed) {
+#	print Dumper "in second of of look up model";
+    #     return $model;
+    
+	#  }
+    }
+    return undef;
+}	
 
- sub queryForOntologyTermIds {
-  my ($self) = @_;
-
-  my $dbh = $self->getQueryHandle();
-  my $query = "select ontology_term_id, source_id from sres.ontologyterm";
-
-  my $sh = $dbh->prepare($query);
-  $sh->execute();
-
-  my %terms;
-  while(my ($id, $sourceId) = $sh->fetchrow_array()) {
-    $terms{$sourceId} = $id;
-  }
-  $sh->finish();
-
-  return \%terms;
+    
+sub queryForOntologyTermIds {
+    my ($self) = @_;
+    
+    my $dbh = $self->getQueryHandle();
+    my $query = "select ontology_term_id, source_id from sres.ontologyterm";
+    
+    my $sh = $dbh->prepare($query);
+    $sh->execute();
+    
+    my %terms;
+    while(my ($id, $sourceId) = $sh->fetchrow_array()) {
+	$terms{$sourceId} = $id;
+    }
+    $sh->finish();
+    
+    return \%terms;
 }
 # sub queryForChebiTermIds {
 #  my ($self) = @_;
@@ -283,7 +329,7 @@ sub lookupModel {
 sub undoTables {
   my ($self) = @_;
 
-  return ('ApiDB.PhenotypeResult','ApiDB.PhenotypeModel');
+  return ('ApiDB.PhenotypeResult','ApiDB.PhenotypeModel','ApiDB.NaFeaturePhenotypeModel');
 }
 
 1;
