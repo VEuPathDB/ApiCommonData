e008f00fa3785bc41d21ebf3c31cf558179f76ea
John Iodice <iodice@pennmedicine.upenn.edu>
2017-06-07 21:30:59 +0000

    new function is_number
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@82138 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createStoredProcedures.sql
+++ Load/lib/sql/apidbschema/createStoredProcedures.sql
@@ -727,4 +727,38 @@ GRANT execute ON apidb.display_function TO gus_w;
 
 -------------------------------------------------------------------------------
 
+-- determine whether the value of a string can be converted to a number
+-- return 0 or 1
+
+CREATE OR REPLACE FUNCTION apidb.is_number (p_val VARCHAR2)
+   RETURN NUMBER
+IS
+v_val   NUMBER;
+BEGIN
+   BEGIN
+      IF p_val IS NULL OR TRIM (p_val) = ''
+      THEN
+         RETURN 0;
+      END IF;
+
+      SELECT TO_NUMBER (p_val)
+        INTO v_val
+        FROM DUAL;
+
+      RETURN 1;
+   EXCEPTION
+      WHEN OTHERS
+      THEN
+         RETURN 0;
+   END;
+END;
+/
+
+show errors;
+
+GRANT execute ON apidb.is_number TO gus_r;
+GRANT execute ON apidb.is_number TO gus_w;
+
+-------------------------------------------------------------------------------
+
 exit
