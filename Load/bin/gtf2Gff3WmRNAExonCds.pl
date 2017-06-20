84827507fb88fc06c8f8ce68c3388662e68beb82
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2017-06-20 02:33:59 +0000

    a script to convert an unmc gtf file to an standard gff3 file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@82366 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/gtf2Gff3WmRNAExonCds.pl
@@ -0,0 +1,126 @@
+#!/usr/bin/perl
+
+use strict;
+
+## a script to convert annotation from Gtf format to standard gff3 format
+## the gtf file contains mRNA, exon, and CDS
+
+## usage: gtf2Gff3WmRNAExonCds.pl MacaM_Rhesus_Genome_Annotation_v7.8.2.gtf MMUL > whole_genome.gff
+## MMUL is the prefix that want to add in the gene ID
+
+my ($input, $gIdPrefix) = @ARGV;
+
+print "##gff-version 3\n";
+print "##date-created at ".localtime()."\n";
+
+
+my (%geneIds, %transIds, $lineCtr);
+open (IN, $input) || die "can not open input file to read.\n";
+while (<IN>) {
+  $lineCtr++;
+  ## grep the gene_id and transId for current line
+  my ($transKey, $gid, $tid);
+  if($_ =~ /\tgene_id \"(\S+)\"\;.* transcript_id \"(\S+)\"\;/) {
+    $geneIds{$1} = $gIdPrefix."_".$1;
+    $transKey = $1."-".$2;
+    $transIds{$transKey} = $geneIds{$1}.".".$2;
+    $transIds{$transKey} =~ s/\.(.+?)_transcript_/\./;
+  } else {
+    die "Can not find either gene_id or transcript_id at the line $lineCtr\n";
+  }
+}
+close IN;
+
+q{
+foreach my $k (sort keys %geneIds) {
+  print STDERR "$k, $geneIds{$k}\n";
+}
+foreach my $kk (sort keys %transIds) {
+  print STDERR "  $kk, $transIds{$kk}\n";
+}
+};
+
+my (%geneStarts, %geneEnds, %geneSeqId, %geneSource, %geneStrand, %ctrs);
+open (INN, $input) || die "can not open input file to read.\n";
+while (<INN>) {
+  chomp;
+
+  ## skip the comment line(s)
+  next if ($_ =~ /^\#/); 
+
+  my @elems = split (/\t/, $_);
+
+  if($elems[8] =~ /gene_id \"(\S+)\"\;.* transcript_id \"(\S+)\"\;/) {
+
+    my $currentGeneId = $1;
+    my $currentTransId = $2;
+    my $curTransKey = $currentGeneId."-".$currentTransId;
+
+    my ($id, $parent);
+    if ($elems[2] =~ /mRNA/) {
+      $id = $transIds{$curTransKey};   ## the transcript ID 
+      $parent  = $geneIds{$currentGeneId};   ## the gene ID
+      print STDERR "\$id = $id, \$parent = $parent\n";
+
+      ## geneStart and geneEnd
+      $geneStarts{$currentGeneId} = ($geneStarts{$currentGeneId}) ? 
+	getMin ($elems[3], $elems[4], $geneStarts{$currentGeneId}) : getMin($elems[3], $elems[4]);
+      $geneEnds{$currentGeneId} = ($geneEnds{$currentGeneId}) ? 
+	getMax($elems[3], $elems[4], $geneEnds{$currentGeneId}) : getMax($elems[3], $elems[4]);
+
+      ## other gene info
+      $geneSeqId{$currentGeneId} = $elems[0];
+      $geneSource{$currentGeneId} = $elems[1];
+      $geneStrand{$currentGeneId} = $elems[6];
+
+    } else {
+      $ctrs{$curTransKey}{$elems[2]}++;
+      $id = $transIds{$curTransKey}.":".$elems[2].":".$ctrs{$curTransKey}{$elems[2]};
+      $parent = $transIds{$curTransKey};
+    }
+
+    $elems[8] = "ID \"$id\"; Parent \"$parent\"; ".$elems[8];
+  }
+  &printGff3Column (\@elems);
+
+}
+close INN;
+
+
+## print gene line
+foreach my $k (sort keys %geneIds) {
+  my @items;
+  $items[0] = $geneSeqId{$k};
+  $items[1] = $geneSource{$k};
+  $items[2] = "gene";
+  $items[3] = $geneStarts{$k};
+  $items[4] = $geneEnds{$k};
+  $items[5] = "100";
+  $items[6] = $geneStrand{$k};
+  $items[7] = ".";
+  $items[8] = "ID \"$geneIds{$k}\";";
+  &printGff3Column (\@items);
+}
+
+##################
+
+sub getMax {
+    my @array = @_;
+
+    my @sortedArray = sort {$a <=> $b} @array;
+    return $sortedArray[-1];
+}
+
+sub getMin {
+    my @array = @_;
+    my @sortedArray = sort {$a <=> $b} @array;
+    return $sortedArray[0];
+}
+
+sub printGff3Column {
+  my $array = shift;
+  foreach my $i (0..$#{$array}) {
+    ($i == 8) ? print "$array->[$i]\n" : print "$array->[$i]\t";
+  }
+  return 0;
+}
