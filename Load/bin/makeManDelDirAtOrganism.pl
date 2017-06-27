408cba58484234e7f3409276f49b6ddd7c3ca197
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2017-06-27 16:46:56 +0000

    typo fix
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@82605 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeManDelDirAtOrganism.pl
+++ Load/bin/makeManDelDirAtOrganism.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-## usage: a script to create all directorys after organismAbbrev in the manual delivery directory
+## usage: a script to create all directories after organismAbbrev in the manual delivery directory
 
 my @dir_name= ("proteinMicroarray","bindingSites","cellularLocation","chipChip","chipSeq","comparativeGenomics","dbxref","dnaSeq","epitope","EST","function","genePrediction","genome","genomeFeature","interaction","isolate","massSpec","microarrayExpression","microarrayPlatform","mRNA","phenotype","phylogeny","reagent","rnaSeq","sageTag","SNP","structure","alias","comment");
 foreach my $each_dir (@dir_name)
