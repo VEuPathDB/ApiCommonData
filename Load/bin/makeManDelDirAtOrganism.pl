06565a16fe61c0ea32c5cfb27c437b17ec2e08c5
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2017-06-27 16:39:19 +0000

    initial check in for a script to make the directorys after the organismAbbrev in the manual delivery directory
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@82603 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/makeManDelDirAtOrganism.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+## usage: a script to create all directorys after organismAbbrev in the manual delivery directory
+
+my @dir_name= ("proteinMicroarray","bindingSites","cellularLocation","chipChip","chipSeq","comparativeGenomics","dbxref","dnaSeq","epitope","EST","function","genePrediction","genome","genomeFeature","interaction","isolate","massSpec","microarrayExpression","microarrayPlatform","mRNA","phenotype","phylogeny","reagent","rnaSeq","sageTag","SNP","structure","alias","comment");
+foreach my $each_dir (@dir_name)
+{
+       mkdir( $each_dir ) || print $!;
+       print $each_dir . "\s HAS BEEN CREATED!\n";
+}
