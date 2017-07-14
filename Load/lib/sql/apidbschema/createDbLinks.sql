37239bffe63a02c84627ebd8795c02581a1d1a44
Mark Heiges <mheiges@gmail.com>
2017-07-14 19:35:13 +0000

    add acctdb links (refs #26870)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@83237 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDbLinks.sql
+++ Load/lib/sql/apidbschema/createDbLinks.sql
@@ -18,6 +18,16 @@ CONNECT TO APICOMM_DBLINK
 IDENTIFIED BY po34weep
 USING 'apicommDevN';
 
+CREATE PUBLIC DATABASE LINK acctdbN.profile
+CONNECT TO ACCTDB_DBLINK
+IDENTIFIED BY po34weep
+USING 'acctdbN';
+
+CREATE PUBLIC DATABASE LINK acctdbS.profile
+CONNECT TO ACCTDB_DBLINK
+IDENTIFIED BY po34weep
+USING 'acctdbS';
+
 -- rm15873 is for development of apicomm release-maintenance stuff
 -- https://redmine.apidb.org/issues/15873
 CREATE PUBLIC DATABASE LINK rm15873.login_comment
