a87655a8939bbf2c25eef090d7ac99e15aa9a500
Jane Pulman <jane.pulman@liverpool.ac.uk>
2017-08-25 10:49:05 +0000

    fixing quote issue in insert in core table info step
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@84154 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGoSubset.sql
+++ Load/lib/sql/apidbschema/createGoSubset.sql
@@ -36,14 +36,14 @@ INSERT INTO core.TableInfo
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-SELECT core.tableinfo_sq.nextval, ‘GoSubset’,
+SELECT core.tableinfo_sq.nextval,'GoSubset',
        'Standard', 'go_subset_id',
        d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
        p.project_id, 0
 FROM dual,
      (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
      (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
-WHERE ‘gosubset’ NOT IN (SELECT lower(name) FROM core.TableInfo
+WHERE 'gosubset' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 
