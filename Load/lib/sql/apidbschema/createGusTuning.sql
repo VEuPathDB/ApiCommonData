0d718f7d45ca70c1a17262f4486d380127d3edaa
John Iodice <iodice@pennmedicine.upenn.edu>
2017-09-26 15:26:32 +0000

    create index in same schema as its table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@84748 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createGusTuning.sql
+++ Load/lib/sql/apidbschema/createGusTuning.sql
@@ -249,7 +249,7 @@ create index dots.SnpDiff_ix on dots.NaFeatureImp
 -- create index sres.DbRefLowerId on sres.DbRef (external_database_release_id, lower(primary_identifier), db_ref_id);
 -- create index sres.DbRefLowerId2 on sres.DbRef (external_database_release_id, lower(secondary_identifier), db_ref_id);
 
--- create index orthomcl_id_ix on dots.AaSequenceImp(subclass_view, string1, aa_sequence_id);
+-- create index dots.orthomcl_id_ix on dots.AaSequenceImp(subclass_view, string1, aa_sequence_id);
 
 
 -- add this to prevent race condition in which we write duplicate rows
@@ -517,8 +517,8 @@ FROM dual,
 WHERE 'otuabundance' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     where DATABASE_ID = D.DATABASE_ID);
 
-CREATE INDEX otuabun_revix1 ON results.OtuAbundance (protocol_app_node_id, otu_abundance_id) TABLESPACE indx;
-CREATE INDEX otuabun_revix2 ON results.OtuAbundance (na_sequence_id, otu_abundance_id) TABLESPACE indx;
+CREATE INDEX results.otuabun_revix1 ON results.OtuAbundance (protocol_app_node_id, otu_abundance_id) TABLESPACE indx;
+CREATE INDEX results.otuabun_revix2 ON results.OtuAbundance (na_sequence_id, otu_abundance_id) TABLESPACE indx;
 
 --------------------------------------------------------------------------------
 
@@ -567,7 +567,7 @@ FROM dual,
 WHERE 'alphadiversity' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     where DATABASE_ID = D.DATABASE_ID);
 
-CREATE INDEX alphad_revix1 ON results.AlphaDiversity (protocol_app_node_id, alpha_diversity_id) TABLESPACE indx;
+CREATE INDEX results.alphad_revix1 ON results.AlphaDiversity (protocol_app_node_id, alpha_diversity_id) TABLESPACE indx;
 
 -------------------------------------------------------------------------------------------
 -- unique constraints in the Results schema
