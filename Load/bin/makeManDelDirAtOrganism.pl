3db78e7c5f79796e96ea4f7e568bd4596b48581b
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2017-10-12 19:12:36 +0000

    add quantitativeMassSpec dir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@85020 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeManDelDirAtOrganism.pl
+++ Load/bin/makeManDelDirAtOrganism.pl
@@ -3,7 +3,7 @@
 use strict;
 ## usage: a script to create all directories after organismAbbrev in the manual delivery directory
 
-my @dir_name= ("proteinMicroarray","bindingSites","cellularLocation","chipChip","chipSeq","comparativeGenomics","dbxref","dnaSeq","epitope","EST","function","genePrediction","genome","genomeFeature","interaction","isolate","massSpec","microarrayExpression","microarrayPlatform","mRNA","phenotype","phylogeny","reagent","rnaSeq","sageTag","SNP","structure","alias","comment");
+my @dir_name= ("quantitativeMassSpec","proteinMicroarray","bindingSites","cellularLocation","chipChip","chipSeq","comparativeGenomics","dbxref","dnaSeq","epitope","EST","function","genePrediction","genome","genomeFeature","interaction","isolate","massSpec","microarrayExpression","microarrayPlatform","mRNA","phenotype","phylogeny","reagent","rnaSeq","sageTag","SNP","structure","alias","comment");
 foreach my $each_dir (@dir_name)
 {
        mkdir( $each_dir ) || print $!;
