39a27c22c27c519aac0ae158e0ef82b6e0870406
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2017-12-08 15:38:24 +0000

    Add reaction source id and other requirements for loading MPMP
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@85861 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/MPMPMetabolicPathway.pm
+++ Load/lib/perl/MPMPMetabolicPathway.pm
@@ -29,8 +29,10 @@ sub makeGusObjects {
         print Dumper ($pathwayHash);
     }
 
-    my $typeToTableMap = {molecularentity => 'chEBI::Compounds', compound => 'chEBI::Compounds', enzyme => 'SRes::EnzymeClass', pathway => 'SRes::Pathway' };
-    my $typeToOntologyTerm = {molecularentity => 'molecular entity', compound => 'molecular entity', pathway => 'metabolic process', enzyme => 'enzyme'};
+    my $typeToTableMap = {molecularentity => 'chEBI::Compounds', compound => 'chEBI::Compounds', enzyme => 'SRes::EnzymeClass',  transporter => 'SRes::EnzymeClass', gene => 'DoTS::GeneFeature',
+                          translocator => 'SRes::EnzymeClass', protein => 'SRes::EnzymeClass', compartment => 'SRes::Pathway', location => 'SRes::Pathway', pathway => 'SRes::Pathway' };
+    my $typeToOntologyTerm = {molecularentity => 'molecular entity', compound => 'molecular entity', pathway => 'metabolic process', compartment => 'metabolic process', 
+                              location => 'metabolic process', enzyme => 'enzyme', protein => 'enzyme', transporter => 'enzyme', translocator => 'enzyme', gene => 'gene'};
 
 
     my $pathway = $pathwayHash->{'data'};
@@ -50,33 +52,51 @@ sub makeGusObjects {
     foreach my $node (@{$pathwayHash->{'elements'}->{'nodes'}}) {
         my $nodeData = $node->{'data'};
         unless ($nodeData->{'node_type'} eq 'pathway_internal') {
-            my $type = $typeToOntologyTerm->{$nodeData->{'node_type'}};
+            my $type = $typeToOntologyTerm->{lc($nodeData->{'node_type'})};
             my $typeId = $self->mapAndCheck($type, $self->getOntologyTerms());
-            my $tableName = $typeToTableMap->{$nodeData->{'node_type'}};
+            my $tableName = $typeToTableMap->{lc($nodeData->{'node_type'})};
             my $tableId = $self->mapAndCheck($tableName, $self->getTableIds());
             my $rowId = $self->getRowIds()->{$tableName}->{$nodeData->{'node_identifier'}};
+            
+            # handle entities acting as small molecules that have gene ids (e.g, tRNAs)
+            unless (defined($rowId)) {
+                if ($type eq 'molecular entity') {
+                    $tableName = $typeToTableMap->{'gene'};
+                    $tableId = $self->mapAndCheck($tableName, $self->getTableIds());
+                    $rowId = $self->getRowIds()->{$tableName}->{$nodeData->{'node_identifier'}};
+                }
+            }
+
             unless (defined($rowId)) {
                 $tableId = undef;
             }
+
+            my $x = defined ($nodeData->{'x'}) ? $nodeData->{'x'} : $node->{'position'}->{'x'};
+            my $y = defined ($nodeData->{'y'}) ? $nodeData->{'y'} : $node->{'position'}->{'y'};
             my $gusNode = GUS::Model::SRes::PathwayNode->new({'display_label' => $nodeData->{'display_label'},
                                                               'pathway_node_type_id' => $typeId,
                                                               'table_id' => $tableId,
                                                               'row_id' => $rowId,
-                                                              'x' => $nodeData->{'x'},
-                                                              'y' => $nodeData->{'y'},
+                                                              'x' => $x,
+                                                              'y' => $y,
                                                             });
             if (defined($nodeData->{'cellular_location'})) {
-                $gusNode->{'cellular_location'} = $nodeData->{'cellular_location'};
+                $gusNode->setCellularLocation($nodeData->{'cellular_location'});
             }
             $gusNode->setParent($gusPathway);
             my $uniqueId = $nodeData->{'id'};
             $self->addNode($gusNode, $uniqueId); 
 
-            if ($nodeData->{'node_type'} eq 'enzyme') {
-                my $gusReaction = GUS::Model::ApiDB::PathwayReaction->new({
-                                                                            'source_id' => $nodeData->{'reaction_source_id'}
-                                                                         });                
-                $self->addReaction($gusReaction, $uniqueId);
+            if ($type eq 'enzyme' || $type eq 'metabolic process' || $type eq 'gene') {
+                my $gusReaction = GUS::Model::ApiDB::PathwayReaction->new();
+                if ($nodeData->{'reaction_source_id'} ne 'null') {
+                    $gusReaction->setSourceId($nodeData->{'reaction_source_id'});
+                    $self->addReaction($gusReaction, $uniqueId);
+                }else {
+                    $gusReaction->setSourceId('MPMP_'.$pathwaySourceId.'_'.$uniqueId);
+                    $self->addReaction($gusReaction, $uniqueId);
+                    
+                }
             }
         }
     }        
@@ -94,6 +114,16 @@ sub makeGusObjects {
         $gusRelationship->setParent($gusAssociatedNode, "associated_node_id");
 
         my $gusReaction = defined($self->getReactionByUniqueId($nodeUniqueId)) ? $self->getReactionByUniqueId($nodeUniqueId) : $self->getReactionByUniqueId($associatedNodeUniqueId);
+
+        ###handle spontaneous compound-compound conversions (edges with no enzyme)
+        unless (defined($gusReaction)) {
+            my $node = $self->getNodeByUniqueId($nodeUniqueId);
+            my $associatedNode = $self->getNodeByUniqueId($associatedNodeUniqueId);
+            $gusReaction = GUS::Model::ApiDB::PathwayReaction->new();
+            $gusReaction->setSourceId('MPMP_'.$pathwaySourceId.'_'.$nodeUniqueId.'_'.$associatedNodeUniqueId);
+        }
+
+        die("Could not find GUS reaction\n") unless defined($gusReaction);
 
         my $pathwayReactionRel = GUS::Model::ApiDB::PathwayReactionRel->new();
 
