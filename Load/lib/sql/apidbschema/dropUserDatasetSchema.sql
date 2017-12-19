27fcf1602d0110d8b86f76deea9f813381fd10dc
Steve Fischer <sfischer@upenn.edu>
2017-12-19 16:06:15 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@86073 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/sql/apidbschema/dropUserDatasetSchema.sql
@@ -0,0 +1,10 @@
+DROP USER ApidbUserDatasets CASCADE;
+
+DELETE FROM core.TableInfo
+WHERE database_id IN (SELECT database_id
+                      FROM core.DatabaseInfo
+                      WHERE name = 'ApidbUserDatasets');
+
+DELETE FROM core.DatabaseInfo WHERE name = 'ApidbUserDatasets';
+
+exit;
