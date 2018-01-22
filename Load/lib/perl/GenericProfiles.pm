92cc90211ee8650c284db137bd67ffdb6287e3dd
John Brestelli <jbrestell@gmail.com>
2018-01-22 19:07:36 +0000

    self was not defined
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@86516 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/GenericProfiles.pm
+++ Load/lib/perl/GenericProfiles.pm
@@ -2,6 +2,8 @@ package ApiCommonData::Load::GenericProfiles;
 use base qw(CBIL::TranscriptExpression::DataMunger::NoSampleConfigurationProfiles);
 
 sub getProtocolName {
+  my ($self) = @_;
+
   my $technologyType = $self->getTechnologyType();
 
   return $technologyType;
