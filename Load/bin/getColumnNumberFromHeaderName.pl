8a39acfb04393f90df61a5cbe5a0f1fb185e5dc0
John Brestelli <jbrestell@gmail.com>
2018-01-31 19:30:17 +0000

    script to map header name to column number
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@86867 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/getColumnNumberFromHeaderName.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $headerName = $ARGV[0];
+
+my $filename = $ARGV[1];
+
+open(FILE, $filename) or die "Cannot open file $filename for reading: $!";
+
+my $firstline = <FILE>;
+chomp $firstline;
+
+close FILE;
+
+my @headers = split(/\t/, $firstline);
+
+for(my $i = 0; $i < scalar @headers; $i++) {
+  if($headers[$i] =~ /$headerName/i) {
+    my $colNum = $i + 1;
+    print "$headers[$i]\t$colNum\n";
+  }
+}
