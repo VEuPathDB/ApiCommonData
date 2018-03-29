a174bd3acbe654830a36106bf79010122c1b908e
Jay Humphrey <jaycolin@apidb.org>
2018-03-29 15:12:26 +0000

    added options -v (get values) and -e (exact match)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@88083 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/getColumnNumberFromHeaderName.pl
+++ Load/bin/getColumnNumberFromHeaderName.pl
@@ -1,23 +1,67 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
-my $headerName = $ARGV[0];
+use Getopt::Long;
 
-my $filename = $ARGV[1];
-
-open(FILE, $filename) or die "Cannot open file $filename for reading: $!";
-
-my $firstline = <FILE>;
-chomp $firstline;
-
-close FILE;
-
-my @headers = split(/\t|,/, $firstline);
-
-for(my $i = 0; $i < scalar @headers; $i++) {
-  if($headers[$i] =~ /$headerName/i) {
-    my $colNum = $i + 1;
-    print "$headers[$i]\t$colNum\n";
-  }
+unless (0 < @ARGV){
+  printf(join("\n",(
+    "Usage: getColumnNumberFromHeaderName.pl [-v|values] [-e|exact] [column_header] [file]",
+    "\t-e exact match (default is not case-sensitive and allows partial match)",
+    "\t-v get unique values in this column with count for each value (implies -e even if it is not specified)",
+    "")));
+  exit;
 }
+
+my ($values,$exact);
+
+GetOptions('v|values' => \$values, 'e|exact' => \$exact);
+
+$exact = 1 if $values;
+
+my($headerName, @files) = @ARGV;
+
+foreach my $filename (@files){
+
+  printf("\n\nReading: %s\n", $filename);
+
+  open(FILE, $filename) or die "Cannot open file $filename for reading: $!";
+  
+  my $firstline = <FILE>;
+  chomp $firstline;
+  
+  
+  my @headers = split(/\t|,/, $firstline);
+  
+  my $colNum;
+  
+  for(my $i = 0; $i < scalar @headers; $i++) {
+    my $match = 0;
+    if($exact){
+      $match = ($headers[$i] =~ /^$headerName$/);
+    }
+    else{
+      $match = ($headers[$i] =~ /$headerName/i);
+    }
+    if($match) {
+      $colNum = $i;
+      printf("Found \"%s\" at column %d\n", $headers[$i],$colNum + 1);
+    }
+  }
+  
+  if( $values && defined($colNum)){
+    my %vals;
+    while(my $line = <FILE>){
+      my @data = split(/\t|,/, $line);
+      $vals{ $data[$colNum] } ||= 0;
+      $vals{ $data[$colNum] }++;
+    }
+    printf("\nvalue\tcount\n-----\t-----\n%s\n", join("\n", map { sprintf("%s\t%5d", $_, $vals{$_}) } sort keys %vals));
+  }
+  elsif(!defined($colNum)){
+    printf("Column \"%s\" not found in %s\n", $headerName, $filename);
+  }
+  close FILE;
+}
+
