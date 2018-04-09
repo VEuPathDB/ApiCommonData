d476cc3ad867a2455c62027e144b23ade332f201
Wei Li <weili1@pennmedicine.upenn.edu>
2018-04-09 19:32:07 +0000

    init dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@88334 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/SingleSRADump.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl 
+
+use strict; 
+use lib "$ENV{GUS_HOME}/lib/perl";
+use CBIL::Util::Sra;
+use Getopt::Long;
+
+my($isPairedEnd,$sampleId);
+
+&GetOptions("isPairedEnd|q!" => \$isPairedEnd, 
+            'sampleId=s' => \$sampleId,
+            );
+
+my @tmp;
+foreach my $s (split(/,\s*/,$sampleId)){
+    push(@tmp,$s);
+}
+
+&getFastqForSampleIds(\@tmp,"$sampleId","$sampleId.paired",0,$isPairedEnd ? 1 : 0);
