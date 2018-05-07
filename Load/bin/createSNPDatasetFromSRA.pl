4715decd6bdb94ee3c510e9f7daa505a0ab8a83e
Haiming Wang <hwang@uga.edu>
2018-05-07 14:09:46 +0000

    minor changes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@88902 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/createSNPDatasetFromSRA.pl
+++ Load/bin/createSNPDatasetFromSRA.pl
@@ -5,6 +5,7 @@ use DBI;
 use Getopt::Long;
 
 my ($proj, $study, $org, $exp, $version);
+my $cnv = 0;
 my %hash;
 
 &GetOptions( 'project_name=s'    => \$proj,
@@ -12,6 +13,7 @@ my %hash;
              'organism_abbrev=s' => \$org,
              'experiment_name=s' => \$exp,
              'version=s'         => \$version,
+             'hasCNV!'           => \$cnv
            );
 
 unless($study && $proj && $org && $exp && $version) {
@@ -21,7 +23,7 @@ This script creates dataset xml and manual delivery files for HTS_SNPs from SRA.
 It takes SRA study id, organism abbreviation and dataset name as input. 
 For instance,
 
-%>createSNPDatasetFromSRA.pl --project_name FungiDB --study ERP009642 --organism_abbrev afumAf293 --experiment_name Azole-Resistance_Mutations --version 2015-04-24
+%>createSNPDatasetFromSRA.pl --project_name FungiDB --study ERP009642 --organism_abbrev afumAf293 --experiment_name Azole-Resistance_Mutations --version 2015-04-24 --hasCNV
 
 Output will be dataset xml and empty sample files under manual delivery file dir 
 EOL
@@ -33,7 +35,7 @@ open O1, ">$exp.xml";
 
 print O1 "<datasets>\n";
 
-my $dbh = DBI->connect("dbi:SQLite:/home/hwang/projects/R_getSRA/SRAmetadb.sqlite", "", "", { RaiseError => 1 }) or die $DBI::errstr;
+my $dbh = DBI->connect("dbi:SQLite:/eupath/data/EuPathDB/manualDelivery/SRAmetadb/SRAmetadb.sqlite", "", "", { RaiseError => 1 }) or die $DBI::errstr;
 
 my $stmt = qq(select run_accession,sample_alias,sample_attribute from sra where study_accession = '$study' order by run_accession);
 my $sth  = $dbh->prepare($stmt);
@@ -49,6 +51,7 @@ while(my @row = $sth->fetchrow_array()) {
   my $attr   = $row[2]; # mixed information, such as strain, isolate etc. can be used as sample name in analysisConfig
 
   $sample =~ s/\s/_/g;
+  $sample =~ s/\//-/g;
 
 print O1 <<EOL;
   <dataset class="SNPs_HTS_Sample_QuerySRA">
@@ -58,6 +61,10 @@ print O1 <<EOL;
     <prop name="sraQueryString">$run_id</prop>
   </dataset>
 
+EOL
+
+if($cnv) {
+print O1 <<EOL;
   <dataset class="copyNumberVariationSamples">
     <prop name="projectName">$proj</prop>
     <prop name="organismAbbrev">$org</prop>
@@ -67,6 +74,7 @@ print O1 <<EOL;
   </dataset>
 
 EOL
+}
 
 system("touch ../final/$sample");
 system("touch ../final/$sample.paired");
@@ -76,6 +84,5 @@ system("touch ../final/$sample.qual.paired");
 }
 
 print O1 "</datasets>\n";
-
 
 $dbh->disconnect(); 
