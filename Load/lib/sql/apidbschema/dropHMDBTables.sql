88316acc5db0bdb99f014eed45de418d5772b6cd
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2018-07-12 09:31:23 +0000

    Remove references to tables that we are no longer creating
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@90018 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropHMDBTables.sql
+++ Load/lib/sql/apidbschema/dropHMDBTables.sql
@@ -1,31 +1,21 @@
 DROP TABLE hmdb.chemical_data;
-DROP TABLE hmdb.comments;
 DROP TABLE hmdb.database_accession;
 DROP TABLE hmdb.names;
-DROP TABLE hmdb.reference;
-DROP TABLE hmdb.relation;
 DROP TABLE hmdb.default_structures;
 DROP TABLE hmdb.autogen_structures;
 DROP TABLE hmdb.structures;
-DROP TABLE hmdb.vertice;
-DROP TABLE hmdb.ontology;
 DROP TABLE hmdb.compounds;
 
 DROP SEQUENCE hmdb.chemical_data_SQ;
-DROP SEQUENCE hmdb.comments_SQ;
 DROP SEQUENCE hmdb.compounds_SQ;
 DROP SEQUENCE hmdb.database_accession_SQ;
 DROP SEQUENCE hmdb.names_SQ;
-DROP SEQUENCE hmdb.ontology_SQ;
-DROP SEQUENCE hmdb.reference_SQ;
-DROP SEQUENCE hmdb.relation_SQ;
-DROP SEQUENCE hmdb.vertice_SQ;
 DROP SEQUENCE hmdb.structures_SQ;
 DROP SEQUENCE hmdb.default_structures_SQ;
 DROP SEQUENCE hmdb.autogen_structures_SQ;
 
 DELETE FROM core.TableInfo
-WHERE lower(name) in ('chemical_data', 'comments', 'compounds', 'database_accession', 'names', 'ontology', 'reference', 'relation', 'vertice', 'structures', 'default_structures', 'autogen_structures')
+WHERE lower (name) in ('chemical_data', 'compounds', 'database_accession', 'names', 'structures', 'default_structures', 'autogen_structures')
   AND database_id = (SELECT database_id
                      FROM core.DatabaseInfo
                      WHERE lower(name) = 'hmdb');
