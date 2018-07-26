5f2a1f22cd2b900aa01655ee12f5b724fb829f2d
John Brestelli <jbrestell@gmail.com>
2018-07-26 13:15:58 +0000

    package that will do nothing;  used to get a workflow dataset name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@90364 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/DoNothing.pm
@@ -0,0 +1,22 @@
+package ApiCommonData::Load::DoNothing;
+use base qw(CBIL::TranscriptExpression::DataMunger::Loadable);
+
+sub munge {
+  my ($self) = @_;
+
+  my $configFilePath = $self->getConfigFilePath();
+
+  if(-e $configFilePath) {
+    open(CONFIG, ">> $configFilePath") or die "Cannot open config file for writing: $!";
+  }
+  else {
+    open(CONFIG, "> $configFilePath") or die "Cannot open config file for writing: $!";
+    $self->printConfigHeader(\*CONFIG);
+  }
+
+  close CONFIG;
+}
+
+
+
+1;
