3187c9d149c8fdb2c880c842f59552eafea475e5
John Iodice <iodice@pennmedicine.upenn.edu>
2018-09-05 01:40:06 +0000

    add profile tables
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@91317 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createUserDatasetTypeTables.sql
+++ Load/lib/sql/apidbschema/createUserDatasetTypeTables.sql
@@ -1,6 +1,3 @@
-
-
-
 create table ApiDBUserDatasets.UD_GeneId (
 USER_DATASET_ID          NUMBER(20),     
 gene_SOURCE_ID                             VARCHAR2(100),  
@@ -121,6 +118,115 @@ FROM dual,
 WHERE 'UD_NaFeatureExpression' NOT IN (SELECT name FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
+--------------------------------------------------------------------------------
+create table ApiDBUserDatasets.ProfileSet (
+ profile_set_id number(20),
+ user_dataset_id number(20),
+ modification_date            date not null,
+ user_read                    number(1) not null,
+ user_write                   number(1) not null,
+ group_read                   number(1) not null,
+ group_write                  number(1) not null,
+ other_read                   number(1) not null,
+ other_write                  number(1) not null,
+ row_user_id                  number(12) not null,
+ row_group_id                 number(4) not null,
+ row_project_id               number(4) not null,
+ row_alg_invocation_id        number(12) not null,
+ foreign key (user_dataset_id) references ApiDBUserDatasets.InstalledUserDataset,
+ primary key (profile_set_id)
+);
+
+create index ApiDBUserDatasets.pset_idx1
+  on ApiDBUserDatasets.ProfileSet
+     (profile_set_id, user_dataset_id)
+  tablespace indx;
+
+create sequence ApiDBUserDatasets.profileset_sq;
+
+grant insert, select, update, delete on ApiDBUserDatasets.ProfileSet to gus_w;
+grant select on ApiDBUserDatasets.ProfileSet to gus_r;
+grant select on ApiDBUserDatasets.profileSet_sq to gus_w;
+
+insert into core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+select core.tableinfo_sq.nextval, 'ProfileSet',
+       'Standard', 'PROFILE_SET_ID',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+from dual,
+     (select max(project_id) as project_id from core.ProjectInfo) p,
+     (select database_id
+      from core.DatabaseInfo
+      where lower(name) = lower('ApidbUserDatasets')) d
+where lower('ProfileSet') not in (select lower(name)
+                             from core.TableInfo
+                             where database_id = d.database_id);
+--------------------------------------------------------------------------------
+create table ApiDBUserDatasets.Profile (
+ profile_id                   number(20),
+ profile_set_id               number(20),
+ dataset_type                 varchar2(50),
+ dataset_subtype              varchar2(50),
+ profile_type                 varchar2(20),
+ source_id                    varchar2(400),
+ profile_study_id             number(5),
+ profile_set_name             varchar2(400),
+ profile_set_suffix           varchar2(50),
+ profile_as_string            varchar2(4000),
+ max_value                    number,
+ min_value                    number,
+ max_timepoint                varchar2(200),
+ min_timepoint                varchar2(200),
+ modification_date            date not null,
+ user_read                    number(1) not null,
+ user_write                   number(1) not null,
+ group_read                   number(1) not null,
+ group_write                  number(1) not null,
+ other_read                   number(1) not null,
+ other_write                  number(1) not null,
+ row_user_id                  number(12) not null,
+ row_group_id                 number(4) not null,
+ row_project_id               number(4) not null,
+ row_alg_invocation_id        number(12) not null,
+ foreign key (profile_set_id) references ApiDBUserDatasets.ProfileSet,
+ primary key (profile_id)
+);
+
+create index ApiDBUserDatasets.prf_idx1
+  on ApiDBUserDatasets.Profile
+     (profile_set_id, profile_id)
+  tablespace indx;
+
+create sequence ApiDBUserDatasets.profile_sq;
+
+grant insert, select, update, delete on ApiDBUserDatasets.Profile to gus_w;
+grant select on ApiDBUserDatasets.Profile to gus_r;
+grant select on ApiDBUserDatasets.profile_sq to gus_w;
+
+insert into core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+select core.tableinfo_sq.nextval, 'Profile',
+       'Standard', 'PROFILE_ID',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+from dual,
+     (select max(project_id) as project_id from core.ProjectInfo) p,
+     (select database_id
+      from core.DatabaseInfo
+      where lower(name) = lower('ApidbUserDatasets')) d
+where lower('Profile') not in (select lower(name)
+                             from core.TableInfo
+                             where database_id = d.database_id);
+--------------------------------------------------------------------------------
 exit;
 
 
