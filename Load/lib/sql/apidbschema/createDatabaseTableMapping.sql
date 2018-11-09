e6265e3eaf9b073f5738eeaf5e0ed2ab3b1e779f
John Brestelli <jbrestell@gmail.com>
2018-11-09 15:49:12 +0000

    factor global natural key into separate table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@93116 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDatabaseTableMapping.sql
+++ Load/lib/sql/apidbschema/createDatabaseTableMapping.sql
@@ -4,7 +4,6 @@ CREATE TABLE apidb.DatabaseTableMapping (
        table_name varchar2(35), 
        primary_key_orig number(20), 
        primary_key number(20),
-       global_natural_key varchar2(2500),
        MODIFICATION_DATE     DATE,
        USER_READ             NUMBER(1),
        USER_WRITE            NUMBER(1),
@@ -26,8 +25,6 @@ GRANT SELECT ON ApiDB.DatabaseTableMapping TO gus_r;
 CREATE INDEX apidb.db_tbl_map_idx
 ON ApiDB.DatabaseTableMapping (database_orig, table_name, primary_key_orig, primary_key) tablespace indx;
 
-CREATE INDEX apidb.db_tbl_map_g_idx
-ON ApiDB.DatabaseTableMapping (table_name, global_natural_key, primary_key) tablespace indx;
 
 CREATE SEQUENCE apidb.DatabaseTableMapping_sq;
 
@@ -51,5 +48,51 @@ WHERE 'databasetablemapping' NOT IN (SELECT lower(name) FROM core.TableInfo
                                     WHERE database_id = d.database_id);
 
 
+CREATE TABLE apidb.GlobalNaturalKey (
+       global_natural_key_id number(20),
+       table_name varchar2(35), 
+       primary_key number(20),
+       global_natural_key varchar2(1000), 
+       MODIFICATION_DATE     DATE,
+       USER_READ             NUMBER(1),
+       USER_WRITE            NUMBER(1),
+       GROUP_READ            NUMBER(1),
+       GROUP_WRITE           NUMBER(1),
+       OTHER_READ            NUMBER(1),
+       OTHER_WRITE           NUMBER(1),
+       ROW_USER_ID           NUMBER(12),
+       ROW_GROUP_ID          NUMBER(3),
+       ROW_PROJECT_ID        NUMBER(4),
+       ROW_ALG_INVOCATION_ID NUMBER(12),
+ PRIMARY KEY (global_natural_key_id)  
+ );
+
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.GlobalNaturalKey TO gus_w;
+GRANT SELECT ON ApiDB.GlobalNaturalKey TO gus_r;
+
+CREATE INDEX apidb.db_tbl_map_g_idx
+ON ApiDB.DatabaseTableMapping (table_name, global_natural_key, primary_key) tablespace indx;
+
+CREATE SEQUENCE apidb.GlobalNaturalKey_sq;
+
+GRANT SELECT ON apidb.GlobalNaturalKey_sq TO gus_r;
+GRANT SELECT ON apidb.GlobalNaturalKey_sq TO gus_w;
+
+INSERT INTO core.TableInfo
+    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
+     is_view, view_on_table_id, superclass_table_id, is_updatable, 
+     modification_date, user_read, user_write, group_read, group_write, 
+     other_read, other_write, row_user_id, row_group_id, row_project_id, 
+     row_alg_invocation_id)
+SELECT core.tableinfo_sq.nextval, 'GlobalNaturalKey',
+       'Standard', 'global_natural_key_id',
+       d.database_id, 0, 0, '', '', 1,sysdate, 1, 1, 1, 1, 1, 1, 1, 1,
+       p.project_id, 0
+FROM dual,
+     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
+     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
+WHERE 'globalnaturalkey' NOT IN (SELECT lower(name) FROM core.TableInfo
+                                    WHERE database_id = d.database_id);
 
 exit;
