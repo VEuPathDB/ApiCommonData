fc96994e6adc900c84e0985c069ff505d2091ae4
John Iodice <iodice@pennmedicine.upenn.edu>
2019-01-18 16:53:38 +0000

    shhhh!
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@94631 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/createDbLinks.sql
+++ Load/lib/sql/apidbschema/createDbLinks.sql
@@ -1,38 +1,38 @@
 CREATE PUBLIC DATABASE LINK prodS.login_comment
 CONNECT TO APICOMM_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'apicommS';
 
 CREATE PUBLIC DATABASE LINK prodN.login_comment
 CONNECT TO APICOMM_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'apicommN';
 
 CREATE PUBLIC DATABASE LINK devS.login_comment
 CONNECT TO APICOMM_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'apicommDevS';
 
 CREATE PUBLIC DATABASE LINK devN.login_comment
 CONNECT TO APICOMM_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'apicommDevN';
 
 CREATE PUBLIC DATABASE LINK acctdbN.profile
 CONNECT TO ACCTDB_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'acctdbN';
 
 CREATE PUBLIC DATABASE LINK acctdbS.profile
 CONNECT TO ACCTDB_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'acctdbS';
 
 -- rm15873 is for development of apicomm release-maintenance stuff
 -- https://redmine.apidb.org/issues/15873
 CREATE PUBLIC DATABASE LINK rm15873.login_comment
 CONNECT TO APICOMM_DBLINK
-IDENTIFIED BY po34weep
+IDENTIFIED BY "<password>"
 USING 'rm15873';
 
 
