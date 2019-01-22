b0440d603f9c20919c9cb24193205cbe7797a80e
John Iodice <iodice@pennmedicine.upenn.edu>
2019-01-22 17:02:38 +0000

    dblinks are now created by the DBAs, rather than the workflow
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@94660 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropDbLinks.sql
+++ Load/lib/sql/apidbschema/dropDbLinks.sql
@@ -1,16 +1,16 @@
-DROP PUBLIC DATABASE LINK apidb.login_comment;
-DROP PUBLIC DATABASE LINK alt.login_comment;
+-- DROP PUBLIC DATABASE LINK apidb.login_comment;
+-- DROP PUBLIC DATABASE LINK alt.login_comment;
 
-DROP PUBLIC DATABASE LINK prodS.login_comment;
-DROP PUBLIC DATABASE LINK prodN.login_comment;
+-- DROP PUBLIC DATABASE LINK prodS.login_comment;
+-- DROP PUBLIC DATABASE LINK prodN.login_comment;
 
-DROP PUBLIC DATABASE LINK devS.login_comment;
-DROP PUBLIC DATABASE LINK devN.login_comment;
+-- DROP PUBLIC DATABASE LINK devS.login_comment;
+-- DROP PUBLIC DATABASE LINK devN.login_comment;
 
-DROP PUBLIC DATABASE LINK rm15873.login_comment;
+-- DROP PUBLIC DATABASE LINK rm15873.login_comment;
 
-DROP PUBLIC DATABASE LINK acctdbN.profile;
-Drop PUBLIC DATABASE LINK acctdbS.profile;
+-- DROP PUBLIC DATABASE LINK acctdbN.profile;
+-- DROP PUBLIC DATABASE LINK acctdbS.profile;
 
 
 exit;
