71136d2f0d7791a2f7f3d6348c4285eeaca89a81
John Iodice <iodice@pennmedicine.upenn.edu>
2019-01-22 18:44:19 +0000

    don't hardwire password
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/api_build-28@94679 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/makeToxoVersion4Genes.sql
+++ Load/lib/sql/makeToxoVersion4Genes.sql
@@ -2,7 +2,7 @@
 
 drop table apidb.toxoversion4genes;
 
-copy from apidb/po34weep@toxo43p to apidb/po34weep@toxo440n -
+copy from apidb/password@toxo43p to apidb/password@toxo440n -
 create apidb.toxoversion4genes using -
 select source_id,sequence_id,start_min,end_max,strand,context_start,context_end,product -   
 from ApidbTuning.GeneAttributes;
