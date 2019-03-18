482148dcb31c4ffb5c3a2b5eab33b38f4d848338
Ross Madden <ross.madden@glasgow.ac.uk>
2019-03-18 17:00:23 +0000

    Developing.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@96313 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertCompoundMassSpec.pm
+++ Load/plugin/perl/InsertCompoundMassSpec.pm
@@ -166,15 +166,15 @@ sub run {
 
   # This look up takes time.
   my @compoundSQL = $self->sqlAsArray(Sql=>
-    "select s.structure
+    "c.id
 		  , c.chebi_accession
-		  --, c.id
+		  , select s.structure
 		  from chebi.structures s
 		  , CHEBI.compounds c
 		  where s.type = 'InChIKey'
           and c.id = s.compound_id
 		  and to_char(s.structure) = '$compundLookup'"
-  );
+  ); # TODO this is not fiding the key.
 
   print STDERR "Ross";
   print STDERR Dumper @compoundSQL;
