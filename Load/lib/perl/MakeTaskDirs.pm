8e2368de59263cf694498bc5759c31b6ae776bb4
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2019-03-20 18:18:36 +0000

    substitute the one used for orthoMCL pipline
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@96362 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/MakeTaskDirs.pm
+++ Load/lib/perl/MakeTaskDirs.pm
@@ -1,25 +1,4 @@
 #!/usr/bin/perl
-#vvvvvvvvvvvvvvvvvvvvvvvvv GUS4_STATUS vvvvvvvvvvvvvvvvvvvvvvvvv
-  # GUS4_STATUS | SRes.OntologyTerm              | auto   | absent
-  # GUS4_STATUS | SRes.SequenceOntology          | auto   | absent
-  # GUS4_STATUS | Study.OntologyEntry            | auto   | absent
-  # GUS4_STATUS | SRes.GOTerm                    | auto   | absent
-  # GUS4_STATUS | Dots.RNAFeatureExon            | auto   | absent
-  # GUS4_STATUS | RAD.SageTag                    | auto   | absent
-  # GUS4_STATUS | RAD.Analysis                   | auto   | absent
-  # GUS4_STATUS | ApiDB.Profile                  | auto   | absent
-  # GUS4_STATUS | Study.Study                    | auto   | absent
-  # GUS4_STATUS | Dots.Isolate                   | auto   | absent
-  # GUS4_STATUS | DeprecatedTables               | auto   | absent
-  # GUS4_STATUS | Pathway                        | auto   | absent
-  # GUS4_STATUS | DoTS.SequenceVariation         | auto   | absent
-  # GUS4_STATUS | RNASeq Junctions               | auto   | absent
-  # GUS4_STATUS | Simple Rename                  | auto   | absent
-  # GUS4_STATUS | ApiDB Tuning Gene              | auto   | absent
-  # GUS4_STATUS | Rethink                        | auto   | absent
-  # GUS4_STATUS | dots.gene                      | manual | unreviewed
-die 'This file has broken or unreviewed GUS4_STATUS rules.  Please remove this line when all are fixed or absent';
-#^^^^^^^^^^^^^^^^^^^^^^^^^ End GUS4_STATUS ^^^^^^^^^^^^^^^^^^^^
 
 package ApiCommonData::Load::MakeTaskDirs;
 
@@ -299,7 +278,7 @@ sub makeControllerPropFile {
     print F 
 "masterdir=$baseDir/master
 inputdir=$baseDir/input
-nodedir=$nodePath
+nodeWorkingDirsHome=$nodePath
 slotspernode=$slotsPerNode
 subtasksize=$taskSize
 taskclass=$taskClass
@@ -473,8 +452,7 @@ sub makeBSTaskPropFile {
 	|| die "Can't open $inputDir/task.prop for writing";
 
     print F 
-"blastBinDir=$blastBinDir
-dbFilePath=$dbFileName
+"dbFilePath=$dbFileName
 inputFilePath=$seqFilePath
 dbType=$dbType
 regex='$regex'
