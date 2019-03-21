2c6ea427b3e4f92973e41e2fbb0c710d7f6782dc
Ross Madden <ross.madden@glasgow.ac.uk>
2019-03-21 11:27:41 +0000

    Developing.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@96383 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertCompoundMassSpec.pm
+++ Load/plugin/perl/InsertCompoundMassSpec.pm
@@ -161,7 +161,7 @@ sub run {
 #		  "SELECT cmp.id
 #		   FROM APIDB.pubchemcompound cmp WHERE cmp.pubchem_compund_id = '$compound_id'");
 
-  my $compundLookup = 'InChIKey=' . 'JDVVGAQPNNXQDW-TVNFTVLESA-N';
+  my $compundLookup = 'InChIKey=' . $compound_id;
   print STDERR "lookup $compundLookup \n";
 
   # This look up takes time.
