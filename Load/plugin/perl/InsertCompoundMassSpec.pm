3120a305baaa83f75fe7067f0d0360bf7781e023
Ross Madden <ross.madden@glasgow.ac.uk>
2019-03-25 13:46:20 +0000

    Developing.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@96579 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertCompoundMassSpec.pm
+++ Load/plugin/perl/InsertCompoundMassSpec.pm
@@ -203,7 +203,8 @@ sub run {
   #print STDERR "dir ", $dir, "   file: ", $resultsFile;
 
   #TODO Fix the filepaths.
-  my $args = {mainDirectory=>'/home/rmadden/MetabolomicsDataLoading', makePercentiles=>0, inputFile=>$dir . "/" . $resultsFile, profileSetName=>'RossMetaTest' };
+  my $inputFile = $dir . "/" . $resultsFile;
+  my $args = {mainDirectory=>'/home/rmadden/MetabolomicsDataLoading', makePercentiles=>0, inputFile=>$inputFile, profileSetName=>'RossMetaTest' };
   #TODO What should profileSetName be?
   my $params;
 
