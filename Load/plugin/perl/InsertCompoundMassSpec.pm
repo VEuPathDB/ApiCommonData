e20a658dcf1eb8b3e67d2d3b91782d8dec4ccde0
Ross Madden <ross.madden@glasgow.ac.uk>
2019-06-11 15:26:31 +0000

    DevB
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@98855 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertCompoundMassSpec.pm
+++ Load/plugin/perl/InsertCompoundMassSpec.pm
@@ -287,14 +287,14 @@ sub run {
 
           foreach my $cpd (@{$compoundHash->{$peak}->{'1'}->{$chebi}}){             
             #print STDERR "CPD = $cpd\n"     
-            # my $compoundPeaksChebiRow = GUS::Model::ApiDB::CompoundPeaksChebi->new({
-            #     compound_id=>$chebi,
-            #     isotopomer=>$isotopomer,
-            #     user_compound_name=>$cpd,
-            #     is_preferred_compound=>'1'
-            #   });
-            # $compoundPeaksChebiRow->setParent($compoundPeaksRow);
-            # $compoundPeaksRow->addToSubmitList($compoundPeaksChebiRow);
+            my $compoundPeaksChebiRow = GUS::Model::ApiDB::CompoundPeaksChebi->new({
+                compound_id=>$chebi,
+                isotopomer=>$isotopomer,
+                user_compound_name=>$cpd,
+                is_preferred_compound=>'1'
+              });
+            $compoundPeaksChebiRow->setParent($compoundPeaksRow);
+            $compoundPeaksRow->addToSubmitList($compoundPeaksChebiRow);
           }
 
         }
@@ -313,14 +313,14 @@ sub run {
         
           foreach my $cpd (@{$compoundHash->{$peak}->{'0'}->{$chebi}}){
             #print STDERR "CPD = $cpd\n"         
-            my $compoundPeaksChebiRow = GUS::Model::ApiDB::CompoundPeaksChebi->new({
-                compound_id=>$chebi,
-                isotopomer=>$isotopomer,
-                user_compound_name=>$cpd,
-                is_preferred_compound=>'0'
-              });
-            $compoundPeaksChebiRow->setParent($compoundPeaksRow);
-            $compoundPeaksRow->addToSubmitList($compoundPeaksChebiRow);
+            # my $compoundPeaksChebiRow = GUS::Model::ApiDB::CompoundPeaksChebi->new({
+            #     compound_id=>$chebi,
+            #     isotopomer=>$isotopomer,
+            #     user_compound_name=>$cpd,
+            #     is_preferred_compound=>'0'
+            #   });
+            # $compoundPeaksChebiRow->setParent($compoundPeaksRow);
+            # $compoundPeaksRow->addToSubmitList($compoundPeaksChebiRow);
           }
           # $compoundPeaksChebiRow->setParent($compoundPeaksRow);
           # $compoundPeaksRow->addToSubmitList($compoundPeaksChebiRow)
