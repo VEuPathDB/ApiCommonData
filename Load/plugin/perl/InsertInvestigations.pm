1d13f687ac8cebe0b857e430aaecec808896d0ee
Jay Humphrey <jaycolin@apidb.org>
2019-06-20 20:05:38 +0000

    Experimental: bypass GUS::Model to load into Study.* tables
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/branches/jaycolin@99155 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertInvestigations.pm
+++ Load/plugin/perl/InsertInvestigations.pm
@@ -187,7 +187,7 @@ sub run {
     }
 
     eval {
-    $investigation->parseInvestigation();
+      $investigation->parseInvestigation();
     };
     if($@) {
       $self->logOrError($@);
@@ -228,41 +228,47 @@ sub run {
       next;
     }
 
-    foreach my $study (@$studies) {
+## !!! EXPERIMENTAL !!! ##
+    $self->log("EXPERIMENTAL PRELOADING/SIDELOADING\n");
+    $self->sideloadStudy($investigation);
+    $self->{_PROTOCOL_APP_NODE_MAP} = {};
+## !!! EXPERIMENTAL !!! ##
+#   $studies = $investigation->getStudies();
+#   foreach my $study (@$studies) {
+#   	my $file = $study->getFileName();
+#     while($study->hasMoreData()) {
 
-      while($study->hasMoreData()) {
+#       eval {
+#         $investigation->parseStudy($study);
+#         $investigation->dealWithAllOntologies();
+#       };
+#       if($@) {
+#         $self->logOrError($@);
+#         next;
+#       }
 
-        eval {
-          $investigation->parseStudy($study);
-          $investigation->dealWithAllOntologies();
-        };
-        if($@) {
-          $self->logOrError($@);
-          next;
-        }
+#       my $isatabDatasets = $study->{_insert_investigations_datasets};
 
-        my $isatabDatasets = $study->{_insert_investigations_datasets};
+#       $self->checkProtocolsAndSetIds($study->getProtocols());
 
-        $self->checkProtocolsAndSetIds($study->getProtocols());
+#       my $iOntologyTermAccessions = $investigation->getOntologyAccessionsHash();
 
-        my $iOntologyTermAccessions = $investigation->getOntologyAccessionsHash();
+#       $self->checkOntologyTermsAndSetIds($iOntologyTermAccessions);
 
-        $self->checkOntologyTermsAndSetIds($iOntologyTermAccessions);
-
-        my $investigationId = $self->loadInvestigation($investigation);
+#       my $investigationId = $self->loadInvestigation($investigation);
 
 
-        $self->checkMaterialEntitiesHaveMaterialType($study->getNodes());
+#       $self->checkMaterialEntitiesHaveMaterialType($study->getNodes());
 
 
-        $self->checkDatabaseNodesAreHandled($isatabDatasets, $study->getNodes());
-        $self->checkDatabaseProtocolApplicationsAreHandledAndMark($isatabDatasets, $study->getEdges());
-        $self->loadStudy($study,$investigationId, $charFh);
-      }
+#       $self->checkDatabaseNodesAreHandled($isatabDatasets, $study->getNodes());
+#       $self->checkDatabaseProtocolApplicationsAreHandledAndMark($isatabDatasets, $study->getEdges());
+#       $self->loadStudy($study,$investigationId, $charFh);
+#     }
 
-      # clear out the protocol app node hash
-      $self->{_PROTOCOL_APP_NODE_MAP} = {};
-    }
+#     # clear out the protocol app node hash
+#     $self->{_PROTOCOL_APP_NODE_MAP} = {};
+#   }
 
     $investigationCount++;
   }
@@ -273,7 +279,7 @@ sub run {
   }
 
 
-  $self->loadCharacteristics($charFile);
+# $self->loadCharacteristics($charFile);
 
   $self->logRowsInserted() if($self->getArg('commit'));
 
@@ -282,70 +288,73 @@ sub run {
 
 sub countLines {
   my ($self, $charFile) = @_;
-	open(FILE, "<", $charFile);
-	my $count += tr/\n/\n/ while sysread(FILE, $_, 2 ** 16);
-	close(FILE);
-	return $count;
+  open(FILE, "<", $charFile);
+  my $count += tr/\n/\n/ while sysread(FILE, $_, 2 ** 16);
+  close(FILE);
+  return $count;
 }
 
-sub loadCharacteristics{
-  my ($self, $charFile) = @_;
-  my $configFile = "$charFile" . ".ctrl";
-  my $logFile = "$charFile" . ".log";
-  my @fields = (
-    'CHARACTERISTIC_ID SEQUENCE(MAX,1)',
-    'protocol_app_node_id',
-    'qualifier_id',
-    'unit_id',
-    'value char(2000)',
-    'ontology_term_id'
-  );
-  $self->writeConfigFile($configFile, $charFile, "Study.Characteristic", \@fields);
+#sub loadCharacteristics{
+#   my ($self, $charFile) = @_;
+#   my $configFile = "$charFile" . ".ctrl";
+#   my $logFile = "$charFile" . ".log";
+#   my @fields = (
+#     'CHARACTERISTIC_ID SEQUENCE(MAX,1)',
+#     'protocol_app_node_id',
+#     'qualifier_id',
+#     'unit_id',
+#     'value char(2000)',
+#     'ontology_term_id'
+#   );
+#   my $table = "Study.Characteristic";
+#   $self->writeConfigFile($configFile, $charFile, "Study.Characteristic", \@fields);
+# 
+#   $self->runSqlldr($charFile, [[ $table, 'CHARACTERISTIC_ID', 'Study.CHARACTERISTIC_sq' ]]);
+# 
+# my $login       = $self->getConfig->getDatabaseLogin();
+# my $password    = $self->getConfig->getDatabasePassword();
+# my $dbiDsn      = $self->getConfig->getDbiDsn();
+# my ($dbi, $type, $db) = split(':', $dbiDsn);
+# my $directMode = 'false';
+# if ($self->countLines($charFile) > 100000){
+#     $directMode = 'true';
+#     $self->log("SQLLDR will use DIRECT path\n");
+# }
+# if($self->getArg('commit')) {
+#     my $exitstatus = system("sqlldr $login/$password\@$db control=$configFile log=$logFile rows=1000 direct=$directMode");
 
-  my $login       = $self->getConfig->getDatabaseLogin();
-  my $password    = $self->getConfig->getDatabasePassword();
-  my $dbiDsn      = $self->getConfig->getDbiDsn();
-  my ($dbi, $type, $db) = split(':', $dbiDsn);
-  my $directMode = 'false';
-  if ($self->countLines($charFile) > 100000){
-      $directMode = 'true';
-      $self->log("SQLLDR will use DIRECT path\n");
-  }
-  if($self->getArg('commit')) {
-      my $exitstatus = system("sqlldr $login/$password\@$db control=$configFile log=$logFile rows=1000 direct=$directMode");
+#     if($exitstatus != 0){
+#   die "ERROR: sqlldr returned exit status $exitstatus";
+#     }
 
-      if($exitstatus != 0){
-	  die "ERROR: sqlldr returned exit status $exitstatus";
-      }
+#     open(LOG, $logFile) or die "Cannot open log file $logFile: $!";
 
-      open(LOG, $logFile) or die "Cannot open log file $logFile: $!";
+#     while(<LOG>) {
+#   $self->log($_);
+#     }
+#     close LOG;
 
-      while(<LOG>) {
-	  $self->log($_);
-      }
-      close LOG;
+#     unlink $logFile;
+# }
 
-      unlink $logFile;
-  }
+# unlink $configFile;
 
-  unlink $configFile;
-
-  my $sequenceName = "Study.CHARACTERISTIC_sq";
-  my $dbh = $self->getQueryHandle();
-  my ($sequenceValue) = $dbh->selectrow_array("select ${sequenceName}.nextval from dual");
-  my ($maxPrimaryKey) = $dbh->selectrow_array("select MAX(CHARACTERISTIC_ID) FROM Study.CHARACTERISTIC");
-  my $sequenceDifference = $maxPrimaryKey - $sequenceValue;
-  $self->log("Increasing CHARACTERISTIC_ID by $sequenceDifference\n");
-  if($sequenceDifference > 0) {
-    $dbh->do("alter sequence $sequenceName increment by $sequenceDifference");
-    $dbh->do("select ${sequenceName}.nextval from dual");
-    $dbh->do("alter sequence $sequenceName increment by 1");
-  }
-
-  return "Processed lines from data files";
-
-
-}
+# my $sequenceName = "Study.CHARACTERISTIC_sq";
+# my $dbh = $self->getQueryHandle();
+# my ($sequenceValue) = $dbh->selectrow_array("select ${sequenceName}.nextval from dual");
+# my ($maxPrimaryKey) = $dbh->selectrow_array("select MAX(CHARACTERISTIC_ID) FROM Study.CHARACTERISTIC");
+# my $sequenceDifference = $maxPrimaryKey - $sequenceValue;
+# $self->log("Increasing CHARACTERISTIC_ID by $sequenceDifference\n");
+# if($sequenceDifference > 0) {
+#   $dbh->do("alter sequence $sequenceName increment by $sequenceDifference");
+#   $dbh->do("select ${sequenceName}.nextval from dual");
+#   $dbh->do("alter sequence $sequenceName increment by 1");
+# }
+#
+# return "Processed lines from data files";
+#
+#
+#}
 
 
 sub lookupStudyLinks {
@@ -437,13 +446,11 @@ sub loadStudy {
 
   $gusStudy->setDescription($description);
 
-### DISABLE CONSTRAINTS
   my $panNameToIdMap = $self->loadNodes($study->getNodes(), $gusStudy, $charFh);
 
   my ($protocolParamsToIdMap, $protocolNamesToIdMap) = $self->loadProtocols($study->getProtocols());
 
   $self->loadEdges($study->getEdges, $panNameToIdMap, $protocolParamsToIdMap, $protocolNamesToIdMap);
-### ENABLE CONSTRAINTS
 }
 
 
@@ -458,6 +465,7 @@ sub loadNodes {
 
   my $nodeCount = 0;
   $self->getDb()->manageTransaction(0, 'begin');
+
 
   foreach my $node (@$nodes) {
     my $pan;
@@ -1086,9 +1094,175 @@ sub loadInvestigation{
 }
 
 
+sub getConfig {
+  my ($self) = @_;
+
+  if (!$self->{config}) {
+    my $gusConfigFile = $self->getArg('gusconfigfile');
+     $self->{config} = GUS::Supported::GusConfig->new($gusConfigFile);
+   }
+
+  $self->{config};
+}
+
+
+
+sub sideloadStudy {
+  my ($self, $inv) = @_;
+## PAN data
+  my ($dataFh, $dataFile) = tempfile(SUFFIX => '.dat');
+  my $configFile = $dataFile . ".ctrl";
+  my $table = 'Study.ProtocolAppNode';
+  my $allnodes = [];
+  my $alledges = [];
+  my $allprots = [];
+## each study
+  my $studies = $inv->getStudies();
+  my %nodeStudyInvestigation;
+  foreach my $study (@$studies){
+    my $investigationId = $self->loadInvestigation($inv);
+    my $identifier = $study->getIdentifier();
+    my $description = $study->getDescription();
+    my $extDbRlsId = $self->{_external_database_release_id};
+    my $gusStudy = GUS::Model::Study::Study->new({name => $identifier, source_id => $identifier, investigation_id =>$investigationId, external_database_release_id=>$extDbRlsId});
+    $gusStudy->submit() unless ($gusStudy->retrieveFromDB());
+    $gusStudy->setDescription($description);
+    my $studyId = $gusStudy->getId();
+    my $invId = $gusStudy->getInvestigationId();
+    my $firstpass=1;
+    while($study->hasMoreData()) {
+	  	$inv->parseStudy($study);
+      if($firstpass){
+        $inv->dealWithAllOntologies();
+        $self->checkProtocolsAndSetIds($study->getProtocols());
+        my $iOntologyTermAccessions = $inv->getOntologyAccessionsHash();
+        $self->checkOntologyTermsAndSetIds($iOntologyTermAccessions);
+        $self->checkMaterialEntitiesHaveMaterialType($study->getNodes());
+        $firstpass=0;
+      }
+	  	my $edges = $study->getEdges();
+      push(@$alledges, @$edges);
+	  	my $prots = $study->getProtocols();
+      push(@$allprots, @$prots);
+	  	my $nodes = [];
+	  	foreach my $edge (@$edges){
+	  		my $inputs = $edge->getInputs();
+	  		my $outputs = $edge->getOutputs();
+	  		push(@$nodes, @$outputs);
+	  	}
+	  	unless(0 < scalar @$nodes){
+	  		$nodes = $study->getNodes();
+	  	}
+      foreach my $node (@$nodes) {
+        my $name = $node->getValue();
+        $nodeStudyInvestigation{$name} = [$studyId, $invId]; ## for loading STUDYLINK
+        my $desc = $node->getDescription();
+        my ($isaType) = reverse(split(/::/, ref($node)));
+        my $typeId;
+        if($node->hasAttribute("MaterialType")) {
+          my $materialTypeOntologyTerm = $node->getMaterialType();
+          my $gusOntologyTerm = $self->getOntologyTermGusObj($materialTypeOntologyTerm, 0);
+          $typeId = $gusOntologyTerm->getId();
+        }
+        printf $dataFh ("%s\n", join("\t", $typeId, $name, $desc, $isaType));
+        push(@$allnodes, $node);
+      }
+    }
+  }
+  close($dataFh);
+## /each study
+  ## PROTOCOLAPPNODE
+  my @fields = (
+    'PROTOCOL_APP_NODE_ID SEQUENCE(MAX,1)',
+    'TYPE_ID',
+    'NAME',
+    'DESCRIPTION',
+    'ISA_TYPE'
+  );
+  $self->writeConfigFile($configFile, $dataFile, $table, \@fields);
+  $self->runSqlldr($dataFile, [[ $table, 'PROTOCOL_APP_NODE_ID', 'Study.PROTOCOLAPPNODE_SQ' ]]);
+    # keep the cache up to date as we add new nodes
+  my $panNameToIdMap = $self->updateProtocolAppNodeMapId($allnodes);
+
+  ## STUDYLINK
+  my ($linksFh, $linksFile) = tempfile(SUFFIX => '.dat');
+  my $linksCtrlFile = $linksFile . ".ctrl";
+  while( my ($name,$panId) = each %{$panNameToIdMap} ){
+    die ("UNMAPPED node $name (PANID $panId)") unless(defined($nodeStudyInvestigation{$name}));
+    my ($studyId, $invId) = @{ $nodeStudyInvestigation{$name} };
+    printf $linksFh ("%s\t%s\n%s\t%s\n", $studyId, $panId, $invId, $panId);
+  }
+  close($linksFh);
+  @fields = (
+    'STUDY_LINK_ID SEQUENCE(MAX,1)',
+    'STUDY_ID',
+    'PROTOCOL_APP_NODE_ID',
+  );
+  $table = 'Study.StudyLink';
+  $self->writeConfigFile($linksCtrlFile, $linksFile, $table, \@fields);
+  $self->runSqlldr($linksFile, [[ $table, 'STUDY_LINK_ID', 'Study.STUDYLINK_SQ' ]]);
+
+  ## CHARACTERISTIC
+  my ($charFh, $charFile) = tempfile(SUFFIX => '.dat');
+  my $charCtrlFile = $charFile . ".ctrl";
+  foreach my $node(@$allnodes){
+    my $name = $node->getValue(); 
+    my $panId = $panNameToIdMap->{$name};
+    die "ERROR: PAN with name $name was not inserted into Study.ProtocolAppNode\n" unless $panId;
+    my $chars = $self->mungeCharacteristics([$node]);
+    foreach my $row (@$chars){
+      printf $charFh ("%s\n", join("\t", $panId, @$row));
+    }
+  }
+  close($charFh);
+  my @fields = (
+    'CHARACTERISTIC_ID SEQUENCE(MAX,1)',
+    'protocol_app_node_id',
+    'qualifier_id',
+    'unit_id',
+    'value char(2000)',
+    'ontology_term_id'
+  );
+  my $table = "Study.Characteristic";
+  $self->writeConfigFile($charCtrlFile, $charFile, $table, \@fields);
+  $self->runSqlldr($charFile, [[ $table, 'CHARACTERISTIC_ID', 'Study.CHARACTERISTIC_sq' ]]);
+
+  ## PROTOCOLS AND EDGES
+  printf STDERR ("Loading %d Protocols\n", scalar @$allprots);
+  my ($protocolParamsToIdMap, $protocolNamesToIdMap) = $self->loadProtocols($allprots);
+  printf STDERR ("Loading %d Edges\n", scalar @$alledges);
+  $self->loadEdges($alledges, $panNameToIdMap, $protocolParamsToIdMap, $protocolNamesToIdMap);
+}
+
+sub updateProtocolAppNodeMapId {
+  my ($self, $nodes) = @_;
+  my $database = $self->getDb();
+  my $algInvocationId = $database->getDefaultAlgoInvoId();
+  my $sql = "select pan.name, pan.protocol_app_node_id from STUDY.PROTOCOLAPPNODE pan where pan.row_alg_invocation_id=?";
+  my $dbh = $self->getQueryHandle();
+  my $sh = $dbh->prepare($sql);
+  my %studyNodes;
+  $sh->execute($algInvocationId);
+  while(my ($name, $panId) = $sh->fetchrow_array()) {
+    if(defined($self->{_PROTOCOL_APP_NODE_MAP}->{$name}) && $self->{_PROTOCOL_APP_NODE_MAP}->{$name} ne $panId){
+      $self->log("ERROR: Duplicate PROTOCOL_APP_NODE_ID $name!= $panId\n");
+    }
+    $self->{_PROTOCOL_APP_NODE_MAP}->{$name} = $panId;
+  }
+  $sh->finish();
+
+  foreach my $node (@$nodes) {
+    my $name= $node->getValue();
+    $node->{_PROTOCOL_APP_NODE_ID} = $self->{_PROTOCOL_APP_NODE_MAP}->{$name};
+    unless(defined($self->{_PROTOCOL_APP_NODE_MAP}->{$name})){
+      die "ERROR: impossible $name has no PROTOCOL_APP_NODE_ID\n";
+    }
+  }
+  return $self->{_PROTOCOL_APP_NODE_MAP};
+}
+  
 sub writeConfigFile {
   my ($self, $configFile, $dataFile, $table, $fieldsArray) = @_;
-
   my $modDate = uc(strftime("%d-%b-%Y", localtime));
   my $fields = join(",\n", @$fieldsArray);
   my $database = $self->getDb();
@@ -1103,12 +1277,12 @@ sub writeConfigFile {
   my $otherRead = $database->getDefaultOtherRead();
   my $otherWrite = $database->getDefaultOtherWrite();
 
-  open(CONFIG, "> $configFile") or die "Cannot open file $configFile For writing:$!";
+  open(CONFIG, ">$configFile") or die "Cannot open file $configFile For writing:$!";
 
   print CONFIG "LOAD DATA
 INFILE '$dataFile'
 APPEND
-INTO TABLE Study.Characteristic
+INTO TABLE $table
 REENABLE DISABLED_CONSTRAINTS
 FIELDS TERMINATED BY '\\t'
 TRAILING NULLCOLS
@@ -1129,17 +1303,114 @@ row_alg_invocation_id constant $algInvocationId
   close CONFIG;
 }
 
-sub getConfig {
-  my ($self) = @_;
-
-  if (!$self->{config}) {
-    my $gusConfigFile = $self->getArg('gusconfigfile');
-     $self->{config} = GUS::Supported::GusConfig->new($gusConfigFile);
-   }
-
-  $self->{config};
+sub runSqlldr {
+  my ($self,$dataFile, $sequences) = @_;
+  my $configFile = $dataFile . ".ctrl";
+  my $logFile = $dataFile . ".log";
+  my $login       = $self->getConfig->getDatabaseLogin();
+  my $password    = $self->getConfig->getDatabasePassword();
+  my $dbiDsn      = $self->getConfig->getDbiDsn();
+  my ($dbi, $type, $db) = split(':', $dbiDsn);
+  my $directMode = 'false';
+  if ($self->countLines($dataFile) > 100000){
+    $directMode = 'true';
+    $self->log("SQLLDR will use DIRECT path\n");
+  }
+  if($self->getArg('commit')) {
+    my $exitstatus = system("sqlldr $login/$password\@$db control=$configFile log=$logFile rows=1000 direct=$directMode");
+    if($exitstatus != 0){
+      die "ERROR: sqlldr returned exit status $exitstatus";
+    }
+    open(LOG, $logFile) or die "Cannot open log file $logFile: $!";
+    while(<LOG>) {
+      chomp;
+      $self->log($_);
+    }
+    close LOG;
+    unlink $logFile;
+  }
+  unlink $configFile;
+  unlink $dataFile;
+  foreach my $seq (@$sequences){
+    my ($table,$field, $sequenceName) = @$seq;
+    my $dbh = $self->getQueryHandle();
+    my ($sequenceValue) = $dbh->selectrow_array("select ${sequenceName}.nextval from dual");
+    my ($maxPrimaryKey) = $dbh->selectrow_array("select MAX($field) FROM $table");
+    my $sequenceDifference = $maxPrimaryKey - $sequenceValue;
+    $self->log("Increasing $field by $sequenceDifference\n");
+    if($sequenceDifference > 0) {
+      $dbh->do("alter sequence $sequenceName increment by $sequenceDifference");
+      $dbh->do("select ${sequenceName}.nextval from dual");
+      $dbh->do("alter sequence $sequenceName increment by 1");
+    }
+  }
 }
 
+sub mungeCharacteristics {
+  my($self, $nodes) = @_;
+  my @charsForLoader;
+  foreach my $node(@$nodes){
+    if($node->hasAttribute("MaterialType")) {
+      # my $materialTypeOntologyTerm = $node->getMaterialType();
+      # my $gusOntologyTerm = $self->getOntologyTermGusObj($materialTypeOntologyTerm, 0);
+      # my $ontologyTermId = $gusOntologyTerm->getId();
+      # $pan->setTypeId($ontologyTermId); # CANNOT Set Parent because OntologyTerm Table has type and subtype.  Both have fk to Sres.ontologyterm
+
+      my $characteristics = $node->getCharacteristics();
+
+      foreach my $characteristic (@$characteristics) {
+        if(lc $characteristic->getTermSourceRef() eq 'ncbitaxon') {
+          my $taxonId = $self->{_ontology_term_to_identifiers}->{$characteristic->getTermSourceRef()}->{$characteristic->getTermAccessionNumber()};
+          # $pan->setTaxonId($taxonId);
+        }
+
+    #   my $gusChar = GUS::Model::Study::Characteristic->new();
+    #   $gusChar->setParent($pan);
+
+        my $charQualifierOntologyTerm = $self->getOntologyTermGusObj($characteristic, 1);
+        my $charQualifierId = $charQualifierOntologyTerm->getId();
+
+      # ALWAYS Set the qualifier_id
+
+    #   $gusChar->setQualifierId($charQualifierOntologyTerm->getId()); # CANNOT SET Parent because ontology term id and Unit id.  both fk to sres.ontologyterm
+
+        my $charUnitId;
+
+        if($characteristic->getUnit()) {
+          my $unitOntologyTerm = $self->getOntologyTermGusObj($characteristic->getUnit(), 0);
+    #     $gusChar->setUnitId($unitOntologyTerm->getId());
+          $charUnitId = $unitOntologyTerm->getId();
+        }
+
+        my ($charValue, $charOntologyTermId);
+
+        if(lc $characteristic->getTermSourceRef() eq 'ncbitaxon') {
+          my $value = $self->{_ontology_term_to_names}->{$characteristic->getTermSourceRef()}->{$characteristic->getTermAccessionNumber()};
+    #     $gusChar->setValue($value);
+          $charValue = $value;
+        }
+        elsif($characteristic->getTermAccessionNumber() && $characteristic->getTermSourceRef()) {
+          my $valueOntologyTerm = $self->getOntologyTermGusObj($characteristic, 0);
+    #     $gusChar->setOntologyTermId($valueOntologyTerm->getId());
+              $charOntologyTermId = $valueOntologyTerm->getId();
+        }
+        else {
+    #     $gusChar->setValue($characteristic->getTerm());
+              $charValue = $characteristic->getTerm();
+        }
+
+        # strip off carriage return
+        $charValue =~ s/\r//;
+
+
+    #   print $charFh join("\t", ($pan->getId(),
+        push @charsForLoader, [$charQualifierId, $charUnitId, $charValue, $charOntologyTermId];
+
+      }
+    }
+  }
+  return \@charsForLoader;
+}
 
 
 sub undoTables {
@@ -1157,6 +1428,7 @@ sub undoTables {
     'Study.Study',
      );
 }
+
 
 1;
 
