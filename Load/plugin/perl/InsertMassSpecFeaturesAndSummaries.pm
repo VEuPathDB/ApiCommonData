cd3e9ad510ded101716ea60cc27f898457180795
Wei Li <weili1@pennmedicine.upenn.edu>
2019-07-30 17:45:21 +0000

    dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@99878 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
+++ Load/plugin/perl/InsertMassSpecFeaturesAndSummaries.pm
@@ -539,9 +539,9 @@ sub addRecordsToGenes {
 
           # ensure that all peptides match on to this protein
 
-#          my $proteinMatchPercent = $self->checkThatPeptidesMatch($record,$prot->[1]);
+          my $proteinMatchPercent = $self->checkThatPeptidesMatch($record,$prot->[1]);
           if($proteinMatchPercent >= $proteinMatchCuttoff) {
-          if($self->checkThatAllPeptidesMatch($record,$prot->[1])) {
+#          if($self->checkThatAllPeptidesMatch($record,$prot->[1])) {
             $wasFound = 1;
             $self->copyRecord($record, $aaSeqId);
           }else{
