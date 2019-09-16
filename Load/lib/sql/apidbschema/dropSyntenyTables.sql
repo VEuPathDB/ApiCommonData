20697bf1399bc2571d827b84267b116b8f34d989
John Brestelli <jbrestell@gmail.com>
2019-09-16 14:37:01 +0000

    rm syntenicscale table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonData/trunk@101072 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/sql/apidbschema/dropSyntenyTables.sql
+++ Load/lib/sql/apidbschema/dropSyntenyTables.sql
@@ -2,9 +2,6 @@ drop table ApiDB.SyntenicGene;
 drop sequence ApiDB.SyntenicGene_sq;
 delete from Core.TableInfo where name = 'SyntenicGene';
 
-drop table ApiDB.SyntenicScale;
-drop sequence ApiDB.SyntenicScale_sq;
-delete from Core.TableInfo where name = 'SyntenicScale';
 
 drop table ApiDB.Synteny;
 drop sequence ApiDB.Synteny_sq;
