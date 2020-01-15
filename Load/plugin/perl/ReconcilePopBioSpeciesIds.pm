3073707c3cb1f8bee01f842c4f4f7c25af54b885
Bob MacCallum <uncoolbob@gmail.com>
2020-01-15 12:32:17 -0500

    got most of reconcilation working. few more bits to tidy up

--- Load/plugin/perl/ReconcilePopBioSpeciesIds.pm
+++ Load/plugin/perl/ReconcilePopBioSpeciesIds.pm
@@ -27,7 +27,7 @@ my $argsDeclaration =
   [
 
    stringArg({name           => 'extDbRlsSpec',
-	      descr          => 'external database release spec',
+	      descr          => 'external database (more accurately, dataset) release spec',
 	      reqd           => 1,
 	      constraintFunc => undef,
 	      isList         => 0, }),
@@ -74,7 +74,7 @@ sub run {
     my $investigationId = $investigation->getStudyId(); # yes this is correct
     my $study = GUS::Model::Study::Study->new({investigation_id=>$investigationId, external_database_release_id=>$extDbRlsId});
     $study->retrieveFromDB();
-    $self->log("LOG\tgot study named >".$study->getName()."< for investigation_id $investigationId");
+    $self->log("LOG\tgot study named '".$study->getName()."'");
     my $studyId = $study->getStudyId();
 
     # get all protocolapp nodes where isa_type = 'Sample' belonging to this Study
@@ -86,6 +86,8 @@ and sl.protocol_app_node_id = pan.protocol_app_node_id
 and pan.isa_type = 'Sample'
 EOT
 
+    $self->log("TODO\thandle project fallback species<<<<<");
+
     my $dbh = $self->getQueryHandle();
     my $studyToSamples = $dbh->prepare($studyToSamplesSql);
     $studyToSamples->execute($studyId);
@@ -94,7 +96,7 @@ EOT
       my $sample = GUS::Model::Study::ProtocolAppNode->new({protocol_app_node_id => $samplePanId});
       $sample->retrieveFromDB();
 
-      $self->log("LOG\tGot a sample: '".$sample->getName()."'");
+      $self->log("LOG\tGot a sample: '".$sample->getName()."'") if ($self->getArg('verbose'));
 
       # get all PANs that are immediate outputs of sample
       # that have characteristics with qualifier "species assay result"
@@ -116,14 +118,39 @@ EOT
 
       my $sampleToSpeciesAssayResults = $dbh->prepare($sampleToSpeciesAssayResultsSql);
       $sampleToSpeciesAssayResults->execute($samplePanId);
-      # foreach species term
+
+      # reimplemented from https://github.com/bobular/VBPopBio/blob/30495e55b549b94fd053918b568de599db949150/api/Bio-Chado-VBPopBio/lib/Bio/Chado/VBPopBio/Result/Stock.pm#L586
+      my $result; # ID of final species term
+      my $qualifier = 'unambiguous';
+      my $internalResult; # Boolean flag
+
+      ## perform business logic with the ontology term values of the species_PANs and end up with one species term id --> $result
       while (my ($speciesTermId) = $sampleToSpeciesAssayResults->fetchrow_array()) {
+
+	# retrieve the term just for human-readable logging purposes
 	my $speciesTerm = GUS::Model::SRes::OntologyTerm->new({ontology_term_id => $speciesTermId});
 	$speciesTerm->retrieveFromDB();
+	$self->log("LOG\t\tRaw species: '".$speciesTerm->getName()) if ($self->getArg('verbose'));
 
-	$self->log("LOG\t\tRaw species: '".$speciesTerm->getName()."'");
+	if (!defined $result) {
+	  $result = $speciesTermId;
+	} elsif ($self->isAchildofB($speciesTermId, $result)) {
+	  # return the leaf-wards term unless we already chose an internal node
+	  $result = $speciesTermId unless ($internalResult);
+	} elsif ($speciesTermId == $result || $self->isAchildofB($result, $speciesTermId)) {
+	  # that's fine - stick with the leaf term
+	} else {
+	  # we need to return a common 'ancestral' internal node
+	  $result = $self->commonAncestor($result, $speciesTermId);
+	  $internalResult = 1;
+	  $qualifier = 'ambiguous';
+	}
       }
-      ## perform business logic with the ontology term values of the species_PANs and end up with one species term --> final_species
+
+
+      my $finalTerm = GUS::Model::SRes::OntologyTerm->new({ontology_term_id => $result});
+      $finalTerm->retrieveFromDB();
+      $self->log("LOG\t\tFinal species: '".$finalTerm->getName()."' ($qualifier)") if ($self->getArg('verbose'));
 
       ## add new characteristic to 'sample' with value final_species
 
@@ -133,10 +160,69 @@ EOT
     $self->error("could not find Study with extDbRlsSpec '$extDbRlsSpec'");
   }
 
-  return("TO DO: Add return value here.");
+  return("TO DO: Add return value/message here.");
 }
 
 
+
+sub isAchildofB {
+  my ($self, $termA_id, $termB_id) = @_;
+
+  my $sql = << 'EOT';
+with r1(subject_term_id, object_term_id) as (
+  select subject_term_id, object_term_id
+  from sres.ontologyrelationship r
+  where object_term_id = ?
+  union all
+  select r2.subject_term_id, r2.object_term_id
+  from sres.ontologyrelationship r2, r1
+  where r2.object_term_id = r1.subject_term_id
+)
+select * from r1 where subject_term_id = ?
+EOT
+
+  my $dbh = $self->getQueryHandle();
+  my $isChild = $dbh->prepare($sql);
+  $isChild->execute($termB_id, $termA_id);
+
+  # does it return something?
+  my ($retval) = $isChild->fetchrow_array();
+  return $retval ? 1 : 0;
+}
+
+
+sub commonAncestor {
+  my ($self, $termA_id, $termB_id) = @_;
+
+#
+# find common ancestor (first row contains it)
+#
+
+  my $sql = << 'EOT';
+with r1(subject_term_id, object_term_id, query_term_id, lvl) as (
+  select subject_term_id, object_term_id, subject_term_id as query_term_id, 1 as lvl
+  from sres.ontologyrelationship r
+  where subject_term_id in (?, ?)
+  union all
+  select r2.subject_term_id, r2.object_term_id, query_term_id, lvl+1
+  from sres.ontologyrelationship r2, r1
+  where r2.subject_term_id = r1.object_term_id
+)
+select object_term_id
+from r1
+group by object_term_id
+having count(distinct query_term_id) = 2
+order by avg(lvl)
+EOT
+
+  my $dbh = $self->getQueryHandle();
+  my $commonAncestor = $dbh->prepare($sql);
+  $commonAncestor->execute($termA_id, $termB_id);
+
+  # does it return something?
+  my ($term_id) = $commonAncestor->fetchrow_array();
+  return $term_id;
+}
 
 sub undoTables {
   my ($self) = @_;
