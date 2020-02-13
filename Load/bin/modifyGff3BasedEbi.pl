bda6efd6ef6db3c9bc3aea3c44ab22a1f582c467
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2020-02-13 15:30:22 -0500

    rename removeCDS4PseudogeneInGff3.pl to modifyGff3BasedEbi.pl

---
+++ Load/bin/modifyGff3BasedEbi.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+## this script contain all special requirements that EBI wants for gff3
+
+use strict;
+
+my $input = $ARGV[0];
+
+open (IN, $input) || die "can not open input file to read.\n";
+while (<IN>) {
+  chomp;
+  my @items=split (/\t/, $_);
+
+  next if ($items[2] eq "CDS" && $items[8] =~ /biotype=pseudogenic_exon;/);
+
+  if ($items[2] eq "CDS" ) {
+    $_ =~ s/ID=(\S+?)-CDS\d+;/ID=$1;/i;
+  }
+
+  print "$_\n";
+
+}
+close IN;
