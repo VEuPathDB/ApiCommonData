555dee70b6aac1fa5260e4e6e7d883268247e1ac
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2020-02-13 15:27:07 -0500

    rename CDS to use piptide ID

--- Load/bin/removeCDS4PseudogeneInGff3.pl
+++ Load/bin/removeCDS4PseudogeneInGff3.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+## this script contain all special requirements that EBI wants for gff3
+
 use strict;
 
 my $input = $ARGV[0];
@@ -11,6 +13,11 @@ while (<IN>) {
 
   next if ($items[2] eq "CDS" && $items[8] =~ /biotype=pseudogenic_exon;/);
 
+  if ($items[2] eq "CDS" ) {
+    $_ =~ s/ID=(\S+?)-CDS\d+;/ID=$1;/i;
+  }
+
   print "$_\n";
+
 }
 close IN;
