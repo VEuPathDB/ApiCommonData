7147033ec349b3a5f43a55f25fe097af47b3a936
Mark Hickman <markhick@sas.upenn.edu>
2020-04-15 13:24:48 -0400

    clean up mark-orthomcl branch before merging

--- Load/bin/processSequenceVariations.pl
+++ Load/bin/processSequenceVariations.pl
@@ -335,7 +335,7 @@ while($merger->hasNext()) {
 
     $variation->{ref_na_sequence_id} = $naSequenceId;
 
-    my $varSequenceSourceId = "$sequenceId";
+    my $varSequenceSourceId = "$sequenceId.$strain";
     my $varNaSequenceId = $naSequenceIds->{$varSequenceSourceId};
 
     if(!$varNaSequenceId && !$isLegacyVariations) {
