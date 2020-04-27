44e6046368df4756c1b25e91780d21d23539ee16
John Iodice <iodice@upenn.edu>
2020-04-27 14:35:44 -0400

    drop ChEBI tables

--- Load/lib/sql/apidbschema/dropEntireGusSchema.sql
+++ Load/lib/sql/apidbschema/dropEntireGusSchema.sql
@@ -12,5 +12,24 @@ drop user SRES cascade;
 drop user SRESVER cascade;
 drop user STUDY cascade;
 drop user STUDYVER cascade;
--- drop user CHEBI cascade;
+
+-- drop CHEBI tables (but not schema):
+alter table apidb.CompoundPeaksChebi drop foreign key fk_cpdpks_cid;
+alter table results.CompoundMassSpec drop foreign key fk_cpdms_cid;
+
+drop table chebi.database_accession;
+drop table chebi.chemical_data;
+drop table chebi.names;
+drop table chebi.comments;
+drop table chebi.ontology;
+drop table chebi.vertice;
+drop table chebi.relation;
+drop table chebi.reference;
+drop table chebi.structures;
+drop table chebi.default_structures;
+drop table chebi.autogen_structures;
+drop table chebi.compounds;
+
+exit;
+
 exit
