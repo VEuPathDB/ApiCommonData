f629ff712e5172b72c4eb7843682c9edb15183b2
Kathryn Crouch <crouch.k@gmail.com>
2020-05-20 11:23:30 -0400

    Reformatting and cleanup

--- Load/lib/perl/EBIUtils.pm
+++ Load/lib/perl/EBIUtils.pm
@@ -1,58 +1,57 @@
 package ApiCommonData::Load::EBIUtils;
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(getGenomicSequenceIdMapSql);
 use strict;
 use warnings;
 
 use lib $ENV{GUS_HOME} . "/lib/perl";
-
-#use Exporter qw(import);
-#our @EXPORT_OK = qw(getGenomicSequenceIdMapSql);
-
 use Getopt::Long;
-use File::Temp qw/ tempfile /;
 use DBI;
-use DBD::Oracle;
 use CBIL::Util::PropertySet;
-use File::Copy;
 
 sub getGenomicSequenceIdMapSql{
 
-my ($organismAbbrev)  = shift;
+    my ($organismAbbrev)  = shift;
 
-##Create db handle
+    ##Create db handle
 
-my $gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config";
- 
-my @properties;
-my $gusconfig = CBIL::Util::PropertySet->new($gusConfigFile, \@properties, 1);
+    my $gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config";
+     
+    my @properties;
+    my $gusconfig = CBIL::Util::PropertySet->new($gusConfigFile, \@properties, 1);
 
-my $dbiDsn = $gusconfig->{props}->{dbiDsn};
-my $dbiUser = $gusconfig->{props}->{databaseLogin};
-my $dbiPswd = $gusconfig->{props}->{databasePassword};
+    my $dbiDsn = $gusconfig->{props}->{dbiDsn};
+    my $dbiUser = $gusconfig->{props}->{databaseLogin};
+    my $dbiPswd = $gusconfig->{props}->{databasePassword};
 
-my $dbh = DBI->connect($dbiDsn, $dbiUser, $dbiPswd) or die DBI->errstr;
-$dbh->{RaiseError} = 1;
-$dbh->{AutoCommit} = 0;
+    my $dbh = DBI->connect($dbiDsn, $dbiUser, $dbiPswd) or die DBI->errstr;
+    $dbh->{RaiseError} = 1;
+    $dbh->{AutoCommit} = 0;
 
-my $sql = "select s.source_id, s.secondary_identifier 
-from dots.externalnasequence s, apidb.organism o
-where s.taxon_id = o.taxon_id
-and abbrev = '$organismAbbrev'
-and secondary_identifier is not null";
+    my $sql = "select s.source_id
+    , s.secondary_identifier 
+    from dots.externalnasequence s
+    , apidb.organism o
+    where s.taxon_id = o.taxon_id
+    and abbrev = '$organismAbbrev'
+    and secondary_identifier is not null";
 
-my $sh = $dbh->prepare($sql);
-$sh->execute();
+    my $sh = $dbh->prepare($sql);
+    $sh->execute();
 
-my %map;
-while(my ($sequenceId, $seqRegionName) = $sh->fetchrow_array()) {
-  $map{$seqRegionName} = $sequenceId;
-}
+    my %map;
+    while(my ($sequenceId, $seqRegionName) = $sh->fetchrow_array()) {
+      $map{$seqRegionName} = $sequenceId;
+    }
 
-$sh->finish();
+    $sh->finish();
 
-$dbh->disconnect;
+    $dbh->disconnect;
 
 
-return \%map;
+    return \%map;
 
 }
 
