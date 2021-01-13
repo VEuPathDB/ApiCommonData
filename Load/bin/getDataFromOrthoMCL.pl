48544a2c5831f186f48c4db2ecb1626b4f778aa3
Mark Hickman <markhick@sas.upenn.edu>
2021-01-13 17:47:10 -0500

    correct for ebibrc4 branch

--- Load/bin/getDataFromOrthoMCL.pl
+++ Load/bin/getDataFromOrthoMCL.pl
@@ -13,27 +13,14 @@ die "cannot open output files\n" unless ($outputAllOrthoGrps && $outputOrthoSeqs
 
 my $url = "https://orthomcl.org/orthomcl/service/record-types/group/searches/GroupsBySequenceCount/reports/attributesTabular?core_count_min=0&core_count_max=1000000&peripheral_count_min=0&peripheral_count_max=100000&reportConfig={\"attributes\":[\"primary_key\",\"number_of_members\",\"ec_numbers\",\"avg_connectivity\",\"avg_percent_identity\"],\"includeHeader\":true,\"attachmentType\":\"text\"}";
 
-<<<<<<< HEAD
-
-#my $cmd = qq(wget "$url" -o $outputAllOrthoGrps.log > $outputAllOrthoGrps.tmp);
-my $cmd = qq(wget "$url" -o $outputAllOrthoGrps.log -O $outputAllOrthoGrps.tmp);
-
-=======
 my $cmd = qq(wget '$url' -o $outputAllOrthoGrps.log -O $outputAllOrthoGrps);
->>>>>>> 21fa25c1f... change ortho derived ec webservices
 print STDERR "\nRunning\n$cmd\n";
 system($cmd);
 
 
 $url = "https://orthomcl.org/orthomcl/service/record-types/sequence/searches/ByEcAssignment/reports/attributesTabular?reportConfig={\"attributes\":[\"primary_key\",\"source_id\",\"num_core\",\"num_peripheral\",\"group_name\",\"ec_numbers\"],\"includeHeader\":true,\"attachmentType\":\"plain\"}";
 
-<<<<<<< HEAD
-
-$cmd = qq(wget "$url" -o $outputOrthoSeqsWithECs.log -O $outputOrthoSeqsWithECs.tmp);
-
-=======
 $cmd = qq(wget '$url' -o $outputOrthoSeqsWithECs.log -O $outputOrthoSeqsWithECs);
->>>>>>> 21fa25c1f... change ortho derived ec webservices
 print STDERR "Running\n$cmd\n";
 system($cmd);
 
