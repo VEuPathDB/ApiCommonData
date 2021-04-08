9d29937a0fcf816e4e367a548bac615cddca0b07
Lin Xu <linxu123@yew.penn.apidb.org>
2021-04-08 17:05:08 -0400

    update gene2probename script

--- Load/bin/geneToProbeset.pl
+++ Load/bin/geneToProbeset.pl
@@ -21,44 +21,30 @@ close IN;
 #$probeset{""} = '';
 
 
-my %gene;
+my %geneProbeHash;
+
 open(IN, "<", "/eupath/data/EuPathDB/workflows/VectorBase/49/data/agamPEST/gsnap/arrayStudies/GPL1321_Affy-plasmodiumanopheles/geneProbeMapping.tab") or die $!;
-open(OUT, ">", "/home/linxu123/out.txt") or die $!;
+open(OUT, ">", "/home/linxu123/outnew.txt") or die $!;
 
 while (my $line = <IN>) {
     chomp($line);
     $line =~ s/\r//g;
-    $line =~ s/,/-/g; ## format x,y to x-y
+    $line =~ s/,/-/g; 
     my @all = split/\t/,$line;
-    @all[1..$#all] = @probeset{@all[1..$#all]}; ##  '$#all' means the index of the last element in an array
-  
-=head   
- ----- for comma delimited csv file
-
-    for my $i (0..$#all){
-        unless ($all[$i]){
-            $all[$i] = '';
-        }
-    }
     
-=cut
-
-
-    my %count;
     for my $i (1..$#all){
-        $count{$all[$i]}++;
-    }
-        
-    my @uniq_times = keys %count;
-       
-    for my $i (0..$#uniq_times){ 
-        print OUT "$all[0]\t$uniq_times[$i]\n";
-    }
+	$geneProbeHash{$all[0]} -> {$probeset{$all[$i]}}++    
 
+    }    
 
 }
 
-
+foreach my $gene (sort keys %geneProbeHash){
+    foreach my $prob (sort keys %{$geneProbeHash{$gene}}){
+        print OUT "$gene\t $prob\n"
+    }
+}
 
 close IN;
 close OUT;
+
