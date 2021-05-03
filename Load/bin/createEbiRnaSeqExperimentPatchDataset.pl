2cee99f18996a0a94dcaf0e496771b5fc7c418d3
Wei Li <weili1@pennmedicine.upenn.edu>
2021-05-03 17:18:42 -0400

    dev

--- Load/bin/createEbiRnaSeqExperimentPatchDataset.pl
+++ Load/bin/createEbiRnaSeqExperimentPatchDataset.pl
@@ -7,58 +7,46 @@ my $usage =<<EOL;
 
 This script creates dataset xml for ebiRnaSeqExperimentPatch.
 
-%>createEbiRnaSeqExperimentPatchDataset.pl file_with_ebiRNASeq_patch_ids file_with_ebiOrganismName
+%>createEbiRnaSeqExperimentPatchDataset.pl Realigned_RnaSeqExperiments EbiVersion
 
 Output will be dataset xml
 EOL
 
-my $file = shift or die $usage;
-my $file_with_ebiOrganismName = shift or die $usage;
-my $ebiVersion = "build_49";
-my $version = "01-2020";
-open R, $file_with_ebiOrganismName;
-while(<R>){
-chomp;
-  open R1, "$_.xml";
-  open R2, ">$_.xml.new";
-  while (<R1>) {
-  if (eof) { last; } else { print R2; };
-  } 
-}
-close(R);
+my $Realigned_RnaSeqExperiments = shift or die $usage;
+my $ebiVersion = shift or die $usage;
+
 close(R1);
 close(R2);
 
-open S, $file;
+open S, $Realigned_RnaSeqExperiments;
 while(<S>) {
   chomp;
-  my ($ebiOrganismName, $expName, $isStrandSpecific) = split /\t/, $_;
-  $isStrandSpecific = lc $isStrandSpecific;
-  open O1, ">>$ebiOrganismName.xml.new";
-print O1 <<EOL;
+  my ($component, $ebiOrganismName, $expName) = split /\t/, $_;
+  $expName =~ s/^\s+|\s+$//g;
+  open R, "$component/$ebiOrganismName.xml";
+  open O, ">>$component/$ebiOrganismName.xml.patch";
+  while (<R>) {
+   chomp;
+   if (/$expName/){
+	my $version = <R>;
+    my $isStrandSpecific = <R>;
+chomp($version);
+chomp($isStrandSpecific);
+print O <<EOL;
 <dataset class="ebiRnaSeqExperimentPatch">
      <prop name="projectName">\$\$projectName\$\$</prop>
      <prop name="organismAbbrev">\$\$organismAbbrev\$\$</prop>
      <prop name="name">$expName</prop>
-     <prop name="version">$version</prop>
-     <prop name="isStrandSpecific">$isStrandSpecific</prop>
+     $version
+     $isStrandSpecific
      <prop name="ebiOrganismName">$ebiOrganismName</prop>
      <prop name="ebiVersion">$ebiVersion</prop>
 </dataset>
 EOL
-close(O1);
+}
+}
+close(O);
+close(R);
 }
 close(S);
 
-open R, $file_with_ebiOrganismName;
-while(<R>){
-chomp;
-  open R1, "$_.xml.new";
-  open R2, ">$_.xml";
-  while (<R1>) {
-  if (eof) { print R2 "</dataset>\n</datasets>\n"; } else { print R2; };
-  }
-}
-close(R);
-close(R1);
-close(R2);
