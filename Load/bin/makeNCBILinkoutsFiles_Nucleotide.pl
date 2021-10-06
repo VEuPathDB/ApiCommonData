15a9291646aa9691c96b8fe93f91fca44d9b648b
Wei Li <weili1@pennmedicine.upenn.edu>
2021-10-06 17:33:20 -0400

    new dev

---
+++ Load/bin/makeNCBILinkoutsFiles_Nucleotide.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl
+## To make the xml files for NCBI linkouts
+
+use strict;
+use DBI;
+use lib "$ENV{GUS_HOME}/lib/perl";
+use CBIL::Util::PropertySet;
+use Getopt::Long;
+use XML::Writer;
+use IO::File;
+use HTML::Entities;
+use XML::LibXML;
+
+my ($output, $gusConfigFile, $debug, $verbose, $tuningTablePrefix);
+
+&GetOptions("output=s" => \$output,
+            "verbose!" => \$verbose,
+            "tuningTablePrefix=s" => \$tuningTablePrefix,
+            "gusConfigFile=s" => \$gusConfigFile,
+	   );
+
+if (!$output || !$tuningTablePrefix) {
+  die ' USAGE: makeNCBILinkoutsFiles_Nucleotide.pl -output <output> -tuningTablePrefix <tuningTablePrefix>'
+}
+
+my $doc = XML::LibXML->load_xml(string => <<__END_XML__);
+<?xml version="1.0"?>
+<!DOCTYPE LinkSet PUBLIC "-//NLM//DTD LinkOut 1.0//EN" 
+"https://www.ncbi.nlm.nih.gov/projects/linkout/doc/LinkOut.dtd" 
+[
+  <!ENTITY contig.url "http://cryptodb.org/cryptodb/showRecord.do?name=ContigRecordClasses.ContigRecordClass&amp;id=">
+  <!ENTITY gene.url "http://cryptodb.org/cryptodb/showRecord.do?name=GeneRecordClasses.GeneRecordClass&amp;id=">
+]>
+<LinkSet/>
+__END_XML__
+
+open(OUT, "> $output") or die "Cannot open $output for writing: $!";
+
+print OUT "$doc";
+
+close OUT;
+
+$gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config" unless($gusConfigFile);
+
+unless(-e $gusConfigFile) {
+  print STDERR "gus.config file not found! \n";
+  exit;
+}
+
+my @properties = ();
+my $gusconfig = CBIL::Util::PropertySet->new($gusConfigFile, \@properties, 1);
+
+my $usr = $gusconfig->{props}->{databaseLogin};
+my $pwd = $gusconfig->{props}->{databasePassword};
+my $dsn = $gusconfig->{props}->{dbiDsn};
+
+print "Establishing dbi login\n" if $verbose;
+my $dbh = DBI->connect($dsn, $usr, $pwd) ||  die "Couldn't connect to database: " . DBI->errstr;
+$dbh->{RaiseError} = 1;
+$dbh->{AutoCommit} = 0;
+
+my $sql = &getNucleotideQuery($tuningTablePrefix);
+my $sth = $dbh->prepare($sql) || die "Couldn't prepare the SQL statement: " . $dbh->errstr;
+$sth->execute() ||  die "Couldn't execute statement: " . $sth->errstr;
+
+my (%nucleotide, $linkId);
+
+while (my ($source_id) = $sth->fetchrow_array()) {
+    $linkId++;
+    $nucleotide{$linkId}->{ProviderId} = 5941;
+    $nucleotide{$linkId}->{Database} = 'Nucleotide';
+    $nucleotide{$linkId}->{Query} = $source_id;
+    $nucleotide{$linkId}->{Base} = '&contig.url;';
+    $nucleotide{$linkId}->{Rule} = '&lo.pacc;';
+    $nucleotide{$linkId}->{SubjectType} = 'DNA/protein sequence';
+
+}
+
+  my $output = IO::File->new(">> ".$output);
+  my $writer = XML::Writer->new(OUTPUT => $output, DATA_MODE => "true", DATA_INDENT =>2);
+  $writer->startTag('LinkSet');
+    for my $k (sort {$a <=> $b} keys(%nucleotide)){
+      $writer->startTag('Link');
+      $writer->startTag('LinkId');
+      $writer->characters($k);
+      $writer->endTag('LinkId');
+      $writer->startTag('ProviderId');
+      $writer->characters($nucleotide{$k}->{ProviderId});
+      $writer->endTag('ProviderId');
+      $writer->startTag('ObjectSelector');
+      $writer->startTag('Database');
+      $writer->characters($nucleotide{$k}->{Database});
+      $writer->endTag('Database');
+      $writer->startTag('ObjectList');
+      $writer->startTag('Query');
+      $writer->characters($nucleotide{$k}->{Query});
+      $writer->endTag('Query');
+      $writer->endTag('ObjectList');
+      $writer->endTag('ObjectSelector');
+      $writer->startTag('ObjectUrl');
+      $writer->startTag('Base');
+      $writer->characters($nucleotide{$k}->{Base});
+      $writer->endTag('Base');
+      $writer->startTag('Rule');
+      $writer->characters($nucleotide{$k}->{Rule});
+      $writer->endTag('Rule');
+      $writer->startTag('SubjectType');
+      $writer->characters($nucleotide{$k}->{SubjectType});
+      $writer->endTag('SubjectType');
+      $writer->endTag('ObjectUrl');
+      $writer->endTag('Link');
+	}
+
+	$writer->endTag('LinkSet');
+
+    $writer->end();
+    $output->close();
+
+$dbh->disconnect;
+
+sub getNucleotideQuery {
+    my $prefix = shift;
+    my $sql = "select SOURCE_ID from ApidbTuning.${prefix}_genomicseqattributes where is_top_level =1 ORDER by SOURCE_ID";
+    return $sql;
+}
