7542a94ada0bc49f3e66a2568f66d93fe7e4e736
Wojtek Bazant <wbazant@vet.upenn.edu>
2022-01-18 08:51:36 -0500

    Move getting args into run() - good for subclassing

--- Load/plugin/perl/InsertEntityGraph.pm
+++ Load/plugin/perl/InsertEntityGraph.pm
@@ -180,6 +180,8 @@ sub run {
   }
   $self->userError("No investigation files") unless @investigationFiles;
 
+  my $schema = $self->getArg('schema');
+  my $extDbRlsId = $self->getExtDbRlsId($self->getArg('extDbRlsSpec'));
   my $investigationCount;
   foreach my $investigationFile (@investigationFiles) {
     my $dirname = dirname $investigationFile;
@@ -200,7 +202,7 @@ sub run {
     else {
       $investigation = CBIL::ISA::Investigation->new($investigationBaseName, $dirname, "\t");
     }
-    $self->loadInvestigation($investigation);
+    $self->loadInvestigation($investigation, $extDbRlsId, $schema);
     $investigationCount++;
   }
   $self->logRowsInserted() if($self->getArg('commit'));
@@ -210,7 +212,7 @@ sub run {
 
 
 sub loadInvestigation {
-  my ($self, $investigation) = @_;
+  my ($self, $investigation, $extDbRlsId, $schema) = @_;
   do {
     my %errors;
     $investigation->setOnError(sub {
@@ -234,14 +236,14 @@ sub loadInvestigation {
         my $description = $study->getDescription();
 
         my $nodes = $study->getNodes();
-        my $protocols = $self->protocolsCheckProcessTypesAndSetIds($study->getProtocols());
+        my $protocols = $self->protocolsCheckProcessTypesAndSetIds($study->getProtocols(), $schema);
         my $edges = $study->getEdges();
 
         my $iOntologyTermAccessions = $investigation->getOntologyAccessionsHash();
 
         my ($ontologyTermToIdentifiers, $ontologyTermToNames) = $self->checkOntologyTermsAndFetchIds($iOntologyTermAccessions);
 
-        $self->loadStudy($ontologyTermToIdentifiers, $ontologyTermToNames, $identifier, $description, $nodes, $protocols, $edges, $nodeToIdMap)
+        $self->loadStudy($ontologyTermToIdentifiers, $ontologyTermToNames, $identifier, $description, $nodes, $protocols, $edges, $nodeToIdMap, $extDbRlsId)
          unless %errors;
       }
     }
@@ -264,9 +266,9 @@ sub countLines {
 
 
 sub protocolsCheckProcessTypesAndSetIds {
-  my ($self, $protocols) = @_;
+  my ($self, $protocols, $schema) = @_;
 
-  my $sql = sprintf("select name, process_type_id from %s.processtype", $self->getArg('schema'));
+  my $sql = "select name, process_type_id from $schema.processtype";
 
   my $dbh = $self->getQueryHandle();
   my $sh = $dbh->prepare($sql);
@@ -292,10 +294,8 @@ sub protocolsCheckProcessTypesAndSetIds {
 
 sub loadStudy {
   my ($self, $ontologyTermToIdentifiers, $ontologyTermToNames,
-    $identifier, $description, $nodes, $protocols, $edges, $nodeToIdMap) = @_;
-
-  my $extDbRlsSpec = $self->getArg('extDbRlsSpec');
-  my $extDbRlsId = $self->getExtDbRlsId($extDbRlsSpec);
+    $identifier, $description, $nodes, $protocols, $edges, $nodeToIdMap,
+    $extDbRlsId) = @_;
 
   my $internalAbbrev = $identifier;
   $internalAbbrev =~ s/-/_/g; #clean name/id for use in oracle table name
