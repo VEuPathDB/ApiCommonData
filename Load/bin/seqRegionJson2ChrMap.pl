c944c292f3a14c15903f78f79e782afe989f8447
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2023-01-06 12:39:15 -0500

    more dev for reading seq_region JSON file from EBI

--- Load/bin/seqRegionJson2ChrMap.pl
+++ Load/bin/seqRegionJson2ChrMap.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ## usage: seqRegionJson2ChrMap.pl aedes_aegypti_lvpagwg_seq_region.json > chromosomeMap.txt
-## grep all synonyms and assign them to sequence IDs, then generated chromosomeMap based on all sequence IDs
+## grep sequence IDs from name(or maybe BRC4_seq_region_name) and chromosome info from synonyms, and generated chromosomeMap.txt file
 
 use strict;
 use JSON;
@@ -10,40 +10,53 @@ use Data::Dumper;
 my ($jsonFile) = @ARGV;
 
 open (IN, $jsonFile) || die "can not open $jsonFile to read\n";
-my $json = <IN>;
+#my $json = <IN>;
+my $json;
+while (<IN>) {
+  chomp;
+  $_ =~ s/^\s+//;
+  $_ =~ s/\s+$//;
+  next if ($_ =~ /^$/);
+  $json .= $_;
+}
 close IN;
 
 my $text = decode_json($json);
+#print STDERR Dumper($text);
 
 my %chrMap;
 
 foreach my $t (@{$text}) {
-  foreach my $k (sort keys %{$t}) {
-    if ($t->{coord_system_level} =~ /chromosome/i) {
-#      foreach my $synonym (@{$t->{synonyms}}) {
-#	$chrMap{$synonym} = $t->{name};
-#      }
-      $chrMap{$t->{name}} = $t->{name} if ($t->{name} !~ /^mt/i);
+
+#  foreach my $k (sort keys %{$t}) {
+#    if ($t->{coord_system_level} eq "chromosome" && ($t->{location} ne "mitochondrial_chromosome" && $t->{location} ne "apicoplast_chromosome" ) ) {
+    if ($t->{coord_system_level} eq "chromosome" && $t->{location} eq "nuclear_chromosome" ) {
+
+#      my ($chrId, $chrNum);
+      my $chrId = $t->{name};
+      my $chrNum;
+      foreach my $synonym (@{$t->{synonyms}}) {
+	## $synonym->{source} = INSDC, GenBank, INSDC_submitted_name
+	## $synonym->{name} = 1, CM043769.1, PB_01
+#	$chrId = $synonym->{name} if ($synonym->{source} =~ /genbank/i );
+	$chrNum = $synonym->{name} if ($synonym->{source} =~ /^insdc$/i);
+      }
+
+      if ($chrId && $chrNum) {
+	$chrMap{$chrId} = $chrNum;
+      } else {
+	print STDERR "double check $t->{name}\n";
+      }
     }
-  }
-}
-
-#my %chrMapNum2Id;
-#foreach my $kk (sort keys %chrMap) {
-#  push @{$chrMapNum2Id{$chrMap{$kk}}}, $kk;
-#}
-
-my $chrCount;
-#foreach my $k3 (sort keys %chrMapNum2Id) {
-foreach my $k (sort keys %chrMap) {
-  $chrCount++;
-  print "$k\t$k\t$chrCount\n";
-
-#  foreach my $id (@{$chrMapNum2Id{$k3}}) {
-#    print "$id\t$k3\t$chrCount\n";
-#    print "$k3\t$k3\t$chrCount\n";
 #  }
 }
 
-#print STDERR Dumper($text);
+
+my $chrCount;
+foreach my $k (sort keys %chrMap) {
+  $chrCount++;
+  print "$k\t$chrMap{$k}\t$chrCount\n";
+
+}
+
 
