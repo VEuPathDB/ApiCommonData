fcb38de4283360c1f7133ee683d58f4befe60d84
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2023-03-01 09:55:30 -0500

    take BRC4_seq_region_name as source_id, and sort the keys of the hash based on the values in the hash

--- Load/bin/seqRegionJson2ChrMap.pl
+++ Load/bin/seqRegionJson2ChrMap.pl
@@ -28,17 +28,15 @@ my %chrMap;
 
 foreach my $t (@{$text}) {
 
-#  foreach my $k (sort keys %{$t}) {
 #    if ($t->{coord_system_level} eq "chromosome" && ($t->{location} ne "mitochondrial_chromosome" && $t->{location} ne "apicoplast_chromosome" ) ) {
     if ($t->{coord_system_level} eq "chromosome" && $t->{location} eq "nuclear_chromosome" ) {
 
-#      my ($chrId, $chrNum);
-      my $chrId = $t->{name};
+#      my $chrId = $t->{name};
+      my $chrId = $t->{BRC4_seq_region_name};
       my $chrNum;
       foreach my $synonym (@{$t->{synonyms}}) {
 	## $synonym->{source} = INSDC, GenBank, INSDC_submitted_name
 	## $synonym->{name} = 1, CM043769.1, PB_01
-#	$chrId = $synonym->{name} if ($synonym->{source} =~ /genbank/i );
 	$chrNum = $synonym->{name} if ($synonym->{source} =~ /^insdc$/i);
       }
 
@@ -48,12 +46,12 @@ foreach my $t (@{$text}) {
 	print STDERR "double check $t->{name}\n";
       }
     }
-#  }
 }
 
 
 my $chrCount;
-foreach my $k (sort keys %chrMap) {
+foreach my $k (sort {$chrMap{$a} <=> $chrMap{$b} } keys %chrMap) {  ## sort the keys of the hash based on the values in the hash
+#foreach my $k (sort keys %chrMap) {
   $chrCount++;
   print "$k\t$chrMap{$k}\t$chrCount\n";
 
