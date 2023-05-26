689bc291ab2cabf65c348e81ec6006dc808a58ff
Elizabeth <foxcapade@gmail.com>
2023-05-26 10:25:27 -0400

    formatting

--- Load/lib/sql/apidbschema/createVdiControlTables.sql
+++ Load/lib/sql/apidbschema/createVdiControlTables.sql
@@ -3,56 +3,38 @@
 --   QUOTA UNLIMITED ON users;
 
 CREATE TABLE vdi.dataset (
-  dataset_id CHAR(32)
-    PRIMARY KEY
-    NOT NULL
-, owner NUMBER
-    NOT NULL
-, type_name VARCHAR2(64)
-    NOT NULL
-, type_version VARCHAR2(64)
-    NOT NULL
-, is_deleted NUMBER
-    DEFAULT 0
-    NOT NULL
+  dataset_id   CHAR(32)     PRIMARY KEY NOT NULL
+, owner        NUMBER                   NOT NULL
+, type_name    VARCHAR2(64)             NOT NULL
+, type_version VARCHAR2(64)             NOT NULL
+, is_deleted   NUMBER       DEFAULT 0   NOT NULL
 );
 
 CREATE TABLE vdi.sync_control (
-  dataset_id CHAR(32)
-    NOT NULL
-, shares_update_time TIMESTAMP WITH TIME ZONE
-    NOT NULL
-, data_update_time TIMESTAMP WITH TIME ZONE
-    NOT NULL
-, meta_update_time TIMESTAMP WITH TIME ZONE
-    NOT NULL
+  dataset_id         CHAR(32)                 NOT NULL
+, shares_update_time TIMESTAMP WITH TIME ZONE NOT NULL
+, data_update_time   TIMESTAMP WITH TIME ZONE NOT NULL
+, meta_update_time   TIMESTAMP WITH TIME ZONE NOT NULL
 , FOREIGN KEY (dataset_id) REFERENCES vdi.dataset (dataset_id)
 );
 
 CREATE TABLE vdi.dataset_install_message (
-  dataset_id CHAR(32)
-    NOT NULL
-, install_type VARCHAR2(64)
-    NOT NULL
-, status VARCHAR2(64)
-    NOT NULL
-, message CLOB
+  dataset_id   CHAR(32)     NOT NULL
+, install_type VARCHAR2(64) NOT NULL
+, status       VARCHAR2(64) NOT NULL
+, message      CLOB 
 , FOREIGN KEY (dataset_id) REFERENCES vdi.dataset (dataset_id)
 );
 
 CREATE TABLE vdi.dataset_visibility (
-  dataset_id CHAR(32)
-    NOT NULL
-, user_id NUMBER
-    NOT NULL
+  dataset_id CHAR(32) NOT NULL
+, user_id    NUMBER   NOT NULL
 , FOREIGN KEY (dataset_id) REFERENCES vdi.dataset (dataset_id)
 );
 
 CREATE TABLE vdi.dataset_project (
-  dataset_id CHAR(32)
-    NOT NULL
-, project_id VARCHAR2(64)
-    NOT NULL
+  dataset_id CHAR(32)     NOT NULL
+, project_id VARCHAR2(64) NOT NULL
 , FOREIGN KEY (dataset_id) REFERENCES vdi.dataset (dataset_id)
 );
 
