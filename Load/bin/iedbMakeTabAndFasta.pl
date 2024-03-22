c2cfad45524dcb91f3e74b79feb361977d588ef3
Saikou Y Bah <saikouybah@gmail.com>
2024-03-22 07:56:06 -0400

    Added the nextflow and perl script to process the epitope fasta file to make the tab file and dowload the epitope source protein in fasta format

---
+++ Load/bin/iedbMakeTabAndFasta.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+
+my ($inputPepFasta, $peptideTab, $peptideProtein, $workDir);
+&GetOptions("inputPepFasta=s"=> \$inputPepFasta,
+            "peptideTab=s"=> \$peptideTab,
+            "peptideProtein=s"=> \$peptideProtein,
+	    "workDir=s"=> \$workDir,
+    ) ;
+
+unless (-e $inputPepFasta && $peptideTab && $peptideProtein && $workDir) {
+    &usage("Both input files must exist;  output file must be declared")
+}
+
+sub usage {
+    my ($e) = @_;
+
+    print STDERR "runNextflow.pl --inputPepFasta <FILE> --peptideTab OUT --peptideProtein OUT --workDir WorkDirectory\n";
+    die $e if($e);
+}
+
+
+my $nfConfigFile='iedbNextflow.config';
+  unless (-e $nfConfigFile){
+
+ my $nfConfig = <<CONFIG;
+params {
+    pepFasta = "$inputPepFasta";
+    pepTab = "$peptideTab";
+    peptideProteinFasta = "$peptideProtein";
+ }
+ process {
+  executor = local
+  
+ }
+docker {
+  enabled = true
+}
+CONFIG
+open(FH, ">$nfConfigFile") or die "Cannot write config file $nfConfigFile: $!\n";
+print FH  $nfConfig;
+close(FH);
+
+}
+
+my $executable = join("/", $ENV{'GUS_HOME'}, 'bin', 'processIedb');
+
+my $logFile = join("/", $workDir, "nextflow.log");
+
+my $cmd = "export NXF_WORK=$workDir/work && nextflow -bg -C $nfConfigFile -log $logFile run $executable";      
