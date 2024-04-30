2fd0f277d59abd021bbe04c5ceeffc20ce352baf
Bob MacCallum <uncoolbob@gmail.com>
2024-04-30 15:44:08 -0400

    changed to currently supported osgeo docker image

--- Load/bin/loadGadm.bash
+++ Load/bin/loadGadm.bash
@@ -15,7 +15,7 @@ databaseName=gadm
 gpkgFileBaseName="${gpkgFile##*/}"
 
 POSTGRES_IMAGE="docker://postgis/postgis:15-3.4";
-GDAL_IMAGE="docker://osgeo/gdal:ubuntu-full-latest";
+GDAL_IMAGE="docker://ghcr.io/osgeo/gdal:alpine-normal-latest";
 
 mkdir -p ${workDir}/postgresData ${workDir}/postgresSocket ${workDir}/postgresInit
 
