0c8639573286ca2ba8b1e995bb90d7f81c353c79
John Brestelli <jbrestell@gmail.com>
2024-10-22 11:33:11 -0400

    remove DESeq pairwise analysis

--- Load/lib/perl/RnaSeqAnalysisEbi.pm
+++ Load/lib/perl/RnaSeqAnalysisEbi.pm
@@ -100,155 +100,26 @@ sub munge {
             $self->makeProfiles('unstranded', $featureType, $quantificationType, $valueType, $makePercentiles, 0);
         }
         }
-
-return;
-#DESeq2 Analysis starts here  
-    if (keys %{$samplesHash} <2 || $skipDeSeq) {
-        print Dumper  "note: there are less than two conditions DESeq2 analysis  and or DEGseq analysis can not be done\n";
-        next;
-    }
-    else { 
-        my @array1;
-        my @array2;
-        my %pairHash;
-    # check on the number of reps 
-        foreach my $key (keys %{$samplesHash}) {
-            push @array1, $key;
-            push @array2, $key;
-        }
-        
-        foreach my $element (@array1) {
-            foreach my $second (@array2) {  
-                if ($element eq $second) {
-                
-                }
-                else {
-                    my $pair = $element."_vs_".$second;
-                    $pairHash{$pair} =1;
-                }
-            }
-        }
-        
-        foreach my $key (keys %pairHash) {
-            my @temps = split "_vs_" , $key;
-            my $sampleNameClean = $key;
-            my $reference = $temps[1];
-            my $comparator = $temps[0];
-            my %dataframeHash;
-            my $ref = $reference; 
-            my $comp = $comparator;
-            $dataframeHash{$reference} = $samplesHash->{$ref};
-            $dataframeHash{$comparator} = $samplesHash->{$comp};
-
-         ##### trying to workout ref checks etc to deal with display names not matching sample names 
-            my $Rrep1 = $dataframeHash{$reference}[0];
-            my $Rrep2 = $dataframeHash{$reference}[1];
-            my @Rbase1 = split "", $Rrep1;
-            my @Rbase2 = split"", $Rrep2;
-            my $RrepBaseName;
-            my $CrepBaseName;
-            for (my $i=0; $i <= @Rbase1; $i++) {
-                if ($Rbase1[$i] eq $Rbase2[$i]) {
-                    $RrepBaseName.= $Rbase1[$i];
-                }
-                else {
-                    last;
-                }
-            }
-            my $refCheck = $RrepBaseName;
-            my $degRef = $Rrep1;
-           
-            my $Crep1 = $dataframeHash{$comparator}[0];
-            my $Crep2 = $dataframeHash{$comparator}[1];
-            my @Cbase1 = split "", $Crep1;
-            my @Cbase2 = split"", $Crep2;
-            for (my $i=0; $i <= @Cbase1; $i++) {
-                if ($Cbase1[$i] eq $Cbase2[$i]) {
-                    $CrepBaseName.= $Cbase1[$i];
-                }
-                else {
-                    last;
-                }
-            }
-            my $compCheck = $CrepBaseName;
-            my $degComp = $Crep1;
-
-            if((@{$dataframeHash{$reference}} < 2) || (@{$dataframeHash{$comparator}} < 2 )) {
-                next; #placeholder for DEGseq
-            }
-            else { 
-                my $suffix = 'differentialExpression';
-                my $dataframeHashref = \%dataframeHash;
-        #making new DESeq object
-                if($isStrandSpecific) {
-                    my $DeseqAnalysis = ApiCommonData::Load::DeseqAnalysisEbi->new({sampleName => $sampleNameClean,
-                                                 mainDirectory => $self->getMainDirectory,
-                                                 samplesHash => $dataframeHashref,
-                                                 reference => $ref,
-                                                 referenceCheck => $refCheck,
-                                                 comparator => $comp,
-                                                 comparatorCheck => $compCheck,
-                                                 suffix => 'FirststrandDESeq2Analysis',
-                                                 profileSetName => $profileSetName});
-                    $DeseqAnalysis->setProtocolName("GSNAP/DESeq2Analysis");
-                    $DeseqAnalysis->setDisplaySuffix( ' [DESeq2Analysis - firststrand - unique]');
-                    $DeseqAnalysis->setTechnologyType($self->getTechnologyType());
-                    $DeseqAnalysis->munge();
-                    
-                    
-                    my $DeseqAnalysis = ApiCommonData::Load::DeseqAnalysisEbi->new({sampleName => $sampleNameClean,
-                                                 mainDirectory => $self->getMainDirectory,
-                                                 samplesHash => $dataframeHashref,
-                                                 reference => $ref,
-                                                 referenceCheck => $refCheck,
-                                                 comparator => $comp,
-                                                 comparatorCheck => $compCheck,
-                                                 suffix => 'SecondstrandDESeq2Analysis',
-                                                 profileSetName => $profileSetName});
-                    $DeseqAnalysis->setProtocolName("GSNAP/DESeq2Analysis");
-                    $DeseqAnalysis->setDisplaySuffix( ' [DESeq2Analysis - secondstrand - unique]');
-                    $DeseqAnalysis->setTechnologyType($self->getTechnologyType());
-                    $DeseqAnalysis->munge();
-                    
-                }
-                else {
-                    my $DeseqAnalysis = ApiCommonData::Load::DeseqAnalysisEbi->new({sampleName => $sampleNameClean,
-                                                 mainDirectory => $self->getMainDirectory,
-                                                 samplesHash => $dataframeHashref,
-                                                 reference => $ref,
-                                                 referenceCheck => $refCheck,
-                                                 comparator => $comp,
-                                                 comparatorCheck => $compCheck,
-                                                 suffix => 'UnstrandedDESeq2Analysis',
-                                                 profileSetName => $profileSetName});
-                    $DeseqAnalysis->setProtocolName("GSNAP/DESeq2Analysis");
-                    $DeseqAnalysis->setDisplaySuffix( ' [DESeq2Analysis - unstranded - unique]');
-                    $DeseqAnalysis->setTechnologyType($self->getTechnologyType());
-                    $DeseqAnalysis->munge();
-                    
-                }
-            }
-        }
-    }
+    return;
 }
 
 sub makeProfiles {
     my ($self, $strand, $featureType, $quantificationType, $valueType, $makePercentiles, $isUnique) = @_;
-    
+
     my $samples = $self->getSamples();
-    
+
     my $profileSetName = $self->getProfileSetName();
-    
+
     my $isStrandSpecific = $self->getIsStrandSpecific() ? $self->getIsStrandSpecific() : 'FALSE';
-    
+
     # cleanup for non unique
     if(!$isUnique) {
       $valueType = "nonunique.$valueType";
       $makePercentiles = 0;
     }
-    
+
     my $outputFile = "$OUTPUT_FILE_BASE.$featureType.$quantificationType.$strand.$valueType";
-    
+
     my $profile = CBIL::StudyAssayResults::DataMunger::ProfileFromSeparateFiles->
 	new({mainDirectory => $self->getMainDirectory,
 	     outputFile => $outputFile,
@@ -259,19 +130,19 @@ sub makeProfiles {
 	     profileSetName => $profileSetName,
          sampleNameAsDir => 1,
 	    });
-    
+
     my $header = 0;
-    
+
     $profile->setHasHeader($header);
-    
+
     my $protocolName = 'HTSeq';
-    
+
     $profile->setProtocolName("GSNAP/$protocolName");
-    
+
     $profile->addProtocolParamValue('Strand', $strand);
     $profile->addProtocolParamValue('FeatureType', $featureType);
     $profile->addProtocolParamValue('QuantificationType', $protocolName);
-    
+
     if ($protocolName eq 'HTSeq') {
         $quantificationType =~ /^htseq-(.+)$/;
         my $mode = $1;
@@ -284,9 +155,9 @@ sub makeProfiles {
     else {
       $profile->setDisplaySuffix(" [$quantificationType - $strand - $valueType - nonunique]");
     }
-    
+
     $profile->setTechnologyType($self->getTechnologyType());
-    
+
     $profile->munge();
     
     return($outputFile);
