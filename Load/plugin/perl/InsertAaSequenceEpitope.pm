8f683ffdfa4d23ada2a3c7713e306e2d87c2881f
John Brestelli <jbrestell@gmail.com>
2024-11-26 10:20:11 -0500

    use lookup hash for splicedaasequences

--- Load/plugin/perl/InsertAaSequenceEpitope.pm
+++ Load/plugin/perl/InsertAaSequenceEpitope.pm
@@ -15,18 +15,26 @@ use GUS::Model::DoTS::AASequenceImp;
 
 sub getArgsDeclaration {
     my $argsDeclaration  =
-	
-       [
 
-	fileArg({ name => 'peptideResultFile',
-		     descr => 'peptide analysis results file in text format containing the blast and exact matches',
-		     constraintFunc=> undef,
-		     reqd  => 1,
-		     isList => 0,
-		     mustExist => 1,
-		     format=>'Text',
-             }),
-	];
+        [
+
+         fileArg({ name => 'peptideResultFile',
+                   descr => 'peptide analysis results file in text format containing the blast and exact matches',
+                   constraintFunc=> undef,
+                   reqd  => 1,
+                   isList => 0,
+                   mustExist => 1,
+                   format=>'Text',
+                 }),
+
+         stringArg({name => 'genomeExtDbRlsSpec',
+                    descr => 'ExternalDatabase release spec for the primary genome',
+                    constraintFunc=> undef,
+                    reqd  => 1,
+                    isList => 0
+                   }),
+
+        ];
     
     return $argsDeclaration;
 }
@@ -94,26 +102,34 @@ sub new {
 sub run {
 	my ($self) = @_;
 	my $peptideResultFile = $self->getArg('peptideResultFile');
-	$self->loadEpitopes($peptideResultFile)	
+	my $resultString = $self->loadEpitopes($peptideResultFile);
 
+    return ($resultString);
 }
 
 
-sub fetchAASourceID {
- 	 my ($self, $name) = @_;
+sub fetchAASequenceIdFromSourceID {
+    my ($self, $origSourceId) = @_;
+
+    if($self->{aa_sequence_id}->{$origSourceId}) {
+        return $self->{aa_sequence_id}->{$origSourceId};
+    }
+
+    my $extDbRlsSpec = $self->getArg("genomeExtDbRlsSpec");
+    my $extDbRlsId = $self->getExtDbRlsId($extDbRlsSpec);
+
+    my $sql = "select aa_sequence_id, source_id from dots.translatedaasequence where external_database_release_id = ?";
+    my $dbh = $self->getQueryHandle();
+    my $sh = $dbh->prepare($sql);
+    $sh->execute($origSourceId);
 
 
-	#print("Now I am in the function");
-	#print Dumper $name;
+    while(my ($aaSequenceId, $sourceId) = $sh->fetchrow_array()) {
+        $self->{aa_sequence_id}->{$sourceId} = $aaSequenceId;
+    }
+    $sh->finish();
 
-  	my $aa_sequence = GUS::Model::DoTS::AASequenceImp->new({'source_id' => $name});
-
-	#some error handling here
-	#we expect to get one row...
-  	$aa_sequence->retrieveFromDB();
-
-
-  	return $aa_sequence;
+    return $self->{aa_sequence_id}->{$origSourceId};
 
 }
 
@@ -121,50 +137,47 @@ sub fetchAASourceID {
 
 sub loadEpitopes {
 
-	my ($self, $peptideResultFile) = @_;
-	
-	open(my $peptides, $peptideResultFile) or die "Could not open file '$peptideResultFile' $!";
+    my ($self, $peptideResultFile) = @_;
 
-		
-		while (my $row = <$peptides>) {
-       		chomp $row;
-        	my @counts_list = split("\t", $row);
-		my $aa_sequence_source_id = $counts_list[0];
-		#my $aa_sequence => $self->fetchAASourceID($aa_sequence_source_id) || $self->error ("Can't retrieve aa_sequence_id for row with source id $aa_sequence_source_id");
-        	my $iedb_id = $counts_list[1];
-        	my $peptide_match = $counts_list[2];
-        	my $protein_match = $counts_list[3];
-        	my $species_match = $counts_list[4];
-        	my $number_of_matches = $counts_list[10];
-        	my $blast_hit_align_len = $counts_list[11];
-        	my $alignment = $counts_list[14];
+    open(my $peptides, $peptideResultFile) or die "Could not open file '$peptideResultFile' $!";
+
+    my $count;
+    while (my $row = <$peptides>) {
+        chomp $row;
+        my @counts_list = split("\t", $row);
+        my $aa_sequence_source_id = $counts_list[0];
+        #my $aa_sequence => $self->fetchAASourceID($aa_sequence_source_id) || $self->error ("Can't retrieve aa_sequence_id for row with source id $aa_sequence_source_id");
+        my $iedb_id = $counts_list[1];
+        my $peptide_match = $counts_list[2];
+        my $protein_match = $counts_list[3];
+        my $species_match = $counts_list[4];
+        my $number_of_matches = $counts_list[10];
+        my $blast_hit_align_len = $counts_list[11];
+        my $alignment = $counts_list[14];
 
 
-		my $aa_sequence = $self->fetchAASourceID($aa_sequence_source_id);
-			
-		my $row_peptide = GUS::Model::ApiDB::AASequenceEpitope->new({
-								aa_sequence_id => $aa_sequence->getAaSequenceId(),
-								iedb_id => $iedb_id,
-								peptide_match => $peptide_match,
-								protein_match => $protein_match,
-								species_match => $species_match,
-								blast_hit_identity => $number_of_matches,
-								blast_hit_align_len => $blast_hit_align_len,
-								alignment => $alignment});
-		
+        my $aa_sequence_id = $self->fetchAASequenceIdFromSourceID($aa_sequence_source_id);
 
-		# question - why is setParent not working?
-		# check if this is a foreign key constraint		
-		#$row_peptide->setParent($aa_sequence);
+        my $row_peptide = GUS::Model::ApiDB::AASequenceEpitope->new({
+            aa_sequence_id => $aa_sequence_id,
+            iedb_id => $iedb_id,
+            peptide_match => $peptide_match,
+            protein_match => $protein_match,
+            species_match => $species_match,
+            blast_hit_identity => $number_of_matches,
+            blast_hit_align_len => $blast_hit_align_len,
+            alignment => $alignment});
 
-	 	$row_peptide->submit();
-		$self->undefPointerCache();
-		
+        $row_peptide->submit();
+        $self->undefPointerCache();
 
-		}
-	}
-	
-	
+        $count++;
+    }
+
+    return "Loaded $count rows into AASequenceEpitope";
+}
+
+
 sub undoTables {
   my ($self) = @_;
 
