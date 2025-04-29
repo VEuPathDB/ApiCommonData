b48e0abc3b46f7ea92d2ac61d81798c5c000215e
steve-fischer-200 <sfischer@upenn.edu>
2025-04-29 14:36:15 -0400

    debug

--- Load/plugin/perl/CreateDenormalizedTables.pm
+++ Load/plugin/perl/CreateDenormalizedTables.pm
@@ -1,11 +1,9 @@
 
-package ApiCommonData::Load::Plugin::LoadNothing;
+package ApiCommonData::Load::Plugin::CreateDenormalizedTables;
 @ISA = qw(GUS::PluginMgr::Plugin);
 
 use strict;
-
-
-
+use File::Basename;
 use GUS::PluginMgr::Plugin;
 
 my $purposeBrief = <<PURPOSEBRIEF;
@@ -35,19 +33,21 @@ Input is a MY_DENORM_TABLE.psql file that controls creation of or insertion into
 The files are named using this pattern: MY_DENORM_TALE.psql
 
 The following values are passed in to the psql as macro values:
+ - :SCHEMA     - the schema where denormalized tables are made
  - :ALG_INV_ID - algorithm invocation (for undo)
- - :FILTER - taxon_id, if organism specific
+ - :TAXON_ID - taxon_id, if organism specific
  - :CREATE_OR_INSERT - a CREATE TABLE AS or a INSERT FROM clause, depending on the mode we're running in.  (only used for organism specific)
+ - :SUFFIX - to make temp tables unique.  
 
 We rely on postgres-specific syntax (IF EXISTS), so this is not oracle compatible.
 
 A typical file might look like this:
-create table MY_TMP_TABLE_1&suffix as (select ...)  -- use plugin-supplied suffix (timestamp) to avoid collisions with other plugins
-create table MY_TMP_TABLE_2&suffix as (select ...)
+create table MY_TMP_TABLE_1:SUFFIX as (select ...)  -- use plugin-supplied suffix (timestamp) to avoid collisions with other plugins
+create table MY_TMP_TABLE_2:SUFFIX as (select ...)
 &operation (select ...)         -- operation is one of: CREATE TABLE AS or INSERT FROM. insert uses &algInvId macro to mark rows for future undo
 create index MY_DENORM_IX if not exists
-drop table MY_TMP_TABLE_1&suffix
-drop table MY_TMP_TABLE_2&suffix
+drop table MY_TMP_TABLE_1:SUFFIX
+drop table MY_TMP_TABLE_2:SUFFIX
 
 The plugin runs in one of two modes:
  - create
@@ -98,19 +98,19 @@ my $argsDeclaration =
 	      constraintFunc => undef,
 	      reqd => 1,
 	      isList => 0
-	     })
+	     }),
    stringArg({name => 'projectId',
 	      descr => 'eg PlasmoDB',
 	      constraintFunc => undef,
 	      reqd => 1,
 	      isList => 0
-	     })
+	     }),
    stringArg({name => 'organismAbbrev',
 	      descr => 'internal organism abbrev, if this table has organism specific rows',
 	      constraintFunc => undef,
 	      reqd => 0,
 	      isList => 0
-	     })
+	     }),
    stringArg({name => 'taxonId',
 	      descr => 'used as filter in the sql, if this table has organism specific rows',
 	      constraintFunc => undef,
@@ -136,45 +136,49 @@ sub new {
 }
 
 sub run {
-  my $psqlFile = $self->getArg('psqlFile');
+  my ($self) = @_;
+  my $psqlFilePath = $self->getArg('psqlFile');
   my $schema = $self->getArg('schema');
   my $projectId = $self->getArg('projectId');
   my $organismAbbrev = $self->getArg('organismAbbrev');
   my $mode = $self->getArg('mode');
   my $taxonId = $self->getArg('taxonId');
   my $algInvId   = $self->getAlgInvocation()->getId();
+
   my $dbh = $self->getQueryHandle();
   $dbh->{RaiseError} = 1;
   $dbh->{AutoCommit} = 1;
-  my $commitMode = $self->getArg('commit')
+  my $commitMode = $self->getArg('commit');
 
-  $psqlFile =~ /(.+).psql/ or $error("psqlFile name must be in form MY_DENORM_TABLE.psql");
+  my $fileName = basename($psqlFilePath);
+  $fileName =~ /(.+).psql/ or $self->error("psqlFile name must be in form MY_DENORM_TABLE.psql");
   my $tableName = $1;
+  my $suffix = "_" . $tableName.$organismAbbrev.time;  # unique suffix for this run.
 
   if ($mode eq 'insert') {
     $self->error("organismAbbrev and taxonId are required args if mode is 'insert'") unless $organismAbbrev && $taxonId;
     checkForPreviousRows("$schema.$tableName", $organismAbbrev, $dbh);
   }
 
-  open my $fh, '<', $psqlFile or die "error opening $psqlFile: $!";
+  open my $fh, '<', $psqlFilePath or $self->error("error opening $psqlFilePath: $!");
   my $sqls = do { local $/; <$fh> };
 
   my @sqlList = split(/;\n\s*/, $sqls);
   foreach my $sql (@sqlList) {
     my $newSql = $sql;
     $newSql =~ s/\:ALG_INV_ID/$algInvId/g;
-    $newSql =~ s/\:FILTER/$taxonId/g;
-    $newSql =~ $mode eq 'create'?
-      s/\:CREATE_OR_INSERT/'CREATE TABLE $schema.$tableName AS '/g :
-      s/\:CREATE_OR_INSERT/'INSERT INTO $schema.$tableName   '/g;
-    $self->log($commitMode? "FOR REAL" : "TEST ONLY") . " - SQL: \n$newSql\n\n";
+    $newSql =~ s/\:TAXON_ID/$taxonId/g;
+    $newSql =~ s/\:SCHEMA/$schema/g;
+    $newSql =~ s/\:SUFFIX/$suffix/g;
+    if ($mode eq 'create') {
+      $newSql =~ s/\:CREATE_OR_INSERT/CREATE TABLE $schema.$tableName AS /g;
+    } else {
+      $newSql =~ s/\:CREATE_OR_INSERT/INSERT INTO $schema.$tableName /g;
+    }
+    $self->log($commitMode? "FOR REAL" : "TEST ONLY" . " - SQL: \n$newSql\n\n");
     if ($commitMode) {
       $dbh->do($newSql);
     }
-    
-  
-  my $sth = $dbh->prepareAndExecute($sql);
-
   }
 }
 
@@ -226,17 +230,18 @@ sub undoPreprocess {
   my $schemas = $self->getAlgorithmParam($dbh, $rowAlgInvocationList, $SCHEMA_ARG);
 
   my $mode = $modes->[0];
-  my $fileName = $fileNames->[0];
+  my $filePath = $fileNames->[0];
+  my $fileName = basename($filePath);
   my $schema = $schemas->[0];
 
-  $fileName =~/(.*).psql/ or $self->error("Invalid file name $filename");
+  $fileName =~ /(.+).psql/ or $self->error("Invalid file name $fileName");
   my $tableName = $1;
 
   if ($mode eq 'insert') {
      $self->{_undo_tables} = ["$schema.$tableName"];
   } elsif ($mode eq 'create') {
     my $sql = "drop table $schema.$tableName";
-    $dbh->do($sql) || $error->("Failed executing $sql");
+    $dbh->do($sql) || $self->error("Failed executing $sql");
     $self->log("Dropped $schema.$tableName");
   } else {
     $self->error("Invalid mode param: $mode");
