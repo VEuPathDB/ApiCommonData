8f87df38a2a725cda1684cd2ef511152750c6ffb
Richard Demko <rdemko2332@gmail.com>
2025-05-13 14:28:25 -0400

    Changing number order from numeric to significance

--- Load/plugin/perl/InsertOrthoGroupStats.pm
+++ Load/plugin/perl/InsertOrthoGroupStats.pm
@@ -1,4 +1,4 @@
-package ApiCommonData::Load::Plugin::InsertOrthoGroupStats;
+package OrthoMCLData::Load::Plugin::InsertOrthoGroupStats;
 
 @ISA = qw(GUS::PluginMgr::Plugin);
 
@@ -94,22 +94,63 @@ sub run {
     while (my $line = <$data>) {
         chomp $line;
         $rowCount++;
-        my ($group,$min,$twentyfifth,$median,$seventyfifth,$max) = split(/\t/, $line);
+        my ($group,$max,$seventyfifth,$median,$twentyfifth,$min) = split(/\t/, $line);
+      
         foreach my $statType (@statTypes) {
             if ($statType eq "min") {
-                addRow($group,$statType,$min,$proteinSubset);
+                if ($min == 0) {
+                    addRow($group,$statType,$min,$proteinSubset);
+                }
+                else  {
+                    my ($minValue, $minExponent) = split /e-/, $min;
+                    my $minValueRounded = sprintf("%.2f", $minValue);
+                    my $minFormatted = "${minValueRounded}e-$minExponent";
+                    addRow($group,$statType,$minFormatted,$proteinSubset);
+                }
             }
             elsif ($statType eq "25 PCT") {
-                addRow($group,$statType,$twentyfifth,$proteinSubset);
+                if ($twentyfifth == 0) {
+                    addRow($group,$statType,$twentyfifth,$proteinSubset);
+                }
+                else  {
+                    my ($twentyfifthValue, $twentyfifthExponent) = split /e-/, $twentyfifth;
+                    my $twentyfifthValueRounded = sprintf("%.2f", $twentyfifthValue);
+                    my $twentyfifthFormatted = "${twentyfifthValueRounded}e-$twentyfifthExponent";
+                    addRow($group,$statType,$twentyfifthFormatted,$proteinSubset);
+                }
             }
             elsif ($statType eq "median") {
-                addRow($group,$statType,$median,$proteinSubset);
+                if ($median == 0) {
+                    addRow($group,$statType,$median,$proteinSubset);
+                }
+                else  {
+                    my ($medValue, $medExponent) = split /e-/, $median;
+                    my $medValueRounded = sprintf("%.2f", $medValue);
+                    my $medFormatted = "${medValueRounded}e-$medExponent";
+                    addRow($group,$statType,$medFormatted,$proteinSubset);
+                }
             }
             elsif ($statType eq "75 PCT") {
-                addRow($group,$statType,$seventyfifth,$proteinSubset);
+                if ($seventyfifth == 0) {
+                    addRow($group,$statType,$seventyfifth,$proteinSubset);
+                }
+                else  {
+                    my ($seventyfifthValue, $seventyfifthExponent) = split /e-/, $seventyfifth;
+                    my $seventyfifthValueRounded = sprintf("%.2f", $seventyfifthValue);
+                    my $seventyfifthFormatted = "${seventyfifthValueRounded}e-$seventyfifthExponent";
+                    addRow($group,$statType,$seventyfifthFormatted,$proteinSubset);
+                }
             }
             elsif ($statType eq "max") {
-                addRow($group,$statType,$max,$proteinSubset);
+                if ($max == 0) {
+                    addRow($group,$statType,$max,$proteinSubset);
+                }
+                else  {
+                    my ($maxValue, $maxExponent) = split /e-/, $max;
+                    my $maxValueRounded = sprintf("%.2f", $maxValue);
+                    my $maxFormatted = "${maxValueRounded}e-$maxExponent";
+                    addRow($group,$statType,$maxFormatted,$proteinSubset);
+                }
             }
         }
 
@@ -122,9 +163,9 @@ sub run {
 sub addRow {
   my ($group,$statType,$evalue,$proteinSubset) = @_;
   my $row = GUS::Model::ApiDB::OrthologGroupStats->new({GROUP_ID => $group,
-                                                        STAT_TYPE => $statType,
-                                                        EVALUE => $evalue,
-                                                        PROTEIN_SUBSET => $proteinSubset
+                                                         STAT_TYPE => $statType,
+                                                         EVALUE => $evalue,
+                                                         PROTEIN_SUBSET => $proteinSubset
                                                      });
   $row->submit();
   $row->undefPointerCache();
