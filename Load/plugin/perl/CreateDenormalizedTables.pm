dd970dc5bae0cbcc49fff78de710206c266b855e
steve-fischer-200 <sfischer@upenn.edu>
2025-05-16 16:09:54 -0400

    debug

--- Load/plugin/perl/CreateDenormalizedTables.pm
+++ Load/plugin/perl/CreateDenormalizedTables.pm
@@ -170,7 +170,7 @@ sub run {
 
   my $startTimeAll = time;
   $self->processPsqlFile($fileName, $tableName, $schema, $organismAbbrev, $mode, $taxonId, $projectId, $commitMode, $dbh);
-  if (-e "$psqlDirPath/${tableName}_ix.psql") {
+  if ($mode eq 'parent' && -e "$psqlDirPath/${tableName}_ix.psql") {
     $self->processPsqlFile("$psqlDirPath/${tableName}_ix.psql", 'dontcare', $schema, $organismAbbrev, 'dontcare', 'dontcare', 'dontcare', $commitMode, $dbh);
   }
   my $t = time - $startTimeAll;
