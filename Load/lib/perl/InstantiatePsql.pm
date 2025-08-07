17d9174d205047c4d2f0bf459af9e19d21ea1090
Richard Demko <rdemko2332@gmail.com>
2025-08-07 15:16:22 -0400

    Changes to resolve hyphen issue in organismName when creating denormalized table

--- Load/lib/perl/InstantiatePsql.pm
+++ Load/lib/perl/InstantiatePsql.pm
@@ -4,37 +4,44 @@ use strict;
 # library to perform psql macro instantiation, as used by the CreateDenormalizedTables plugin
 
 sub instantiateSql {
-  my ($sql, $tableName, $schema, $organismAbbrev, $mode, $taxonId, $projectId) = @_;
+    my ($sql, $tableName, $schema, $organismAbbrev, $mode, $taxonId, $projectId) = @_;
 
-  if ($mode eq 'parent') {
-    $sql =~ s/\:CREATE_AND_POPULATE/CREATE TABLE $schema.${tableName}_temporary AS /g;
+  # Escape any double quotes within the names (PostgreSQL identifier quoting)
+  $schema         =~ s/"/""/g;
+  $tableName      =~ s/"/""/g;
+  $organismAbbrev =~ s/"/""/g;
 
-    my $s = "
-;
+    if ($mode eq 'parent') {
+	$sql =~ s/\:CREATE_AND_POPULATE/CREATE TABLE "$schema"."${tableName}_temporary" AS /g;
 
-create table $schema.$tableName (like $schema.${tableName}_temporary including all)
+	my $s = qq{
+
+create table "$schema"."$tableName" (like "$schema"."${tableName}_temporary" including all)
 partition by list (org_abbrev);
 
-drop table $schema.${tableName}_temporary;
-";
+drop table "$schema"."${tableName}_temporary";
+};
     $sql =~ s/\:DECLARE_PARTITION/$s/g;
 
-  } elsif ($mode eq 'child') {
+    } elsif ($mode eq 'child') {
 
-    my $s = "
-create table :SCHEMA.${tableName}_$organismAbbrev
-partition of :SCHEMA.$tableName
+	my $s = qq{
+
+create table ":SCHEMA"."${tableName}_$organismAbbrev"
+partition of ":SCHEMA"."$tableName"
 for values in (':ORG_ABBREV');
 
-insert into :SCHEMA.${tableName}_$organismAbbrev
-";
+insert into ":SCHEMA"."${tableName}_$organismAbbrev"
+};
     $sql =~ s/\:CREATE_AND_POPULATE/$s/g;
     $sql =~ s/\:DECLARE_PARTITION//g;
   }
+
   $sql =~ s/\:TAXON_ID/$taxonId/g;
   $sql =~ s/\:PROJECT_ID/$projectId/g;
   $sql =~ s/\:ORG_ABBREV/$organismAbbrev/g;
   $sql =~ s/\:SCHEMA/$schema/g;
+
   return $sql;
 }
 
@@ -44,6 +51,5 @@ sub substituteDelims {
     $sql =~ s/\:PLPGSQL_DELIM/;/g;
     return $sql;
 }
-
 
 1;
