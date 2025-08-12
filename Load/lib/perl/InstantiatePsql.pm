0667022971df8be0e34b02eea63d5e75cd0b31cf
Richard Demko <rdemko2332@gmail.com>
2025-08-12 16:47:57 -0400

    Removing double quotes where not needed

--- Load/lib/perl/InstantiatePsql.pm
+++ Load/lib/perl/InstantiatePsql.pm
@@ -6,36 +6,29 @@ use strict;
 sub instantiateSql {
     my ($sql, $tableName, $schema, $organismAbbrev, $mode, $taxonId, $projectId) = @_;
 
-    # Escape quotes for PostgreSQL-safe quoted identifiers
-    $schema    =~ s/"/""/g;
-    $tableName =~ s/"/""/g;
-
     my $refOrgAbb = $organismAbbrev;
     $refOrgAbb =~ s/\./\_/g;
     $refOrgAbb =~ s/\-/\_/g;
 
-    my $org_abbrev_literal = $organismAbbrev;
-    $org_abbrev_literal =~ s/'/''/g;              # escape single quotes for SQL
-   
     if ($mode eq 'parent') {
         my $s = qq{
 
-create table "$schema"."$tableName" (like "$schema"."${tableName}_temporary" including all)
+create table $schema.$tableName (like $schema.${tableName}_temporary including all)
 partition by list (org_abbrev);
 
-drop table "$schema"."${tableName}_temporary";
+drop table $schema.${tableName}_temporary;
 };
-        $sql =~ s/\:CREATE_AND_POPULATE/CREATE TABLE "$schema"."${tableName}_temporary" AS /g;
+        $sql =~ s/\:CREATE_AND_POPULATE/CREATE TABLE $schema.${tableName}_temporary AS /g;
         $sql =~ s/\:DECLARE_PARTITION/$s/g;
 
     } elsif ($mode eq 'child') {
         my $s = qq{
 
-create table ":SCHEMA"."${tableName}_${organismAbbrev}"
-partition of ":SCHEMA"."$tableName"
+create table :SCHEMA."${tableName}_${organismAbbrev}"
+partition of :SCHEMA.$tableName
 for values in (':ORG_ABBREV');
 
-insert into ":SCHEMA"."${tableName}_${organismAbbrev}"
+insert into :SCHEMA."${tableName}_${organismAbbrev}"
 };
         $sql =~ s/\:CREATE_AND_POPULATE/$s/g;
         $sql =~ s/\:DECLARE_PARTITION//g;
