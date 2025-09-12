2ecf94b53585143990a98c62743b2c2dfe8ac2f1
Richard Demko <rdemko2332@gmail.com>
2025-09-12 16:59:32 -0400

    Adding mergeTransaction functionality to speed up load of previous group mapping

--- Load/plugin/perl/InsertPreviousGroupMapping.pm
+++ Load/plugin/perl/InsertPreviousGroupMapping.pm
@@ -50,16 +50,17 @@ sub run {
     my $rowCount = 0;
     my $groupCount = 0;
 
-    my $dbh = $self->getQueryHandle();
+    $self->getDb()->manageTransaction(0, 'begin');
 
     open(my $data, '<', $fileName) || die "Could not open file $fileName: $!";
     while (my $line = <$data>) {
-	my $rowCount++;
 	chomp $line;
+
         # Format is like "OG6_0000000    OG7_0000000:110/120 OG7_0000001:10/120"
 	my ($oldGroupId, $newGroupIdsLine) = split(/\t/, $line);
 	my @newGroupIds = split(/\s/, $newGroupIdsLine);
 	foreach my $newGroupIdInfo (@newGroupIds) {
+
 	    if ($newGroupIdInfo =~ /(OG\S+):(\d+)\/(\d+)/) {
                 # New group Id
 		my $newGroupId = $1;
@@ -67,23 +68,30 @@ sub run {
 		my $overlapCount = $2;
                 # Number of sequences in the old group
 		my $groupSize = $3;
+
                 my $row = GUS::Model::ApiDB::GroupMapping->new({OLD_GROUP_ID => $oldGroupId,NEW_GROUP_ID => $newGroupId,OVERLAP_COUNT => $overlapCount, GROUP_SIZE => $groupSize});
-                $row->submit();
-	        $rowCount+=1;
+		$row->submit(undef, 1);
+		$row->undefPointerCache();
+
+		if($rowCount++; % 1000 == 0) {
+		    $self->getDb()->manageTransaction(0, 'commit');
+		    $self->getDb()->manageTransaction(0, 'begin');
+		}
+
 	    }
 	    else {
 		die "Improper new group info format: $newGroupIdInfo\n";
 	    }
 	}
-	$self->undefPointerCache();
+
 	$groupCount+=1;
 	if ($groupCount % 10000 == 0) {
-	    print "Processed $groupCount groups\n";
+     	    print "Processed $groupCount groups\n";
 	}
     }
- print "$rowCount rows added.\n"
+    $self->getDb()->manageTransaction(0, 'commit');
+    print "$rowCount rows added.\n"
 }
-
 
 sub undoTables {
     my ($self) = @_;
