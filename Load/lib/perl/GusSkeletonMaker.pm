1df4c2bd26bc0f84bc3691f31a6b37e79cecb493
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2026-03-11 15:55:40 -0400

    and uncomment it

--- Load/lib/perl/GusSkeletonMaker.pm
+++ Load/lib/perl/GusSkeletonMaker.pm
@@ -191,7 +191,7 @@ sub makeGeneSkeleton{
 
        ) {
 
-      #next if ($bioperlTranscript->has_tag("pseudo"));
+      next if ($bioperlTranscript->has_tag("pseudo"));
       my $translatedAAFeat = &makeTranslatedAAFeat($plugin, $dbRlsId);
       $gusTranscript->addChild($translatedAAFeat);
       $translatedAAFeat->{bioperlTranscript} = $bioperlTranscript;
