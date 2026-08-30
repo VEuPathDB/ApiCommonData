94688abbfde2c211f4c3807ea124e56646bc5a8d
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2026-08-30 19:01:19 -0400

    coding lincRNA_gene and lincRNA

--- Load/lib/perl/GusSkeletonMaker.pm
+++ Load/lib/perl/GusSkeletonMaker.pm
@@ -70,6 +70,7 @@ my $soTerms = { #'coding_gene'=>'protein_coding',
 		'ORF' => 'ORF',
 		'miRNA_gene' => 'ncRNA_gene',
 		'lncRNA_gene' => 'lncRNA_gene',
+		'lincRNA_gene' => 'lincRNA_gene',
 		'mRNA' => 'mRNA',
 		'pseudogenic_transcript' => 'pseudogenic_transcript',
 		'miRNA' => 'miRNA',
@@ -77,6 +78,7 @@ my $soTerms = { #'coding_gene'=>'protein_coding',
 		'misc_RNA' => 'ncRNA',  ## coding for ncRNA since misc_RNA is not in ONTOLOGYTERM table yet
 		'ncRNA' => 'ncRNA',
 		'lncRNA' => 'lncRNA',
+		'lincRNA' => 'lincRNA',
 		'rRNA' => 'rRNA',
 		'RNase_MRP_RNA' => 'RNase_MRP_RNA',
 		'RNase_P_RNA' => 'RNase_P_RNA',
@@ -269,7 +271,7 @@ sub makeGusGene {
   my $type = $bioperlGene->primary_tag();
 
   $plugin->error("Trying to make gus skeleton from a tree rooted with an unexpected type: '$type'") 
-     unless (grep {$type eq $_} ("haplotype_block","coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene", "tmRNA_gene", "scRNA_gene", "miRNA_gene", "transposable_element_gene","telomerase_RNA_gene", "antisense_RNA_gene", "lncRNA_gene", "pre_miRNA_gene", "siRNA_gene", "guide_RNA_gene", "pseudogene", "piRNA_gene", "circular_ncRNA_gene", "nc_primary_transcript_gene", "miRNA_primary_transcript_gene"));
+     unless (grep {$type eq $_} ("haplotype_block","coding_gene", "tRNA_gene", "rRNA_gene", "snRNA_gene", "snoRNA_gene", "misc_RNA_gene", "misc_feature_gene", "repeated_gene","pseudo_gene","SRP_RNA_gene","RNase_MRP_RNA_gene","RNase_P_gene","RNase_MRP_gene","RNase_P_RNA_gene","ncRNA_gene", "tmRNA_gene", "scRNA_gene", "miRNA_gene", "transposable_element_gene","telomerase_RNA_gene", "antisense_RNA_gene", "lncRNA_gene", "pre_miRNA_gene", "siRNA_gene", "guide_RNA_gene", "pseudogene", "piRNA_gene", "circular_ncRNA_gene", "nc_primary_transcript_gene", "miRNA_primary_transcript_gene", "lincRNA_gene"));
 
   my $gusGene = $plugin->makeSkeletalGusFeature($bioperlGene, $genomicSeqId,
 						$dbRlsId, 
